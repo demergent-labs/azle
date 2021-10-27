70c300e0a5909ad5df6aa4be9cee3537d8d6e4eb
Jordan Last <jordan.michael.last@gmail.com>
2021-10-26 21:49:21 -0500

    basic update calls and orthogonal persistence work baby!

---
+++ src/ic.ts
@@ -0,0 +1,32 @@
+export type Query<T> = T;
+export type Update<T> = T;
+export type i64 = number;
+export type i32 = number;
+export type i16 = number;
+export type i8 = number;
+export type u64 = number;
+export type u32 = number;
+export type u16 = number;
+export type u8 = number;
+// declare var ic: any;
+// export const ic = (globalThis as any).ic;
+
+// import {
+//     Query,
+//     Update
+// } from 'azle';
+// declare var ic: any;
+
+// export function echo(message: string): Query<string> {
+//     return message;
+// }
+
+// export function set(key: string, value: string): Update<boolean> {
+//     ic[key] = value;
+
+//     return true;
+// }
+
+// export function get(key: string): Query<string> {
+//     return ic[key];
+// }
