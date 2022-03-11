e6bb5fe349f55c11b6a6d7a15d91957266e9b9ef
Jordan Last <jordan.michael.last@gmail.com>
2022-03-10 19:24:16 -0600

    add esbuild and update some examples

--- examples/imports/src/imports.ts
+++ examples/imports/src/imports.ts
@@ -6,6 +6,7 @@ import {
     two,
     three
 } from './library';
+import { sha224 } from 'hash.js';
 
 export function getOne(): Query<string> {
     return one();
@@ -17,4 +18,8 @@ export function getTwo(): Query<string> {
 
 export function getThree(): Query<string> {
     return three();
+}
+
+export function sha224Hash(message: string): Query<string> {
+    return sha224().update(message).digest('hex');
 }
