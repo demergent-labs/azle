7f776c40a8ea59705afca9e3fbec805b76e8fd4b
Tarek <trkmos@gmail.com>
2022-04-03 06:25:02 +0200

    update generated files formatting

--- examples/motoko_examples/superheroes/src/declarations/index.js
+++ examples/motoko_examples/superheroes/src/declarations/index.js
@@ -1,4 +1,4 @@
-import { Actor, HttpAgent } from '@dfinity/agent';
+import { Actor, HttpAgent } from "@dfinity/agent";
 
 // Imports and re-exports candid interface
 import { idlFactory } from './superheroes.did.js';
@@ -7,20 +7,18 @@ export { idlFactory } from './superheroes.did.js';
 export const canisterId = process.env.SUPERHEROES_CANISTER_ID;
 
 /**
- *
+ * 
  * @param {string | import("@dfinity/principal").Principal} canisterId Canister ID of Agent
  * @param {{agentOptions?: import("@dfinity/agent").HttpAgentOptions; actorOptions?: import("@dfinity/agent").ActorConfig}} [options]
  * @return {import("@dfinity/agent").ActorSubclass<import("./superheroes.did.js")._SERVICE>}
  */
-export const createActor = (canisterId, options) => {
+ export const createActor = (canisterId, options) => {
   const agent = new HttpAgent({ ...options?.agentOptions });
-
+  
   // Fetch root key for certificate validation during development
-  if (process.env.NODE_ENV !== 'production') {
-    agent.fetchRootKey().catch((err) => {
-      console.warn(
-        'Unable to fetch root key. Check to ensure that your local replica is running'
-      );
+  if(process.env.NODE_ENV !== "production") {
+    agent.fetchRootKey().catch(err=>{
+      console.warn("Unable to fetch root key. Check to ensure that your local replica is running");
       console.error(err);
     });
   }
@@ -32,9 +30,9 @@ export const createActor = (canisterId, options) => {
     ...options?.actorOptions,
   });
 };
-
+  
 /**
  * A ready-to-use agent for the superheroes canister
  * @type {import("@dfinity/agent").ActorSubclass<import("./superheroes.did.js")._SERVICE>}
  */
-export const superheroes = createActor(canisterId);
+ export const superheroes = createActor(canisterId);
--- examples/motoko_examples/superheroes/src/declarations/superheroes.did.js
+++ examples/motoko_examples/superheroes/src/declarations/superheroes.did.js
@@ -1,15 +1,13 @@
 export const idlFactory = ({ IDL }) => {
   const Superhero = IDL.Record({
-    superpowers: IDL.Vec(IDL.Text),
-    name: IDL.Text,
+    'superpowers' : IDL.Vec(IDL.Text),
+    'name' : IDL.Text,
   });
   return IDL.Service({
-    create: IDL.Func([Superhero], [IDL.Int32], []),
-    deleteHero: IDL.Func([IDL.Int32], [IDL.Bool], []),
-    read: IDL.Func([IDL.Int32], [IDL.Opt(Superhero)], ['query']),
-    update: IDL.Func([IDL.Int32, Superhero], [IDL.Bool], []),
+    'create' : IDL.Func([Superhero], [IDL.Int32], []),
+    'deleteHero' : IDL.Func([IDL.Int32], [IDL.Bool], []),
+    'read' : IDL.Func([IDL.Int32], [IDL.Opt(Superhero)], ['query']),
+    'update' : IDL.Func([IDL.Int32, Superhero], [IDL.Bool], []),
   });
 };
-export const init = ({ IDL }) => {
-  return [];
-};
+export const init = ({ IDL }) => { return []; };
