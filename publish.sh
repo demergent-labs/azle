1c0c43eaa8c10c28efc39a8b21ed68a71fe10e79
Jordan Last <jordan.michael.last@gmail.com>
2022-04-05 10:14:11 -0500

    release 0.1.0-rc.0

--- publish.sh
+++ publish.sh
@@ -1,3 +1,53 @@
 #!/bin/bash
 
-# TODO let's start doing an automatic publishing
+set -e
+
+directories=(
+    "examples/complex_types"
+    "examples/counter"
+    "examples/ic_api"
+    "examples/imports"
+    "examples/inline_types"
+    "examples/key_value_store"
+    "examples/motoko_examples/calc"
+    "examples/motoko_examples/counter"
+    "examples/optional_types"
+    "examples/primitive_types"
+    "examples/query"
+    "examples/simple_erc20"
+    "examples/simple_user_accounts"
+    "examples/update"
+)
+
+root_dir=$PWD
+
+read -p "Enter new version number:" VERSION
+
+sed -E -i "s/(\"version\": \")(.*)(\")/\1$VERSION\3/" package.json
+npm install
+
+git commit -am "release $VERSION"
+git push origin release
+
+# git tag $VERSION
+# git push origin $VERSION
+
+# if [[ "$VERSION" == *"-rc."* ]];
+# then
+#     npm publish --tag next
+# else
+#     npm publish
+# fi
+
+# for directory in "${directories[@]}"
+# do
+#     cd $directory
+
+#     sed -E -i "s/(\"version\": \")(.*)(\")/\1$VERSION\3/" package.json
+#     npm install
+
+#     cd $root_dir
+# done
+
+# cd generator-sudograph
+# sed -E -i "s/(\"version\": \")(.*)(\")/\1$VERSION\3/" package.json
