e53e2115ac8af837f256a842926263bb649c117c
Jordan Last <jordan.michael.last@gmail.com>
2022-04-05 11:04:01 -0500

    release 0.1.0-rc.8

--- examples/query/src/query.ts
+++ examples/query/src/query.ts
@@ -1,5 +1,5 @@
-import { Query } from 'azle';
+import { Query, int32 } from 'azle';
 
-export function query(): Query<string> {
+export function query(): Query<int32> {
     return 'This is a query function';
 }
