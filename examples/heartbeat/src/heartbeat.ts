b1df08ec27795173cf9f9695657c89f7819bb306
Jordan Last <jordan.michael.last@gmail.com>
2022-04-06 08:05:12 -0500

    add heartbeat test

--- examples/heartbeat/src/heartbeat.ts
+++ examples/heartbeat/src/heartbeat.ts
@@ -1,16 +1,16 @@
-// TODO remember that Heartbeat can be asynchronous
-// TODO implement the heartbeat generator code
-// TODO there might be some huge shared mutable state issues with heartbeat and the unsafe code we're using
-
-// TODO if there are issues with shared mutable state and heartbeat, then there should be with other calls as well
-// TODO make sure our use of unsafe is not unsafe
-
 import {
+    Query,
     Heartbeat,
     ic
 } from 'azle';
 
-export function* heartbeat(): Heartbeat {
-    const randomness = yield ic.rawRand();
-    ic.print('this is the heartbeat', randomness.toString());
+let inititalized = false;
+
+export function heartbeat(): Heartbeat {
+    inititalized = true;
+    ic.print('heartbeat initialized', inititalized);
+}
+
+export function getInitialized(): Query<boolean> {
+    return inititalized;
 }
