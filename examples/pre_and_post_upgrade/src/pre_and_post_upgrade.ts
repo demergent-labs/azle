98196bd63dc6eaacbef919a3a30816310ca922ea
Jordan Last <jordan.michael.last@gmail.com>
2022-04-07 12:31:10 -0500

    add example for pre and post upgrade

---
+++ examples/pre_and_post_upgrade/src/pre_and_post_upgrade.ts
@@ -0,0 +1,67 @@
+// TODO the tests should perform multiple deploys
+// TODO That will be a bit hard to do until this: https://forum.dfinity.org/t/upgrade-a-canister-even-if-the-wasm-module-hash-has-not-changed/11989
+
+import {
+    Query,
+    Update,
+    Stable,
+    Init,
+    PreUpgrade,
+    PostUpgrade,
+    ic,
+    nat64
+} from 'azle';
+
+type StableStorage = Stable<{
+    entries: Entry[];
+}>;
+
+type Entry = {
+    key: string;
+    value: nat64;
+};
+
+let entries: {
+    [key: string]: nat64;
+} = {};
+
+export function init(): Init {
+    ic.print('init');
+
+    ic.stableStorage<StableStorage>().entries = [];
+}
+
+export function preUpgrade(): PreUpgrade {
+    ic.print('preUpgrade');
+
+    ic.stableStorage<StableStorage>().entries = Object.entries(entries).map((entry) => {
+        return {
+            key: entry[0],
+            value: entry[1]
+        };
+    });
+}
+
+export function postUpgrade(): PostUpgrade {
+    ic.print('preUpgrade');
+
+    entries = ic.stableStorage<StableStorage>().entries.reduce((result, entry) => {
+        return {
+            ...result,
+            [entry.key]: entry.value
+        };
+    }, {});
+}
+
+export function setEntry(entry: Entry): Update<void> {
+    entries[entry.key] = entry.value;
+}
+
+export function getEntries(): Query<Entry[]> {
+    return Object.entries(entries).map((entry) => {
+        return {
+            key: entry[0],
+            value: entry[1]
+        };
+    });
+}
