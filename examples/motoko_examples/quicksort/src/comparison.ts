1e2d3ad83ffb73a3a87a192aee66376af20d7385
Dan Steren <dan@dansteren.com>
2022-05-12 10:10:57 -0600

    Refactor comparison library to match style guide

--- examples/motoko_examples/quicksort/src/comparison.ts
+++ examples/motoko_examples/quicksort/src/comparison.ts
@@ -4,26 +4,44 @@ import {
 
 export const Int = {
     abs: Math.abs,
-    compare: (a: int, b: int): OrderType => {
-        if (a > b) {
-            return OrderType.Greater;
-        }
-        if (a < b) {
-            return OrderType.Less;
-        }
-        return OrderType.Equal;
+    compare
+};
+
+function compare (a: int, b: int): OrderType {
+    if (a > b) {
+        return OrderType.Greater;
     }
+    if (a < b) {
+        return OrderType.Less;
+    }
+    return OrderType.Equal;
 }
 
 export enum OrderType {
     Less = -1,
     Equal = 0,
     Greater = 1
-}
+};
 
 export const Order = {
-    isLess: (order: OrderType) => order === OrderType.Less,
-    isEqual: (order: OrderType) => order === OrderType.Equal,
-    isGreater: (order: OrderType) => order === OrderType.Greater,
-    equal: (o1: OrderType, o2: OrderType) => o1 === OrderType.Less && o2 === OrderType.Less
+    isLess,
+    isEqual,
+    isGreater,
+    equal
+};
+
+function isLess (order: OrderType): boolean {
+    return order === OrderType.Less;
+}
+
+function isEqual (order: OrderType): boolean {
+    return order === OrderType.Equal;
+}
+
+function isGreater (order: OrderType): boolean {
+    return order === OrderType.Greater;
+}
+
+function equal (o1: OrderType, o2: OrderType): boolean {
+    return o1 === OrderType.Less && o2 === OrderType.Less;
 }
