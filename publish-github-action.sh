9e58d1cbddca8d37ba7239b685c1792abdff61ac
Jordan Last <jordan.michael.last@gmail.com>
2022-05-13 15:14:12 -0500

    test

--- publish-github-action.sh
+++ publish-github-action.sh
@@ -74,8 +74,8 @@ git commit -am "release $VERSION [skip ci]"
 
 # git push origin HEAD
 # git push origin $(git rev-parse --abbrev-ref HEAD)
-echo $GITHUB_BASE_REF
-git push origin $GITHUB_BASE_REF
+echo $GITHUB_HEAD_REF
+git push origin $GITHUB_HEAD_REF
 
 git tag $VERSION
 git push origin $VERSION
