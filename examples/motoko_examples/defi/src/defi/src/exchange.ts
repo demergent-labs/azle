152e0f3dde193f1f7fd52d5ab38b6118c9f82354
Tarek <trkmos@gmail.com>
2022-05-16 21:54:40 +0200

    update book and add exchaange

--- examples/motoko_examples/defi/src/defi/src/exchange.ts
+++ examples/motoko_examples/defi/src/defi/src/exchange.ts
@@ -1,4 +1,4 @@
-import { ic, Opt } from 'azle';
+import { ic, nat, Opt, Principal } from 'azle';
 import { Book } from './book';
 import { Order, OrderId, Token } from './types';
 
@@ -9,48 +9,182 @@ export type Orders = {
   [key: OrderId]: Order;
 };
 
-//  public class Exchange(trading_pair: TradingPair, book: Book.Book) {
+// @TODO returning a dummy id for now till we figure out how we get the actual id
+export const ledger = function (): Principal {
+  return 'nlhft-2iaaa-aaaae-qaaua-cai';
+};
 
 export interface Exchange {
   tradingPair: TradingPair;
   book: Book;
   orders: Orders;
   getOrder(orderId: OrderId): Opt<Order>;
-  getTradingPairs(): TradingPair;
+  getOrders(): Order[];
+  cancelOrder(id: OrderId): Opt<Order>;
+  detectMatch(order: Order): void;
 }
 
-export const Exchange = function (tradingPair: TradingPair, book: Book) {
+export const Exchange = function (
+  tradingPair: TradingPair,
+  book: Book
+): Exchange {
   const _tradingPair = tradingPair;
   const _book = book;
-  const _orders: Orders = {};
+
+  // The map of all orders (not differentiated by pairs).
+  const orders: Orders = {};
+
+  function getOrders(): Order[] {
+    ic.print(
+      `List orders on exchange " ${_tradingPair[0]} / ${_tradingPair[1]}`
+    );
+    let vals = Object.values(orders);
+    return vals;
+  }
+
+  function getOrder(id: OrderId): Opt<Order> {
+    return orders[id] || null;
+  }
+
+  // Cancel order WITHOUT verifying ownership.
+  function cancelOrder(id: OrderId): Opt<Order> {
+    let order = orders[id];
+    delete orders[id];
+    return order || null;
+  }
+
+  function addOrders(orders: Order[]) {
+    orders.forEach((order) => {
+      addOrder(order);
+    });
+  }
+
+  function addOrder(o: Order) {
+    orders[o.id] = o;
+    detectMatch(o);
+  }
+
+  function detectMatch(order: Order) {
+    const a = order;
+
+    // Find matching orders.
+    let matches: Order[] = [];
+    Object.values(orders).forEach((b) => {
+      if (
+        a.id !== b.id &&
+        a.from == b.to &&
+        a.to == b.from &&
+        a.fromAmount * b.fromAmount >= a.toAmount * b.toAmount
+      ) {
+        matches.push(b);
+      }
+    });
+
+    matches.forEach((b) => {
+      let a_to_amount = 0n;
+      let b_to_amount = 0n;
+
+      if (b.fromAmount >= a.toAmount) {
+        a_to_amount = a.toAmount;
+      }
+
+      if (a.fromAmount >= b.toAmount) {
+        b_to_amount = b.toAmount;
+      }
+
+      // Check if some orders can be completed partially.
+      if (a_to_amount === 0n && b_to_amount > 0n) {
+        a_to_amount = b.fromAmount;
+        // Verify that we can complete the partial order with natural number tokens remaining.
+        if ((a_to_amount * a.fromAmount) % a.toAmount !== 0n) {
+          return;
+        }
+      }
+      if (b_to_amount === 0n && a_to_amount > 0n) {
+        b_to_amount = a.fromAmount;
+        // Verify that we can complete the partial order with natural number tokens remaining.
+        if ((b_to_amount * b.fromAmount) % b.toAmount !== 0n) {
+          return;
+        }
+      }
+
+      if (a_to_amount > 0n && b_to_amount > 0n) {
+        // processTrade(a, b, a_to_amount, b_to_amount);
+      }
+    });
+  }
+
+  function processTrade(
+    orderA: Order,
+    orderB: Order,
+    aToAmount: nat,
+    bToAmount: nat
+  ) {
+    ic.print(
+      `Process trade between order " ${orderA.id} # " and order " ${orderB.id}`
+    );
+
+    const ra = orders[orderA.id];
+    const rb = orders[orderB.id];
+
+    delete orders[orderA.id];
+    delete orders[orderB.id];
+
+    // Calculate "cost" to each
+    const aFromAmount = (aToAmount * orderA.fromAmount) / orderA.toAmount;
+    const bFromAmount = (bToAmount * orderB.fromAmount) / orderB.toAmount;
+
+    // Update order with remaining tokens
+    const a: Order = {
+      id: orderA.id,
+      owner: orderA.owner,
+      from: orderA.from,
+      fromAmount: orderA.fromAmount - aFromAmount,
+      to: orderA.to,
+      toAmount: orderA.toAmount - aToAmount,
+    };
+
+    const b: Order = {
+      id: orderB.id,
+      owner: orderB.owner,
+      from: orderB.from,
+      fromAmount: orderB.fromAmount - bFromAmount,
+      to: orderB.to,
+      toAmount: orderB.toAmount - bToAmount,
+    };
+
+    // Update DEX balances (book)
+    const removedA = book.removeTokens(a.owner, a.from, aFromAmount);
+    book.addTokens(a.owner, a.to, aToAmount);
+    const removedB = book.removeTokens(b.owner, b.from, bFromAmount);
+    book.addTokens(b.owner, b.to, bToAmount);
+
+    // The DEX keeps any tokens not required to satisfy the parties.
+    const dex_amount_a = aFromAmount - bToAmount;
+    if (dex_amount_a > 0) {
+      book.addTokens(ledger(), a.from, dex_amount_a);
+    }
+    const dex_amount_b = bFromAmount - aToAmount;
+    if (dex_amount_b > 0) {
+      book.addTokens(ledger(), b.from, dex_amount_b);
+    }
+
+    // Maintain the orders only if not empty
+    if (a.fromAmount !== 0n) {
+      orders[a.id] = a;
+    }
+    if (b.fromAmount !== 0n) {
+      orders[b.id] = b;
+    }
+  }
 
   return Object.freeze({
     tradingPair: _tradingPair,
     book: _book,
-    orders: _orders,
-    getOrder: function (orderId: OrderId): Opt<Order> {
-      return _orders[orderId];
-    },
-    getTradingPairs: function () {
-      ic.print(_tradingPair);
-      return _tradingPair;
-    },
+    orders,
+    getOrders,
+    getOrder,
+    cancelOrder,
+    detectMatch,
   });
 };
-
-let orders: Orders = {};
-
-// The map of all orders (not differentiated by pairs).
-
-export function getOrder(id: OrderId): Opt<Order> {
-  return orders[id] || null;
-}
-
-// export function getOrders() : Order[] {
-//     ic.print("List orders on exchange " # Principal.toText(trading_pair.0) # "/" # Principal.toText(trading_pair.1));
-//     let buff : B.Buffer<T.Order> = B.Buffer(10);
-//     for (o in orders.vals()) {
-//         buff.add(o);
-//     };
-//     buff.toArray();
-// };
--- examples/motoko_examples/defi/src/defi/src/main.ts
+++ examples/motoko_examples/defi/src/defi/src/main.ts
@@ -1,14 +1,16 @@
 import { Query, ic } from 'azle';
+import { Book } from './book';
 import { Exchange, TradingPair } from './exchange';
+import { Order } from './types';
 
 export function whoami(): Query<string> {
   return ic.caller();
 }
 
-export function getOrders(): Query<TradingPair> {
+export function getOrders(): Query<Order[]> {
   let e = Exchange(
     ['nlhft-2iaaa-aaaae-qaaua-cai', 'nlhft-2iaaa-aaaae-qaaua-cai'],
-    {}
+    Book()
   );
-  return e.getTradingPairs();
+  return e.getOrders();
 }
