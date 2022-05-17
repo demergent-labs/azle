7dec79fa681d0c7ebf644fca2d3a221b026eea50
Jordan Last <jordan.michael.last@gmail.com>
2022-05-17 15:07:23 -0500

    test

--- publish-github-action.sh
+++ publish-github-action.sh
@@ -1,69 +1,71 @@
 #!/bin/bash
 
-set -e
+echo $1
 
-# TODO let's pass these directories in from the github actions yaml
-directories=(
-    "examples/call_raw"
-    "examples/complex_types"
-    "examples/counter"
-    "examples/cross_canister_calls"
-    "examples/func_types"
-    "examples/generators"
-    "examples/heartbeat"
-    "examples/ic_api"
-    "examples/imports"
-    "examples/init"
-    "examples/inline_types"
-    "examples/key_value_store"
-    "examples/ledger_canister"
-    "examples/management_canister"
-    "examples/motoko_examples/calc"
-    "examples/motoko_examples/counter"
-    "examples/motoko_examples/quicksort"
-    "examples/optional_types"
-    "examples/pre_and_post_upgrade"
-    "examples/primitive_types"
-    "examples/query"
-    "examples/simple_erc20"
-    "examples/simple_user_accounts"
-    "examples/stable_storage"
-    "examples/tuple_types"
-    "examples/update"
-)
+# set -e
 
-root_dir=$PWD
+# # TODO let's pass these directories in from the github actions yaml
+# directories=(
+#     "examples/call_raw"
+#     "examples/complex_types"
+#     "examples/counter"
+#     "examples/cross_canister_calls"
+#     "examples/func_types"
+#     "examples/generators"
+#     "examples/heartbeat"
+#     "examples/ic_api"
+#     "examples/imports"
+#     "examples/init"
+#     "examples/inline_types"
+#     "examples/key_value_store"
+#     "examples/ledger_canister"
+#     "examples/management_canister"
+#     "examples/motoko_examples/calc"
+#     "examples/motoko_examples/counter"
+#     "examples/motoko_examples/quicksort"
+#     "examples/optional_types"
+#     "examples/pre_and_post_upgrade"
+#     "examples/primitive_types"
+#     "examples/query"
+#     "examples/simple_erc20"
+#     "examples/simple_user_accounts"
+#     "examples/stable_storage"
+#     "examples/tuple_types"
+#     "examples/update"
+# )
 
-VERSION=$1
+# root_dir=$PWD
 
-sed -E -i "s/(\"version\": \")(.*)(\")/\1$VERSION\3/" package.json
-npm install
+# VERSION=$1
 
-if [[ "$VERSION" == *"-rc."* ]];
-then
-    npm publish --tag next
-else
-    npm publish
-fi
+# sed -E -i "s/(\"version\": \")(.*)(\")/\1$VERSION\3/" package.json
+# npm install
 
-# TODO loop through checking for the status instead of sleeping
-echo -e "sleeping for 30 seconds to ensure azle@$VERSION is fully registered on npm"
+# if [[ "$VERSION" == *"-rc."* ]];
+# then
+#     npm publish --tag next
+# else
+#     npm publish
+# fi
 
-sleep 30
+# # TODO loop through checking for the status instead of sleeping
+# echo -e "sleeping for 30 seconds to ensure azle@$VERSION is fully registered on npm"
 
-for directory in "${directories[@]}"
-do
-    cd $directory
+# sleep 30
 
-    sed -E -i "s/(\"azle\": \")(.*)(\")/\1$VERSION\3/" package.json
-    npm install
+# for directory in "${directories[@]}"
+# do
+#     cd $directory
 
-    cd $root_dir
-done
+#     sed -E -i "s/(\"azle\": \")(.*)(\")/\1$VERSION\3/" package.json
+#     npm install
 
-git add --all
-git commit -am "azle-bot automated release $VERSION"
-git push origin $GITHUB_HEAD_REF
+#     cd $root_dir
+# done
 
-git tag $VERSION
-git push origin $VERSION
+# git add --all
+# git commit -am "azle-bot automated release $VERSION"
+# git push origin $GITHUB_HEAD_REF
+
+# git tag $VERSION
+# git push origin $VERSION
