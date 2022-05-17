974d1ae0d08e4206749b8a172370464910be3b4b
Dan Steren <dan@dansteren.com>
2022-05-17 17:00:57 -0600

    Add additional tests

--- examples/motoko_examples/echo/test/test.ts
+++ examples/motoko_examples/echo/test/test.ts
@@ -45,6 +45,36 @@ const tests: Test[] = [
                 ok: result === phrase
             };
         }
+    },
+    {
+        name: 'say nothing',
+        test: async () => {
+            const phrase = ""
+            const result = await echo_canister.say(phrase);
+
+            return {
+                ok: result === phrase
+            };
+        }
+    },
+    {
+        name: 'say a lot',
+        test: async () => {
+            const phrase = `
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+            `
+            const result = await echo_canister.say(phrase);
+
+            return {
+                ok: result === phrase
+            };
+        }
     }
 ];
 
