4509ba72a61d93d4c6c9527e52230ed68c40adf7
Jordan Last <jordan.michael.last@gmail.com>
2022-05-18 08:00:48 -0500

    test

--- publish-github-action.sh
+++ publish-github-action.sh
@@ -11,34 +11,35 @@ directories_json_string_with_linebreaks=$2
 directories_json_string="${directories_json_string_with_linebreaks//$'\\n'/''}"
 directories=$(echo "$directories_json_string" | jq -c -r '.[]')
 
-sed -E -i "s/(\"version\": \")(.*)(\")/\1$VERSION\3/" package.json
-npm install
+# sed -E -i "s/(\"version\": \")(.*)(\")/\1$VERSION\3/" package.json
+# npm install
 
-if [[ "$VERSION" == *"-rc."* ]];
-then
-    npm publish --tag next
-else
-    npm publish
-fi
+# if [[ "$VERSION" == *"-rc."* ]];
+# then
+#     npm publish --tag next
+# else
+#     npm publish
+# fi
 
 # TODO loop through checking for the status instead of sleeping
-echo -e "sleeping for 30 seconds to ensure azle@$VERSION is fully registered on npm"
+# echo -e "sleeping for 30 seconds to ensure azle@$VERSION is fully registered on npm"
 
-sleep 30
+# sleep 30
 
 for directory in "${directories[@]}"
 do
-    cd $directory
+    echo $directory
+    cd $directory # TODO running into issue here for some reason
 
-    sed -E -i "s/(\"azle\": \")(.*)(\")/\1$VERSION\3/" package.json
-    npm install
+    # sed -E -i "s/(\"azle\": \")(.*)(\")/\1$VERSION\3/" package.json
+    # npm install
 
-    cd $root_dir
+    # cd $root_dir
 done
 
-git add --all
-git commit -am "azle-bot automated release $VERSION"
-git push origin $GITHUB_HEAD_REF
+# git add --all
+# git commit -am "azle-bot automated release $VERSION"
+# git push origin $GITHUB_HEAD_REF
 
-git tag $VERSION
-git push origin $VERSION
+# git tag $VERSION
+# git push origin $VERSION
