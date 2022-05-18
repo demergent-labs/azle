b4e248c15bea488dcefc842193bacca9c9e1e916
Jordan Last <jordan.michael.last@gmail.com>
2022-05-18 08:29:34 -0500

    Test

--- publish-github-action.sh
+++ publish-github-action.sh
@@ -10,38 +10,34 @@ directories_json_string_with_linebreaks=$2
 directories_json_string="${directories_json_string_with_linebreaks//$'\\n'/''}"
 directories=$(echo "$directories_json_string" | jq -c -r '.[]')
 
-# sed -E -i "s/(\"version\": \")(.*)(\")/\1$VERSION\3/" package.json
-# npm install
+sed -E -i "s/(\"version\": \")(.*)(\")/\1$VERSION\3/" package.json
+npm install
 
-# if [[ "$VERSION" == *"-rc."* ]];
-# then
-#     npm publish --tag next
-# else
-#     npm publish
-# fi
+if [[ "$VERSION" == *"-rc."* ]];
+then
+    npm publish --tag next
+else
+    npm publish
+fi
 
-# # TODO loop through checking for the status instead of sleeping
-# echo -e "sleeping for 30 seconds to ensure azle@$VERSION is fully registered on npm"
+# TODO loop through checking for the status instead of sleeping
+echo -e "sleeping for 30 seconds to ensure azle@$VERSION is fully registered on npm"
 
-# sleep 30
-
-# echo $root_dir
+sleep 30
 
 for directory in ${directories[@]}
 do
-    echo $directory
     cd $directory
-    # cd examples/heartbeat
 
-    # sed -E -i "s/(\"azle\": \")(.*)(\")/\1$VERSION\3/" package.json
-    # npm install
+    sed -E -i "s/(\"azle\": \")(.*)(\")/\1$VERSION\3/" package.json
+    npm install
 
     cd $root_dir
 done
 
-# git add --all
-# git commit -am "azle-bot automated release $VERSION"
-# git push origin $GITHUB_HEAD_REF
+git add --all
+git commit -am "azle-bot automated release $VERSION"
+git push origin $GITHUB_HEAD_REF
 
-# git tag $VERSION
-# git push origin $VERSION
+git tag $VERSION
+git push origin $VERSION
