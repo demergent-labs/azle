73e8df0faf02a7bc72f4a2bc80fc7699d0b3cf56
Dan Steren <dan@dansteren.com>
2022-05-18 12:08:07 -0600

    Add motoko factorial example
    Note: This doesn't support very large numbers.
    
    See https://github.com/demergent-labs/azle/issues/321

---
+++ examples/motoko_examples/factorial/src/factorial.ts
@@ -0,0 +1,18 @@
+import {
+    Query,
+    nat
+} from 'azle';
+
+// Calculate the product of all positive integers less than or equal to `n`.
+export function fac(n: nat): Query<nat> {
+    return go(n);
+}
+
+// We implement the recursion in a helper function.
+function go(m: nat): nat {
+    if(m == 0n) {
+        return 1n;
+    } else {
+        return m * go(m - 1n);
+    }
+}
