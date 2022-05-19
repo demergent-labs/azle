564db8d1b672710d24d21adca3835b6025649ca0
Dan Steren <dan@dansteren.com>
2022-05-19 14:48:09 -0600

    Implement changes from code review

--- examples/motoko_examples/factorial/src/factorial.ts
+++ examples/motoko_examples/factorial/src/factorial.ts
@@ -1,18 +1,15 @@
-import {
-    Query,
-    nat
-} from 'azle';
+import { nat, Query } from 'azle';
 
 // Calculate the product of all positive integers less than or equal to `n`.
 export function fac(n: nat): Query<nat> {
-    return go(n);
+  return go(n);
 }
 
 // We implement the recursion in a helper function.
 function go(m: nat): nat {
-    if(m == 0n) {
-        return 1n;
-    } else {
-        return m * go(m - 1n);
-    }
+  if (m == 0n) {
+    return 1n;
+  } else {
+    return m * go(m - 1n);
+  }
 }
