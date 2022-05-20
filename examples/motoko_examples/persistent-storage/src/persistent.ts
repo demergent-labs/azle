87e503e3134b2d9e1d264c868f961e1e2e50dbbb
Quint Daenen <me@di-wu.be>
2022-05-20 21:22:36 +0200

    Add Motoko persistent storage example.

---
+++ examples/motoko_examples/persistent-storage/src/persistent.ts
@@ -0,0 +1,30 @@
+import {
+    ic,
+    Init,
+    nat,
+    Query,
+    Stable,
+    Update
+} from 'azle';
+
+type StableStorage = Stable<{
+    counter: nat
+}>;
+
+export function init(): Init {
+    ic.stableStorage<StableStorage>().counter = 0n;
+}
+
+export function increment(): Update<nat> {
+    ic.stableStorage<StableStorage>().counter += 1n;
+    return ic.stableStorage<StableStorage>().counter;
+}
+
+export function get(): Query<nat> {
+    return ic.stableStorage<StableStorage>().counter;
+}
+
+export function reset(): Update<nat> {
+    ic.stableStorage<StableStorage>().counter = 0n;
+    return ic.stableStorage<StableStorage>().counter;
+}
---
+++ examples/motoko_examples/persistent-storage/test/test.ts
@@ -0,0 +1,84 @@
+import {
+    run_tests,
+    Test
+} from 'azle/test';
+import { execSync } from 'child_process';
+import { createActor } from '../test/dfx_generated/persistent';
+
+const persistentStorage_canister = createActor(
+    'rrkah-fqaaa-aaaaa-aaaaq-cai', {
+        agentOptions: {
+            host: 'http://127.0.0.1:8000'
+        }
+    }
+);
+
+const tests: Test[] = [
+    {
+        name: 'clear canister memory',
+        prep: async () => {
+            execSync(`dfx canister uninstall-code persistent || true`, {
+                stdio: 'inherit'
+            });
+        }
+    },
+    {
+        // TODO hopefully we can get rid of this: https://forum.dfinity.org/t/generated-declarations-in-node-js-environment-break/12686/16?u=lastmjs
+        name: 'waiting for createActor fetchRootKey',
+        wait: 5000
+    },
+    {
+        name: 'deploy',
+        prep: async () => {
+            execSync(`dfx deploy`, {
+                stdio: 'inherit'
+            });
+        }
+    },
+    {
+        name: 'increment',
+        test: async () => {
+            const result = await persistentStorage_canister.increment();
+            return {
+                ok: result === 1n
+            };
+        }
+    },
+    {
+        name: 'reset',
+        test: async () => {
+            const result = await persistentStorage_canister.reset();
+            return {
+                ok: result === 0n
+            };
+        }
+    },
+    {
+        name: 'increment',
+        test: async () => {
+            const result = await persistentStorage_canister.increment();
+            return {
+                ok: result === 1n
+            };
+        }
+    },
+    {
+        name: 'deploy (upgrade)',
+        prep: async () => {
+            execSync(`dfx deploy --upgrade-unchanged`, {
+                stdio: 'inherit'
+            });
+        }
+    },
+    {
+        name: 'get',
+        test: async () => {
+            const result = await persistentStorage_canister.get();
+            return {
+                ok: result === 1n
+            };
+        }
+    },
+];
+
+run_tests(tests);
