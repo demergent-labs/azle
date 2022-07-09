f8e364b06d8f1f1f1bcb1a50f04af80e904f8d84
Benjamin DeMann <benjamin.demann@gmail.com>
2022-07-08 18:07:28 -0600

    simplify buffer for stable64_read

--- src/compiler/typescript_to_rust/generators/ic_object/functions/stable64_read.ts
+++ src/compiler/typescript_to_rust/generators/ic_object/functions/stable64_read.ts
@@ -21,9 +21,8 @@ export function generateIcObjectFunctionStable64Read(): Rust {
                 .azle_try_from_js_value(_context)
                 .unwrap();
 
-            let mut vec_buf: Vec<u8> = vec![0; length as usize];
-            let mut buf: &mut [u8] = &mut vec_buf[..];
-            ic_cdk::api::stable::stable64_read(offset, buf);
+            let mut buf: Vec<u8> = vec![0; length as usize];
+            ic_cdk::api::stable::stable64_read(offset, &mut buf);
             Ok(buf.to_vec().azle_into_js_value(_context))
         }
     `;
