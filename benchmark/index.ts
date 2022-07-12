08ce8e4f32aefb6cfe10e37197eef7c3dee2666b
Jordan Last <jordan.michael.last@gmail.com>
2022-07-12 15:41:00 -0500

    adding finishing touches to benchmarks, starting github action

--- benchmark/index.ts
+++ benchmark/index.ts
@@ -95,6 +95,8 @@ export async function run_benchmarks(
     const benchmark_promises: BenchmarkResult[] = await benchmarks.reduce(async (result: Promise<BenchmarkResult[]>, benchmark) => {
         const resolved_result = await result;
 
+        console.log(`Starting ${num_benchmark_iterations} run${num_benchmark_iterations === 1 ? '' : 's'} of benchmark ${benchmark.canister_method}${benchmark.benchmark_description !== '' ? `: ${benchmark.benchmark_description}` : ''}\n`);
+        
         const benchmark_iteration_promises = new Array(num_benchmark_iterations).fill(0).map((_) => {
             return run_benchmark(
                 benchmark.canister_method,
@@ -194,8 +196,6 @@ async function run_benchmark(
     benchmark_description: string = canister_method,
     args: any[] = []
 ): Promise<BenchmarkResult> {
-    console.log(`benchmark ${canister_method}${benchmark_description !== '' ? `: ${benchmark_description}` : ''}\n`);
-
     const azle_perf_result: PerfResult = await (azle_canister as any)[canister_method](...args);
     const motoko_perf_result: PerfResult = await (motoko_canister as any)[canister_method](...args);
     const rust_perf_result: PerfResult = await (rust_canister as any)[canister_method](...args);
@@ -341,43 +341,7 @@ function create_markdown_report(benchmark_results: BenchmarkResult[]): string {
 The format for benchmark numbers is (x / y) where:
   - x = Wasm instructions counted only in the function body
   - y = Wasm instructions counted in the function body and the function prelude
-    `;
-
-    const header_names = [
-        'Description',
-        'Azle Wasm Instructions',
-        'Motoko Wasm Instructions',
-        'Rust Wasm Instructions',
-        'Azle/Motoko Change Multiplier',
-        'Azle/Rust Change Multiplier',
-        'Motoko/Azle Change Multiplier',
-        'Motoko/Rust Change Multiplier',
-        'Rust/Azle Change Multiplier',
-        'Rust/Motoko Change Multiplier'
-    ];
-
-    const header = `| ${header_names.join(' | ')} |`;
-
-    const header_separator = `| ${header_names.map((_) => '---').join(' | ')} |`;
-
-    const data_rows = benchmark_results.map((benchmark_result) => {
-        const description = `${benchmark_result.canister_method}${benchmark_result.benchmark_description !== '' ? `: ${benchmark_result.benchmark_description}` : ''}`;
-
-        const azle_wasm_instructions = `(${format_number_to_rust(benchmark_result.azle_wasm_instructions.wasm_body_only.toFixed(0))} / ${format_number_to_rust(benchmark_result.azle_wasm_instructions.wasm_including_prelude.toFixed(0))})`;
-        const motoko_wasm_instructions = `(${format_number_to_rust(benchmark_result.motoko_wasm_instructions.wasm_body_only.toFixed(0))} / ${format_number_to_rust(benchmark_result.motoko_wasm_instructions.wasm_including_prelude.toFixed(0))})`;
-        const rust_wasm_instructions = `(${format_number_to_rust(benchmark_result.rust_wasm_instructions.wasm_body_only.toFixed(0))} / ${format_number_to_rust(benchmark_result.rust_wasm_instructions.wasm_including_prelude.toFixed(0))})`;
-
-        const azle_motoko_change_multiplier = `(${format_number_to_rust(benchmark_result.azle_motoko_change_multiplier.wasm_body_only.toFixed(0))}x / ${format_number_to_rust(benchmark_result.azle_motoko_change_multiplier.wasm_including_prelude.toFixed(0))}x)`;
-        const azle_rust_change_multiplier = `(${format_number_to_rust(benchmark_result.azle_rust_change_multiplier.wasm_body_only.toFixed(0))}x / ${format_number_to_rust(benchmark_result.azle_rust_change_multiplier.wasm_including_prelude.toFixed(0))}x)`;
-
-        const motoko_azle_change_multiplier = `(${format_number_to_rust(benchmark_result.motoko_azle_change_multiplier.wasm_body_only.toFixed(0))}x / ${format_number_to_rust(benchmark_result.motoko_azle_change_multiplier.wasm_including_prelude.toFixed(0))}x)`;
-        const motoko_rust_change_multiplier = `(${format_number_to_rust(benchmark_result.motoko_rust_change_multiplier.wasm_body_only.toFixed(0))}x / ${format_number_to_rust(benchmark_result.motoko_rust_change_multiplier.wasm_including_prelude.toFixed(0))}x)`;
-
-        const rust_azle_change_multiplier = `(${format_number_to_rust(benchmark_result.rust_azle_change_multiplier.wasm_body_only.toFixed(0))}x / ${format_number_to_rust(benchmark_result.rust_azle_change_multiplier.wasm_including_prelude.toFixed(0))}x)`;
-        const rust_motoko_change_multiplier = `(${format_number_to_rust(benchmark_result.rust_motoko_change_multiplier.wasm_body_only.toFixed(0))}x / ${format_number_to_rust(benchmark_result.rust_motoko_change_multiplier.wasm_including_prelude.toFixed(0))}x)`;
-
-        return `| ${description} | ${azle_wasm_instructions} | ${motoko_wasm_instructions} |${rust_wasm_instructions} | ${azle_motoko_change_multiplier} | ${azle_rust_change_multiplier} | ${motoko_azle_change_multiplier} | ${motoko_rust_change_multiplier} | ${rust_azle_change_multiplier} | ${rust_motoko_change_multiplier} |`;
-    });
+`;
 
     const average_azle_wasm_instructions_body_only = format_number_to_rust(calculate_average(benchmark_results.map((x) => x.azle_wasm_instructions.wasm_body_only)).toFixed(0));
     const average_azle_wasm_instructions_including_prelude = format_number_to_rust(calculate_average(benchmark_results.map((x) => x.azle_wasm_instructions.wasm_including_prelude)).toFixed(0));
@@ -406,7 +370,69 @@ The format for benchmark numbers is (x / y) where:
     const average_rust_motoko_change_multiplier_body_only  = format_number_to_rust(calculate_average(benchmark_results.map((x) => x.rust_motoko_change_multiplier.wasm_body_only)).toFixed(0));
     const average_rust_motoko_change_multiplier_including_prelude = format_number_to_rust(calculate_average(benchmark_results.map((x) => x.rust_motoko_change_multiplier.wasm_including_prelude)).toFixed(0));
 
-    return `${title}\n${description}\n${header}\n${header_separator}\n${data_rows.join('\n')}\n\nAverage Azle Wasm Instructions: (${average_azle_wasm_instructions_body_only} / ${average_azle_wasm_instructions_including_prelude})\n\nAverage Motoko Wasm Instructions: (${average_motoko_wasm_instructions_body_only} / ${average_motoko_wasm_instructions_including_prelude})\n\nAverage Rust Wasm Instructions: (${average_rust_wasm_instructions_body_only} / ${average_rust_wasm_instructions_including_prelude})\n\nAverage Azle/Motoko Change Multiplier: (${average_azle_motoko_change_multiplier_body_only}x / ${average_azle_motoko_change_multiplier_including_prelude}x)\n\nAverage Azle/Rust Change Multiplier: (${average_azle_rust_change_multiplier_body_only}x / ${average_azle_rust_change_multiplier_including_prelude}x)\n\nAverage Motoko/Azle Change Multiplier: (${average_motoko_azle_change_multiplier_body_only}x / ${average_motoko_azle_change_multiplier_including_prelude}x)\n\nAverage Motoko/Rust Change Multiplier: (${average_motoko_rust_change_multiplier_body_only}x / ${average_motoko_rust_change_multiplier_including_prelude}x)\n\nAverage Rust/Azle Change Multiplier: (${average_rust_azle_change_multiplier_body_only}x / ${average_rust_azle_change_multiplier_including_prelude}x)\n\nAverage Rust/Motoko Change Multiplier: (${average_rust_motoko_change_multiplier_body_only}x / ${average_rust_motoko_change_multiplier_including_prelude}x)`;
+    const averages_title = `## Averages`;
+    const averages_header_names = [
+        'Azle Wasm Instructions',
+        'Motoko Wasm Instructions',
+        'Rust Wasm Instructions',
+        'Azle/Motoko Change Multiplier',
+        'Azle/Rust Change Multiplier',
+        'Motoko/Azle Change Multiplier',
+        'Motoko/Rust Change Multiplier',
+        'Average Rust/Azle Change Multiplier',
+        'Rust/Motoko Change Multiplier'
+    ];
+    const averages_header = `| ${averages_header_names.join(' | ')} |`;
+    const averages_header_separator = `| ${averages_header_names.map((_) => '---').join(' | ')} |`;
+    const average_rows = [
+        `(${average_azle_wasm_instructions_body_only} / ${average_azle_wasm_instructions_including_prelude})`,
+        `(${average_motoko_wasm_instructions_body_only} / ${average_motoko_wasm_instructions_including_prelude})`,
+        `(${average_rust_wasm_instructions_body_only} / ${average_rust_wasm_instructions_including_prelude})`,
+        `(${average_azle_motoko_change_multiplier_body_only}x / ${average_azle_motoko_change_multiplier_including_prelude}x)`,
+        `(${average_azle_rust_change_multiplier_body_only}x / ${average_azle_rust_change_multiplier_including_prelude}x)`,
+        `(${average_motoko_azle_change_multiplier_body_only}x / ${average_motoko_azle_change_multiplier_including_prelude}x)`,
+        `(${average_motoko_rust_change_multiplier_body_only}x / ${average_motoko_rust_change_multiplier_including_prelude}x)`,
+        `(${average_rust_azle_change_multiplier_body_only}x / ${average_rust_azle_change_multiplier_including_prelude}x)`,
+        `(${average_rust_motoko_change_multiplier_body_only}x / ${average_rust_motoko_change_multiplier_including_prelude}x)`
+    ];
+
+    const primitive_ops_header_names = [
+        'Description',
+        'Azle Wasm Instructions',
+        'Motoko Wasm Instructions',
+        'Rust Wasm Instructions',
+        'Azle/Motoko Change Multiplier',
+        'Azle/Rust Change Multiplier',
+        'Motoko/Azle Change Multiplier',
+        'Motoko/Rust Change Multiplier',
+        'Rust/Azle Change Multiplier',
+        'Rust/Motoko Change Multiplier'
+    ];
+
+    const primitive_ops_header = `| ${primitive_ops_header_names.join(' | ')} |`;
+
+    const primitive_ops_header_separator = `| ${primitive_ops_header_names.map((_) => '---').join(' | ')} |`;
+
+    const primitive_ops_data_rows = benchmark_results.map((benchmark_result) => {
+        const description = `${benchmark_result.canister_method}${benchmark_result.benchmark_description !== '' ? `: ${benchmark_result.benchmark_description}` : ''}`;
+
+        const azle_wasm_instructions = `(${format_number_to_rust(benchmark_result.azle_wasm_instructions.wasm_body_only.toFixed(0))} / ${format_number_to_rust(benchmark_result.azle_wasm_instructions.wasm_including_prelude.toFixed(0))})`;
+        const motoko_wasm_instructions = `(${format_number_to_rust(benchmark_result.motoko_wasm_instructions.wasm_body_only.toFixed(0))} / ${format_number_to_rust(benchmark_result.motoko_wasm_instructions.wasm_including_prelude.toFixed(0))})`;
+        const rust_wasm_instructions = `(${format_number_to_rust(benchmark_result.rust_wasm_instructions.wasm_body_only.toFixed(0))} / ${format_number_to_rust(benchmark_result.rust_wasm_instructions.wasm_including_prelude.toFixed(0))})`;
+
+        const azle_motoko_change_multiplier = `(${format_number_to_rust(benchmark_result.azle_motoko_change_multiplier.wasm_body_only.toFixed(0))}x / ${format_number_to_rust(benchmark_result.azle_motoko_change_multiplier.wasm_including_prelude.toFixed(0))}x)`;
+        const azle_rust_change_multiplier = `(${format_number_to_rust(benchmark_result.azle_rust_change_multiplier.wasm_body_only.toFixed(0))}x / ${format_number_to_rust(benchmark_result.azle_rust_change_multiplier.wasm_including_prelude.toFixed(0))}x)`;
+
+        const motoko_azle_change_multiplier = `(${format_number_to_rust(benchmark_result.motoko_azle_change_multiplier.wasm_body_only.toFixed(0))}x / ${format_number_to_rust(benchmark_result.motoko_azle_change_multiplier.wasm_including_prelude.toFixed(0))}x)`;
+        const motoko_rust_change_multiplier = `(${format_number_to_rust(benchmark_result.motoko_rust_change_multiplier.wasm_body_only.toFixed(0))}x / ${format_number_to_rust(benchmark_result.motoko_rust_change_multiplier.wasm_including_prelude.toFixed(0))}x)`;
+
+        const rust_azle_change_multiplier = `(${format_number_to_rust(benchmark_result.rust_azle_change_multiplier.wasm_body_only.toFixed(0))}x / ${format_number_to_rust(benchmark_result.rust_azle_change_multiplier.wasm_including_prelude.toFixed(0))}x)`;
+        const rust_motoko_change_multiplier = `(${format_number_to_rust(benchmark_result.rust_motoko_change_multiplier.wasm_body_only.toFixed(0))}x / ${format_number_to_rust(benchmark_result.rust_motoko_change_multiplier.wasm_including_prelude.toFixed(0))}x)`;
+
+        return `| ${description} | ${azle_wasm_instructions} | ${motoko_wasm_instructions} |${rust_wasm_instructions} | ${azle_motoko_change_multiplier} | ${azle_rust_change_multiplier} | ${motoko_azle_change_multiplier} | ${motoko_rust_change_multiplier} | ${rust_azle_change_multiplier} | ${rust_motoko_change_multiplier} |`;
+    });
+
+    return `${title}\n${description}\n\n${averages_title}\n\n${averages_header}\n${averages_header_separator}\n| ${average_rows.join('|')} |\n\n## Primitive Operations\n\n${primitive_ops_header}\n${primitive_ops_header_separator}\n${primitive_ops_data_rows.join('\n')}`;
 }
 
 function create_csv_report(benchmark_results: BenchmarkResult[]): string {
