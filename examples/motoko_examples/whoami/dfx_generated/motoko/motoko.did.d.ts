60a400bd0e7fbd5270fc8cee6107755b8262d481
Dan Steren <dan@dansteren.com>
2022-08-02 15:33:13 -0600

    Add benchmarks

--- examples/motoko_examples/whoami/dfx_generated/motoko/motoko.did.d.ts
+++ examples/motoko_examples/whoami/dfx_generated/motoko/motoko.did.d.ts
@@ -1,10 +1,15 @@
 import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 
+export interface PerfResult {
+    wasm_body_only: bigint;
+    wasm_including_prelude: bigint;
+}
 export interface WhoAmI {
     argument: ActorMethod<[], Principal>;
+    get_perf_result: ActorMethod<[], [] | [PerfResult]>;
     id: ActorMethod<[], Principal>;
-    idQuick: ActorMethod<[], Principal>;
+    id_quick: ActorMethod<[], Principal>;
     installer: ActorMethod<[], Principal>;
     whoami: ActorMethod<[], Principal>;
 }
--- examples/motoko_examples/whoami/dfx_generated/motoko/motoko.did.js
+++ examples/motoko_examples/whoami/dfx_generated/motoko/motoko.did.js
@@ -1,8 +1,13 @@
 export const idlFactory = ({ IDL }) => {
+    const PerfResult = IDL.Record({
+        wasm_body_only: IDL.Nat64,
+        wasm_including_prelude: IDL.Nat64
+    });
     const WhoAmI = IDL.Service({
         argument: IDL.Func([], [IDL.Principal], ['query']),
+        get_perf_result: IDL.Func([], [IDL.Opt(PerfResult)], ['query']),
         id: IDL.Func([], [IDL.Principal], []),
-        idQuick: IDL.Func([], [IDL.Principal], []),
+        id_quick: IDL.Func([], [IDL.Principal], []),
         installer: IDL.Func([], [IDL.Principal], ['query']),
         whoami: IDL.Func([], [IDL.Principal], [])
     });
