11113aa422a393d93375446fdb50328139ddb1c3
Dan Steren <dan@dansteren.com>
2022-08-02 16:57:36 -0600

    Add benchmarks

--- examples/motoko_examples/quicksort/dfx_generated/azle/azle.did.js
+++ examples/motoko_examples/quicksort/dfx_generated/azle/azle.did.js
@@ -1,6 +1,11 @@
 export const idlFactory = ({ IDL }) => {
+    const PerfResult = IDL.Record({
+        wasm_body_only: IDL.Nat64,
+        wasm_including_prelude: IDL.Nat64
+    });
     return IDL.Service({
-        sort: IDL.Func([IDL.Vec(IDL.Int)], [IDL.Vec(IDL.Int)], ['query'])
+        get_perf_result: IDL.Func([], [IDL.Opt(PerfResult)], ['query']),
+        sort: IDL.Func([IDL.Vec(IDL.Int)], [IDL.Vec(IDL.Int)], [])
     });
 };
 export const init = ({ IDL }) => {
