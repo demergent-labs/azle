f84fa48b2b45fd2d6ef5eb5e3fc6de9c619c2213
Jordan Last <jordan.michael.last@gmail.com>
2022-08-03 16:52:32 -0500

    add candid_decode and candid_encode documentation

--- examples/candid_encoding/src/index.ts
+++ examples/candid_encoding/src/index.ts
@@ -1,9 +1,11 @@
 import { blob, ic, Query } from 'azle';
 
+// encodes a Candid string to Candid bytes
 export function candid_encode(candid_string: string): Query<blob> {
     return ic.candid_encode(candid_string);
 }
 
+// decodes Candid bytes to a Candid string
 export function candid_decode(candid_encoded: blob): Query<string> {
     return ic.candid_decode(candid_encoded);
 }
