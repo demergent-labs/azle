2bd634e07475b0f0452be08bcf7d2e5e08369221
Dan Steren <dan@dansteren.com>
2022-08-03 11:20:27 -0600

    Implement code review suggestions

--- examples/motoko_examples/superheroes/canisters/rust/src/lib.rs
+++ examples/motoko_examples/superheroes/canisters/rust/src/lib.rs
@@ -1,6 +1,13 @@
 use radix_trie::Trie;
 use std::cell::RefCell;
 
+thread_local! {
+    // The superhero data store
+    static DB_REF_CELL: RefCell<Trie<SuperheroId, Superhero>> = RefCell::new(Trie::new());
+    // The next available superhero identifier
+    static NEXT_REF_CELL: RefCell<SuperheroId> = RefCell::new(0);
+}
+
 //#region Performance
 #[derive(candid::CandidType, Clone)]
 pub struct PerfResult {
@@ -41,31 +48,24 @@ struct Superhero {
 #[derive(candid::CandidType, Debug, serde::Deserialize, Clone)]
 struct List(String, Option<Box<List>>);
 
-thread_local! {
-    // The superhero data store
-    static DB: RefCell<Trie<SuperheroId, Superhero>> = RefCell::new(Trie::new());
-    // The next available superhero identifier
-    static NEXT: RefCell<SuperheroId> = RefCell::new(0);
-}
-
 #[ic_cdk_macros::query]
 fn read(id: SuperheroId) -> Option<Superhero> {
-    DB.with(|db| db.borrow().get(&id).cloned())
+    DB_REF_CELL.with(|db_ref_cell| db_ref_cell.borrow().get(&id).cloned())
 }
 
 #[ic_cdk_macros::update]
 fn create(superhero: Superhero) -> SuperheroId {
     let perf_start = ic_cdk::api::call::performance_counter(0);
 
-    let superhero_id = NEXT.with(|next| {
-        let index = next.borrow();
+    let superhero_id = NEXT_REF_CELL.with(|next_ref_cell| {
+        let index = next_ref_cell.borrow();
         *index
     });
-    NEXT.with(|next| {
-        *next.borrow_mut() += 1;
+    NEXT_REF_CELL.with(|next_ref_cell| {
+        *next_ref_cell.borrow_mut() += 1;
     });
-    DB.with(|db| {
-        db.borrow_mut().insert(superhero_id, superhero);
+    DB_REF_CELL.with(|db_ref_cell| {
+        db_ref_cell.borrow_mut().insert(superhero_id, superhero);
     });
 
     let perf_end = ic_cdk::api::call::performance_counter(0);
@@ -78,11 +78,11 @@ fn create(superhero: Superhero) -> SuperheroId {
 fn update(superhero_id: SuperheroId, superhero: Superhero) -> bool {
     let perf_start = ic_cdk::api::call::performance_counter(0);
 
-    let result = DB.with(|db| db.borrow().get(&superhero_id).cloned());
+    let result = DB_REF_CELL.with(|db_ref_cell| db_ref_cell.borrow().get(&superhero_id).cloned());
     let exists = result.is_some();
     if exists {
-        DB.with(|db| {
-            db.borrow_mut().insert(superhero_id, superhero);
+        DB_REF_CELL.with(|db_ref_cell| {
+            db_ref_cell.borrow_mut().insert(superhero_id, superhero);
         });
     }
 
@@ -96,7 +96,7 @@ fn update(superhero_id: SuperheroId, superhero: Superhero) -> bool {
 fn delete_hero(superhero_id: SuperheroId) -> bool {
     let perf_start = ic_cdk::api::call::performance_counter(0);
 
-    let removed = DB.with(|db| db.borrow_mut().remove(&superhero_id));
+    let removed = DB_REF_CELL.with(|db_ref_cell| db_ref_cell.borrow_mut().remove(&superhero_id));
 
     let perf_end = ic_cdk::api::call::performance_counter(0);
     record_performance(perf_start, perf_end);
