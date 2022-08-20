4e4393f2dfc9804df4e8455de6a37390bed1578d
Benjamin DeMann <benjamin.demann@gmail.com>
2022-08-19 21:20:34 -0600

    switch to token stream and quotes

--- src/compiler/typescript_to_rust_redesign/azle_generate/src/generators/canister_methods/functions.rs
+++ src/compiler/typescript_to_rust_redesign/azle_generate/src/generators/canister_methods/functions.rs
@@ -1,23 +1,23 @@
-use proc_macro2::Ident;
+use proc_macro2::{Ident, TokenStream};
 use quote::{
     format_ident,
     quote,
 };
-use swc_ecma_ast::{FnDecl, TsType, TsKeywordTypeKind, TsTypeAnn, TsKeywordType, TsTypeRef, Param, TsArrayType };
+use swc_ecma_ast::{FnDecl, TsType, TsKeywordTypeKind, TsTypeAnn, TsKeywordType, TsTypeRef, Param, TsArrayType, Id };
 
 pub fn generate_function_token_stream(ast_fnc_decl_query: &FnDecl) -> proc_macro2::TokenStream {
     let function_name = ast_fnc_decl_query.ident.to_string().replace("#0", "");
     let function_name_ident = format_ident!("{}", function_name);
 
     let ts_type_ann = &ast_fnc_decl_query.function.return_type.as_ref();
-    let return_type_ident = generate_return_type_ident(ts_type_ann);
+    let return_type = generate_return_type(ts_type_ann);
 
     let param_name_idents = generate_param_name_idents(&ast_fnc_decl_query.function.params);
-    let param_type_idents = generate_param_type_idents(&ast_fnc_decl_query.function.params);
-    let params = generate_params_quote(param_name_idents, param_type_idents);
+    let param_types = generate_param_types(&ast_fnc_decl_query.function.params);
+    let params = generate_params_quote(param_name_idents, param_types);
 
     quote! {
-        async fn #function_name_ident(#(#params),*) -> #return_type_ident {
+        async fn #function_name_ident(#(#params),*) -> #return_type {
             Default::default()
         }
     }
@@ -30,7 +30,7 @@ fn generate_param_name_idents(params: &Vec<Param>) -> Vec<Ident> {
     }).collect()
 }
 
-fn generate_params_quote(names: Vec<Ident>, types: Vec<Ident>) -> Vec<proc_macro2::TokenStream> {
+fn generate_params_quote(names: Vec<Ident>, types: Vec<TokenStream>) -> Vec<TokenStream> {
     names.iter().enumerate().map(|(i, name)| {
         let param_type = types[i].clone();
         quote!{
@@ -39,18 +39,13 @@ fn generate_params_quote(names: Vec<Ident>, types: Vec<Ident>) -> Vec<proc_macro
     }).collect()
 }
 
-fn string_to_token_stream(string: String) -> proc_macro2::TokenStream {
-    let ident = format_ident!("{}", string);
-    quote!{#ident}
-}
-
-fn parse_ts_keyword_type(ts_keyword_type: TsKeywordType) -> Ident {
+fn parse_ts_keyword_type(ts_keyword_type: TsKeywordType) -> TokenStream {
     let kind = ts_keyword_type.kind;
-    let result = match &kind {
-        TsKeywordTypeKind::TsBooleanKeyword => "bool",
-        TsKeywordTypeKind::TsStringKeyword => "String",
-        TsKeywordTypeKind::TsVoidKeyword => "RustVoid",
-        TsKeywordTypeKind::TsNullKeyword => "RustNull",
+    match &kind {
+        TsKeywordTypeKind::TsBooleanKeyword => quote!(bool),
+        TsKeywordTypeKind::TsStringKeyword => quote!(String),
+        TsKeywordTypeKind::TsVoidKeyword => quote!{()},
+        TsKeywordTypeKind::TsNullKeyword => quote!{(())},
         TsKeywordTypeKind::TsObjectKeyword => todo!(),
         TsKeywordTypeKind::TsNumberKeyword => todo!(),
         TsKeywordTypeKind::TsBigIntKeyword => todo!(),
@@ -60,62 +55,60 @@ fn parse_ts_keyword_type(ts_keyword_type: TsKeywordType) -> Ident {
         TsKeywordTypeKind::TsUndefinedKeyword => todo!(),
         TsKeywordTypeKind::TsUnknownKeyword => todo!(),
         TsKeywordTypeKind::TsAnyKeyword => todo!(),
-    };
-    format_ident!("{}", result)
+    }
 }
 
-fn parse_ts_type_ref(ts_type_ref: TsTypeRef) -> Ident {
+fn parse_ts_type_ref(ts_type_ref: TsTypeRef) -> TokenStream {
     let type_name = ts_type_ref.type_name.as_ident().unwrap().sym.chars().as_str();
-    let result = match &type_name[..] {
-        "blob" => "RustBlob",
-        "float32" => "f32",
-        "float64" => "f64",
-        "int" => "CandidInt",
-        "int8" => "i8",
-        "int16" => "i16",
-        "int32" => "i32",
-        "int64" => "i64",
-        "nat" => "CandidNat",
-        "nat8" => "u8",
-        "nat16" => "u16",
-        "nat32" => "u32",
-        "nat64" => "u64",
-        "Principal" => "CandidPrincipal",
-        "empty" => "CandidEmpty",
-        "reserved" => "CandidReserved",
+    match &type_name[..] {
+        "blob" => quote!(Vec<u8>),
+        "float32" => quote!(f32),
+        "float64" => quote!(f64),
+        "int" => quote!(candid::Int),
+        "int8" => quote!(i8),
+        "int16" => quote!(i16),
+        "int32" => quote!(i32),
+        "int64" => quote!(i64),
+        "nat" => quote!(candid::Nat),
+        "nat8" => quote!(u8),
+        "nat16" => quote!(u16),
+        "nat32" => quote!(u32),
+        "nat64" => quote!(u64),
+        "Principal" => quote!(candid::Principal),
+        "empty" => quote!(candid::Empty),
+        "reserved" => quote!(candid::Reserved),
         _ => todo!(),
-    };
-    format_ident!("{}", result)
+    }
 }
 
-fn parse_ts_array_type(ts_array_type: TsArrayType) -> Ident {
+fn parse_ts_array_type(ts_array_type: TsArrayType) -> TokenStream {
     let elem_type = *ts_array_type.elem_type;
-    let elem_type_ident = generate_type_ident(elem_type);
-    format_ident!("Vec<{}>", elem_type_ident)
+    let elem_type_ident = ts_type_to_rust_type(elem_type);
+    quote!{Vec<#elem_type_ident>}
 }
 
-fn generate_return_type_ident(ts_type_ann:&Option<&TsTypeAnn>) -> Ident {
+fn generate_return_type(ts_type_ann:&Option<&TsTypeAnn>) -> TokenStream {
     let type_ann = ts_type_ann.clone().unwrap();
     let type_ref = type_ann.type_ann.as_ts_type_ref().unwrap();
     let type_params = type_ref.type_params.clone().unwrap();
 
     let params = *type_params.params[0].clone();
-    generate_type_ident(params)
+    ts_type_to_rust_type(params)
 }
 
-fn generate_param_type_idents(params: &Vec<Param>) -> Vec<Ident> {
+fn generate_param_types(params: &Vec<Param>) -> Vec<TokenStream> {
     params.iter().map(|param| {
         let type_ann = &param.pat.as_ident().unwrap().type_ann.as_ref();
 
         let type_ann = type_ann.clone().unwrap();
         let ts_type = *type_ann.type_ann.clone();
 
-        generate_type_ident(ts_type)
+        ts_type_to_rust_type(ts_type)
     }).collect()
 }
 
 
-fn generate_type_ident(ts_type: TsType) -> Ident {
+fn ts_type_to_rust_type(ts_type: TsType) -> TokenStream {
     match ts_type {
         TsType::TsKeywordType(ts_keyword_type) => parse_ts_keyword_type(ts_keyword_type),
         TsType::TsTypeRef(ts_type_ref) => parse_ts_type_ref(ts_type_ref),
