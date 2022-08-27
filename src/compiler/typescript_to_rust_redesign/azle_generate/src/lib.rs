6f7a1dec49b005dbba41165a360c95ff7471b709
Benjamin DeMann <benjamin.demann@gmail.com>
2022-08-27 13:08:37 -0600

    pretty pass on lib

--- src/compiler/typescript_to_rust_redesign/azle_generate/src/lib.rs
+++ src/compiler/typescript_to_rust_redesign/azle_generate/src/lib.rs
@@ -2,10 +2,10 @@
 // TODO then we can move on from there
 
 use proc_macro::TokenStream;
-use quote::{
-    quote
-};
-use std::{path::Path, collections::HashSet, iter::FromIterator};
+use quote::quote;
+use std::{collections::HashSet, iter::FromIterator, path::Path};
+use swc_common::{sync::Lrc, SourceMap};
+use swc_ecma_ast::Program;
 use swc_ecma_parser::{
     lexer::Lexer,
     Parser,
@@ -13,55 +13,54 @@ use swc_ecma_parser::{
     // FileInput,
     // SourceFileInput,
     Syntax,
-    TsConfig
+    TsConfig,
 };
-use swc_ecma_ast::{
-    Program,
-};
-use swc_common::{
-    sync::Lrc,
-    SourceMap,
-};
-use syn::{
-    parse_macro_input,
-    LitStr
-};
+use syn::{parse_macro_input, LitStr};
 
 mod generators {
     pub mod canister_methods;
 }
 
 use generators::canister_methods::{
-    get_ast_fn_decls_from_programs,
-    generate_query_function_infos,
-    get_query_fn_decls, get_update_fn_decls, generate_update_function_token_streams, get_ast_type_alias_decls_from_programs, FunctionInformation, StructInfo,
+    generate_query_function_infos, generate_update_function_token_streams,
+    get_ast_fn_decls_from_programs, get_ast_type_alias_decls_from_programs, get_query_fn_decls,
+    get_update_fn_decls, FunctionInformation, StructInfo,
 };
 
 use crate::generators::canister_methods::generate_type_alias_token_streams;
 
-fn collect_function_type_dependencies(function_info: &Vec<FunctionInformation>) -> HashSet<String>{
-    let dependencies = function_info
-        .iter()
-        .fold(vec![], |acc, fun_info| {
-            vec![acc, fun_info.dependant_types.clone()].concat()
-        });
+fn collect_function_type_dependencies(function_info: &Vec<FunctionInformation>) -> HashSet<String> {
+    let dependencies = function_info.iter().fold(vec![], |acc, fun_info| {
+        vec![acc, fun_info.dependant_types.clone()].concat()
+    });
     HashSet::from_iter(dependencies.iter().cloned())
 }
 
-fn collect_inline_dependencies(function_info: &Vec<FunctionInformation>) -> Vec<proc_macro2::TokenStream> {
-    function_info
-        .iter()
-        .fold(vec![], |acc, fun_info| {
-            vec![acc, get_better_name(&fun_info.inline_dependant_types)].concat()
-        })
+fn collect_inline_dependencies(
+    function_info: &Vec<FunctionInformation>,
+) -> Vec<proc_macro2::TokenStream> {
+    function_info.iter().fold(vec![], |acc, fun_info| {
+        vec![
+            acc,
+            collect_inline_dependencies_for_struct(&fun_info.inline_dependant_types),
+        ]
+        .concat()
+    })
 }
 
-fn get_better_name(struct_info: &Box<Vec<StructInfo>>) -> Vec<proc_macro2::TokenStream> {
+fn collect_inline_dependencies_for_struct(
+    struct_info: &Box<Vec<StructInfo>>,
+) -> Vec<proc_macro2::TokenStream> {
     struct_info.iter().fold(vec![], |acc2, dependencies| {
         let this_dependency_token_stream = &dependencies.structure;
-        let sub_dependency_token_streams = get_better_name(&dependencies.inline_dependencies);
-        // let cool_strings: Vec<String> = sub_dependency_token_streams.iter().map(|token_stream| token_stream.to_string()).collect();
-        vec![acc2, sub_dependency_token_streams, vec![this_dependency_token_stream.clone()]].concat()
+        let sub_dependency_token_streams =
+            collect_inline_dependencies_for_struct(&dependencies.inline_dependencies);
+        vec![
+            acc2,
+            vec![this_dependency_token_stream.clone()],
+            sub_dependency_token_streams,
+        ]
+        .concat()
     })
 }
 
@@ -83,27 +82,23 @@ pub fn azle_generate(ts_file_names_token_stream: TokenStream) -> TokenStream {
     let ast_fnc_decls_update = get_update_fn_decls(&ast_fnc_decls);
 
     // println!("ast_fnc_decls_query: {:#?}", ast_fnc_decls_query);
-    let inline_structs_counts = 0;
+    let mut inline_dep_count = 0;
 
-    let query_function_info = generate_query_function_infos(&ast_fnc_decls_query, inline_structs_counts);
-    let inline_structs_counts = query_function_info.1;
-    let query_function_info = query_function_info.0;
+    let (query_function_info, count) =
+        generate_query_function_infos(&ast_fnc_decls_query, inline_dep_count);
+    inline_dep_count = count;
     let query_function_token_streams: Vec<proc_macro2::TokenStream> = query_function_info
         .iter()
-        .map(|fun_info| {
-            fun_info.token_stream.clone()
-        })
+        .map(|fun_info| fun_info.token_stream.clone())
         .collect();
 
     // let query_inline_type_aliases = quote!();
-    let update_function_info = generate_update_function_token_streams(&ast_fnc_decls_update, inline_structs_counts);
-    let count = update_function_info.1;
-    let update_function_info = update_function_info.0;
+    let (update_function_info, count) =
+        generate_update_function_token_streams(&ast_fnc_decls_update, inline_dep_count);
+    inline_dep_count = count;
     let update_function_token_streams: Vec<proc_macro2::TokenStream> = update_function_info
         .iter()
-        .map(|fun_info| {
-            fun_info.token_stream.clone()
-        })
+        .map(|fun_info| fun_info.token_stream.clone())
         .collect();
     // let update_inline_type_aliases = quote!();
 
@@ -111,22 +106,34 @@ pub fn azle_generate(ts_file_names_token_stream: TokenStream) -> TokenStream {
     let query_function_dependant_types = collect_function_type_dependencies(&query_function_info);
     let update_function_dependant_types = collect_function_type_dependencies(&update_function_info);
 
-    let type_alias_dependant_types: HashSet<&String> = query_function_dependant_types.union(&update_function_dependant_types).collect();
+    let type_alias_dependant_types: HashSet<&String> = query_function_dependant_types
+        .union(&update_function_dependant_types)
+        .collect();
 
     let query_function_inline_dependant_types = collect_inline_dependencies(&query_function_info);
     let update_function_inline_dependant_types = collect_inline_dependencies(&update_function_info);
     // TODO it would be great to add the inline_types we found from doing the type aliases while we are at it
-    let inline_types = vec![query_function_inline_dependant_types, update_function_inline_dependant_types].concat();
+    let inline_types = vec![
+        query_function_inline_dependant_types,
+        update_function_inline_dependant_types,
+    ]
+    .concat();
 
-    let type_aliases_map = generate_type_alias_token_streams(&type_alias_dependant_types, &ast_type_alias_decls, count);
-    let type_aliases_map = type_aliases_map.0;
+    let (type_aliases_map, count) = generate_type_alias_token_streams(
+        &type_alias_dependant_types,
+        &ast_type_alias_decls,
+        inline_dep_count,
+    );
+    inline_dep_count = count;
 
-    let type_aliases = type_aliases_map.iter().fold(quote!(), |acc, (_name, token_stream)| {
-        quote!{
-            #acc
-            #token_stream
-        }
-    });
+    let type_aliases = type_aliases_map
+        .iter()
+        .fold(quote!(), |acc, (_, token_stream)| {
+            quote! {
+                #acc
+                #token_stream
+            }
+        });
 
     quote! {
         #(#inline_types)*
@@ -151,28 +158,32 @@ pub fn azle_generate(ts_file_names_token_stream: TokenStream) -> TokenStream {
                 std::fs::write("index.did", export_candid()).unwrap();
             }
         }
-    }.into()
+    }
+    .into()
 }
 
 fn get_programs(ts_file_names: &Vec<&str>) -> Vec<Program> {
-    ts_file_names.iter().map(|ts_file_name| {
-        let filepath = Path::new(ts_file_name).to_path_buf();
+    ts_file_names
+        .iter()
+        .map(|ts_file_name| {
+            let filepath = Path::new(ts_file_name).to_path_buf();
 
-        let cm: Lrc<SourceMap> = Default::default();
+            let cm: Lrc<SourceMap> = Default::default();
 
-        let fm = cm.load_file(&filepath).unwrap();
+            let fm = cm.load_file(&filepath).unwrap();
 
-        let lexer = Lexer::new(
-            Syntax::Typescript(TsConfig::default()),
-            Default::default(),
-            StringInput::from(&*fm),
-            None
-        );
+            let lexer = Lexer::new(
+                Syntax::Typescript(TsConfig::default()),
+                Default::default(),
+                StringInput::from(&*fm),
+                None,
+            );
 
-        let mut parser = Parser::new_from(lexer);
+            let mut parser = Parser::new_from(lexer);
 
-        let program = parser.parse_program().unwrap();
+            let program = parser.parse_program().unwrap();
 
-        program
-    }).collect()
+            program
+        })
+        .collect()
 }
