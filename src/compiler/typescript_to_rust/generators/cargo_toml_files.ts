888f41496f0bbc97e8ff760b12575c0ed3716402
Jordan Last <jordan.michael.last@gmail.com>
2022-09-02 16:24:33 -0500

    experiment with removing proc_macro, generate rust code directly and integrate into canister compilation process

--- src/compiler/typescript_to_rust/generators/cargo_toml_files.ts
+++ src/compiler/typescript_to_rust/generators/cargo_toml_files.ts
@@ -6,7 +6,8 @@ export function generateWorkspaceCargoToml(rootPath: string): Toml {
 
         [workspace]
         members = [
-            "${rootPath}"
+            "${rootPath}",
+            "${rootPath}/azle_generate"
         ]
 
         [profile.release]
@@ -40,7 +41,7 @@ export function generateLibCargoToml(canisterName: string): Toml {
         getrandom = { version = "0.2.3", features = ["custom"] }
         serde = "1.0.137"
         azle-js-value-derive = { path = "./azle_js_value_derive" }
-        azle_generate_macro = { path = "./azle_generate_macro" }
+        # azle_generate_macro = { path = "./azle_generate_macro" }
         async-recursion = "1.0.0"
     `;
 }
--- src/compiler/typescript_to_rust_redesign/azle_generate/src/main.rs
+++ src/compiler/typescript_to_rust_redesign/azle_generate/src/main.rs
@@ -1,7 +1,12 @@
 use azle_generate::azle_generate;
 
 fn main() {
-    let result = azle_generate(&vec!["./src/playground/index.ts"]);
+    let args: Vec<String> = std::env::args().collect();
+
+    let ts_file_names_string = &args[1];
+    let ts_file_names: Vec<&str> = ts_file_names_string.split(",").collect();
+
+    let result = azle_generate(&ts_file_names);
 
     println!("{}", result);
 }
