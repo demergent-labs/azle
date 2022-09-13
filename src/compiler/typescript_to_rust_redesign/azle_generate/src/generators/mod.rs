62802840b5d054945f0ad4a751999d816c4b1a95
Dan Steren <dan@dansteren.com>
2022-09-13 15:43:56 -0600

    Move generators module definition

---
+++ src/compiler/typescript_to_rust_redesign/azle_generate/src/generators/mod.rs
@@ -0,0 +1,7 @@
+pub mod azle_into_js_value;
+pub mod azle_try_from_js_value;
+pub mod canister_methods;
+pub mod cross_canister_call_functions;
+pub mod funcs;
+pub mod ic_object;
+pub mod stacktrace;
