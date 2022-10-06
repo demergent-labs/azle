1336e73f8cb0a70090ebe7baf42b6d6397420e1e
Jordan Last <jordan.michael.last@gmail.com>
2022-10-06 18:08:48 -0500

    add http  counter

--- examples/motoko_examples/http_counter/test/tests.ts
+++ examples/motoko_examples/http_counter/test/tests.ts
@@ -1,28 +1,132 @@
 import { Test } from 'azle/test';
-import { _SERVICE } from '../dfx_generated/azle/azle.did';
-import { ActorSubclass } from '@dfinity/agent';
+import { execSync } from 'child_process';
 
-export function get_tests(update_canister: ActorSubclass<_SERVICE>): Test[] {
+export function get_tests(): Test[] {
     return [
         {
-            name: 'update',
+            name: 'init get count',
             test: async () => {
-                const result = await update_canister.update('Why hello there');
-
                 return {
-                    ok: result === undefined
+                    ok: getCount() === getExpectedGetCountResult(0)
                 };
             }
         },
         {
-            name: 'get_current_message',
+            name: 'first increment',
             test: async () => {
-                const result = await update_canister.get_current_message();
-
                 return {
-                    ok: result === 'Why hello there'
+                    ok: count() === getExpectedCountResult(1)
+                };
+            }
+        },
+        {
+            name: 'second increment',
+            test: async () => {
+                return {
+                    ok: count() === getExpectedCountResult(2)
+                };
+            }
+        },
+        {
+            name: 'get count',
+            test: async () => {
+                return {
+                    ok: getCount() === getExpectedGetCountResult(2)
+                };
+            }
+        },
+        {
+            name: 'gzipped increment',
+            test: async () => {
+                return {
+                    ok: countGzip() === 'update'
+                };
+            }
+        },
+        {
+            name: 'get gzipped count',
+            test: async () => {
+                return {
+                    ok: getCountGzip() === 'query'
+                };
+            }
+        },
+        {
+            name: 'get streaming count',
+            test: async () => {
+                return {
+                    ok: getCountStream() === getExpectedGetCountStreamResult(3)
+                };
+            }
+        },
+        {
+            name: 'final get count',
+            test: async () => {
+                return {
+                    ok: getCount() === getExpectedGetCountResult(3)
                 };
             }
         }
     ];
+}
+
+function getCanisterID(): string {
+    return execSync(`dfx canister id http_counter`).toString().trim();
+}
+
+function count(): string {
+    const canister_id = getCanisterID();
+    return execSync(
+        `curl --silent -X POST "${canister_id}.localhost:8000/" --resolve "${canister_id}.localhost:8000:127.0.0.1"`
+    )
+        .toString()
+        .trim();
+}
+
+function countGzip(): string {
+    const canister_id = getCanisterID();
+    return execSync(
+        `curl --compressed --silent -X POST "${canister_id}.localhost:8000/" --resolve "${canister_id}.localhost:8000:127.0.0.1"`
+    )
+        .toString()
+        .trim();
+}
+
+function getCount(): string {
+    const canister_id = getCanisterID();
+    return execSync(
+        `curl --silent "${canister_id}.localhost:8000/" --resolve "${canister_id}.localhost:8000:127.0.0.1"`
+    )
+        .toString()
+        .trim();
+}
+
+function getCountStream(): string {
+    const canister_id = getCanisterID();
+    return execSync(
+        `curl --silent "${canister_id}.localhost:8000/stream" --resolve "${canister_id}.localhost:8000:127.0.0.1"`
+    )
+        .toString()
+        .trim();
+}
+
+function getCountGzip(): string {
+    const canister_id = getCanisterID();
+    return execSync(
+        `curl --compressed --silent "${canister_id}.localhost:8000/" --resolve "${canister_id}.localhost:8000:127.0.0.1"`
+    )
+        .toString()
+        .trim();
+}
+
+function getExpectedGetCountResult(expectedCount: number): string {
+    return `Counter is ${expectedCount}\n/`;
+}
+
+function getExpectedGetCountStreamResult(expectedCount: number): string {
+    return `Counter is ${expectedCount} streaming`;
+}
+
+function getExpectedCountResult(expectedCount: number): string {
+    return `Counter updated to ${expectedCount}`;
 }
