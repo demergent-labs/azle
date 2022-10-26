26a2c26b299034dbd47ce05b21efa470c68d74b8
Dan Steren <dan@dansteren.com>
2022-10-25 18:08:38 -0600

    WIP: Get multi-line error for heartbeat
    For errors where two separate lines of code are related, we want to show
    code snippets that span both locations. Then we should fold them when
    displaying so that only the relevant lines are showing.
    
    Such is the case when we declare multiple heartbeat methods. The two
    slices should share the same source snippet, rather than creating two
    individual snippets that are each only one line long.
    
    This is my first stab at getting this working. It works, just needs more
    polishing.
    
    The result is an error like this:
    
    error: cannot expose more than one heartbeat method
     --> src/index.ts:4:7
      |
    4 |     : Heartbeat {
      |       ^^^^^^^^^ heartbeat first defined here
      |
    ...
    8 | export function heartbeat2(): Heartbeat {
      |                               ^^^^^^^^^ and later defined here
      |
    
    Note: Ideally the origin referenced would point to the second snippet,
    not the first.

--- src/compiler/typescript_to_rust/azle_generate/src/ts_ast/program/system_canister_method_builder/heartbeat.rs
+++ src/compiler/typescript_to_rust/azle_generate/src/ts_ast/program/system_canister_method_builder/heartbeat.rs
@@ -1,7 +1,17 @@
+use annotate_snippets::{
+    display_list::{DisplayList, FormatOptions},
+    snippet::{Annotation, AnnotationType, Slice, Snippet, SourceAnnotation},
+};
+use swc_common::source_map::Pos;
+
 use crate::{
     cdk_act::{nodes::ActHeartbeatMethod, CanisterMethodType},
     generators::canister_methods::method_body,
-    ts_ast::program::{azle_program::AzleProgramVecHelperMethods, AzleProgram},
+    ts_ast::{
+        program::{azle_program::AzleProgramVecHelperMethods, AzleProgram},
+        source_map::GetSourceFileInfo,
+        AzleFnDecl,
+    },
 };
 
 pub fn build_canister_method_system_heartbeat(
@@ -10,7 +20,9 @@ pub fn build_canister_method_system_heartbeat(
     let heartbeat_fn_decls = programs.get_azle_fn_decls_of_type(&CanisterMethodType::Heartbeat);
 
     if heartbeat_fn_decls.len() > 1 {
-        panic!("Only one Heartbeat function can be defined");
+        let error_message = create_error_message(heartbeat_fn_decls);
+
+        panic!("{}", error_message);
     }
 
     let heartbeat_fn_decl_option = heartbeat_fn_decls.get(0);
@@ -36,4 +48,84 @@ pub fn build_canister_method_system_heartbeat(
     } else {
         None
     }
+}
+
+fn create_error_message(heartbeat_fn_decls: Vec<AzleFnDecl>) -> String {
+    let source_map = heartbeat_fn_decls[0].source_map;
+    let first_heartbeat = &heartbeat_fn_decls[0];
+    let second_heartbeat = &heartbeat_fn_decls[1];
+
+    let return_type_span = first_heartbeat
+        .fn_decl
+        .function
+        .return_type
+        .as_ref()
+        .unwrap() // TODO
+        .type_ann
+        .as_ts_type_ref()
+        .unwrap() // TODO
+        .span;
+
+    let second_return_type_span = second_heartbeat
+        .fn_decl
+        .function
+        .return_type
+        .as_ref()
+        .unwrap() // TODO
+        .type_ann
+        .as_ts_type_ref()
+        .unwrap() // TODO
+        .span;
+
+    let total_range = (return_type_span.lo, second_return_type_span.hi);
+
+    let source = source_map.get_source_from_range(total_range);
+
+    let line_number = source_map.get_line_number(return_type_span);
+    let line_range = source_map.get_range(return_type_span);
+    // let modified_range = (range.0 - total_range.0, range.1 - total_range.0);
+    let origin = source_map.get_origin(second_return_type_span);
+
+    let char_diff = total_range.0.to_usize() - line_range.0;
+    let range2 = source_map.get_multi_line_range(second_return_type_span, char_diff);
+
+    let error_snippet = Snippet {
+        title: Some(Annotation {
+            label: Some("cannot expose more than one heartbeat method"),
+            id: None,
+            annotation_type: AnnotationType::Error,
+        }),
+        footer: vec![],
+        slices: vec![
+            Slice {
+                source: &source,
+                line_start: line_number,
+                origin: Some(&origin),
+                fold: true,
+                annotations: vec![SourceAnnotation {
+                    label: "heartbeat first defined here",
+                    annotation_type: AnnotationType::Error,
+                    range: line_range,
+                    // range: modified_range,
+                }],
+            },
+            Slice {
+                source: &source,
+                line_start: line_number,
+                origin: None,
+                fold: true,
+                annotations: vec![SourceAnnotation {
+                    label: "and later defined here",
+                    annotation_type: AnnotationType::Error,
+                    range: range2,
+                }],
+            },
+        ],
+        opt: FormatOptions {
+            color: true,
+            ..Default::default()
+        },
+    };
+
+    format!("{}", DisplayList::from(error_snippet))
 }
