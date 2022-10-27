3e4c93a2d52203e88e61576ae9b43c7cbada1f96
Jordan Last <jordan.michael.last@gmail.com>
2022-10-26 20:56:56 -0500

    update counter example

--- examples/counter/test/dfx_generated/counter/counter.did.d.ts
+++ examples/counter/test/dfx_generated/counter/counter.did.d.ts
@@ -1,5 +1,7 @@
 import type { Principal } from '@dfinity/principal';
+import type { ActorMethod } from '@dfinity/agent';
+
 export interface _SERVICE {
-    incrementCount: () => Promise<bigint>;
-    readCount: () => Promise<bigint>;
+    increment_count: ActorMethod<[], bigint>;
+    read_count: ActorMethod<[], bigint>;
 }
