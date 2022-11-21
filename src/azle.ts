2f7abd90ebe3e8b938c0b02332ed41588391a5d4
Dan Steren <dan@dansteren.com>
2022-11-21 16:15:56 -0700

    Use regex to detect end of error message

--- src/azle.ts
+++ src/azle.ts
@@ -419,9 +419,13 @@ function runAzleGenerate(
         const lineWhereErrorMessageStarts = stdErrLines.findIndex((line) =>
             line.startsWith("thread 'main' panicked")
         );
-        const lineWhereErrorMessageEnds = stdErrLines.findIndex((line) =>
-            line.includes("', src/azle_generate")
-        );
+        const starts_with_quote_comma_and_contains_a_path_and_ends_with_line_and_column_number =
+            /',\s.*\/.*\.rs:\d*:\d*/;
+        const lineWhereErrorMessageEnds = stdErrLines.findIndex((line) => {
+            return starts_with_quote_comma_and_contains_a_path_and_ends_with_line_and_column_number.test(
+                line
+            );
+        });
         if (
             lineWhereErrorMessageStarts === -1 ||
             lineWhereErrorMessageEnds === -1
@@ -440,10 +444,12 @@ function runAzleGenerate(
             "thread 'main' panicked at '",
             ''
         );
-        const panicLocation = /', src\/.*/;
         errorLines[errorLines.length - 1] = errorLines[
             errorLines.length - 1
-        ].replace(panicLocation, '');
+        ].replace(
+            starts_with_quote_comma_and_contains_a_path_and_ends_with_line_and_column_number,
+            ''
+        );
 
         return Err({
             error: `${generalErrorMessage}\n\n${errorLines.join('\n')}`,
