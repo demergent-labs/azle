bb68cbf8287b12288e485288c7b5a310687d472f
Dan Steren <dan@demergentlabs.org>
2023-01-05 18:42:53 -0700

    Make exported StableBTreeMaps work
    This is totally just copy/paste from above but it works.

--- src/compiler/typescript_to_rust/azle_generate/src/ts_ast/azle_program/stable_b_tree_maps.rs
+++ src/compiler/typescript_to_rust/azle_generate/src/ts_ast/azle_program/stable_b_tree_maps.rs
@@ -51,7 +51,48 @@ impl AzleProgram {
                             },
                             _ => acc,
                         },
-                        ModuleItem::ModuleDecl(_) => acc,
+                        ModuleItem::ModuleDecl(module_decl) => {
+                            // TODO: This is copy/pasted from above. Go back and consolidate this.
+                            match module_decl {
+                                swc_ecma_ast::ModuleDecl::ExportDecl(export_decl) => {
+                                    match &export_decl.decl {
+                                        Decl::Var(var_decl) => {
+                                            let stable_maps_in_var_decl_decls = var_decl.decls.iter().fold(
+                                                vec![],
+                                                |inner_acc, var_declarator| match &var_declarator.init {
+                                                    Some(init) => match &**init {
+                                                        Expr::New(new_expr) => match &*new_expr.callee {
+                                                            Expr::Ident(ident) => {
+                                                                if ident.get_name() == "StableBTreeMap" {
+                                                                    let azle_new_expr = AzleNewExpr {
+                                                                        new_expr: new_expr.clone(),
+                                                                        source_map: &self.source_map
+                                                                    };
+
+                                                                    let stable_b_tree_map_node_option = azle_new_expr.to_azle_stable_b_tree_map_node();
+                                                                    match stable_b_tree_map_node_option {
+                                                                        Ok(stable_b_tree_map_node) => vec![inner_acc, vec![stable_b_tree_map_node]].concat(),
+                                                                        Err(err) => panic!("{}", err),
+                                                                    }
+                                                                } else {
+                                                                    inner_acc
+                                                                }
+                                                            }
+                                                            _ => inner_acc,
+                                                        },
+                                                        _ => inner_acc,
+                                                    },
+                                                    None => inner_acc,
+                                                },
+                                            );
+                                            vec![acc, stable_maps_in_var_decl_decls].concat()
+                                        },
+                                        _ => acc,
+                                    }
+                                },
+                                _ => acc,
+                            }
+                        },
                     })
             }
             Program::Script(_) => vec![],
