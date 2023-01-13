de890bce891b16da6b00369c6bac55a8016de6c4
Dan Steren <dan@demergentlabs.org>
2023-01-13 12:18:30 -0700

    Isolate ref cells and use statements

---
+++ src/compiler/typescript_to_rust/azle_generate/src/generators/header/use_statements.rs
@@ -0,0 +1,14 @@
+pub fn generate_use_statements() -> proc_macro2::TokenStream {
+    quote::quote! {
+        use azle_vm_value_derive::{CdkActTryIntoVmValue, CdkActTryFromVmValue};
+        use ic_cdk::api::call::CallResult;
+        use std::borrow::BorrowMut;
+        use rand::{
+            Rng,
+            SeedableRng,
+            rngs::StdRng
+        };
+        use slotmap::Key as AzleSlotMapKey; // Renamed to avoid clashes with user-defined types
+        use std::str::FromStr;
+    }
+}
