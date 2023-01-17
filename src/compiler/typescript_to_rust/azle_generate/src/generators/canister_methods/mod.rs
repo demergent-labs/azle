d3355fd89ce101c3cb7420b0a827953236a8c68a
Dan Steren <dan@demergentlabs.org>
2023-01-17 00:02:54 -0700

    Move inspect message

--- src/compiler/typescript_to_rust/azle_generate/src/generators/canister_methods/mod.rs
+++ src/compiler/typescript_to_rust/azle_generate/src/generators/canister_methods/mod.rs
@@ -3,6 +3,7 @@ use cdk_framework::{nodes::ActCanisterMethod, traits::CanisterMethodBuilder, Req
 
 pub mod heartbeat;
 pub mod init;
+pub mod inspect_message;
 pub mod method_body;
 pub mod post_upgrade;
 
