8ef4985ebdc3a97dbe3162fd4975f4539cc8bec1
Benjamin DeMann <benjamin.demann@gmail.com>
2023-01-20 13:38:31 -0700

    pr fixes

--- examples/composite_queries/canisters/canister1/canister1.ts
+++ examples/composite_queries/canisters/canister1/canister1.ts
@@ -1,23 +1,13 @@
-import {
-    Update,
-    ic,
-    nat,
-    ok,
-    Principal,
-    CompositeQuery,
-    CanisterResult,
-    Query,
-    CompositeQueryManual
-} from 'azle';
+import { ic, Manual, nat, ok, Principal, Query, Update } from 'azle';
 import { Canister2 } from '../canister2/types';
 import { Canister1 } from '../canister1/types';
-import { NatQueryResult as NatQueryResult, StringQueryResult } from './types';
+import { NatQueryResult, StringQueryResult } from './types';
 
-let canister1 = ic.canisters.Canister1<Canister1>(
+const canister1 = ic.canisters.Canister1<Canister1>(
     Principal.fromText('rrkah-fqaaa-aaaaa-aaaaq-cai')
 );
 
-let canister2 = ic.canisters.Canister2<Canister2>(
+const canister2 = ic.canisters.Canister2<Canister2>(
     Principal.fromText('ryjl3-tyaaa-aaaaa-aaaba-cai')
 );
 
@@ -25,28 +15,25 @@ let counter: nat = 0n;
 
 // Composite query calling a query
 export async function simple_composite_query(): Promise<
-    CompositeQuery<StringQueryResult>
+    Query<StringQueryResult>
 > {
     return await canister2.simple_query().call();
 }
 
 // Composite query calling a manual query
-export async function manual_query(): Promise<
-    CompositeQuery<StringQueryResult>
-> {
+export async function manual_query(): Promise<Query<StringQueryResult>> {
     return await canister2.manual_query().call();
 }
 
 // Manual composite query calling a manual query
 export async function totally_manual_query(): Promise<
-    CompositeQueryManual<StringQueryResult>
+    Query<Manual<StringQueryResult>>
 > {
     ic.reply(await canister2.manual_query().call());
 }
 
 // Composite query calling another composite query
-export async function deep_query(): Promise<CompositeQuery<StringQueryResult>> {
-    // return await canister2.deep_query().call();
+export async function deep_query(): Promise<Query<StringQueryResult>> {
     const result = await canister2.deep_query().call();
     if (!ok(result)) {
         return {
@@ -65,9 +52,7 @@ export async function deep_query(): Promise<CompositeQuery<StringQueryResult>> {
 }
 
 // Composite query calling an update method. SHOULDN'T WORK
-export async function update_query(): Promise<
-    CompositeQuery<StringQueryResult>
-> {
+export async function update_query(): Promise<Query<StringQueryResult>> {
     return await canister2.update_query().call();
 }
 
@@ -78,7 +63,7 @@ export async function simple_query(): Promise<Query<StringQueryResult>> {
 
 // Composite query being called by an update method. SHOULDN'T WORK
 export async function simple_update(): Promise<Update<StringQueryResult>> {
-    let result = await canister2.deep_query().call();
+    const result = await canister2.deep_query().call();
     if (!ok(result)) {
         return {
             err: result.err
@@ -96,61 +81,67 @@ export async function simple_update(): Promise<Update<StringQueryResult>> {
 }
 
 // Composite query that modifies the state. Should revert after the call is done
-export function inc_counter(): CompositeQuery<nat> {
+export async function inc_counter(): Promise<Query<nat>> {
     counter += 1n;
     return counter;
 }
 
 // Composite query calling queries on the same canister. SHOULDN'T WORK
-export async function inc_canister1(): Promise<CompositeQuery<NatQueryResult>> {
+export async function inc_canister1(): Promise<Query<NatQueryResult>> {
     counter += 1n;
 
-    let canister1_a: CanisterResult<nat> = await canister1.inc_counter().call();
-    if ('err' in canister1_a)
+    const canister1_a_result = await canister1.inc_counter().call();
+    if (!ok(canister1_a_result))
         return {
-            err: canister1_a.err
+            err: canister1_a_result.err
         };
 
-    let canister1_b: CanisterResult<nat> = await canister1.inc_counter().call();
-    if ('err' in canister1_b)
+    const canister1_b_result = await canister1.inc_counter().call();
+    if (!ok(canister1_b_result))
         return {
-            err: canister1_b.err
+            err: canister1_b_result.err
         };
 
-    if (canister1_a.ok === undefined || canister1_b.ok === undefined) {
+    if (
+        canister1_a_result.ok === undefined ||
+        canister1_b_result.ok === undefined
+    ) {
         return {
             err: 'One of the increments was unsuccessful'
         };
     }
 
     return {
-        ok: counter + canister1_a.ok + canister1_b.ok
+        ok: counter + canister1_a_result.ok + canister1_b_result.ok
     };
 }
 
 // Composite query calling queries that modify the state
-export async function inc_canister2(): Promise<CompositeQuery<NatQueryResult>> {
+export async function inc_canister2(): Promise<Query<NatQueryResult>> {
     counter += 1n;
 
-    let canister2_a: CanisterResult<nat> = await canister2.inc_counter().call();
-    if ('err' in canister2_a)
+    const canister2_a_result = await canister2.inc_counter().call();
+    if (!ok(canister2_a_result))
         return {
-            err: canister2_a.err
+            err: canister2_a_result.err
         };
 
-    let canister2_b: CanisterResult<nat> = await canister2.inc_counter().call();
-    if ('err' in canister2_b)
+    const canister2_b_result = await canister2.inc_counter().call();
+    if (!ok(canister2_b_result))
         return {
-            err: canister2_b.err
+            err: canister2_b_result.err
         };
 
-    if (canister2_a.ok === undefined || canister2_b.ok === undefined) {
+    if (
+        canister2_a_result.ok === undefined ||
+        canister2_b_result.ok === undefined
+    ) {
         return {
             err: 'One of the increments was unsuccessful'
         };
     }
 
     return {
-        ok: counter + canister2_a.ok + canister2_b.ok
+        ok: counter + canister2_a_result.ok + canister2_b_result.ok
     };
 }
--- examples/composite_queries/canisters/canister2/canister2.ts
+++ examples/composite_queries/canisters/canister2/canister2.ts
@@ -1,23 +1,14 @@
-import {
-    CanisterResult,
-    CompositeQuery,
-    ic,
-    nat,
-    Principal,
-    Query,
-    QueryManual,
-    Update
-} from 'azle';
+import { ic, Manual, nat, Principal, Query, Update } from 'azle';
 import { StringQueryResult } from '../canister1/types';
 import { Canister3 } from '../canister3/types';
 
-let canister3 = ic.canisters.Canister3<Canister3>(
+const canister3 = ic.canisters.Canister3<Canister3>(
     Principal.fromText('r7inp-6aaaa-aaaaa-aaabq-cai')
 );
 
 let counter: nat = 0n;
 
-export function inc_counter(): CompositeQuery<nat> {
+export async function inc_counter(): Promise<Query<nat>> {
     counter += 1n;
     return counter;
 }
@@ -30,10 +21,10 @@ export function update_query(): Update<string> {
     return 'Hello from a Canister 2 update';
 }
 
-export function manual_query(): QueryManual<string> {
+export function manual_query(): Query<Manual<string>> {
     ic.reply('Hello from Canister 2 manual_query');
 }
 
-export async function deep_query(): Promise<CompositeQuery<StringQueryResult>> {
+export async function deep_query(): Promise<Query<StringQueryResult>> {
     return await canister3.deep_query().call();
 }
