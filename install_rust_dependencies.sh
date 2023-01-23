6e851caca4febb1fee6795b9ada08f919cdb5660
Jordan Last <jordan.michael.last@gmail.com>
2023-01-23 13:12:51 -0600

    final Rust installation process is very robust

--- install_rust_dependencies.sh
+++ install_rust_dependencies.sh
@@ -2,6 +2,7 @@
 
 # TODO If we want to save a little bit of time we might be able to instruct rustup to not install some components initially, like clippy and docs
 # TODO we might want to implement all of this in Node.js in the future for platform-independence etc
+# TODO once ic-cdk-optimizer and ic-wasm are no longer required, we can probably just check for the wasm32-unknown-unknown target being installed: $global_azle_rustup_bin target list | grep -q "wasm32-unknown-unknown (installed)"
 
 azle_version="$1"
 rust_version="$2"
@@ -16,55 +17,29 @@ export CARGO_HOME="$global_azle_config_dir"
 export RUSTUP_HOME="$global_azle_config_dir"
 
 function run() {
-    is_rustup_already_installed
-    rustup_already_installed=$?
-    is_wasm32_unknown_unknown_already_installed $rustup_already_installed
-    wasm32_unknown_unknown_already_installed=$?
     ic_wasm_already_installed=$(test -e "$global_azle_bin_dir"/ic-wasm; echo $?)
     ic_cdk_optimizer_already_installed=$(test -e "$global_azle_bin_dir"/ic-cdk-optimizer; echo $?)
 
-    if [ "$rustup_already_installed" -eq 1 ] || [ "$wasm32_unknown_unknown_already_installed" -eq 1 ] || [ "$ic_wasm_already_installed" -eq 1 ] || [ "$ic_cdk_optimizer_already_installed" -eq 1 ]; then
+    if [ "$ic_wasm_already_installed" -eq 1 ] || [ "$ic_cdk_optimizer_already_installed" -eq 1 ]; then
         echo -e "\nAzle "$azle_version" prerequisite installation (this may take a few minutes)\n"
 
         mkdir -p "$global_azle_config_dir"
         mkdir -p "$global_azle_logs_dir"
 
-        install_rustup "$rustup_already_installed"
-
-        # "$global_azle_rustup_bin" target list
-
-        install_wasm32_unknown_unknown "$wasm32_unknown_unknown_already_installed"
+        install_rustup
+        install_wasm32_unknown_unknown
         install_ic_wasm "$ic_wasm_already_installed"
         install_ic_cdk_optimizer "$ic_cdk_optimizer_already_installed"
+        echo -e "\n"
     else
         update_rustup
-    fi
-}
-
-function is_rustup_already_installed() {
-    temp=$(command -v "$global_azle_rustup_bin")
-    if [ -n "$temp" ]; then
-        "$global_azle_rustup_bin" target list | grep -q "(installed)"
-    else
-        return 1
-    fi
-}
-
-function is_wasm32_unknown_unknown_already_installed() {
-    if [ "$1" -eq 0 ]; then
-        "$global_azle_rustup_bin" target list | grep -q "wasm32-unknown-unknown (installed)"
-        return $?
-    else
-        return 1
     fi
 }
 
 function install_rustup() {
     echo -e "1/4) Installing Rust"
 
-    if [ -z "$1" ]; then
-        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y --default-toolchain="$rust_version" &> "$global_azle_logs_dir"/step_1_rust
-    fi
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y --default-toolchain="$rust_version" &> "$global_azle_logs_dir"/step_1_rust
 }
 
 function update_rustup() {
@@ -74,9 +49,7 @@ function update_rustup() {
 function install_wasm32_unknown_unknown() {
     echo -e "2/4) Installing wasm32-unknown-unknown"
 
-    if [ "$1" -eq 1 ]; then
-        "$global_azle_rustup_bin" target add wasm32-unknown-unknown &> "$global_azle_logs_dir"/step_2_wasm32_unknown_unknown
-    fi
+    "$global_azle_rustup_bin" target add wasm32-unknown-unknown &> "$global_azle_logs_dir"/step_2_wasm32_unknown_unknown
 }
 
 function install_ic_wasm() {
@@ -88,7 +61,7 @@ function install_ic_wasm() {
 }
 
 function install_ic_cdk_optimizer() {
-    echo -e "4/4) Installing ic-cdk-optimizer\n"
+    echo -e "4/4) Installing ic-cdk-optimizer"
 
     if [ "$1" -eq 1 ]; then
         "$global_azle_cargo_bin" install ic-cdk-optimizer --version 0.3.4  &> "$global_azle_logs_dir"/step_4_ic_cdk_optimizer
