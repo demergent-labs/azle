89e1b57525ad53d452477e1c8d55365aa0a0ec03
Jordan Last <jordan.michael.last@gmail.com>
2023-01-27 15:17:42 -0600

    add classes to query

--- examples/query/canisters/azle/index.ts
+++ examples/query/canisters/azle/index.ts
@@ -3,3 +3,14 @@ import { Query } from 'azle';
 export function simple_query(): Query<string> {
     return 'This is a query function';
 }
+
+// class API
+
+import { query } from 'azle';
+
+export default class {
+    @query
+    simple_query(): string {
+        return 'This is a query function';
+    }
+}
