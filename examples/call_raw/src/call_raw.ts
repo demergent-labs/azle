b589ae2a45be6acffb4a6349f0c746601ad51842
Jordan Last <jordan.michael.last@gmail.com>
2023-01-28 16:02:41 -0600

    refactor call_raw to classes

--- examples/call_raw/src/call_raw.ts
+++ examples/call_raw/src/call_raw.ts
@@ -1,14 +1,4 @@
-import {
-    blob,
-    CanisterResult,
-    ic,
-    nat,
-    nat64,
-    ok,
-    Principal,
-    Update,
-    Variant
-} from 'azle';
+import { ic, nat, nat64, ok, Principal, Update, Variant } from 'azle';
 
 type ExecuteCallRawResult = Variant<{
     ok: string;
@@ -66,4 +56,60 @@ export async function execute_call_raw128(
     return {
         ok: ic.candid_decode(canister_result.ok)
     };
+}
+
+// class API
+
+import { update } from 'azle';
+
+export default class {
+    @update
+    async execute_call_raw(
+        canister_id: Principal,
+        method: string,
+        candid_args: string,
+        payment: nat64
+    ): Promise<ExecuteCallRawResult> {
+        const canister_result = await ic.call_raw(
+            canister_id,
+            method,
+            ic.candid_encode(candid_args),
+            payment
+        );
+
+        if (!ok(canister_result)) {
+            return {
+                err: canister_result.err
+            };
+        }
+
+        return {
+            ok: ic.candid_decode(canister_result.ok)
+        };
+    }
+
+    @update
+    async execute_call_raw128(
+        canister_id: Principal,
+        method: string,
+        candid_args: string,
+        payment: nat
+    ): Promise<ExecuteCallRaw128Result> {
+        const canister_result = await ic.call_raw128(
+            canister_id,
+            method,
+            ic.candid_encode(candid_args),
+            payment
+        );
+
+        if (!ok(canister_result)) {
+            return {
+                err: canister_result.err
+            };
+        }
+
+        return {
+            ok: ic.candid_decode(canister_result.ok)
+        };
+    }
 }
