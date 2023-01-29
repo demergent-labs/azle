d9066bc4e61328db17323f602e47b3c80c0f3cf2
Jordan Last <jordan.michael.last@gmail.com>
2023-01-28 20:20:39 -0600

    remove dfx_generated from func_types

--- examples/func_types/test/dfx_generated/notifiers/notifiers.did.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Principal } from '@dfinity/principal';
-export type NotifierFunc = (arg_0: Array<number>) => Promise<undefined>;
-export interface _SERVICE {
-    get_notifier: () => Promise<[Principal, string]>;
-}
