0dbcde4943f83312723e12c86a43e66a16f474da
Jordan Last <jordan.michael.last@gmail.com>
2023-01-28 18:42:14 -0600

    removing dfx _generateD

--- examples/rejections/test/dfx_generated/some_service/some_service.did.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Principal } from '@dfinity/principal';
-import type { ActorMethod } from '@dfinity/agent';
-
-export interface _SERVICE {
-    accept: ActorMethod<[], boolean>;
-    error: ActorMethod<[], never>;
-    reject: ActorMethod<[string], never>;
-}
