23eca198f9d2e0c1d6ee1163227f9375c748e784
Jordan Last <jordan.michael.last@gmail.com>
2023-01-30 21:06:07 -0600

    remove dfx_generated from call_raw, use pretest

---
+++ examples/call_raw/test/pretest.ts
@@ -0,0 +1,19 @@
+import { execSync } from 'child_process';
+
+async function pretest() {
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
+    execSync(`dfx canister uninstall-code call_raw || true`, {
+        stdio: 'inherit'
+    });
+
+    execSync(`dfx deploy call_raw`, {
+        stdio: 'inherit'
+    });
+
+    execSync(`dfx generate call_raw`, {
+        stdio: 'inherit'
+    });
+}
+
+pretest();
