8d291e0fe606e2fc7315431bdffa14d71be2b964
Jordan Last <jordan.michael.last@gmail.com>
2023-01-30 22:46:48 -0600

    remove dfx_generated for cycles

---
+++ examples/cycles/test/pretest.ts
@@ -0,0 +1,23 @@
+import { execSync } from 'child_process';
+
+async function pretest() {
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
+    execSync(`dfx canister uninstall-code cycles || true`, {
+        stdio: 'inherit'
+    });
+
+    execSync(`dfx canister uninstall-code intermediary || true`, {
+        stdio: 'inherit'
+    });
+
+    execSync(`dfx deploy`, {
+        stdio: 'inherit'
+    });
+
+    execSync(`dfx generate`, {
+        stdio: 'inherit'
+    });
+}
+
+pretest();
