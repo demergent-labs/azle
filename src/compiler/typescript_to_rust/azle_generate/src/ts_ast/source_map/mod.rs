56dea40b941706c92d89bfc51d5d99c656f6757d
Dan Steren <dan@demergentlabs.org>
2023-02-16 10:04:58 -0700

    Reorganize source_map directory

--- src/compiler/typescript_to_rust/azle_generate/src/ts_ast/source_map/mod.rs
+++ src/compiler/typescript_to_rust/azle_generate/src/ts_ast/source_map/mod.rs
@@ -1,4 +1,5 @@
-pub mod source_map;
+pub use get_source_file_info::GetSourceFileInfo;
+pub use get_source_file_info::Range;
 
-pub use source_map::GetSourceFileInfo;
-pub use source_map::Range;
+pub mod get_source_file_info;
+pub mod private_get_source_file_info;
