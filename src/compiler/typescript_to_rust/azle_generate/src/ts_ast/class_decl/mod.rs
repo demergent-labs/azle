32887b43b65237c809fcd753aa4ee9c959904c18
Dan Steren <dan@demergentlabs.org>
2023-02-21 14:58:02 -0700

    Panic on invalid class members

--- src/compiler/typescript_to_rust/azle_generate/src/ts_ast/class_decl/mod.rs
+++ src/compiler/typescript_to_rust/azle_generate/src/ts_ast/class_decl/mod.rs
@@ -1,2 +1,3 @@
+mod errors;
 mod get_dependencies;
 mod to_act_external_canister;
--- src/compiler/typescript_to_rust/azle_generate/src/ts_ast/class_decl/to_act_external_canister.rs
+++ src/compiler/typescript_to_rust/azle_generate/src/ts_ast/class_decl/to_act_external_canister.rs
@@ -14,25 +14,23 @@ impl SourceMapped<'_, ClassDecl> {
     }
 
     fn build_external_canister_methods(&self) -> Vec<ActExternalCanisterMethod> {
-        if self.class.body.len() == 0 {
-            panic!("external canister declarations must contain a constructor and at least one property")
-        }
-
         self.class
             .body
             .iter()
-            .fold(vec![], |mut acc, class_member| {
-                if let ClassMember::ClassProp(class_prop) = class_member {
+            .fold(vec![], |mut acc, class_member| match class_member {
+                ClassMember::ClassProp(class_prop) => {
                     let class_prop_with_source_map = SourceMapped::new(class_prop, self.source_map);
                     let possible_canister_method =
                         class_prop_with_source_map.to_act_external_canister_method();
                     if let Some(canister_method) = possible_canister_method {
                         acc.push(canister_method);
                     }
+                    acc
                 }
-                // TODO: Handle other types of class members. Decide if we should
-                // Error out, or give a warning, or what.
-                acc
+                _ => panic!(
+                    "{}",
+                    self.build_invalid_class_member_error_message(class_member)
+                ),
             })
     }
 }
