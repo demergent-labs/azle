d0186cd9a6d6e84ccd23554bc21b9819403cc7de
Dan Steren <dan@demergentlabs.org>
2023-03-06 13:23:14 -0700

    Fix async await result handler

--- src/compiler/typescript_to_rust/azle_generate/src/generators/async_await_result_handler.rs
+++ src/compiler/typescript_to_rust/azle_generate/src/generators/async_await_result_handler.rs
@@ -1,6 +1,4 @@
-use cdk_framework::act::node::{
-    canister_method::QueryOrUpdateMethod, data_type::type_annotation::ToTypeAnnotation,
-};
+use cdk_framework::act::node::{canister_method::QueryOrUpdateMethod, traits::HasReturnValue};
 use proc_macro2::TokenStream;
 use quote::quote;
 
@@ -85,11 +83,7 @@ fn generate_match_arms(methods: &Vec<QueryOrUpdateMethod>) -> Vec<TokenStream> {
 
 fn generate_match_arm(method: &QueryOrUpdateMethod) -> TokenStream {
     let name = &method.name;
-    // TODO We need to pass the parent name in here
-    let return_type = &method.return_type.to_type_annotation(
-        &ts_keywords::ts_keywords(),
-        "TodoNeedParentTypeInfoHereInAsyncAwaitResultHandler".to_string(),
-    );
+    let return_type = method.create_return_type_annotation(&ts_keywords::ts_keywords(), name);
     quote!(
         #name => {
             let reply_value: #return_type = js_value.clone().try_from_vm_value(&mut *_azle_boa_context).unwrap();
