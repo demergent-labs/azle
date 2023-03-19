d18e07839caef55d3fcc75f7b3b384be46809dc4
Dan Steren <dan@demergentlabs.org>
2023-03-19 14:39:36 -0600

    WIP: Start building guard_functions

--- src/compiler/typescript_to_rust/azle_generate/src/canister_method/guard_function/mod.rs
+++ src/compiler/typescript_to_rust/azle_generate/src/canister_method/guard_function/mod.rs
@@ -1,34 +1,43 @@
-use cdk_framework::act::{node::GuardFunction, CanisterMethods};
+use cdk_framework::act::node::GuardFunction;
+use quote::quote;
+use swc_ecma_ast::FnDecl;
 
-use crate::TsAst;
+use crate::{
+    ts_ast::{azle_program::HelperMethods, source_map::SourceMapped, GetName},
+    TsAst,
+};
 
 impl TsAst {
-    pub fn build_guard_functions(&self, canister_methods: &CanisterMethods) -> Vec<GuardFunction> {
-        // TODO: See https://github.com/demergent-labs/azle/issues/859
+    pub fn build_guard_functions(&self) -> Vec<GuardFunction> {
+        self.azle_programs
+            .get_fn_decls()
+            .iter()
+            .filter(|fn_decl| fn_decl.has_guard_result_return_type())
+            .map(|fn_decl| {
+                let name = fn_decl.ident.get_name().to_string();
 
-        // let guard_function_names = get_guard_function_names(canister_methods);
-        // TODO: Ideally we'd know which file the canister method was declared in and only look in
-        // that file for a method with the same name. If we have a.ts and b.ts and a.ts has a
-        // canister method that references method_a, and we find it in both a.ts and b.ts we should
-        // know that we want the one from a.ts not b.ts.
+                // TODO: Construct a function body for these guard functions
 
-        // TODO: This should likely reduce duplication in the list of names. If in file a.ts we have
-        // two canister methods that both use the guard function "some_guard", then our list should
-        // only contain "some_guard" once. If a.ts and b.ts both have that same function though then
-        // we should probably not de-duplicate and we should make a function for each
+                GuardFunction {
+                    name,
+                    body: quote! {Ok(())},
+                }
+            })
+            .collect()
+    }
+}
 
-        // I think we don't have enough information here.
-
-        // Go through all module_items in all programs/modules and look for methods with the same name
-
-        // We can either take each name in the list and go through all the module items looking for
-        // a method with a corresponding name. In a list of size n that requires n passes through
-        // all the module items.
-
-        // Alternatively we can go through the module items once and if the name matches...
-
-        // TODO: MORE DESIGN NEEDED BEFORE BEING ABLE TO PROCEED!!!
-
-        vec![]
+impl SourceMapped<'_, FnDecl> {
+    pub fn has_guard_result_return_type(&self) -> bool {
+        match &self.function.return_type {
+            Some(ts_type_ann) => match &*ts_type_ann.type_ann {
+                swc_ecma_ast::TsType::TsTypeRef(ts_type_ref) => match &ts_type_ref.type_name {
+                    swc_ecma_ast::TsEntityName::Ident(ident) => ident.get_name() != "GuardResult",
+                    swc_ecma_ast::TsEntityName::TsQualifiedName(_) => false,
+                },
+                _ => false,
+            },
+            None => false,
+        }
     }
 }
