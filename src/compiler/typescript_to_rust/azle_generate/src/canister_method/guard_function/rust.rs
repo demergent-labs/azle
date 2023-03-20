9339def976f099aad735188e425c36056bf750e6
Dan Steren <dan@demergentlabs.org>
2023-03-20 11:16:48 -0600

    Not require guard functions to be exported

--- src/compiler/typescript_to_rust/azle_generate/src/canister_method/guard_function/rust.rs
+++ src/compiler/typescript_to_rust/azle_generate/src/canister_method/guard_function/rust.rs
@@ -2,24 +2,13 @@ use proc_macro2::TokenStream;
 use quote::quote;
 
 pub fn generate(function_name: &String) -> TokenStream {
+    let function_call = format!("{function_name}()");
     quote! {
         BOA_CONTEXT_REF_CELL.with(|box_context_ref_cell| {
             let mut boa_context = box_context_ref_cell.borrow_mut();
 
-            let exports_js_value =
-                _azle_unwrap_boa_result(boa_context.eval("exports"), &mut boa_context);
-            let exports_js_object = exports_js_value.as_object().unwrap();
-
-            let guard_fn_js_value = exports_js_object.get(#function_name, &mut boa_context).unwrap();
-            let guard_fn_js_object = match guard_fn_js_value.as_object() {
-                Some(js_value) => js_value,
-                None => ic_cdk::api::trap(&format!("ReferenceError: {} is not on the global exports object", #function_name.to_string()))
-            };
-
-            let js_guard_fn_return_value = _azle_unwrap_boa_result(
-                guard_fn_js_object.call(&boa_engine::JsValue::Null, &[], &mut boa_context),
-                &mut boa_context
-            );
+            let js_guard_fn_return_value =
+                _azle_unwrap_boa_result(boa_context.eval(#function_call), &mut boa_context);
 
             match js_guard_fn_return_value.try_from_vm_value(&mut *boa_context) {
                 Ok(return_value) => return_value,
