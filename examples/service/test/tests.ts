f55b60b719c720811e3c96422a9e6301eed073e2
Jordan Last <jordan.michael.last@gmail.com>
2023-03-22 15:56:31 -0500

    updates

--- examples/service/test/tests.ts
+++ examples/service/test/tests.ts
@@ -1,20 +1,81 @@
 import { Test } from 'azle/test';
 import { _SERVICE } from './dfx_generated/service/service.did';
 import { ActorSubclass } from '@dfinity/agent';
-import { Principal } from '@dfinity/principal';
+import { execSync } from 'child_process';
 
+// TODO these tests should be rewritten to use @dfinity/agent once this issue is resolved: https://github.com/dfinity/agent-js/issues/702
 export function getTests(serviceCanister: ActorSubclass<_SERVICE>): Test[] {
     return [
-        // {
-        //     name: 'test',
-        //     test: async () => {
-        //         const result = await serviceCanister.test(
-        //             Principal.fromText('aaaaa-aa')
-        //         );
-        //         return {
-        //             Ok: result.toString() === 'aaaaa-aa'
-        //         };
-        //     }
-        // }
+        {
+            name: 'serviceParam',
+            test: async () => {
+                const result = execSync(
+                    `dfx canister call service serviceParam '(service "aaaaa-aa")'`
+                )
+                    .toString()
+                    .trim();
+
+                console.log(result);
+
+                return {
+                    Ok: result === '(service "aaaaa-aa")'
+                };
+            }
+        },
+        {
+            name: 'serviceReturnType',
+            test: async () => {
+                const result = execSync(
+                    `dfx canister call service serviceReturnType`
+                )
+                    .toString()
+                    .trim();
+
+                console.log(result);
+
+                return {
+                    Ok: result === '(service "ryjl3-tyaaa-aaaaa-aaaba-cai")'
+                };
+            }
+        },
+        {
+            name: 'serviceList',
+            test: async () => {
+                const result = execSync(
+                    `dfx canister call service serviceList '(vec { service "r7inp-6aaaa-aaaaa-aaabq-cai"; service "rrkah-fqaaa-aaaaa-aaaaq-cai" })'`
+                )
+                    .toString()
+                    .trim();
+
+                console.log(result);
+
+                return {
+                    Ok:
+                        result ===
+                        `(
+  vec {
+    service "r7inp-6aaaa-aaaaa-aaabq-cai";
+    service "rrkah-fqaaa-aaaaa-aaaaq-cai";
+  },
+)`
+                };
+            }
+        },
+        {
+            name: 'serviceCrossCanisterCall',
+            test: async () => {
+                const result = execSync(
+                    `dfx canister call service serviceCrossCanisterCall '(service "ryjl3-tyaaa-aaaaa-aaaba-cai")'`
+                )
+                    .toString()
+                    .trim();
+
+                console.log(result);
+
+                return {
+                    Ok: result === '(variant { Ok = "SomeService update1" })'
+                };
+            }
+        }
     ];
 }
--- src/compiler/typescript_to_rust/azle_generate/src/ts_ast/class_decl/to_act_external_canister.rs
+++ src/compiler/typescript_to_rust/azle_generate/src/ts_ast/class_decl/to_act_external_canister.rs
@@ -1,4 +1,8 @@
-use cdk_framework::act::node::{external_canister::Method, ExternalCanister};
+use cdk_framework::{
+    act::node::{external_canister::Method, ExternalCanister},
+    traits::ToIdent,
+};
+use quote::quote;
 use swc_ecma_ast::{ClassDecl, ClassMember};
 
 use crate::ts_ast::{source_map::SourceMapped, GetName};
@@ -8,7 +12,73 @@ impl SourceMapped<'_, ClassDecl> {
         let name = self.ident.get_name().to_string();
         let methods = self.build_external_canister_methods();
 
-        ExternalCanister { name, methods }
+        ExternalCanister {
+            name,
+            methods,
+            to_vm_value: |name| {
+                let service_name = name.to_ident();
+
+                quote! {
+                    impl CdkActTryIntoVmValue<&mut boa_engine::Context, boa_engine::JsValue> for #service_name {
+                        fn try_into_vm_value(self, context: &mut boa_engine::Context) -> Result<boa_engine::JsValue, CdkActTryIntoVmValueError> {
+                            Ok(context.eval(
+                                format!(
+                                    "new {}(Principal.fromText(\"{}\"))",
+                                    stringify!(#service_name),
+                                    self.0.principal.to_string()
+                                )
+                            ).unwrap())
+                        }
+                    }
+                }
+            },
+            list_to_vm_value: |name| {
+                let service_name = name.to_ident();
+
+                quote! {
+                    impl CdkActTryIntoVmValue<&mut boa_engine::Context, boa_engine::JsValue> for Vec<#service_name> {
+                        fn try_into_vm_value(self, context: &mut boa_engine::Context) -> Result<boa_engine::JsValue, CdkActTryIntoVmValueError> {
+                            try_into_vm_value_generic_array(self, context)
+                        }
+                    }
+                }
+            },
+            from_vm_value: |name| {
+                let service_name = name.to_ident();
+
+                quote! {
+                    impl CdkActTryFromVmValue<#service_name, &mut boa_engine::Context> for boa_engine::JsValue {
+                        fn try_from_vm_value(self, context: &mut boa_engine::Context) -> Result<#service_name, CdkActTryFromVmValueError> {
+                            let js_object = self.as_object().unwrap();
+                            let canister_id_js_value = js_object.get("canisterId", context).unwrap();
+                            let canister_id_js_object = canister_id_js_value.as_object().unwrap();
+                            let canister_id_to_string_js_value = canister_id_js_object.get("toText", context).unwrap();
+                            let canister_id_to_string_js_object = canister_id_to_string_js_value.as_object().unwrap();
+                            let canister_id_string_js_value = canister_id_to_string_js_object.call(
+                                &canister_id_js_value,
+                                &[],
+                                context
+                            ).unwrap();
+                            let canister_id_js_string = canister_id_string_js_value.to_string(context).unwrap();
+                            let canister_id_string = canister_id_js_string.to_std_string_escaped();
+
+                            Ok(#service_name::new(ic_cdk::export::Principal::from_str(&canister_id_string).unwrap()))
+                        }
+                    }
+                }
+            },
+            list_from_vm_value: |name| {
+                let service_name = name.to_ident();
+
+                quote! {
+                    impl CdkActTryFromVmValue<Vec<#service_name>, &mut boa_engine::Context> for boa_engine::JsValue {
+                        fn try_from_vm_value(self, context: &mut boa_engine::Context) -> Result<Vec<#service_name>, CdkActTryFromVmValueError> {
+                            try_from_vm_value_generic_array(self, context)
+                        }
+                    }
+                }
+            },
+        }
     }
 
     fn build_external_canister_methods(&self) -> Vec<Method> {
