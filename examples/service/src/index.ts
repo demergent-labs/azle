69196cf546d9699c07a04b25a36c1c94ba6ffb80
Jordan Last <jordan.michael.last@gmail.com>
2023-03-22 12:07:24 -0500

    add the service example

--- examples/service/src/index.ts
+++ examples/service/src/index.ts
@@ -1,11 +1,42 @@
-import { CanisterResult, $query, query, ExternalCanister } from 'azle';
+import {
+    CanisterResult,
+    $query,
+    $update,
+    query,
+    update,
+    ExternalCanister,
+    Principal,
+    Variant
+} from 'azle';
 
-class MyService extends ExternalCanister {
+class ExternalService extends ExternalCanister {
     @query
-    method1: () => CanisterResult<string>;
+    query1: () => CanisterResult<boolean>;
+
+    @update
+    update1: () => CanisterResult<string>;
 }
 
 $query;
-export function test(myService: MyService): MyService {
-    return myService;
+export function serviceParam(
+    externalService: ExternalService
+): ExternalService {
+    return externalService;
+}
+
+$query;
+export function serviceReturnType(): ExternalService {
+    return new ExternalService(Principal.fromText('aaaaa-aa'));
+}
+
+$update;
+export async function serviceCrossCanisterCall(
+    externalService: ExternalService
+): Promise<
+    Variant<{
+        Ok: string;
+        Err: string;
+    }>
+> {
+    return await externalService.update1().call();
 }
