6b4d2a0fb07cdee673196af94ca24546928c52a8
Dan Steren <dan@demergentlabs.org>
2023-03-28 14:51:26 -0600

    Move variant - part 2

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import { Principal } from '@dfinity/principal';
-import { RequireExactlyOne, Variant } from '../variant';
+import { RequireExactlyOne, Variant } from './variant';
 
 export { StableBTreeMap, InsertError } from './stable_b_tree_map';
-export { Variant } from '../variant';
-export { match } from '../variant';
+export { match, Variant } from './variant';
 
 declare var globalThis: any;
 
