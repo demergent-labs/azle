5229862c4662b69779608889707e046e9edacb1f
Dan Steren <dan@demergentlabs.org>
2023-03-30 12:32:23 -0600

    Change order for alphabetization

--- src/compiler/typescript_to_rust/azle_generate/src/lib.rs
+++ src/compiler/typescript_to_rust/azle_generate/src/lib.rs
@@ -24,9 +24,9 @@ pub fn generate_canister(ts_file_names: &Vec<&str>, main_js: String) -> TokenStr
 
 impl TsAst {
     pub fn to_act(&self) -> AbstractCanisterTree {
+        let candid_types = self.build_candid_types();
         let canister_methods = self.build_canister_methods();
         let stable_b_tree_map_nodes = self.build_stable_b_tree_map_nodes();
-        let candid_types = self.build_candid_types();
         let body = body::generate(
             self,
             &canister_methods.query_methods,
@@ -35,8 +35,8 @@ impl TsAst {
             &stable_b_tree_map_nodes,
         );
         let cdk_name = "azle".to_string();
-        let header = header::generate(&self.main_js);
         let guard_functions = self.build_guard_functions();
+        let header = header::generate(&self.main_js);
         let keywords = ts_keywords::ts_keywords();
         let vm_value_conversion = self.build_vm_value_conversion();
 
