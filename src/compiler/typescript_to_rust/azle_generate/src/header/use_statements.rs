67108c1c1e022c08c067f77c34bfe97166c44d01
Dan Steren <dan@demergentlabs.org>
2023-04-10 15:50:16 -0600

    Remove use statements in header

--- src/compiler/typescript_to_rust/azle_generate/src/header/use_statements.rs
+++ src/compiler/typescript_to_rust/azle_generate/src/header/use_statements.rs
@@ -1,15 +1,9 @@
 pub fn generate() -> proc_macro2::TokenStream {
     quote::quote! {
         use azle_vm_value_derive::{CdkActTryIntoVmValue, CdkActTryFromVmValue};
-        use ic_cdk::api::call::CallResult;
-        use std::borrow::BorrowMut;
-        use rand::{
-            Rng,
-            SeedableRng,
-            rngs::StdRng
-        };
-        use slotmap::Key as AzleSlotMapKey; // Renamed to avoid clashes with user-defined types
-        use std::str::FromStr;
-        use std::convert::TryInto;
+        use rand::Rng as _AzleTraitRng;
+        use slotmap::Key as _AzleTraitSlotMapKey;
+        use std::convert::TryInto as _AzleTraitTryInto;
+        use std::str::FromStr as _AzleTraitFromStr;
     }
 }
