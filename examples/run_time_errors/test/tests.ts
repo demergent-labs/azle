009ec9b75d513f2bd553b94dd93f250e62e91c85
Dan Steren <dan@demergentlabs.org>
2023-04-21 10:58:22 -0600

    Refactor tests

--- examples/run_time_errors/test/tests.ts
+++ examples/run_time_errors/test/tests.ts
@@ -4,121 +4,50 @@ import { _SERVICE } from './dfx_generated/run_time_errors/run_time_errors.did';
 
 export function getTests(errorCanister: ActorSubclass<_SERVICE>): Test[] {
     return [
-        {
-            name: 'throw big int',
-            test: async () => {
-                return {
-                    Ok: await testThrow(errorCanister.throwBigint, '3')
-                };
-            }
-        },
-        {
-            name: 'throw boolean',
-            test: async () => {
-                return {
-                    Ok: await testThrow(errorCanister.throwBoolean, 'false')
-                };
-            }
-        },
-        {
-            name: 'throw class',
-            test: async () => {
-                return {
-                    Ok: await testThrow(
-                        errorCanister.throwClass,
-                        'CustomClass toString'
-                    )
-                };
-            }
-        },
-        {
-            name: 'throw custom error',
-            test: async () => {
-                return {
-                    Ok: await testThrow(
-                        errorCanister.throwCustomError,
-                        'Error: This is a custom error'
-                    )
-                };
-            }
-        },
-        {
-            name: 'throw int',
-            test: async () => {
-                return {
-                    Ok: await testThrow(errorCanister.throwInt, '3')
-                };
-            }
-        },
-        {
-            name: 'throw null',
-            test: async () => {
-                return {
-                    Ok: await testThrow(errorCanister.throwNull, 'null')
-                };
-            }
-        },
-        {
-            name: 'throw null reference',
-            test: async () => {
-                return {
-                    Ok: await testThrow(
-                        errorCanister.throwNullReference,
-                        "TypeError: cannot convert 'null' or 'undefined' to object"
-                    )
-                };
-            }
-        },
-        {
-            name: 'throw object',
-            test: async () => {
-                return {
-                    Ok: await testThrow(
-                        errorCanister.throwObject,
-                        '[object Object]'
-                    )
-                };
-            }
-        },
-        {
-            name: 'throw rational',
-            test: async () => {
-                return {
-                    Ok: await testThrow(errorCanister.throwRational, '3.14')
-                };
-            }
-        },
-        {
-            name: 'throw string',
-            test: async () => {
-                return {
-                    Ok: await testThrow(
-                        errorCanister.throwString,
-                        'Hello World'
-                    )
-                };
-            }
-        },
-        {
-            name: 'throw symbol',
-            test: async () => {
-                return {
-                    Ok: await testThrow(errorCanister.throwSymbol, 'Symbol()')
-                };
-            }
-        },
-        {
-            name: 'throw undefined',
-            test: async () => {
-                return {
-                    Ok: await testThrow(
-                        errorCanister.throwUndefined,
-                        'undefined'
-                    )
-                };
-            }
-        }
+        expectToThrow('big int', errorCanister.throwBigint, '3'),
+        expectToThrow('boolean', errorCanister.throwBoolean, 'false'),
+        expectToThrow(
+            'class',
+            errorCanister.throwClass,
+            'CustomClass toString'
+        ),
+        expectToThrow(
+            'custom error',
+            errorCanister.throwCustomError,
+            'Error: This is a custom error'
+        ),
+        expectToThrow('int', errorCanister.throwInt, '3'),
+        expectToThrow('null', errorCanister.throwNull, 'null'),
+        expectToThrow(
+            'null reference',
+            errorCanister.throwNullReference,
+            "TypeError: cannot convert 'null' or 'undefined' to object"
+        ),
+        expectToThrow('object', errorCanister.throwObject, '[object Object]'),
+        expectToThrow('rational', errorCanister.throwRational, '3.14'),
+        expectToThrow('string', errorCanister.throwString, 'Hello World'),
+        expectToThrow('symbol', errorCanister.throwSymbol, 'Symbol()'),
+        expectToThrow('undefined', errorCanister.throwUndefined, 'undefined')
     ];
+}
+
+/** Creates a test that asserts the provided method throws the provided value */
+function expectToThrow(
+    /** The name of the test */
+    name: string,
+    /** The method to call */
+    canisterMethod: () => Promise<void>,
+    /** The value we expect the method to throw */
+    expectedValue: any
+): Test {
+    return {
+        name: `throw ${name}`,
+        test: async () => {
+            return {
+                Ok: await testThrow(canisterMethod, expectedValue)
+            };
+        }
+    };
 }
 
 async function testThrow(
