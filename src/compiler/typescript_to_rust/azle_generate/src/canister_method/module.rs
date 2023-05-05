59d79eff6e3c9cafbfde1c0c17f8174d66d3668e
Dan Steren <dan@demergentlabs.org>
2023-05-05 16:25:47 -0600

    Made algorithm forward-looking

--- src/compiler/typescript_to_rust/azle_generate/src/canister_method/module.rs
+++ src/compiler/typescript_to_rust/azle_generate/src/canister_method/module.rs
@@ -19,82 +19,101 @@ pub trait ModuleHelperMethods {
     fn get_fn_decls<'a>(&'a self, source_map: &'a SourceMap) -> Vec<SourceMapped<'a, FnDecl>>;
 }
 
+struct Accumulator<'a> {
+    pub annotated_fn_decls: Vec<AnnotatedFnDecl<'a>>,
+    pub errors: Vec<Error>,
+}
+
+impl Accumulator<'_> {
+    pub fn new() -> Self {
+        Self {
+            annotated_fn_decls: Default::default(),
+            errors: Default::default(),
+        }
+    }
+}
+
+impl<'a> From<Accumulator<'a>> for (Vec<AnnotatedFnDecl<'a>>, Vec<Error>) {
+    fn from(acc: Accumulator<'a>) -> Self {
+        (acc.annotated_fn_decls, acc.errors)
+    }
+}
+
 impl ModuleHelperMethods for Module {
     // TODO: This should be implemented on SourceMapped<Module>.
     // Then you wouldn't need to pass the source_map in as a param.
+    // ideally body then would be a Vec<SourceMapped<ModuleItem>>
     fn get_annotated_fn_decls<'a>(
         &'a self,
         source_map: &'a SourceMap,
     ) -> (Vec<AnnotatedFnDecl>, Vec<Error>) {
-        // TODO: Consider changing this algorithm from being backward looking
-        // to being forward looking
+        let source_mapped_body: Vec<_> = self
+            .body
+            .iter()
+            .map(|module_item| SourceMapped::new(module_item, source_map))
+            .collect();
 
-        let mut previous_item_was_canister_method_annotation = false;
-
-        self.body
+        source_mapped_body
             .iter()
             .enumerate()
-            .fold((vec![], vec![]), |mut acc, (i, module_item)| {
-                let sm_module_item = SourceMapped::new(module_item, source_map);
+            .fold(Accumulator::new(), |mut acc, (i, module_item)| {
+                // TODO: Change this to something like module_item.as_canister_method_annotation
+                if module_item.is_canister_method_annotation() {
+                    match source_mapped_body.get(i + 1) {
+                        Some(next_item) => {
+                            let next_item = SourceMapped::new(next_item, source_map);
+                            match next_item.as_exported_fn_decl() {
+                                Some(fn_decl) => {
+                                    // TODO: Change this to pop out up above when we check if is_canister_method_annotation
+                                    match Annotation::from_module_item(&module_item) {
+                                        Ok(annotation) => {
+                                            let annotated_fn_decl = AnnotatedFnDecl {
+                                                annotation,
+                                                fn_decl: fn_decl.clone(),
+                                                source_map,
+                                            };
 
-                if previous_item_was_canister_method_annotation {
-                    let annotation_module_item = self.body.get(i - 1).unwrap();
-                    let sm_annotation_module_item =
-                        SourceMapped::new(annotation_module_item, source_map);
-
-                    // TODO: detect non-exported fn_decls and explain they have
-                    // to be exported... or just accept them.
-                    match module_item.as_exported_fn_decl() {
-                        Some(fn_decl) => {
-                            match Annotation::from_module_item(&sm_annotation_module_item) {
-                                Ok(annotation) => {
-                                    let annotated_fn_decl = AnnotatedFnDecl {
-                                        annotation,
-                                        fn_decl: fn_decl.clone(),
-                                        source_map,
-                                    };
-
-                                    match &fn_decl.function.return_type {
-                                        Some(_) => acc.0.push(annotated_fn_decl),
-                                        None => acc.1.push(
-                                            MissingReturnTypeAnnotation::from_annotated_fn_decl(
-                                                &annotated_fn_decl,
+                                            match &fn_decl.function.return_type {
+                                                Some(_) => {
+                                                    acc.annotated_fn_decls.push(annotated_fn_decl)
+                                                }
+                                                None => {
+                                                    let missing_return_type_annotation =
+                                                        MissingReturnTypeAnnotation::from_annotated_fn_decl(&annotated_fn_decl);
+                                                    acc.errors.push(missing_return_type_annotation.into())
+                                                }
+                                            }
+                                        }
+                                        Err(err) => panic!(
+                                            "{}",
+                                            errors::build_parse_error_message(
+                                                err,
+                                                &module_item,
+                                                source_map
                                             )
-                                            .into(),
                                         ),
                                     }
                                 }
-                                Err(err) => panic!(
-                                    "{}",
-                                    errors::build_parse_error_message(
-                                        err,
-                                        annotation_module_item,
-                                        source_map
+                                // There is an annotation not followed by an exported function (but not at end of file)
+                                None => acc.errors.push(
+                                    ExtraneousCanisterMethodAnnotation::from_module_item(
+                                        &module_item,
                                     )
+                                    .into(),
                                 ),
                             }
                         }
-                        None => acc.1.push(
-                            ExtraneousCanisterMethodAnnotation::from_module_item(
-                                &sm_annotation_module_item,
-                            )
-                            .into(),
+                        // There is a dangling canister method annotation at the end of the file.
+                        None => acc.errors.push(
+                            ExtraneousCanisterMethodAnnotation::from_module_item(&module_item)
+                                .into(),
                         ),
-                    }
+                    };
                 }
-
-                if i + 1 == self.body.len() && module_item.is_canister_method_annotation() {
-                    acc.1.push(
-                        ExtraneousCanisterMethodAnnotation::from_module_item(&sm_module_item)
-                            .into(),
-                    )
-                }
-
-                previous_item_was_canister_method_annotation =
-                    module_item.is_canister_method_annotation();
 
                 acc
             })
+            .into()
     }
 
     fn get_fn_decls<'a>(&'a self, source_map: &'a SourceMap) -> Vec<SourceMapped<'a, FnDecl>> {
