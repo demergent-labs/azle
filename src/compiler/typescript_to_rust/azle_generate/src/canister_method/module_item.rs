c1211b95a7d1fb4d42a08ad62883af20e9ad4667
Dan Steren <dan@demergentlabs.org>
2023-05-10 11:59:43 -0600

    Rework is_canister_method_annotation
    This seems way more readable to me and keeps everything more flat.

--- src/compiler/typescript_to_rust/azle_generate/src/canister_method/module_item.rs
+++ src/compiler/typescript_to_rust/azle_generate/src/canister_method/module_item.rs
@@ -1,6 +1,6 @@
 use std::ops::Deref;
 
-use swc_ecma_ast::{Callee, Expr, ExprStmt, FnDecl, ModuleItem, Stmt};
+use swc_ecma_ast::{ExprStmt, FnDecl, ModuleItem};
 
 use crate::{canister_method::annotation::CANISTER_METHOD_ANNOTATIONS, traits::GetName};
 
@@ -12,23 +12,20 @@ pub trait ModuleItemHelperMethods {
 
 impl ModuleItemHelperMethods for ModuleItem {
     fn is_canister_method_annotation(&self) -> bool {
-        match self {
-            ModuleItem::Stmt(Stmt::Expr(expr_stmt)) => {
-                let ident = match &*expr_stmt.expr {
-                    Expr::Call(call_expr) => match &call_expr.callee {
-                        Callee::Expr(box_expr) => match box_expr.deref() {
-                            Expr::Ident(ident) => ident,
-                            _ => return false,
-                        },
-                        _ => return false,
-                    },
-                    Expr::Ident(ident) => ident,
-                    _ => return false,
-                };
-                CANISTER_METHOD_ANNOTATIONS.contains(&ident.get_name())
-            }
-            _ => false,
-        }
+        self.as_stmt()
+            .and_then(|stmt| stmt.as_expr())
+            .map(|expr_stmt| {
+                expr_stmt
+                    .expr
+                    .as_call()
+                    .and_then(|call_expr| call_expr.callee.as_expr())
+                    .and_then(|box_expr| box_expr.deref().as_ident())
+                    .and_then(|ident| Some(ident))
+                    .or(expr_stmt.expr.as_ident())
+                    .map(|ident| CANISTER_METHOD_ANNOTATIONS.contains(&ident.get_name()))
+                    .unwrap_or(false)
+            })
+            .unwrap_or(false)
     }
 
     fn as_exported_fn_decl(&self) -> Option<FnDecl> {
