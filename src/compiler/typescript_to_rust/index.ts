4d8fccfc4f064c7fe07cc1a10afa246288cc57c5
Benjamin DeMann <benjamin.demann@gmail.com>
2023-06-07 10:48:10 -0600

    move generate import symbol table to it's own file

--- src/compiler/typescript_to_rust/index.ts
+++ src/compiler/typescript_to_rust/index.ts
@@ -9,13 +9,12 @@ import {
 } from './typescript_to_javascript/cargo_toml_files';
 import { writeCodeToFileSystem } from './write_code_to_file_system';
 import { generateRustCanister } from './generate_rust_canister';
+import { generateImportSymbolTable } from './generate_import_symbol_table';
 import { Err, ok, unwrap } from '../utils/result';
 import {
     AzleError,
     JSCanisterConfig,
     Plugin,
-    SymbolTable,
-    SymbolTables,
     Toml,
     TsCompilationError,
     TsSyntaxErrorLocation
@@ -118,63 +117,6 @@ export function compileTypeScriptToRust(
             process.exit(0);
         }
     });
-}
-
-function generateImportSymbolTable(files: string[]): SymbolTables {
-    return files.reduce((accumulator: SymbolTables, currentValue: string) => {
-        accumulator[currentValue] = createDefaultSymbolTable();
-        return accumulator;
-    }, {});
-}
-
-function createDefaultSymbolTable(): SymbolTable {
-    return {
-        alias: ['Alias'],
-        blob: ['blob'],
-        bool: [''],
-        call_result: ['CallResult'],
-        empty: ['empty'],
-        float32: ['float32'],
-        float64: ['float64'],
-        func: ['Func'],
-        guard_result: ['GuardResult'],
-        heartbeat_decorator: ['$heartbeat'],
-        init_decorator: ['$init'],
-        inspect_message_decorator: ['$inspectMessage'],
-        int: ['int'],
-        int8: ['int8'],
-        int16: ['int16'],
-        int32: ['int32'],
-        int64: ['int64'],
-        manual: ['Manual'],
-        nat: ['nat'],
-        nat8: ['nat8'],
-        nat16: ['nat16'],
-        nat32: ['nat32'],
-        nat64: ['nat64'],
-        null: [''],
-        oneway_mode: ['Oneway'],
-        opt: ['Opt'],
-        post_upgrade_decorator: ['$postUpgrade'],
-        pre_upgrade_decorator: ['$preUpgrade'],
-        principal: ['Principal'],
-        query_decorator: ['$query'],
-        query_mode: ['Query'],
-        record: ['Record'],
-        result: ['Result'],
-        reserved: ['reserved'],
-        service: ['Service'],
-        service_query_decorator: ['serviceQuery'],
-        service_update_decorator: ['serviceUpdate'],
-        stable_b_tree_map: ['StableBTreeMap'],
-        text: ['text'],
-        tuple: ['Tuple'],
-        update_decorator: ['$update'],
-        update_mode: ['Update'],
-        variant: ['Variant'],
-        vec: ['Vec'],
-        void: ['']
-    };
 }
 
 function isCompileOnlyMode(): boolean {
