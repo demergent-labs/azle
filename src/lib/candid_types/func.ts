a6e01ab4c781bcc1cce2df32b360898a1728bafc
Dan Steren <dan@demergentlabs.org>
2023-06-13 16:57:48 -0600

    Improve JS docs for Funcs

--- src/lib/candid_types/func.ts
+++ src/lib/candid_types/func.ts
@@ -1,8 +1,32 @@
-import { Principal } from './';
+import { Principal, Service } from './';
 
-export type Func<T> = T;
+/**
+ * Represents a reference to a {@link Service} method. The type param specifies
+ * the functional signature of the method but under the hood it is really just
+ * an array with two values:
+ *
+ * - the principal of the {@link Service} where the method is
+ * defined
+ * - the name of the method.
+ *
+ * See https://internetcomputer.org/docs/current/references/candid-ref#type-func---
+ *
+ * @typeParam T - the signature of the referenced {@link Service} method. Must
+ * be one of:
+ *
+ * - {@link Query}
+ * - {@link Update}
+ * - {@link Oneway}
+ */
+export type Func<T extends Mode> = T;
 
-// TODO type these more strictly
-export type Query<T extends (...args: any[]) => any> = [Principal, string];
-export type Update<T extends (...args: any[]) => any> = [Principal, string];
-export type Oneway<T extends (...args: any[]) => any> = [Principal, string];
+export type ServiceMethod = (...args: any[]) => any;
+
+export type Query<T extends ServiceMethod> = [Principal, string];
+export type Update<T extends ServiceMethod> = [Principal, string];
+export type Oneway<T extends ServiceMethod> = [Principal, string];
+
+export type Mode =
+    | Query<ServiceMethod>
+    | Update<ServiceMethod>
+    | Oneway<ServiceMethod>;
