832797a1e17fddd5a08e1277ebc5269491a753c3
Dan Steren <dan@demergentlabs.org>
2023-06-16 17:11:29 -0600

    Resultify heartbeat

--- src/compiler/typescript_to_rust/azle_generate/src/canister_method/heartbeat/rust.rs
+++ src/compiler/typescript_to_rust/azle_generate/src/canister_method/heartbeat/rust.rs
@@ -12,38 +12,42 @@ pub fn generate(
     let function_name = heartbeat_fn_decl.get_function_name();
 
     Ok(quote::quote! {
-        BOA_CONTEXT_REF_CELL.with(|box_context_ref_cell| {
-            let mut boa_context = box_context_ref_cell.borrow_mut();
+        unwrap_or_trap(|| {
+            BOA_CONTEXT_REF_CELL.with(|box_context_ref_cell| {
+                let mut boa_context = box_context_ref_cell.borrow_mut();
 
-            let uuid = uuid::Uuid::new_v4().to_string();
+                let uuid = uuid::Uuid::new_v4().to_string();
 
-            UUID_REF_CELL.with(|uuid_ref_cell| {
-                let mut uuid_mut = uuid_ref_cell.borrow_mut();
+                UUID_REF_CELL.with(|uuid_ref_cell| {
+                    let mut uuid_mut = uuid_ref_cell.borrow_mut();
 
-                *uuid_mut = uuid.clone();
-            });
+                    *uuid_mut = uuid.clone();
+                });
 
-            METHOD_NAME_REF_CELL.with(|method_name_ref_cell| {
-                let mut method_name_mut = method_name_ref_cell.borrow_mut();
+                METHOD_NAME_REF_CELL.with(|method_name_ref_cell| {
+                    let mut method_name_mut = method_name_ref_cell.borrow_mut();
 
-                *method_name_mut = #function_name.to_string()
-            });
+                    *method_name_mut = #function_name.to_string()
+                });
 
-            MANUAL_REF_CELL.with(|manual_ref_cell| {
-                let mut manual_mut = manual_ref_cell.borrow_mut();
+                MANUAL_REF_CELL.with(|manual_ref_cell| {
+                    let mut manual_mut = manual_ref_cell.borrow_mut();
 
-                *manual_mut = true;
-            });
+                    *manual_mut = true;
+                });
 
-            #call_to_heartbeat_js_function
+                #call_to_heartbeat_js_function
 
-            async_await_result_handler(
-                &mut boa_context,
-                &boa_return_value,
-                &uuid,
-                #function_name,
-                true
-            );
-        });
+                async_await_result_handler(
+                    &mut boa_context,
+                    &boa_return_value,
+                    &uuid,
+                    #function_name,
+                    true
+                );
+
+                Ok(())
+            })
+        })
     })
 }
