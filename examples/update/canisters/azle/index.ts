ff127c2aec3f0b480727daf2bc3ed83eab481b91
Dan Steren <dan@demergentlabs.org>
2023-06-16 09:36:26 -0600

    Fixed update example

--- examples/update/canisters/azle/index.ts
+++ examples/update/canisters/azle/index.ts
@@ -6,7 +6,7 @@ type PerfResult = Record<{
     wasmIncludingPrelude: nat64;
 }>;
 
-let perfResult: Opt<PerfResult> = null;
+let perfResult: Opt<PerfResult> = Opt.None;
 
 $query;
 export function getPerfResult(): Opt<PerfResult> {
@@ -14,10 +14,10 @@ export function getPerfResult(): Opt<PerfResult> {
 }
 
 function recordPerformance(start: nat64, end: nat64): void {
-    perfResult = {
+    perfResult = Opt.Some({
         wasmBodyOnly: end - start,
         wasmIncludingPrelude: ic.performanceCounter(0)
-    };
+    });
 }
 //#endregion
 
