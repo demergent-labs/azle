df766d00e6e58d8de1d96f3a9c3adef076acdff2
Dan Steren <dan@demergentlabs.org>
2023-06-16 17:26:20 -0600

    Resultify inspect_message

--- src/compiler/typescript_to_rust/azle_generate/src/canister_method/inspect_message/rust.rs
+++ src/compiler/typescript_to_rust/azle_generate/src/canister_method/inspect_message/rust.rs
@@ -13,16 +13,20 @@ pub fn generate(
     let function_name = inspect_message_fn_decl.get_function_name();
 
     Ok(quote::quote! {
-        BOA_CONTEXT_REF_CELL.with(|box_context_ref_cell| {
-            let mut boa_context = box_context_ref_cell.borrow_mut();
+        unwrap_or_trap(|| {
+            BOA_CONTEXT_REF_CELL.with(|box_context_ref_cell| {
+                let mut boa_context = box_context_ref_cell.borrow_mut();
 
-            METHOD_NAME_REF_CELL.with(|method_name_ref_cell| {
-                let mut method_name_mut = method_name_ref_cell.borrow_mut();
+                METHOD_NAME_REF_CELL.with(|method_name_ref_cell| {
+                    let mut method_name_mut = method_name_ref_cell.borrow_mut();
 
-                *method_name_mut = #function_name.to_string()
-            });
+                    *method_name_mut = #function_name.to_string()
+                });
 
-            #call_to_inspect_message_js_function
-        });
+                #call_to_inspect_message_js_function
+
+                Ok(())
+            })
+        })
     })
 }
