99ec2a696f847c26d1f0f559ee520bf130e37fc3
Benjamin DeMann <benjamin.demann@gmail.com>
2023-06-21 16:55:46 -0600

    Clean up

--- examples/robust_imports/src/index.ts
+++ examples/robust_imports/src/index.ts
@@ -1,22 +1,20 @@
 // Named Imports
 export * from './import_coverage/import_coverage';
 export * from './azle_coverage/azle_coverage';
-import NotAzleType from './types/azle_wrapper';
-import NotAzle, {
-    NotAzleType as NotAzleEither,
-    Tuple
-} from './types/azle_wrapper';
 
-import * as starFruit from './azle_coverage/fruit';
+// TODO Add tests for things that are named to look like azle things
+// import NotAzleType from './types/azle_wrapper';
+// import NotAzle, {
+//     NotAzleType as NotAzleEither,
+//     Tuple
+// } from './types/azle_wrapper';
 
 // TODO figure out type aliases declarations, especially with built in types like string and number
 // type coolString = string;
 // export type coolNat = StarImport.AlmostCoolNat;
 // export type coolInt = int;
-
-// Not Named Imports
-import './azle_coverage/fruit';
-
+// Though ultimately this will be covered by continuing to enforce use of the Alias<> wrapper
+// As part of that:
 // TODO how do we tell the difference between
 // type User1<T extends object> = azle.Record<T>;
 // // and
@@ -28,9 +26,5 @@ import './azle_coverage/fruit';
 //     id: string;
 // }>;
 
-// type Reaction = GrannySmith<{
-//     Good: null;
-//     Bad: null;
-// }>;
-
-// TODO tomororw. What about alias to built in types?
+// Not Named Imports
+import './azle_coverage/fruit'; // Shouldn't do anything. It's just here to make sure it doesn't do anything
