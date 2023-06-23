1070f8ad86ec020bed0579a70dbb5446347a2a5d
Dan Steren <dan@demergentlabs.org>
2023-06-23 10:50:51 -0600

    Use string templates for readability
    Keeping track of the number of indents and newline positions was getting
    out of hand. This is much easier to follow.

--- examples/run_time_errors/test/invalid_funcs.ts
+++ examples/run_time_errors/test/invalid_funcs.ts
@@ -3,6 +3,38 @@ import { Test } from 'azle/test';
 import { _SERVICE } from './dfx_generated/run_time_errors/run_time_errors.did';
 import { expectError } from './tests';
 
+const valueIsNotAFuncErrorMessage = "TypeError: value is not of type 'Func'";
+
+const expectedArrayErrorMessage = `[TypeError: value is not of type 'Func'] {
+  [cause]: TypeError: expected 'Array', given 'Object'
+}`;
+
+const index0IsUndefinedErrorMessage = `[TypeError: value is not of type 'Func'] {
+  [cause]: TypeError: index '0' is undefined
+}`;
+
+const valueIsNotOfTypePrincipalErrorMessage = `[TypeError: value is not of type 'Func'] {
+  [cause]: TypeError: index '0' is not of type 'Principal' {
+    [cause]: TypeError: value is not of type 'Principal'
+  }
+}`;
+
+const propertyToTextIsNotAFunctionErrorMessage = `[TypeError: value is not of type 'Func'] {
+  [cause]: TypeError: index '0' is not of type 'Principal' {
+    [cause]: TypeError: property 'toText' of object is not a function
+  }
+}`;
+
+const index1IsUndefinedErrorMessage = `[TypeError: value is not of type 'Func'] {
+  [cause]: TypeError: index '1' is undefined
+}`;
+
+const valueIsNotAStringErrorMessage = `[TypeError: value is not of type 'Func'] {
+  [cause]: TypeError: index '1' is not of type 'string' {
+    [cause]: TypeError: value is not of type 'string'
+  }
+}`;
+
 export function getInvalidFuncTests(
     errorCanister: ActorSubclass<_SERVICE>
 ): Test[] {
@@ -10,37 +42,37 @@ export function getInvalidFuncTests(
         expectError(
             'return a non-array value as an invalid Func',
             errorCanister.returnNonArrayValueAsInvalidFunc,
-            "TypeError: value is not of type 'Func'"
+            valueIsNotAFuncErrorMessage
         ),
         expectError(
             'return an empty object as an invalid Func',
             errorCanister.returnEmptyObjectAsInvalidFunc,
-            "[TypeError: value is not of type 'Func'] {\n  [cause]: TypeError: expected 'Array', given 'Object'\n}"
+            expectedArrayErrorMessage
         ),
         expectError(
             'return an empty array as an invalid Func',
             errorCanister.returnEmptyArrayAsInvalidFunc,
-            "[TypeError: value is not of type 'Func'] {\n  [cause]: TypeError: index '0' is undefined\n}"
+            index0IsUndefinedErrorMessage
         ),
         expectError(
             'return a non-principal value as an invalid Func',
             errorCanister.returnNonPrincipalValueAsInvalidFunc,
-            "[TypeError: value is not of type 'Func'] {\n  [cause]: TypeError: index '0' is not of type 'Principal' {\n    [cause]: TypeError: value is not of type 'Principal'\n  }\n}"
+            valueIsNotOfTypePrincipalErrorMessage
         ),
         expectError(
             'return an empty object for a principal as an invalid Func',
             errorCanister.returnEmptyObjectPrincipalAsInvalidFunc,
-            "[TypeError: value is not of type 'Func'] {\n  [cause]: TypeError: index '0' is not of type 'Principal' {\n    [cause]: TypeError: property 'toText' of object is not a function\n  }\n}"
+            propertyToTextIsNotAFunctionErrorMessage
         ),
         expectError(
             'return array with only a principal as an invalid Func',
             errorCanister.returnArrayWithOnlyPrincipalAsInvalidFunc,
-            "[TypeError: value is not of type 'Func'] {\n  [cause]: TypeError: index '1' is undefined\n}"
+            index1IsUndefinedErrorMessage
         ),
         expectError(
             'return a non-string canister method name as an invalid Func',
             errorCanister.returnNonStringCanisterMethodNameAsInvalidFunc,
-            "[TypeError: value is not of type 'Func'] {\n  [cause]: TypeError: index '1' is not of type 'string' {\n    [cause]: TypeError: value is not of type 'string'\n  }\n}"
+            valueIsNotAStringErrorMessage
         )
     ];
 }
