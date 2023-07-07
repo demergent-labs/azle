bc7f4476001d2b8bd307767242557c6d310c9f9c
Dan Steren <dan@demergentlabs.org>
2023-07-07 13:50:05 -0600

    Format promise fulfillment

--- src/compiler/typescript_to_rust/azle_generate/src/body/ic_object/functions/async_call/promise_fulfillment.rs
+++ src/compiler/typescript_to_rust/azle_generate/src/body/ic_object/functions/async_call/promise_fulfillment.rs
@@ -5,30 +5,32 @@ pub fn generate() -> proc_macro2::TokenStream {
 
             let call_result_js_value = match call_result {
                 Ok(value) => {
-                    let js_value = value.try_into_vm_value(&mut *boa_context).unwrap();
+                    let js_value = value
+                        .try_into_vm_value(&mut *boa_context)
+                        .unwrap();
 
-                    let canister_result_js_object = boa_engine::object::ObjectInitializer::new(&mut *boa_context)
-                        .property(
-                            "Ok",
-                            js_value,
-                            boa_engine::property::Attribute::all()
-                        )
-                        .build();
+                    let canister_result_js_object =
+                        boa_engine::object::ObjectInitializer::new(&mut *boa_context)
+                            .property("Ok", js_value, boa_engine::property::Attribute::all())
+                            .build();
 
                     let canister_result_js_value = canister_result_js_object.into();
 
                     canister_result_js_value
-                },
+                }
                 Err(err) => {
-                    let js_value = format!("Rejection code {rejection_code}, {error_message}", rejection_code = (err.0 as i32).to_string(), error_message = err.1).try_into_vm_value(&mut *boa_context).unwrap();
+                    let js_value = format!(
+                        "Rejection code {rejection_code}, {error_message}",
+                        rejection_code = (err.0 as i32).to_string(),
+                        error_message = err.1
+                    )
+                    .try_into_vm_value(&mut *boa_context)
+                    .unwrap();
 
-                    let canister_result_js_object = boa_engine::object::ObjectInitializer::new(&mut *boa_context)
-                        .property(
-                            "Err",
-                            js_value,
-                            boa_engine::property::Attribute::all()
-                        )
-                        .build();
+                    let canister_result_js_object =
+                        boa_engine::object::ObjectInitializer::new(&mut *boa_context)
+                            .property("Err", js_value, boa_engine::property::Attribute::all())
+                            .build();
 
                     let canister_result_js_value = canister_result_js_object.into();
 
@@ -36,7 +38,12 @@ pub fn generate() -> proc_macro2::TokenStream {
                 }
             };
 
-            js_promise_resolvers.resolve.call(&boa_engine::JsValue::undefined(), &[call_result_js_value], &mut *boa_context).unwrap();
+            js_promise_resolvers.resolve.call(
+                &boa_engine::JsValue::undefined(),
+                &[call_result_js_value],
+                &mut *boa_context,
+            )
+            .unwrap();
 
             let main_promise = PROMISE_MAP_REF_CELL.with(|promise_map_ref_cell| {
                 let promise_map = promise_map_ref_cell.borrow().clone();
@@ -46,7 +53,13 @@ pub fn generate() -> proc_macro2::TokenStream {
                 main_promise.clone()
             });
 
-            async_await_result_handler(&mut *boa_context, &main_promise, &uuid, &method_name, manual);
+            async_await_result_handler(
+                &mut *boa_context,
+                &main_promise,
+                &uuid,
+                &method_name,
+                manual,
+            );
         });
     }
 }
