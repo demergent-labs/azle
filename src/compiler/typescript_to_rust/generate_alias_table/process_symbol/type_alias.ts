10b3b2962782f2ff90b79f3aab3a3f4fe6a27126
Benjamin DeMann <benjamin.demann@gmail.com>
2023-07-07 09:23:34 -0600

    add support for type alias declarations

--- src/compiler/typescript_to_rust/generate_alias_table/process_symbol/type_alias.ts
+++ src/compiler/typescript_to_rust/generate_alias_table/process_symbol/type_alias.ts
@@ -7,25 +7,66 @@ import {
 } from '../get_symbol_table';
 import { getSourceFile, getDeclarationFromNamespace } from '../utils';
 import { generateAliasTableForSymbol } from '../process_symbol';
+import { generateSingleEntryAliasTable } from '../alias_table';
 
 const TYPE_ALIASES_ARE_STILL_UNIMPLEMENTED = true;
+
+function isAzleTypeAliasDecl(
+    typeAliasDeclaration: ts.TypeAliasDeclaration
+): boolean {
+    let sourceFile = getSourceFile(typeAliasDeclaration);
+    if (!sourceFile) {
+        return false;
+    }
+    return sourceFile.fileName.includes('azle/src/lib');
+}
 
 export function generateAliasTableForTypeAliasDeclaration(
     typeAliasDeclaration: ts.TypeAliasDeclaration,
     alias: string,
     program: ts.Program
 ): AliasTable | undefined {
-    if (TYPE_ALIASES_ARE_STILL_UNIMPLEMENTED) {
-        return undefined; // TODO Add support for type alias declarations
-        // The below code doesn't work, but it's hopefully a good starting point
+    if (isAzleTypeAliasDecl(typeAliasDeclaration)) {
+        // TODO I'm not sure that this is right
+        // TODO test that we can make aliases to all sorts of azle types
+        console.log(`${alias} is a ${typeAliasDeclaration.name.text}`);
+        return generateSingleEntryAliasTable(
+            typeAliasDeclaration.name.text,
+            alias
+        );
     }
-    if (typeAliasDeclaration.typeParameters?.length ?? 0 > 0) {
-        return undefined; // This looks like a candid definition not a possible azle alias
+    // if (TYPE_ALIASES_ARE_STILL_UNIMPLEMENTED) {
+    //     return undefined; // TODO Add support for type alias declarations
+    //     // The below code doesn't work, but it's hopefully a good starting point
+    // }
+    // if (typeAliasDeclaration.typeParameters?.length ?? 0 > 0) {
+    //     return undefined; // This looks like a candid definition not a possible azle alias
+    // }
+    // TODO if there are typeParameters then can we find the corresponding type arguments and not count them against the being a candidate
+    // TODO what if we got the length of the typeParamters and if it was the same as the length of the typearguments then we are good, otherwise return?
+    let aliasedType = typeAliasDeclaration.type;
+    if (ts.isTypeReferenceNode(aliasedType)) {
+        let typeArguments = aliasedType.typeArguments;
+        if (typeArguments) {
+            if (typeArguments.length > 0) {
+                return undefined;
+            }
+        }
     }
     const sourceFile = getSourceFile(typeAliasDeclaration);
     if (sourceFile === undefined) {
         // TODO couldn't find the sourceFile
         return undefined;
+    }
+
+    console.log(`============= ${alias} ================`);
+    console.log(typeAliasDeclaration.name.text);
+    console.log(typeAliasDeclaration.getText(sourceFile));
+    if (typeAliasDeclaration.typeParameters) {
+        console.log(typeAliasDeclaration.typeParameters.length);
+    }
+    if (alias === 'RejectionCode') {
+        console.log(typeAliasDeclaration);
     }
     const symbolTable = getSymbolTable(sourceFile, program);
     if (symbolTable === undefined) {
@@ -79,5 +120,48 @@ export function generateAliasTableForTypeAliasDeclaration(
             return generateAliasTableForSymbol(symbol, alias, program);
         }
     }
+    // TODO make tests for all of these possibilities test all of these
+    if (typeReference.kind === ts.SyntaxKind.BooleanKeyword) {
+        return generateSingleEntryAliasTable('bool', alias);
+    }
+    if (typeReference.kind === ts.SyntaxKind.LiteralType) {
+        if ('literal' in typeReference) {
+            let literal = typeReference.literal;
+            if (
+                typeof literal === 'object' &&
+                literal !== null &&
+                'kind' in literal
+            ) {
+                if (literal.kind === ts.SyntaxKind.NullKeyword) {
+                    return generateSingleEntryAliasTable('null', alias);
+                }
+            }
+        }
+    }
+    if (typeReference.kind === ts.SyntaxKind.StringKeyword) {
+        return generateSingleEntryAliasTable('text', alias);
+    }
+    if (typeReference.kind === ts.SyntaxKind.BigIntKeyword) {
+        return generateSingleEntryAliasTable('int', alias);
+    }
+    if (typeReference.kind === ts.SyntaxKind.NumberKeyword) {
+        return generateSingleEntryAliasTable('int', alias);
+    }
+    if (typeReference.kind === ts.SyntaxKind.VoidKeyword) {
+        return generateSingleEntryAliasTable('void', alias);
+    }
+    if (
+        typeReference.kind === ts.SyntaxKind.FunctionType ||
+        typeReference.kind === ts.SyntaxKind.UnionType
+    ) {
+        // TODO we do not yet have azle types that map to these types
+        return undefined;
+    }
+    console.log('>>>>>>>>>>>>>>>>>>>>IMPORTANT<<<<<<<<<<<<<<<<<<<<');
+    console.log(ts.SyntaxKind[typeReference.kind]);
+    console.log('===============================================');
+    console.log('===============================================');
+    console.log('===============================================');
+    console.log('===============================================');
     // TODO what else could this be??
 }
