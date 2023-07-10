7f23a26a9f26757f3c305670f8fb8ca1352d9690
Dan Steren <dan@demergentlabs.org>
2023-07-10 17:37:37 -0600

    Remove unwraps from promise fulfillment

--- src/compiler/typescript_to_rust/azle_generate/src/body/ic_object/functions/async_call/promise_fulfillment.rs
+++ src/compiler/typescript_to_rust/azle_generate/src/body/ic_object/functions/async_call/promise_fulfillment.rs
@@ -5,9 +5,21 @@ pub fn generate() -> proc_macro2::TokenStream {
 
             let call_result_js_value = match call_result {
                 Ok(value) => {
-                    let js_value = value
-                        .try_into_vm_value(&mut *boa_context)
-                        .unwrap();
+                    let js_value = match value.try_into_vm_value(&mut *boa_context) {
+                        Ok(js_value) => js_value,
+                        Err(vmc_err) => {
+                            js_promise_resolvers
+                                .reject
+                                .call(
+                                    &boa_engine::JsValue::undefined(),
+                                    &[vmc_err.to_js_error().to_opaque(&mut *boa_context)],
+                                    &mut *boa_context,
+                                )
+                                .unwrap_or_trap(&mut *boa_context);
+
+                            return;
+                        }
+                    };
 
                     let canister_result_js_object =
                         boa_engine::object::ObjectInitializer::new(&mut *boa_context)
@@ -19,13 +31,27 @@ pub fn generate() -> proc_macro2::TokenStream {
                     canister_result_js_value
                 }
                 Err(err) => {
-                    let js_value = format!(
+                    let js_value = match format!(
                         "Rejection code {rejection_code}, {error_message}",
                         rejection_code = (err.0 as i32).to_string(),
                         error_message = err.1
                     )
                     .try_into_vm_value(&mut *boa_context)
-                    .unwrap();
+                    {
+                        Ok(js_value) => js_value,
+                        Err(vmc_err) => {
+                            js_promise_resolvers
+                                .reject
+                                .call(
+                                    &boa_engine::JsValue::undefined(),
+                                    &[vmc_err.to_js_error().to_opaque(&mut *boa_context)],
+                                    &mut *boa_context,
+                                )
+                                .unwrap_or_trap(&mut *boa_context);
+
+                            return;
+                        }
+                    };
 
                     let canister_result_js_object =
                         boa_engine::object::ObjectInitializer::new(&mut *boa_context)
@@ -38,17 +64,21 @@ pub fn generate() -> proc_macro2::TokenStream {
                 }
             };
 
-            js_promise_resolvers.resolve.call(
-                &boa_engine::JsValue::undefined(),
-                &[call_result_js_value],
-                &mut *boa_context,
-            )
-            .unwrap();
+            js_promise_resolvers
+                .resolve
+                .call(
+                    &boa_engine::JsValue::undefined(),
+                    &[call_result_js_value],
+                    &mut *boa_context,
+                )
+                .unwrap_or_trap(&mut *boa_context);
 
             let main_promise = PROMISE_MAP_REF_CELL.with(|promise_map_ref_cell| {
                 let promise_map = promise_map_ref_cell.borrow().clone();
 
-                let main_promise = promise_map.get(&uuid).unwrap();
+                let main_promise = promise_map.get(&uuid).unwrap_or_else(|| {
+                    ic_cdk::api::trap("ReferenceError: top-level promise is not defined")
+                });
 
                 main_promise.clone()
             });
