3ae3352da9de5ecc260ad8141d2b46d8ec32500a
Dan Steren <dan@demergentlabs.org>
2023-07-12 13:55:00 -0600

    Split up error conversion methods

--- src/compiler/typescript_to_rust/azle_generate/src/vm_value_conversion/try_into_vm_value_impls/error_conversions.rs
+++ src/compiler/typescript_to_rust/azle_generate/src/vm_value_conversion/try_into_vm_value_impls/error_conversions.rs
@@ -1,18 +1,18 @@
 pub fn generate() -> proc_macro2::TokenStream {
     quote::quote! {
-        impl From<&str> for CdkActTryFromVmValueError {
+        impl From<&str> for CdkActTryIntoVmValueError {
             fn from(value: &str) -> Self {
                 Self(value.to_string())
             }
         }
 
-        impl From<String> for CdkActTryFromVmValueError {
+        impl From<String> for CdkActTryIntoVmValueError {
             fn from(value: String) -> Self {
                 Self(value)
             }
         }
 
-        impl From<boa_engine::JsError> for CdkActTryFromVmValueError {
+        impl From<boa_engine::JsError> for CdkActTryIntoVmValueError {
             fn from(value: boa_engine::JsError) -> Self {
                 Self(value.to_string())
             }
