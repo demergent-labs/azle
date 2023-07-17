251cf1a0cec624a0695c32f266c9838f458b1faf
Dan Steren <dan@demergentlabs.org>
2023-07-17 13:03:00 -0600

    Return Err(String) instead of trapping

--- src/compiler/typescript_to_rust/azle_generate/src/guard_function/rust.rs
+++ src/compiler/typescript_to_rust/azle_generate/src/guard_function/rust.rs
@@ -7,14 +7,18 @@ pub fn generate(function_name: &String) -> TokenStream {
         BOA_CONTEXT_REF_CELL.with(|boa_context_ref_cell| {
             let mut boa_context = boa_context_ref_cell.borrow_mut();
 
-            let js_guard_fn_return_value = boa_context
+            boa_context
                 .eval_script(boa_engine::Source::from_bytes(#function_call))
-                .unwrap_or_trap(&mut boa_context);
+                .map_err(|js_error| {
+                    let error_message = js_value_to_string(
+                        js_error.to_opaque(&mut *boa_context),
+                        &mut *boa_context,
+                    );
 
-            match js_guard_fn_return_value.try_from_vm_value(&mut *boa_context) {
-                Ok(return_value) => return_value,
-                Err(e) => ic_cdk::api::trap(&format!("Uncaught {}", &e.0)),
-            }
+                    format!("Uncaught {}", error_message)
+                })?
+                .try_from_vm_value(&mut *boa_context)
+                .map_err(|vmc_err| format!("Uncaught {}", vmc_err.0))?
         })
     }
 }
