1717619871dab5521539d3c1ae4cdde4640b5dd7
Dan Steren <dan@demergentlabs.org>
2023-08-14 14:58:29 -0600

    Turn strings green

--- src/compiler/typescript_to_rust/azle_generate/src/body/traits/to_std_string/string.rs
+++ src/compiler/typescript_to_rust/azle_generate/src/body/traits/to_std_string/string.rs
@@ -6,20 +6,23 @@ pub fn generate() -> proc_macro2::TokenStream {
         ) -> String {
             match string.to_std_string() {
                 Ok(string) => {
+                    let green_start = "\x1B[32m";
+                    let green_end = "\x1B[0m";
+
                     if nesting_level == 0 {
                         return string;
                     }
                     if !string.contains("'") {
-                        return format!("'{}'", string);
+                        return format!("{green_start}'{}'{green_end}", string);
                     }
                     if !string.contains("\"") {
-                        return format!("\"{}\"", string.replace("\"", "\\\""));
+                        return format!("{green_start}\"{}\"{green_end}", string.replace("\"", "\\\""));
                     }
                     if !string.contains("`") {
-                        return format!("`{}`", string);
+                        return format!("{green_start}`{}`{green_end}", string);
                     }
 
-                    format!("'{}'", string.replace("'", "\\'"))
+                    format!("{green_start}'{}'{green_end}", string.replace("'", "\\'"))
 
                     // Alternatively, we could get rid of most of this logic by
                     // using the debug formatter. It just always uses double
