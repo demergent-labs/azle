3ef5a14cd3c470520c33fdff619081bf3d2f4ce6
Benjamin DeMann <benjamin.demann@gmail.com>
2023-08-25 12:02:24 -0600

    update null_example to decorator syntax

--- examples/null_example/src/null_example.ts
+++ examples/null_example/src/null_example.ts
@@ -1,73 +1,99 @@
-import { ic, int, $query, Record, $update } from 'azle';
+import {
+    ic,
+    candid,
+    query,
+    Record,
+    update,
+    int,
+    Null,
+    record,
+    Void
+} from 'azle';
 
-type PartiallyNullRecord = Record<{
+@record
+class PartiallyNullRecord extends Record {
+    @candid(int)
     firstItem: int;
-    secondItem: null;
+
+    @candid(Null)
+    secondItem: Null;
+
+    @candid(int)
     thirdItem: int;
-}>;
+}
 
-type TwoNullRecord = Record<{
-    firstItem: null;
-    secondItem: null;
-}>;
+@record
+class TwoNullRecord extends Record {
+    @candid(Null)
+    firstItem: Null;
 
-type ThreeNullRecord = Record<{
+    @candid(Null)
+    secondItem: Null;
+}
+
+@record
+class ThreeNullRecord extends Record {
+    @candid(Null)
     firstItem: null;
+
+    @candid(Null)
     secondItem: null;
+
+    @candid(Null)
     thirdItem: null;
-}>;
-
-$query;
-export function nullFunction(param: null): null {
-    return param;
 }
 
-$query;
-export function voidIsNotNull(): void {
-    ic.print(
-        'Even though they are both None in Python, for Candid null and void are different.'
-    );
-}
+export default class {
+    @query([Null], Null)
+    nullFunction(param: null): null {
+        return param;
+    }
 
-$query;
-export function getPartiallyNullRecord(): PartiallyNullRecord {
-    return {
-        firstItem: 1n,
-        secondItem: null,
-        thirdItem: 3n
-    };
-}
+    @query([], Void)
+    voidIsNotNull(): void {
+        ic.print(
+            'Even though they are both None in Python, for Candid null and void are different.'
+        );
+    }
 
-$update;
-export function setPartiallyNullRecord(
-    param: PartiallyNullRecord
-): PartiallyNullRecord {
-    return param;
-}
+    @query([], PartiallyNullRecord)
+    getPartiallyNullRecord(): PartiallyNullRecord {
+        return {
+            firstItem: 1n,
+            secondItem: null,
+            thirdItem: 3n
+        };
+    }
 
-$query;
-export function getSmallNullRecord(): TwoNullRecord {
-    return {
-        firstItem: null,
-        secondItem: null
-    };
-}
+    @update([PartiallyNullRecord], PartiallyNullRecord)
+    setPartiallyNullRecord(param: PartiallyNullRecord): PartiallyNullRecord {
+        return param;
+    }
 
-$update;
-export function setSmallNullRecord(param: TwoNullRecord): TwoNullRecord {
-    return param;
-}
+    @query([], TwoNullRecord)
+    getSmallNullRecord(): TwoNullRecord {
+        return {
+            firstItem: null,
+            secondItem: null
+        };
+    }
 
-$query;
-export function getLargeNullRecord(): ThreeNullRecord {
-    return {
-        firstItem: null,
-        secondItem: null,
-        thirdItem: null
-    };
-}
+    @update([TwoNullRecord], TwoNullRecord)
+    setSmallNullRecord(param: TwoNullRecord): TwoNullRecord {
+        return param;
+    }
 
-$update;
-export function setLargeNullRecord(param: ThreeNullRecord): ThreeNullRecord {
-    return param;
+    @query([], ThreeNullRecord)
+    getLargeNullRecord(): ThreeNullRecord {
+        return {
+            firstItem: null,
+            secondItem: null,
+            thirdItem: null
+        };
+    }
+
+    @update([ThreeNullRecord], ThreeNullRecord)
+    setLargeNullRecord(param: ThreeNullRecord): ThreeNullRecord {
+        return param;
+    }
 }
