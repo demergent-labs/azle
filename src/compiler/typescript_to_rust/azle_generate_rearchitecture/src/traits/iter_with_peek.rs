bb44d8d969653e2f0f5266463224e762063c6d25
Dan Steren <dan@demergentlabs.org>
2023-08-29 14:38:58 -0600

    Add final polish to IterWithPeek

--- src/compiler/typescript_to_rust/azle_generate_rearchitecture/src/traits/iter_with_peek.rs
+++ src/compiler/typescript_to_rust/azle_generate_rearchitecture/src/traits/iter_with_peek.rs
@@ -1,47 +1,52 @@
-// The Pairs struct now works with borrowed items (&'a T)
-pub struct Pairs<'a, T>
+/// An iterator with a peek-ahead feature.
+///
+/// Rather than yielding just the current element, this iterator will
+/// yield a tuple containing the current element and the next element.
+/// The final yield will always have `None` as it's "next" element.
+pub struct PeekAheadIterator<'a, T>
 where
     T: 'a,
 {
     inner: std::iter::Peekable<std::slice::Iter<'a, T>>,
-    last: Option<&'a T>,
+    current: Option<&'a T>,
 }
 
-impl<'a, T> Pairs<'a, T> {
+impl<'a, T> PeekAheadIterator<'a, T> {
     pub fn new(mut iter: std::slice::Iter<'a, T>) -> Self {
-        let last = iter.next();
+        let current = iter.next();
         Self {
             inner: iter.peekable(),
-            last,
+            current,
         }
     }
 }
 
-// The Iterator implementation now yields borrowed items
-impl<'a, T> Iterator for Pairs<'a, T> {
+impl<'a, T> Iterator for PeekAheadIterator<'a, T> {
     type Item = (&'a T, Option<&'a T>);
 
     fn next(&mut self) -> Option<Self::Item> {
-        if let Some(current) = self.last {
-            self.last = self.inner.next();
-            Some((current, self.last))
+        if let Some(current) = self.current {
+            self.current = self.inner.next();
+            Some((current, self.current))
         } else {
             None
         }
     }
 }
 
-// The IterWithPeek trait now returns an iterator that yields borrowed items
 pub trait IterWithPeek<T> {
-    fn iter_with_peek(&self) -> Pairs<'_, T>;
+    fn iter_with_peek(&self) -> PeekAheadIterator<'_, T>;
 }
 
-// The implementation of IterWithPeek has been updated
 impl<T, U> IterWithPeek<T> for U
 where
     U: AsRef<[T]>,
 {
-    fn iter_with_peek(&self) -> Pairs<'_, T> {
-        Pairs::new(self.as_ref().iter())
+    /// Returns an iterator over the slice.
+    ///
+    /// The iterator yields a tuple containing the current item and an
+    /// optional next item.
+    fn iter_with_peek(&self) -> PeekAheadIterator<'_, T> {
+        PeekAheadIterator::new(self.as_ref().iter())
     }
 }
