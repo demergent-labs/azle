a5c1e9cb254a2554ad4dedc7138a8fd581da5c14
Dan Steren <dan@demergentlabs.org>
2023-08-29 14:09:27 -0600

    Update IterWithPeek to return borrowed items
    This makes more sense to me and doesn't require an internal clone,
    which means this should be more performant.

--- src/compiler/typescript_to_rust/azle_generate_rearchitecture/src/traits/iter_with_peek.rs
+++ src/compiler/typescript_to_rust/azle_generate_rearchitecture/src/traits/iter_with_peek.rs
@@ -1,18 +1,14 @@
-pub struct Pairs<I>
+// The Pairs struct now works with borrowed items (&'a T)
+pub struct Pairs<'a, T>
 where
-    I: Iterator,
-    I::Item: Clone,
+    T: 'a,
 {
-    inner: std::iter::Peekable<I>,
-    last: Option<I::Item>,
+    inner: std::iter::Peekable<std::slice::Iter<'a, T>>,
+    last: Option<&'a T>,
 }
 
-impl<I> Pairs<I>
-where
-    I: Iterator,
-    I::Item: Clone,
-{
-    pub fn new(mut iter: I) -> Self {
+impl<'a, T> Pairs<'a, T> {
+    pub fn new(mut iter: std::slice::Iter<'a, T>) -> Self {
         let last = iter.next();
         Self {
             inner: iter.peekable(),
@@ -21,38 +17,31 @@ where
     }
 }
 
-impl<I> Iterator for Pairs<I>
-where
-    I: Iterator,
-    I::Item: Clone,
-{
-    type Item = (I::Item, Option<I::Item>);
+// The Iterator implementation now yields borrowed items
+impl<'a, T> Iterator for Pairs<'a, T> {
+    type Item = (&'a T, Option<&'a T>);
 
     fn next(&mut self) -> Option<Self::Item> {
-        if let Some(current) = self.last.take() {
+        if let Some(current) = self.last {
             self.last = self.inner.next();
-            Some((current, self.last.clone()))
+            Some((current, self.last))
         } else {
             None
         }
     }
 }
 
+// The IterWithPeek trait now returns an iterator that yields borrowed items
 pub trait IterWithPeek<T> {
-    fn iter_with_peek(&self) -> Pairs<std::slice::Iter<'_, T>>
-    where
-        T: Clone;
+    fn iter_with_peek(&self) -> Pairs<'_, T>;
 }
 
+// The implementation of IterWithPeek has been updated
 impl<T, U> IterWithPeek<T> for U
 where
     U: AsRef<[T]>,
-    T: Clone,
 {
-    fn iter_with_peek(&self) -> Pairs<std::slice::Iter<'_, T>>
-    where
-        T: Clone,
-    {
+    fn iter_with_peek(&self) -> Pairs<'_, T> {
         Pairs::new(self.as_ref().iter())
     }
 }
