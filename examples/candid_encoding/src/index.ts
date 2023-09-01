97817e22e313353cf9064317437ff262670cac54
Jordan Last <jordan.michael.last@gmail.com>
2023-08-31 21:35:28 -0500

    candid encoding

--- examples/candid_encoding/src/index.ts
+++ examples/candid_encoding/src/index.ts
@@ -1,13 +1,15 @@
-import { blob, ic, $query } from 'azle';
+import { blob, ic, query, text } from 'azle';
 
-// encodes a Candid string to Candid bytes
-$query;
-export function candidEncode(candidString: string): blob {
-    return ic.candidEncode(candidString);
-}
+export default class {
+    // encodes a Candid string to Candid bytes
+    @query([text], blob)
+    candidEncode(candidString: text): blob {
+        return ic.candidEncode(candidString);
+    }
 
-// decodes Candid bytes to a Candid string
-$query;
-export function candidDecode(candidEncoded: blob): string {
-    return ic.candidDecode(candidEncoded);
+    // decodes Candid bytes to a Candid string
+    @query([blob], text)
+    candidDecode(candidEncoded: blob): text {
+        return ic.candidDecode(candidEncoded);
+    }
 }
--- examples/candid_encoding/test/test.ts
+++ examples/candid_encoding/test/test.ts
@@ -2,10 +2,10 @@ import { getCanisterId, runTests } from 'azle/test';
 import { createActor } from './dfx_generated/candid_encoding';
 import { get_tests } from './tests';
 
-const candid_encoding_canister = createActor(getCanisterId('candid_encoding'), {
+const candidEncodingCanister = createActor(getCanisterId('candid_encoding'), {
     agentOptions: {
         host: 'http://127.0.0.1:8000'
     }
 });
 
-runTests(get_tests(candid_encoding_canister));
+runTests(get_tests(candidEncodingCanister));
