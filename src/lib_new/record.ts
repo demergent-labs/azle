6931c0648dd08174fc907fd4ff0b7fe62f1569fa
Benjamin DeMann <benjamin.demann@gmail.com>
2023-09-05 12:40:14 -0600

    replace decorator with super class

--- src/lib_new/record.ts
+++ src/lib_new/record.ts
@@ -6,7 +6,39 @@ import { Parent, processMap } from './utils';
 // communicating that change to the type checker. If we can get it to do that
 // then we can get rid of this class
 export class Record {
-    constructor(throwAway: any) {}
+    constructor(args: any) {
+        if (
+            Object.entries(this.constructor._azleCandidMap).length !==
+            Object.entries(args).length
+        ) {
+            throw `Wrong number of properties: expected ${
+                Object.entries(this.constructor._azleCandidMap).length
+            } got ${Object.entries(args).length}`;
+        }
+
+        for (const propertyName in this.constructor._azleCandidMap) {
+            if (!(propertyName in args)) {
+                throw `Missing property: ${propertyName}`;
+            }
+            this[propertyName] = args[propertyName];
+        }
+    }
+
+    static getIDL(parents: Parent[]) {
+        const idl = IDL.Rec();
+        idl.fill(
+            IDL.Record(
+                processMap(this._azleCandidMap, [
+                    ...parents,
+                    {
+                        idl: idl,
+                        name: this.name
+                    }
+                ])
+            )
+        );
+        return idl;
+    }
 
     static create<T extends Constructor>(
         this: T,
@@ -19,42 +51,8 @@ export class Record {
 type Constructor<T = {}> = new (...args: any[]) => T;
 
 export function record<T extends new (...args: any[]) => any>(target: T): T {
-    return class extends target {
-        constructor(...args: any[]) {
-            super(...args);
-            if (
-                Object.entries(target._azleCandidMap).length !==
-                Object.entries(args[0]).length
-            ) {
-                throw `Wrong number of properties: expected ${
-                    Object.entries(target._azleCandidMap).length
-                } got ${Object.entries(args[0]).length}`;
-            }
-
-            for (const propertyName in target._azleCandidMap) {
-                if (!(propertyName in args[0])) {
-                    throw `Missing property: ${propertyName}`;
-                }
-                this[propertyName] = args[0][propertyName];
-            }
-        }
-
-        static getIDL(parents: Parent[]) {
-            const idl = IDL.Rec();
-            idl.fill(
-                IDL.Record(
-                    processMap(target._azleCandidMap, [
-                        ...parents,
-                        {
-                            idl: idl,
-                            name: target.name
-                        }
-                    ])
-                )
-            );
-            return idl;
-        }
-    };
+    // return class extends target {};
+    return target;
 }
 
 // makeObjectLit() {
