1846f76a0b3b26b87ce137151cd8d8f4b116d608
Benjamin DeMann <benjamin.demann@gmail.com>
2023-09-05 11:19:16 -0600

    stable memory update

--- examples/stable_memory/src/stable_memory.ts
+++ examples/stable_memory/src/stable_memory.ts
@@ -1,55 +1,48 @@
-import {
-    blob,
-    ic,
-    nat32,
-    nat64,
-    $query,
-    StableGrowResult,
-    Stable64GrowResult,
-    $update
-} from 'azle';
+import { blob, ic, nat32, nat64, query, update, Void } from 'azle';
 
-$query;
-export function stableSize(): nat32 {
-    return ic.stableSize();
-}
+export default class {
+    @query([], nat32)
+    stableSize(): nat32 {
+        return ic.stableSize();
+    }
 
-$query;
-export function stable64Size(): nat64 {
-    return ic.stable64Size();
-}
+    @query([], nat64)
+    stable64Size(): nat64 {
+        return ic.stable64Size();
+    }
 
-$update;
-export function stableGrow(newPages: nat32): StableGrowResult {
-    return ic.stableGrow(newPages);
-}
+    @update([nat32], nat32)
+    stableGrow(newPages: nat32): nat32 {
+        return ic.stableGrow(newPages);
+    }
 
-$update;
-export function stable64Grow(newPages: nat64): Stable64GrowResult {
-    return ic.stable64Grow(newPages);
-}
+    @update([nat64], nat64)
+    stable64Grow(newPages: nat64): nat64 {
+        return ic.stable64Grow(newPages);
+    }
 
-$update;
-export function stableWrite(offset: nat32, buf: blob): void {
-    ic.stableWrite(offset, buf);
-}
+    @update([nat32, blob], Void)
+    stableWrite(offset: nat32, buf: blob): Void {
+        ic.stableWrite(offset, buf);
+    }
 
-$update;
-export function stable64Write(offset: nat64, buf: blob): void {
-    ic.stable64Write(offset, buf);
-}
+    @update([nat64, blob], Void)
+    stable64Write(offset: nat64, buf: blob): Void {
+        ic.stable64Write(offset, buf);
+    }
 
-$query;
-export function stableRead(offset: nat32, length: nat32): blob {
-    return ic.stableRead(offset, length);
-}
+    @query([nat32, nat32], blob)
+    stableRead(offset: nat32, length: nat32): blob {
+        return ic.stableRead(offset, length);
+    }
 
-$query;
-export function stable64Read(offset: nat64, length: nat64): blob {
-    return ic.stable64Read(offset, length);
-}
+    @query([nat64, nat64], blob)
+    stable64Read(offset: nat64, length: nat64): blob {
+        return ic.stable64Read(offset, length);
+    }
 
-$query;
-export function stableBytes(): blob {
-    return ic.stableBytes();
+    @query([], blob)
+    stableBytes(): blob {
+        return ic.stableBytes();
+    }
 }
