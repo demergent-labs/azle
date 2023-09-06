b32ac5dfff0114346f239d388b50c900b6e97ad4
Dan Steren <dan@demergentlabs.org>
2023-09-06 16:07:44 -0600

    Update canister syntax

--- examples/ic_api/src/ic_api.ts
+++ examples/ic_api/src/ic_api.ts
@@ -3,162 +3,175 @@ import {
     empty,
     ic,
     int8,
-    Manual,
     nat,
     nat32,
     nat64,
     Opt,
     Principal,
-    $query,
-    $update
+    query,
+    update,
+    Service,
+    bool,
+    text,
+    principal,
+    Void
 } from 'azle';
+
+// TODO: Actually implement manual. See https://github.com/demergent-labs/azle/issues/1200
+type Manual<T> = Void;
+
+function manual(
+    _target: Service,
+    _name: string,
+    descriptor: PropertyDescriptor
+): any {
+    return descriptor;
+}
 
 // TODO: See https://github.com/demergent-labs/azle/issues/496
 // type ArgDataMultipleParamsResult = {
 //     blob: blob;
 //     int: int8;
-//     boolean: boolean;
-//     string: string;
+//     boolean: bool;
+//     string: text;
 // };
 
-// // returns the argument data as an array.
-// export function argDataZeroParams(): Vec<null> {
-//     return ic.argData();
-// }
-//
-// // returns the argument data as an array.
-// export function argDataOneParam(arg: boolean): boolean {
-//     const argData = ic.argData();
-//     return argData[0];
-// }
-//
-// // returns the argument data as an array.
-// export function argDataMultipleParams(
-//     arg1: blob,
-//     arg2: int8,
-//     arg3: boolean,
-//     arg4: string
-// ): ArgDataMultipleParamsResult {
-//     const argData = ic.argData();
-//     return {
-//         blob: Uint8Array.from(argData[0]),
-//         int: argData[1],
-//         boolean: argData[2],
-//         string: argData[3]
-//     };
-// }
+export default class extends Service {
+    // // returns the argument data as an array.
+    // argDataZeroParams(): Vec<null> {
+    //     return ic.argData();
+    // }
+    //
+    // // returns the argument data as an array.
+    // argDataOneParam(arg: bool): bool {
+    //     const argData = ic.argData();
+    //     return argData[0];
+    // }
+    //
+    // // returns the argument data as an array.
+    // argDataMultipleParams(
+    //     arg1: blob,
+    //     arg2: int8,
+    //     arg3: bool,
+    //     arg4: text
+    // ): ArgDataMultipleParamsResult {
+    //     const argData = ic.argData();
+    //     return {
+    //         blob: Uint8Array.from(argData[0]),
+    //         int: argData[1],
+    //         boolean: argData[2],
+    //         string: argData[3]
+    //     };
+    // }
 
-// returns the argument data as bytes.
-$query;
-export function argDataRaw(
-    arg1: blob,
-    arg2: int8,
-    arg3: boolean,
-    arg4: string
-): blob {
-    return ic.argDataRaw();
-}
+    // returns the argument data as bytes.
+    @query([blob, int8, bool, text], blob)
+    argDataRaw(arg1: blob, arg2: int8, arg3: bool, arg4: text): blob {
+        return ic.argDataRaw();
+    }
 
-// returns the length of the argument data in bytes
-$query;
-export function argDataRawSize(
-    arg1: blob,
-    arg2: int8,
-    arg3: boolean,
-    arg4: string
-): nat32 {
-    return ic.argDataRawSize();
-}
+    // returns the length of the argument data in bytes
+    @query([blob, int8, bool, text], nat32)
+    argDataRawSize(arg1: blob, arg2: int8, arg3: bool, arg4: text): nat32 {
+        return ic.argDataRawSize();
+    }
 
-// returns the principal of the identity that called this function
-$query;
-export function caller(): Principal {
-    return ic.caller();
-}
+    // returns the principal of the identity that called this function
+    @query([], principal)
+    caller(): Principal {
+        return ic.caller();
+    }
 
-// returns the amount of cycles available in the canister
-$query;
-export function canisterBalance(): nat64 {
-    return ic.canisterBalance();
-}
+    // returns the amount of cycles available in the canister
+    @query([], nat64)
+    canisterBalance(): nat64 {
+        return ic.canisterBalance();
+    }
 
-// returns the amount of cycles available in the canister
-$query;
-export function canisterBalance128(): nat {
-    return ic.canisterBalance128();
-}
+    // returns the amount of cycles available in the canister
+    @query([], nat)
+    canisterBalance128(): nat {
+        return ic.canisterBalance128();
+    }
 
-// returns the canister's version number
-$query;
-export function canisterVersion(): nat64 {
-    return ic.canisterVersion();
-}
+    // returns the canister's version number
+    @query([], nat64)
+    canisterVersion(): nat64 {
+        return ic.canisterVersion();
+    }
 
-// When called from a query call, returns the data certificate authenticating certified data set by this canister. Returns None if not called from a query call.
-$query;
-export function dataCertificate(): Opt<blob> {
-    return ic.dataCertificate();
-}
+    // // When called from a query call, returns the data certificate
+    // // authenticating certified data set by this canister. Returns None if not
+    // // called from a query call.
+    // @query([], Opt(blob))
+    // dataCertificate(): Opt<blob> {
+    //     return ic.dataCertificate();
+    // }
 
-// When called from a query call, returns the data certificate authenticating certified data set by this canister. Returns None if called not from a query call.
-$update;
-export function dataCertificateNull(): Opt<blob> {
-    return ic.dataCertificate();
-}
+    // // When called from a query call, returns the data certificate
+    // // authenticating certified data set by this canister. Returns None if
+    // // called not from a query call.
+    // @update([], Opt(blob))
+    // dataCertificateNull(): Opt<blob> {
+    //     return ic.dataCertificate();
+    // }
 
-// returns this canister's id
-$query;
-export function id(): Principal {
-    return ic.id();
-}
+    // returns this canister's id
+    @query([], principal)
+    id(): Principal {
+        return ic.id();
+    }
 
-// Returns the number of instructions that the canister executed since the last
-// entry point.
-$query;
-export function instructionCounter(): nat64 {
-    return ic.instructionCounter();
-}
+    // Returns the number of instructions that the canister executed since the last
+    // entry point.
+    @query([], nat64)
+    instructionCounter(): nat64 {
+        return ic.instructionCounter();
+    }
 
-// determines whether the given principal is a controller of the canister
-$query;
-export function isController(principal: Principal): boolean {
-    return ic.isController(principal);
-}
+    // determines whether the given principal is a controller of the canister
+    @query([principal], bool)
+    isController(principal: Principal): bool {
+        return ic.isController(principal);
+    }
 
-$query;
-export function performanceCounter(): nat64 {
-    return ic.performanceCounter(0);
-}
+    @query([], nat64)
+    performanceCounter(): nat64 {
+        return ic.performanceCounter(0);
+    }
 
-// prints a message through the local replica's output
-$query;
-export function print(message: string): boolean {
-    ic.print(message);
+    // prints a message through the local replica's output
+    @query([text], bool)
+    print(message: text): bool {
+        ic.print(message);
 
-    return true;
-}
+        return true;
+    }
 
-$query;
-export function reject(message: string): Manual<empty> {
-    ic.reject(message);
-}
+    // TODO: See https://github.com/demergent-labs/azle/issues/1200
+    @manual
+    @query([text], empty)
+    reject(message: text): Manual<empty> {
+        ic.reject(message);
+    }
 
-// sets up to 32 bytes of certified data
-$update;
-export function setCertifiedData(data: blob): void {
-    ic.setCertifiedData(data);
-}
+    // sets up to 32 bytes of certified data
+    @update([blob], Void)
+    setCertifiedData(data: blob): Void {
+        ic.setCertifiedData(data);
+    }
 
-// returns the current timestamp
-$query;
-export function time(): nat64 {
-    return ic.time();
-}
+    // returns the current timestamp
+    @query([], nat64)
+    time(): nat64 {
+        return ic.time();
+    }
 
-// traps with a message, stopping execution and discarding all state within the call
-$query;
-export function trap(message: string): boolean {
-    ic.trap(message);
+    // traps with a message, stopping execution and discarding all state within the call
+    @query([], bool)
+    trap(message: text): bool {
+        ic.trap(message);
 
-    return true;
+        return true;
+    }
 }
