006bb95c7921ddd0e20aabff1b372ea47472513b
Benjamin DeMann <benjamin.demann@gmail.com>
2023-09-07 12:43:30 -0600

    list of list updated to new syntax

--- examples/list_of_lists/src/index.ts
+++ examples/list_of_lists/src/index.ts
@@ -1,10 +1,11 @@
 import {
     blob,
+    bool,
+    candid,
     empty,
     float32,
     float64,
-    Func,
-    Query,
+    func,
     int,
     int16,
     int32,
@@ -15,210 +16,209 @@ import {
     nat32,
     nat64,
     nat8,
+    Null,
     Opt,
+    principal,
     Principal,
-    $query,
+    query,
     Record,
     reserved,
+    Service,
+    text,
     Variant,
     Vec
 } from 'azle';
 
-type Person = Record<{
-    name: string;
+class Person extends Record {
+    @candid(text)
+    name: text;
+
+    @candid(nat8)
     age: nat8;
-}>;
-
-type State = Variant<{
-    solid: null;
-    liquid: null;
-    gas: null;
-}>;
-
-type BasicFunc = Func<Query<(param1: string) => string>>;
-
-$query;
-export function listOfStringOne(param: Vec<string>): Vec<string> {
-    return param;
 }
 
-$query;
-export function listOfStringTwo(params: Vec<Vec<string>>): Vec<Vec<string>> {
-    return params;
+class State extends Variant {
+    @candid(Null)
+    solid: Null;
+
+    @candid(Null)
+    liquid: Null;
+
+    @candid(Null)
+    gas: Null;
 }
 
-$query;
-export function listOfStringFour(
-    params: Vec<Vec<Vec<Vec<string>>>>
-): Vec<Vec<Vec<Vec<string>>>> {
-    return params;
-}
+@func([text], text, 'query')
+class BasicFunc {}
 
-$query;
-export function listOfListOfInt8(): Vec<Vec<Vec<Vec<Vec<Vec<Vec<int8>>>>>>> {
-    return [
-        [
+export default class extends Service {
+    @query([Vec(text)], Vec(text))
+    listOfStringOne(param: Vec<text>): Vec<text> {
+        return param;
+    }
+
+    @query([Vec(Vec(text))], Vec(Vec(text)))
+    listOfStringTwo(params: Vec<Vec<text>>): Vec<Vec<text>> {
+        return params;
+    }
+
+    @query([Vec(Vec(Vec(Vec(text))))], Vec(Vec(Vec(Vec(text)))))
+    listOfStringFour(
+        params: Vec<Vec<Vec<Vec<text>>>>
+    ): Vec<Vec<Vec<Vec<text>>>> {
+        return params;
+    }
+
+    @query([], Vec(Vec(Vec(Vec(Vec(Vec(Vec(int8))))))))
+    listOfListOfInt8(): Vec<Vec<Vec<Vec<Vec<Vec<Vec<int8>>>>>>> {
+        return [
             [
                 [
                     [
-                        [[1], [2]],
                         [
-                            [1, 2, 3],
-                            [4, 5, 6]
+                            [[1], [2]],
+                            [
+                                [1, 2, 3],
+                                [4, 5, 6]
+                            ]
                         ]
-                    ]
-                ],
-                [[[[1]]], [[[2]]]],
-                [[[[3]]]]
+                    ],
+                    [[[[1]]], [[[2]]]],
+                    [[[[3]]]]
+                ]
+            ],
+            [
+                [[[[[1]]]], [[[[2]]]]],
+                [[[[[3]]]], [[[[4]]]]]
             ]
-        ],
-        [
-            [[[[[1]]]], [[[[2]]]]],
-            [[[[[3]]]], [[[[4]]]]]
-        ]
-    ];
-}
+        ];
+    }
 
-$query;
-export function listOfNull(param: Vec<Vec<Vec<null>>>): Vec<Vec<Vec<null>>> {
-    return param;
-}
+    @query([Vec(Vec(Vec(Null)))], Vec(Vec(Vec(Null))))
+    listOfNull(param: Vec<Vec<Vec<null>>>): Vec<Vec<Vec<null>>> {
+        return param;
+    }
 
-$query;
-export function listOfBool(
-    param: Vec<Vec<Vec<boolean>>>
-): Vec<Vec<Vec<boolean>>> {
-    return param;
-}
+    @query([Vec(Vec(Vec(bool)))], Vec(Vec(Vec(bool))))
+    listOfBool(param: Vec<Vec<Vec<bool>>>): Vec<Vec<Vec<bool>>> {
+        return param;
+    }
 
-$query;
-export function listOfString(
-    param: Vec<Vec<Vec<string>>>
-): Vec<Vec<Vec<string>>> {
-    return param;
-}
+    @query([Vec(Vec(Vec(text)))], Vec(Vec(Vec(text))))
+    listOfString(param: Vec<Vec<Vec<text>>>): Vec<Vec<Vec<text>>> {
+        return param;
+    }
 
-$query;
-export function listOfOptionString(
-    param: Vec<Vec<Vec<Opt<string>>>>
-): Vec<Vec<Vec<Opt<string>>>> {
-    return param;
-}
+    @query([Vec(Vec(Vec(Opt(text))))], Vec(Vec(Vec(Opt(text)))))
+    listOfOptionString(
+        param: Vec<Vec<Vec<Opt<text>>>>
+    ): Vec<Vec<Vec<Opt<text>>>> {
+        return param;
+    }
 
-$query;
-export function listOfEmpty(): Vec<Vec<Vec<empty>>> {
-    throw new Error('Anything you want');
-}
+    @query([], Vec(Vec(Vec(empty))))
+    listOfEmpty(): Vec<Vec<Vec<empty>>> {
+        throw new Error('Anything you want');
+    }
 
-$query;
-export function listOfReserved(): Vec<Vec<Vec<reserved>>> {
-    return [
-        [['A'], ['n']],
-        [
-            ['y', 't', 'h'],
-            ['i', 'n', 'g']
-        ]
-    ];
-}
+    @query([], Vec(Vec(Vec(reserved))))
+    listOfReserved(): Vec<Vec<Vec<reserved>>> {
+        return [
+            [['A'], ['n']],
+            [
+                ['y', 't', 'h'],
+                ['i', 'n', 'g']
+            ]
+        ];
+    }
 
-$query;
-export function listOfFunc(
-    param: Vec<Vec<Vec<BasicFunc>>>
-): Vec<Vec<Vec<BasicFunc>>> {
-    return param;
-}
+    @query([Vec(Vec(Vec(BasicFunc)))], Vec(Vec(Vec(BasicFunc))))
+    listOfFunc(param: Vec<Vec<Vec<BasicFunc>>>): Vec<Vec<Vec<BasicFunc>>> {
+        return param;
+    }
 
-$query;
-export function listOfPrincipal(
-    param: Vec<Vec<Vec<Principal>>>
-): Vec<Vec<Vec<Principal>>> {
-    return param;
-}
+    @query([Vec(Vec(Vec(principal)))], Vec(Vec(Vec(principal))))
+    listOfPrincipal(param: Vec<Vec<Vec<Principal>>>): Vec<Vec<Vec<Principal>>> {
+        return param;
+    }
 
-$query;
-export function listOfF64(
-    param: Vec<Vec<Vec<float64>>>
-): Vec<Vec<Vec<float64>>> {
-    return param;
-}
+    @query([Vec(Vec(Vec(float64)))], Vec(Vec(Vec(float64))))
+    listOfF64(param: Vec<Vec<Vec<float64>>>): Vec<Vec<Vec<float64>>> {
+        return param;
+    }
 
-$query;
-export function listOfF32(
-    param: Vec<Vec<Vec<float32>>>
-): Vec<Vec<Vec<float32>>> {
-    return param;
-}
+    @query([Vec(Vec(Vec(float32)))], Vec(Vec(Vec(float32))))
+    listOfF32(param: Vec<Vec<Vec<float32>>>): Vec<Vec<Vec<float32>>> {
+        return param;
+    }
 
-$query;
-export function listOfInt(param: Vec<Vec<Vec<int>>>): Vec<Vec<Vec<int>>> {
-    return param;
-}
+    @query([Vec(Vec(Vec(int)))], Vec(Vec(Vec(int))))
+    listOfInt(param: Vec<Vec<Vec<int>>>): Vec<Vec<Vec<int>>> {
+        return param;
+    }
 
-$query;
-export function listOfInt64(param: Vec<Vec<Vec<int64>>>): Vec<Vec<Vec<int64>>> {
-    return param;
-}
+    @query([Vec(Vec(Vec(int64)))], Vec(Vec(Vec(int64))))
+    listOfInt64(param: Vec<Vec<Vec<int64>>>): Vec<Vec<Vec<int64>>> {
+        return param;
+    }
 
-$query;
-export function listOfInt32(param: Vec<Vec<Vec<int32>>>): Vec<Vec<Vec<int32>>> {
-    return param;
-}
+    @query([Vec(Vec(Vec(int32)))], Vec(Vec(Vec(int32))))
+    listOfInt32(param: Vec<Vec<Vec<int32>>>): Vec<Vec<Vec<int32>>> {
+        return param;
+    }
 
-$query;
-export function listOfInt16(param: Vec<Vec<Vec<int16>>>): Vec<Vec<Vec<int16>>> {
-    return param;
-}
+    @query([Vec(Vec(Vec(int16)))], Vec(Vec(Vec(int16))))
+    listOfInt16(param: Vec<Vec<Vec<int16>>>): Vec<Vec<Vec<int16>>> {
+        return param;
+    }
 
-$query;
-export function listOfInt8(param: Vec<Vec<Vec<int8>>>): Vec<Vec<Vec<int8>>> {
-    return param;
-}
+    @query([Vec(Vec(Vec(int8)))], Vec(Vec(Vec(int8))))
+    listOfInt8(param: Vec<Vec<Vec<int8>>>): Vec<Vec<Vec<int8>>> {
+        return param;
+    }
 
-$query;
-export function listOfNat(param: Vec<Vec<Vec<nat>>>): Vec<Vec<Vec<nat>>> {
-    return param;
-}
+    @query([Vec(Vec(Vec(nat)))], Vec(Vec(Vec(nat))))
+    listOfNat(param: Vec<Vec<Vec<nat>>>): Vec<Vec<Vec<nat>>> {
+        return param;
+    }
 
-$query;
-export function listOfNat64(param: Vec<Vec<Vec<nat64>>>): Vec<Vec<Vec<nat64>>> {
-    return param;
-}
+    @query([Vec(Vec(Vec(nat64)))], Vec(Vec(Vec(nat64))))
+    listOfNat64(param: Vec<Vec<Vec<nat64>>>): Vec<Vec<Vec<nat64>>> {
+        return param;
+    }
 
-$query;
-export function listOfNat32(param: Vec<Vec<Vec<nat32>>>): Vec<Vec<Vec<nat32>>> {
-    return param;
-}
+    @query([Vec(Vec(Vec(nat32)))], Vec(Vec(Vec(nat32))))
+    listOfNat32(param: Vec<Vec<Vec<nat32>>>): Vec<Vec<Vec<nat32>>> {
+        return param;
+    }
 
-$query;
-export function listOfNat16(param: Vec<Vec<Vec<nat16>>>): Vec<Vec<Vec<nat16>>> {
-    return param;
-}
+    @query([Vec(Vec(Vec(nat16)))], Vec(Vec(Vec(nat16))))
+    listOfNat16(param: Vec<Vec<Vec<nat16>>>): Vec<Vec<Vec<nat16>>> {
+        return param;
+    }
 
-$query;
-export function listOfNat8(param: Vec<Vec<Vec<nat8>>>): Vec<Vec<Vec<nat8>>> {
-    return param;
-}
+    @query([Vec(Vec(Vec(nat8)))], Vec(Vec(Vec(nat8))))
+    listOfNat8(param: Vec<Vec<Vec<nat8>>>): Vec<Vec<Vec<nat8>>> {
+        return param;
+    }
 
-$query;
-export function listOfRecord(
-    param: Vec<Vec<Vec<Person>>>
-): Vec<Vec<Vec<Person>>> {
-    return param;
-}
+    @query([Vec(Vec(Vec(Person)))], Vec(Vec(Vec(Person))))
+    listOfRecord(param: Vec<Vec<Vec<Person>>>): Vec<Vec<Vec<Person>>> {
+        return param;
+    }
 
-$query;
-export function listOfVariant(
-    param: Vec<Vec<Vec<State>>>
-): Vec<Vec<Vec<State>>> {
-    return param;
-}
+    @query([Vec(Vec(Vec(State)))], Vec(Vec(Vec(State))))
+    listOfVariant(param: Vec<Vec<Vec<State>>>): Vec<Vec<Vec<State>>> {
+        return param;
+    }
 
-$query;
-export function listOfBlob(param: Vec<blob>): Vec<blob> {
-    return param;
-}
+    @query([Vec(blob)], Vec(blob))
+    listOfBlob(param: Vec<blob>): Vec<blob> {
+        return param;
+    }
 
-$query;
-export function listOfListOfBlob(param: Vec<Vec<blob>>): Vec<Vec<blob>> {
-    return param;
+    @query([Vec(Vec(blob))], Vec(Vec(blob)))
+    listOfListOfBlob(param: Vec<Vec<blob>>): Vec<Vec<blob>> {
+        return param;
+    }
 }
