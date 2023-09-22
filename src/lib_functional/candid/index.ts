750563958e11a994d6c8cff9dedc1512804ffa3c
Jordan Last <jordan.michael.last@gmail.com>
2023-09-22 16:11:04 -0500

    primitive_types example tests passing

--- src/lib_functional/candid/index.ts
+++ src/lib_functional/candid/index.ts
@@ -5,35 +5,84 @@ import {
     blob,
     AzleVec,
     AzleOpt,
+    AzleInt,
+    AzleInt64,
+    AzleInt32,
+    AzleInt16,
     AzleInt8,
-    int8,
+    AzleNat,
     AzleNat64,
     AzleNat32,
+    AzleNat16,
+    AzleNat8,
     AzleFloat64,
+    AzleFloat32,
+    nat,
+    nat64,
+    nat32,
+    nat16,
+    nat8,
+    int,
+    int64,
+    int32,
+    int16,
+    int8,
     float64,
-    Principal
+    float32,
+    Principal,
+    AzleNull,
+    Null,
+    AzleReserved,
+    reserved,
+    AzleEmpty,
+    empty,
+    AzleBool,
+    bool,
+    AzlePrincipal
 } from '../../lib_new';
 
 export type TypeMapping<T> = T extends IDL.TextClass
     ? string
-    : T extends IDL.BoolClass
-    ? boolean
+    : T extends AzleBool
+    ? bool
+    : T extends AzleInt
+    ? int
+    : T extends AzleInt64
+    ? int64
+    : T extends AzleInt32
+    ? int32
+    : T extends AzleInt16
+    ? int16
+    : T extends AzleInt8
+    ? int8
+    : T extends AzleNat
+    ? nat
     : T extends AzleNat64
-    ? bigint
+    ? nat64
+    : T extends AzleNat32
+    ? nat32
+    : T extends AzleNat16
+    ? nat16
+    : T extends AzleNat8
+    ? nat8
+    : T extends AzleFloat64
+    ? float64
+    : T extends AzleFloat32
+    ? float32
     : T extends never[]
     ? void
     : T extends AzleVec<infer U>
     ? TypeMapping<U>[]
     : T extends AzleOpt<infer U>
     ? [TypeMapping<U>] | []
-    : T extends AzleNat32
-    ? number
     : T extends AzleBlob
     ? blob
-    : T extends AzleInt8
-    ? int8
-    : T extends AzleFloat64
-    ? float64
-    : T extends IDL.PrincipalClass
+    : T extends AzlePrincipal
     ? Principal
+    : T extends AzleNull
+    ? Null
+    : T extends AzleReserved
+    ? reserved
+    : T extends AzleEmpty
+    ? empty
     : T;
--- src/lib_new/primitives.ts
+++ src/lib_new/primitives.ts
@@ -1,5 +1,12 @@
 import { IDL } from './index';
-import { CandidClass, Parent, toCandidClass } from './utils';
+import { Parent, toCandidClass } from './utils';
+
+export class AzleNat {
+    _kind: 'AzleNat' = 'AzleNat';
+    static getIDL() {
+        return IDL.Nat;
+    }
+}
 
 export class AzleNat64 {
     _kind: 'AzleNat64' = 'AzleNat64';
@@ -12,6 +19,48 @@ export class AzleNat32 {
     _kind: 'AzleNat32' = 'AzleNat32';
     static getIDL() {
         return IDL.Nat32;
+    }
+}
+
+export class AzleNat16 {
+    _kind: 'AzleNat16' = 'AzleNat16';
+    static getIDL() {
+        return IDL.Nat16;
+    }
+}
+
+export class AzleNat8 {
+    _kind: 'AzleNat8' = 'AzleNat8';
+    static getIDL() {
+        return IDL.Nat8;
+    }
+}
+
+export class AzleInt {
+    _kind: 'AzleInt' = 'AzleInt';
+    static getIDL() {
+        return IDL.Int;
+    }
+}
+
+export class AzleInt64 {
+    _kind: 'AzleInt64' = 'AzleInt64';
+    static getIDL() {
+        return IDL.Int64;
+    }
+}
+
+export class AzleInt32 {
+    _kind: 'AzleInt32' = 'AzleInt32';
+    static getIDL() {
+        return IDL.Int32;
+    }
+}
+
+export class AzleInt16 {
+    _kind: 'AzleInt16' = 'AzleInt16';
+    static getIDL() {
+        return IDL.Int16;
     }
 }
 
@@ -29,6 +78,13 @@ export class AzleFloat64 {
     }
 }
 
+export class AzleFloat32 {
+    _kind: 'AzleFloat32' = 'AzleFloat32';
+    static getIDL() {
+        return IDL.Float32;
+    }
+}
+
 export class AzleBlob {
     _kind: 'AzleBlob' = 'AzleBlob';
     static getIDL() {
@@ -36,43 +92,78 @@ export class AzleBlob {
     }
 }
 
-export const bool = IDL.Bool;
+export class AzleNull {
+    _kind: 'AzleNull' = 'AzleNull';
+    static getIDL() {
+        return IDL.Null;
+    }
+}
+
+export class AzleReserved {
+    _kind: 'AzleReserved' = 'AzleReserved';
+    static getIDL() {
+        return IDL.Reserved;
+    }
+}
+
+export class AzleEmpty {
+    _kind: 'AzleEmpty' = 'AzleEmpty';
+    static getIDL() {
+        return IDL.Empty;
+    }
+}
+
+export class AzleBool {
+    _kind: 'AzleBool' = 'AzleBool';
+    static getIDL() {
+        return IDL.Bool;
+    }
+}
+
+export class AzlePrincipal {
+    _kind: 'AzlePrincipal' = 'AzlePrincipal';
+    static getIDL() {
+        return IDL.Principal;
+    }
+}
+
+export const bool: AzleBool = AzleBool as any;
 export type bool = boolean;
 export const blob: AzleBlob = AzleBlob as any;
 export type blob = Uint8Array;
-export const empty = IDL.Empty;
+export const empty: AzleEmpty = AzleEmpty as any;
 export type empty = never;
-export const int = IDL.Int;
+export const int: AzleInt = AzleInt as any;
 export type int = bigint;
 export const int8: AzleInt8 = AzleInt8 as any;
 export type int8 = number;
-export const int16 = IDL.Int16;
+export const int16: AzleInt16 = AzleInt16 as any;
 export type int16 = number;
-export const int32 = IDL.Int32;
+export const int32: AzleInt32 = AzleInt32 as any;
 export type int32 = number;
-export const int64 = IDL.Int64;
+export const int64: AzleInt64 = AzleInt64 as any;
 export type int64 = bigint;
-export const nat = IDL.Nat;
+export const nat: AzleNat = AzleNat as any;
 export type nat = bigint;
-export const nat8 = IDL.Nat8;
+export const nat8: AzleNat8 = AzleNat8 as any;
 export type nat8 = number;
-export const nat16 = IDL.Nat16;
+export const nat16: AzleNat16 = AzleNat16 as any;
 export type nat16 = number;
 export const nat32: AzleNat32 = AzleNat32 as any;
 export type nat32 = number;
 export const nat64: AzleNat64 = AzleNat64 as any;
 export type nat64 = bigint;
-export const Null = IDL.Null;
+export const Null: AzleNull = AzleNull as any;
 export type Null = null;
-export const reserved = IDL.Reserved;
+export const reserved: AzleReserved = AzleReserved as any;
 export type reserved = any;
 export const text = IDL.Text;
 export type text = string;
-export const float32 = IDL.Float32;
+export const float32: AzleFloat32 = AzleFloat32 as any;
 export type float32 = number;
 export const float64: AzleFloat64 = AzleFloat64 as any;
 export type float64 = number;
-export const principal = IDL.Principal;
+export const principal: AzlePrincipal = AzlePrincipal as any;
 export { Principal } from '@dfinity/principal';
 export type Vec<T> = T[];
 export type Tuple<T> = T;
