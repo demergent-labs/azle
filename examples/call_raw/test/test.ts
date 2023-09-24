efc82c155bd8221599b567d5048761c6ca0c261d
Jordan Last <jordan.michael.last@gmail.com>
2023-09-23 19:16:42 -0500

    refactor call_raw

--- examples/call_raw/test/test.ts
+++ examples/call_raw/test/test.ts
@@ -2,10 +2,10 @@ import { getCanisterId, runTests } from 'azle/test';
 import { createActor } from '../test/dfx_generated/call_raw';
 import { get_tests } from './tests';
 
-const call_raw_canister = createActor(getCanisterId('call_raw'), {
+const callRawCanister = createActor(getCanisterId('call_raw'), {
     agentOptions: {
         host: 'http://127.0.0.1:8000'
     }
 });
 
-runTests(get_tests(call_raw_canister));
+runTests(get_tests(callRawCanister));
