f2c965cdd582768c69e2a2f2de71a1bf33222b01
Jordan Last <jordan.michael.last@gmail.com>
2023-09-23 20:18:22 -0500

    candid encoding example refactored

--- examples/candid_encoding/src/index.ts
+++ examples/candid_encoding/src/index.ts
@@ -1,15 +1,12 @@
 import { blob, ic, query, Service, text } from 'azle';
 
-export default class extends Service {
+export default Service({
     // encodes a Candid string to Candid bytes
-    @query([text], blob)
-    candidEncode(candidString: text): blob {
+    candidEncode: query([text], blob, (candidString) => {
         return ic.candidEncode(candidString);
-    }
-
+    }),
     // decodes Candid bytes to a Candid string
-    @query([blob], text)
-    candidDecode(candidEncoded: blob): text {
+    candidDecode: query([blob], text, (candidEncoded) => {
         return ic.candidDecode(candidEncoded);
-    }
-}
+    })
+});
