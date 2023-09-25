4d300d1b506096b8d73fcbf6f4186e8ea31f713c
Jordan Last <jordan.michael.last@gmail.com>
2023-09-25 14:10:08 -0500

    ledger_canister complete

--- examples/ledger_canister/canisters/ledger_canister/index.ts
+++ examples/ledger_canister/canisters/ledger_canister/index.ts
@@ -1,5 +1,6 @@
 import {
     ic,
+    init,
     nat32,
     nat64,
     None,
@@ -25,95 +26,82 @@ import {
     TransferResult
 } from 'azle/canisters/ledger';
 
-export default class extends Service {
-    icpCanister = new Ledger(
-        Principal.fromText(
-            process.env.ICP_CANISTER_PRINCIPAL ??
-                ic.trap('process.env.ICP_CANISTER_PRINCIPAL is undefined')
-        )
-    );
+let icpCanister: typeof Ledger;
 
-    @update([Address, nat64, nat64, Opt(nat64)], TransferResult)
-    async executeTransfer(
-        to: Address,
-        amount: nat64,
-        fee: nat64,
-        createdAtTime: Opt<nat64>
-    ): Promise<TransferResult> {
-        return await ic.call(this.icpCanister.transfer, {
-            args: [
-                {
-                    memo: 0n,
-                    amount: {
-                        e8s: amount
-                    },
-                    fee: {
-                        e8s: fee
-                    },
-                    from_subaccount: None,
-                    to: binaryAddressFromAddress(to),
-                    created_at_time:
-                        createdAtTime.length === 1
-                            ? Some({ timestamp_nanos: createdAtTime[0] })
-                            : None
-                }
-            ]
-        });
-    }
-
-    @update([Address], Tokens)
-    async getAccountBalance(address: Address): Promise<Tokens> {
-        return await ic.call(this.icpCanister.account_balance, {
+export default Service({
+    init: init([], () => {
+        icpCanister = Ledger(
+            Principal.fromText(
+                process.env.ICP_CANISTER_PRINCIPAL ??
+                    ic.trap('process.env.ICP_CANISTER_PRINCIPAL is undefined')
+            )
+        );
+    }),
+    executeTransfer: update(
+        [Address, nat64, nat64, Opt(nat64)],
+        TransferResult,
+        async (to, amount, fee, createdAtTime) => {
+            return await ic.call(icpCanister.transfer, {
+                args: [
+                    {
+                        memo: 0n,
+                        amount: {
+                            e8s: amount
+                        },
+                        fee: {
+                            e8s: fee
+                        },
+                        from_subaccount: None,
+                        to: binaryAddressFromAddress(to),
+                        created_at_time:
+                            createdAtTime.length === 1
+                                ? Some({ timestamp_nanos: createdAtTime[0] })
+                                : None
+                    }
+                ]
+            });
+        }
+    ),
+    getAccountBalance: update([Address], Tokens, async (address) => {
+        return await ic.call(icpCanister.account_balance, {
             args: [
                 {
                     account: binaryAddressFromAddress(address)
                 }
             ]
         });
-    }
-
-    @update([], TransferFee)
-    async getTransferFee(): Promise<TransferFee> {
-        return await ic.call(this.icpCanister.transfer_fee, { args: [{}] });
-    }
-
-    @update([GetBlocksArgs], QueryBlocksResponse)
-    async getBlocks(
-        getBlocksArgs: GetBlocksArgs
-    ): Promise<QueryBlocksResponse> {
-        return await ic.call(this.icpCanister.query_blocks, {
-            args: [getBlocksArgs]
-        });
-    }
-
-    @update([], text)
-    async getSymbol(): Promise<text> {
-        const symbolResult = await ic.call(this.icpCanister.symbol, {});
+    }),
+    getTransferFee: update([], TransferFee, async () => {
+        return await ic.call(icpCanister.transfer_fee, { args: [{}] });
+    }),
+    getBlocks: update(
+        [GetBlocksArgs],
+        QueryBlocksResponse,
+        async (getBlocksArgs) => {
+            return await ic.call(icpCanister.query_blocks, {
+                args: [getBlocksArgs]
+            });
+        }
+    ),
+    getSymbol: update([], text, async () => {
+        const symbolResult = await ic.call(icpCanister.symbol, {});
 
         return symbolResult.symbol;
-    }
-
-    @update([], text)
-    async getName(): Promise<text> {
-        const nameResult = await ic.call(this.icpCanister.name, {});
+    }),
+    getName: update([], text, async () => {
+        const nameResult = await ic.call(icpCanister.name, {});
 
         return nameResult.name;
-    }
-
-    @update([], nat32)
-    async getDecimals(): Promise<nat32> {
-        const decimalsResult = await ic.call(this.icpCanister.decimals, {});
+    }),
+    getDecimals: update([], nat32, async () => {
+        const decimalsResult = await ic.call(icpCanister.decimals, {});
 
         return decimalsResult.decimals;
-    }
-
-    @update([], Archives)
-    async getArchives(): Promise<Archives> {
-        return await ic.call(this.icpCanister.archives, {});
-    }
-
-    @query([principal], text)
-    getAddressFromPrincipal(principal: Principal): string {
+    }),
+    getArchives: update([], Archives, async () => {
+        return await ic.call(icpCanister.archives, {});
+    }),
+    getAddressFromPrincipal: query([principal], text, (principal) => {
         return hexAddressFromPrincipal(principal, 0);
-    }
-}
+    })
+});
