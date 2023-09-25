714d4abb8ec15f473e4cc3e5686e6f3bba811ccd
Dan Steren <dan@demergentlabs.org>
2023-09-25 11:51:13 -0600

    Update quicksort to functional runtime syntax

--- examples/motoko_examples/quicksort/src/index.ts
+++ examples/motoko_examples/quicksort/src/index.ts
@@ -2,9 +2,8 @@ import { int, Vec, query, Service } from 'azle';
 import { Int } from './comparison';
 import * as Quicksort from './quicksort';
 
-export default class extends Service {
-    @query([Vec(int)], Vec(int))
-    sort(xs: Vec<int>): Vec<int> {
+export default Service({
+    sort: query([Vec(int)], Vec(int), (xs) => {
         return Quicksort.sortBy(xs, Int.compare);
-    }
-}
+    })
+});
