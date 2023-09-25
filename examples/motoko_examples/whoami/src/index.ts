7c6b4b22f8c548626d43d42c1b4ec24fe7e02ada
Dan Steren <dan@demergentlabs.org>
2023-09-25 12:56:37 -0600

    Update whoami to functional runtime syntax

--- examples/motoko_examples/whoami/src/index.ts
+++ examples/motoko_examples/whoami/src/index.ts
@@ -6,64 +6,60 @@ import {
     Principal,
     query,
     Service,
-    update,
-    Void
+    update
 } from 'azle';
 
-export default class extends Service {
-    // Initialize the variables to ensure that they aren't `undefined`.
-    // We use the zero principal but any principal could be used.
-    install: Principal = Principal.fromText('aaaaa-aa');
-    someone: Principal = Principal.fromText('aaaaa-aa');
+// We use the zero principal but any principal could be used.
+let install: Principal = Principal.fromText('aaaaa-aa');
+let someone: Principal = Principal.fromText('aaaaa-aa');
 
+const whoami = update([], principal, () => {
+    return ic.caller();
+});
+
+export default Service({
     // Manually save the calling principal and argument for later access.
-    @init([principal], Void)
-    init(somebody: Principal): Void {
-        this.install = ic.caller();
-        this.someone = somebody;
-    }
+    init: init([principal], (somebody) => {
+        install = ic.caller();
+        someone = somebody;
+    }),
 
     // Manually re-save these variables after new deploys.
-    @postUpgrade([principal], Void)
-    postUpgrade(somebody: Principal): Void {
-        this.install = ic.caller();
-        this.someone = somebody;
-    }
+    postUpgrade: postUpgrade([principal], (somebody) => {
+        install = ic.caller();
+        someone = somebody;
+    }),
 
     // Return the principal identifier of the wallet canister that installed this
     // canister.
-    @query([], principal)
-    installer(): Principal {
-        return this.install;
-    }
+    installer: query([], principal, () => {
+        return install;
+    }),
 
     // Return the principal identifier that was provided as an installation
     // argument to this canister.
-    @query([], principal)
-    argument(): Principal {
-        return this.someone;
-    }
+    argument: query([], principal, () => {
+        return someone;
+    }),
 
     // Return the principal identifier of the caller of this method.
-    @update([], principal)
-    whoami(): Principal {
-        return ic.caller();
-    }
+    whoami,
 
     // Return the principal identifier of this canister.
-    @update([], principal)
-    async id(): Promise<Principal> {
-        return await ic.call(this.whoami, {
-            args: []
-        });
-    }
+    id: update([], principal, async () => {
+        // TODO This is not an ideal solution but will work for now
+        const self = Service({
+            whoami
+        })(ic.id());
+
+        return await ic.call(self.whoami);
+    }),
 
     // Return the principal identifier of this canister via the global `ic` object.
     // This is much quicker than `id()` above because it isn't making a cross-
     // canister call to itself. Additionally, it can now be a `Query` which means it
     // doesn't have to go through consensus.
-    @query([], principal)
-    idQuick(): Principal {
+    idQuick: query([], principal, () => {
         return ic.id();
-    }
-}
+    })
+});
