6f998afa1ddf43cc0c73737083675bdda232cb47
Jordan Last <jordan.michael.last@gmail.com>
2023-09-25 09:06:15 -0500

    imports refactored

--- examples/imports/test/test.ts
+++ examples/imports/test/test.ts
@@ -2,10 +2,10 @@ import { getCanisterId, runTests } from 'azle/test';
 import { createActor } from '../test/dfx_generated/imports';
 import { getTests } from './tests';
 
-const imports_canister = createActor(getCanisterId('imports'), {
+const importsCanister = createActor(getCanisterId('imports'), {
     agentOptions: {
         host: 'http://127.0.0.1:8000'
     }
 });
 
-runTests(getTests(imports_canister));
+runTests(getTests(importsCanister));
