120dcbddcdb807426cbff999259a82ad4727baf6
Jordan Last <jordan.michael.last@gmail.com>
2023-09-29 17:18:06 -0500

    fixed reply for void

--- src/lib_new/ic.ts
+++ src/lib_new/ic.ts
@@ -728,12 +728,13 @@ export const ic: Ic = globalThis._azleIc
                       );
               }
           },
-          reply: (data: any, type: CandidClass): void => {
-              if (Array.isArray(type) && type.length === 0) {
+          reply: (data: any, type: CandidType): void => {
+              if (type.name === 'AzleVoid') {
                   // return type is void
                   const bytes = new Uint8Array(IDL.encode([], [])).buffer;
                   return globalThis._azleIc.replyRaw(bytes);
               }
+
               const idlType = toIDLType(type, []);
 
               const encodeReadyResult = idlType.accept(new EncodeVisitor(), {
