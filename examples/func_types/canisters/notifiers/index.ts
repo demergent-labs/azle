fdc695a015fb95c76dacff0bb4132ef539c28ce0
Benjamin DeMann <benjamin.demann@gmail.com>
2023-09-29 14:11:57 -0600

    update func_types example

--- examples/func_types/canisters/notifiers/index.ts
+++ examples/func_types/canisters/notifiers/index.ts
@@ -1,17 +1,15 @@
-import { ic, Principal, query, blob, Func, Service, Void, func } from 'azle';
+import { Canister, ic, Principal, query, blob, Func, Void } from 'azle';
 
-@func([blob], Void, 'oneway')
-export class NotifierFunc extends Func {}
+export const NotifierFunc = Func([blob], Void, 'oneway');
 
-export default class extends Service {
-    @query([], NotifierFunc)
-    getNotifier(): NotifierFunc {
-        return new NotifierFunc(
+export default Canister({
+    getNotifier: query([], NotifierFunc, () => {
+        return [
             Principal.fromText(
                 process.env.NOTIFIERS_PRINCIPAL ??
                     ic.trap('process.env.NOTIFIERS_PRINCIPAL is undefined')
             ),
             'notify'
-        );
-    }
-}
+        ];
+    })
+});
