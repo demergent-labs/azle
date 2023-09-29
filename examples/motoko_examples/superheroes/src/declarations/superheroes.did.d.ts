d15fabe97342842c283c04595b392d3b2cdf77ee
Benjamin DeMann <benjamin.demann@gmail.com>
2023-09-29 15:25:46 -0600

    change superheros to preferred recursive syntax

--- examples/motoko_examples/superheroes/src/declarations/superheroes.did.d.ts
+++ examples/motoko_examples/superheroes/src/declarations/superheroes.did.d.ts
@@ -1,14 +1,18 @@
 import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 
-export type List = [string, [] | [List]];
-export interface Superhero {
-    superpowers: [] | [List];
-    name: string;
-}
+export type rec_28 = [string, [] | [rec_28]];
+export type rec_33 = [string, [] | [rec_33]];
+export type rec_36 = [string, [] | [rec_36]];
 export interface _SERVICE {
-    create: ActorMethod<[Superhero], number>;
-    delete_hero: ActorMethod<[number], boolean>;
-    read: ActorMethod<[number], [] | [Superhero]>;
-    update: ActorMethod<[number, Superhero], boolean>;
+    create: ActorMethod<[{ superpowers: [] | [rec_28]; name: string }], number>;
+    deleteHero: ActorMethod<[number], boolean>;
+    read: ActorMethod<
+        [number],
+        [] | [{ superpowers: [] | [rec_33]; name: string }]
+    >;
+    update: ActorMethod<
+        [number, { superpowers: [] | [rec_36]; name: string }],
+        boolean
+    >;
 }
