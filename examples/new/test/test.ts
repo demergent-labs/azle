5187d1293ea98e0b534fc476bab0123de84eac45
Jordan Last <jordan.michael.last@gmail.com>
2023-10-11 18:10:05 -0500

    add test for the azle new command

--- examples/new/test/test.ts
+++ examples/new/test/test.ts
@@ -1,11 +1,4 @@
-import { getCanisterId, runTests } from 'azle/test';
-import { createActor } from './dfx_generated/hello_world';
+import { runTests } from '../../../test'; // We don't want to install Azle
 import { getTests } from './tests';
 
-const helloWorldCanister = createActor(getCanisterId('hello_world'), {
-    agentOptions: {
-        host: 'http://127.0.0.1:8000'
-    }
-});
-
-runTests(getTests(helloWorldCanister));
+runTests(getTests());
--- examples/new/test/tests.ts
+++ examples/new/test/tests.ts
@@ -1,7 +1,28 @@
-import { Test } from 'azle/test';
-import { _SERVICE } from './dfx_generated/hello_world/hello_world.did';
-import { ActorSubclass } from '@dfinity/agent';
+import { Test } from '../../../test'; // We don't want to install Azle
+import { execSync } from 'child_process';
 
-export function getTests(helloWorldCanister: ActorSubclass<_SERVICE>): Test[] {
-    return [];
+export function getTests(): Test[] {
+    return [
+        {
+            name: 'getMessage',
+            test: async () => {
+                execSync(
+                    `cd hello_world && npm run canister_call_set_message`,
+                    {
+                        stdio: 'inherit'
+                    }
+                );
+
+                const result = execSync(
+                    `cd hello_world && npm run canister_call_get_message`
+                )
+                    .toString()
+                    .trim();
+
+                return {
+                    Ok: result.includes('Hello world!')
+                };
+            }
+        }
+    ];
 }
