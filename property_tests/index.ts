f6a8632e9b4b3ef7b60e006e7dc2c185bac451c4
Jordan Last <jordan.michael.last@gmail.com>
2023-10-20 16:55:12 -0500

    add blob tests, fix test.yml

--- property_tests/index.ts
+++ property_tests/index.ts
@@ -1,12 +1,16 @@
 import fc from 'fast-check';
 import { TestSample, createCanisterArb } from './arbitraries/canister_arb';
-import { writeFileSync } from 'fs';
+import { existsSync, mkdirSync, writeFileSync } from 'fs';
 import { execSync } from 'child_process';
 import { runTests } from '../test';
 
 export function runPropTests(testArb: fc.Arbitrary<TestSample>) {
     fc.assert(
         fc.asyncProperty(createCanisterArb(testArb), async (canister) => {
+            if (!existsSync('src')) {
+                mkdirSync('src');
+            }
+
             writeFileSync('src/index.ts', canister.sourceCode);
 
             execSync(`dfx canister uninstall-code canister || true`, {
