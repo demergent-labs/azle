d59438f3a608cb9c1176abfb8da4a19887235037
Benjamin DeMann <benjamin.demann@gmail.com>
2023-10-25 16:20:56 -0600

    checkpoint

--- property_tests/tests/opt/test/test.ts
+++ property_tests/tests/opt/test/test.ts
@@ -1,26 +1,46 @@
 import fc from 'fast-check';
-import { OptRecordArb } from '../../../arbitraries/candid/constructed/opt_arb';
+import {
+    RecursiveOptArb,
+    RecursiveOpt,
+    createAgentValueFromRecursiveOpt,
+    createCandidTypeFromRecursiveOpt,
+    createCandidValueFromRecursiveOpt
+} from '../../../arbitraries/candid/constructed/opt_arb';
 import { getActor } from '../../../get_actor';
 import { createUniquePrimitiveArb } from '../../../arbitraries/unique_primitive_arb';
 import { JsFunctionNameArb } from '../../../arbitraries/js_function_name_arb';
 import { runPropTests } from '../../..';
 
 const OptTestArb = fc
-    .tuple(createUniquePrimitiveArb(JsFunctionNameArb), fc.array(OptRecordArb))
-    .map(([functionName, optTrees]) => {
-        const paramCandidTypes = optTrees.map((tree) => createCandidType(tree));
-        const returnCandidType =
-            optTrees.length === 0 ? 'Opt(int8)' : createCandidType(optTrees[0]);
+    .tuple(
+        createUniquePrimitiveArb(JsFunctionNameArb),
+        fc.array(RecursiveOptArb)
+    )
+    .map(([functionName, optRecordArb]) => {
+        const optTrees: RecursiveOpt[] = optRecordArb as RecursiveOpt[];
+        const paramCandidTypes = optTrees.map((tree) =>
+            createCandidTypeFromRecursiveOpt(tree)
+        );
         const paramNames = optTrees.map((_, index) => `param${index}`);
+        // If there are not optTrees then we will be returning None so the type
+        // here can be whatever as long as it's wrapped in Opt
+        const returnCandidType =
+            optTrees.length === 0
+                ? 'Opt(int8)'
+                : createCandidTypeFromRecursiveOpt(optTrees[0]);
         const returnStatement = paramNames[0] ?? `None`;
         const expectedResult =
-            optTrees.length === 0 ? [] : createAgentValue(optTrees[0]);
+            optTrees.length === 0
+                ? []
+                : createAgentValueFromRecursiveOpt(optTrees[0]);
 
-        const candidValues = optTrees.map((tree) => createCandidValue(tree));
+        const candidValues = optTrees.map((tree) =>
+            createCandidValueFromRecursiveOpt(tree)
+        );
 
         const areParamsCorrectlyOrdered = paramNames
             .map((paramName, index) => {
-                return `if (!${areOptsEqual(
+                return `if (!${createAreOptsEqualCodeUsage(
                     paramName,
                     candidValues[index]
                 )}) throw new Error('${paramName} is incorrectly ordered')`;
@@ -60,6 +80,41 @@ const OptTestArb = fc
             returnCandidType,
             paramNames,
             body: `
+            ${createAreOptsEqualCodeDeclaration()}
+            ${areParamsCorrectlyOrdered}
+            ${areParamsOpts}
+
+            return ${returnStatement};
+        `,
+            test: {
+                name: `test ${functionName}`,
+                test: async () => {
+                    const actor = getActor('./tests/opt/test');
+
+                    const params = optTrees.map((tree) =>
+                        createAgentValueFromRecursiveOpt(tree)
+                    );
+
+                    const result = await actor[functionName](...params);
+
+                    // TODO be careful on equality checks when we go beyond primitives
+                    // TODO a universal equality checker is going to be very useful
+                    return {
+                        Ok: areOptsEqual(result, expectedResult)
+                    };
+                }
+            }
+        };
+    });
+
+runPropTests(OptTestArb);
+
+function isParamOpt(paramName: string): string {
+    return `(${paramName}.Some !== undefined || ${paramName}.None !== undefined)`;
+}
+
+function createAreOptsEqualCodeDeclaration(): string {
+    return `
             function calculateDepthAndValues(value: any): {
                 depth: number;
                 value: any;
@@ -76,91 +131,29 @@ const OptTestArb = fc
                 return { ...result, depth: result.depth + 1 };
             }
 
-            function compareDepthsAndValues(value1: any, value2: any) {
-                const { depth: depth1, value: deepestValue1 } =
-                    calculateDepthAndValues(value1);
-                const { depth: depth2, value: deepestValue2 } =
-                    calculateDepthAndValues(value2);
+            function areOptsEqual(opt1: any, opt2: any) {
+                const { depth: depth1, value: value1 } =
+                    calculateDepthAndValues(opt1);
+                const { depth: depth2, value: value2 } =
+                    calculateDepthAndValues(opt2);
 
-                return depth1 === depth2 && deepestValue1 === deepestValue2;
+                return depth1 === depth2 && value1 === value2;
             }
-
-            ${areParamsCorrectlyOrdered}
-            ${areParamsOpts}
-
-            return ${returnStatement};
-        `,
-            test: {
-                name: `test ${functionName}`,
-                test: async () => {
-                    const actor = getActor('./tests/opt/test');
-
-                    const params = optTrees.map((tree) =>
-                        createAgentValue(tree)
-                    );
-
-                    const result = await actor[functionName](...params);
-
-                    // TODO be careful on equality checks when we go beyond primitives
-                    // TODO a universal equality checker is going to be very useful
-                    return {
-                        Ok: compareDepthsAndValues(result, expectedResult)
-                    };
-                }
-            }
-        };
-    });
-
-runPropTests(OptTestArb);
-
-function createCandidType(optTree: any): string {
-    if (optTree.nextLayer === null) {
-        // base case
-        return `Opt(${optTree.base.candidType})`;
-    } else {
-        return `Opt(${createCandidType(optTree.nextLayer)})`;
-    }
+        `;
 }
 
-function createAgentValue(optTree: any): any {
-    if (optTree.nextLayer === null) {
-        // base case
-        if (optTree.base.opt && optTree.base.opt.Some !== undefined) {
-            return [optTree.base.opt.Some];
-        } else {
-            return [];
+function createAreOptsEqualCodeUsage(
+    paramName: string,
+    paramValue: any
+): string {
+    function replacer(_key: any, value: any) {
+        if (typeof value === 'bigint') {
+            return value.toString() + 'n';
         }
-    } else {
-        return [createAgentValue(optTree.nextLayer)];
+        return value;
     }
-}
 
-function createCandidValue(optTree: any): any {
-    if (optTree.nextLayer === null) {
-        // base case
-        if (optTree.base.opt && optTree.base.opt.Some !== undefined) {
-            return { Some: optTree.base.opt.Some };
-        } else {
-            return { None: null };
-        }
-    } else {
-        return { Some: createCandidValue(optTree.nextLayer) };
-    }
-}
-
-function replacer(_key: any, value: any) {
-    if (typeof value === 'bigint') {
-        return value.toString() + 'n';
-    }
-    return value;
-}
-
-function isParamOpt(paramName: string): string {
-    return `(${paramName}.Some !== undefined || ${paramName}.None !== undefined)`;
-}
-
-function areOptsEqual(paramName: string, paramValue: any): string {
-    return `compareDepthsAndValues(${paramName}, ${JSON.stringify(
+    return `areOptsEqual(${paramName}, ${JSON.stringify(
         paramValue,
         replacer
     )})`;
@@ -189,21 +182,20 @@ function calculateDepthAndValues(value: [any] | []): {
     return { ...result, depth: result.depth + 1 };
 }
 
-function compareDepthsAndValues(value1: any, value2: any) {
-    const { depth: depth1, value: deepestValue1 } =
-        calculateDepthAndValues(value1);
-    const { depth: depth2, value: deepestValue2 } =
-        calculateDepthAndValues(value2);
+function areOptsEqual(opt1: any, opt2: any) {
+    const { depth: depth1, value: value1 } = calculateDepthAndValues(opt1);
+    const { depth: depth2, value: value2 } = calculateDepthAndValues(opt2);
 
-    return depth1 === depth2 && optEquals(deepestValue1, deepestValue2);
-}
+    if (depth1 !== depth2) {
+        return false;
+    }
 
-function optEquals(value1: any | [], value2: any | []) {
     function isNone(value: any | []) {
         return Array.isArray(value) && value.length === 0;
     }
-    if (isNone(value1) && isNone(value2)) {
+    if (isNone(opt1) && isNone(opt2)) {
         return true;
     }
+
     return value1 === value2;
 }
