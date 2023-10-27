8c1c1324ad8841d1414b9091d28bf296252c9ea8
Benjamin DeMann <benjamin.demann@gmail.com>
2023-10-27 11:15:45 -0600

    fix float64

--- property_tests/arbitraries/candid/primitive/floats/float64_arb.ts
+++ property_tests/arbitraries/candid/primitive/floats/float64_arb.ts
@@ -3,5 +3,5 @@ import { CandidArb } from '../../candid_arb';
 
 export const Float64Arb = CandidArb(
     fc.float64Array({ maxLength: 1, minLength: 1 }).map((sample) => sample[0]),
-    'float32'
+    'float64'
 );
