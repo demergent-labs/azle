b3b4c7ccb1e5d1478c865a8542d14b4d3ad56553
Benjamin DeMann <benjamin.demann@gmail.com>
2023-11-01 11:37:05 -0600

    add record value literal and equals method

--- property_tests/arbitraries/candid/constructed/record_arb.ts
+++ property_tests/arbitraries/candid/constructed/record_arb.ts
@@ -5,8 +5,11 @@ import { UniqueIdentifierArb } from '../../unique_identifier_arb';
 import { JsFunctionNameArb } from '../../js_function_name_arb';
 
 export type Record = {
-    [x: string]: number | bigint | null;
+    [x: string]: RecordFieldType;
 };
+
+export type RecordFieldType = number | bigint | null;
+type Field = [string, Candid<RecordFieldType>];
 
 export const RecordArb = fc
     .tuple(
@@ -16,34 +19,105 @@ export const RecordArb = fc
         })
     )
     .map(([name, fields]): Candid<Record> => {
-        const typeDeclaration = `const ${name} = Record({\n    ${fields
-            .map(
-                ([fieldName, fieldDataType]) =>
-                    `${fieldName}: ${fieldDataType.src.candidType}`
-            )
-            .join(',\n    ')}\n});`;
+        const typeDeclaration = generateTypeDeclaration(name, fields);
 
-        const value =
-            fields.length === 0
-                ? {}
-                : fields.reduce((record, [fieldName, fieldDataType]) => {
-                      return {
-                          ...record,
-                          [fieldName]: fieldDataType.value
-                      };
-                  }, {});
+        const imports = generateImports(fields);
 
-        const imports = new Set([
-            ...fields.map((field) => field[1].src.candidType),
-            'Record'
-        ]);
+        const value = generateValue(fields);
+
+        const valueLiteral = generateValueLiteral(fields);
+
+        const equals = generateEqualsMethod(fields);
 
         return {
             src: {
                 candidType: name,
                 typeDeclaration,
-                imports
+                imports,
+                valueLiteral
             },
-            value
+            value,
+            equals
         };
     });
+
+function generateImports(fields: Field[]): Set<string> {
+    return new Set([
+        ...fields.map((field) => field[1].src.candidType),
+        'Record'
+    ]);
+}
+
+function generateTypeDeclaration(name: string, fields: Field[]): string {
+    return `const ${name} = Record({\n    ${fields
+        .map(
+            ([fieldName, fieldDataType]) =>
+                `${fieldName}: ${fieldDataType.src.candidType}`
+        )
+        .join(',\n    ')}\n});`;
+}
+
+function generateValue(fields: Field[]): Record {
+    return fields.length === 0
+        ? {}
+        : fields.reduce((record, [fieldName, fieldDataType]) => {
+              return {
+                  ...record,
+                  [fieldName]: fieldDataType.value
+              };
+          }, {});
+}
+
+function generateValueLiteral(fields: Field[]): string {
+    if (fields.length === 0) {
+        return '{}';
+    }
+
+    const fieldLiterals = fields
+        .map(
+            ([fieldName, fieldValue]) =>
+                `${fieldName}: ${fieldValue.src.valueLiteral}`
+        )
+        .join(',\n');
+
+    return `{
+        ${fieldLiterals}
+    }`;
+}
+
+function generateEqualsMethod(
+    fields: Field[]
+): (a: Record, b: Record) => boolean {
+    return (a: Record, b: Record): boolean => {
+        if (typeof a !== typeof b) {
+            return false;
+        }
+
+        const aFieldNames = Object.keys(a);
+        const bFieldNames = Object.keys(b);
+        if (aFieldNames.length !== bFieldNames.length) {
+            return false;
+        }
+
+        const areFieldNamesTheSame = aFieldNames.reduce(
+            (acc, aFieldName, index) =>
+                acc && aFieldName === bFieldNames[index],
+            true
+        );
+
+        if (!areFieldNamesTheSame) {
+            return false;
+        }
+
+        const areFieldValuesTheSame = fields.reduce(
+            (acc, [fieldName, fieldCandidType]) => {
+                return (
+                    acc && fieldCandidType.equals(a[fieldName], b[fieldName])
+                );
+            },
+            true
+        );
+
+        return areFieldValuesTheSame;
+    };
+}
