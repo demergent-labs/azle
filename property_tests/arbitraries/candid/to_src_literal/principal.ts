912f404ab631b4f621f7234a820f9bd89d39e6b5
Benjamin DeMann <benjamin.demann@gmail.com>
2023-11-07 14:06:34 -0700

    add comment about principal problem

--- property_tests/arbitraries/candid/to_src_literal/principal.ts
+++ property_tests/arbitraries/candid/to_src_literal/principal.ts
@@ -1,5 +1,6 @@
 import { Principal } from '@dfinity/principal';
 
 export function principalToSrcLiteral(value: Principal): string {
+    // TODO solve the underlying principal problem https://github.com/demergent-labs/azle/issues/1443
     return `DfinityPrincipal.fromText('${value.toText()}')`;
 }
