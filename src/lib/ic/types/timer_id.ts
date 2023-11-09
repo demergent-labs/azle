a675907263aad91ba5871408b82892ac6ea94af0
Jordan Last <jordan.michael.last@gmail.com>
2023-11-09 15:01:53 -0600

    fixing up some types

--- src/lib/ic/types/timer_id.ts
+++ src/lib/ic/types/timer_id.ts
@@ -4,5 +4,5 @@ import { nat64, AzleNat64 } from '../../candid/types/primitive/nats/nat64';
  * Type returned by the {@link ic.setTimer} and {@link ic.setTimerInterval}
  * functions. Pass to {@link ic.clearTimer} to remove the timer.
  */
-export type TimerId = nat64; // TODO: Consider modeling this after the corresponding struct in Rust
 export const TimerId = AzleNat64;
+export type TimerId = nat64; // TODO: Consider modeling this after the corresponding struct in Rust
