d5316690900c07171811facbc1bf6b2ef8ea75c9
Benjamin DeMann <benjamin.demann@gmail.com>
2023-11-16 16:22:15 -0700

    checkpoint

--- property_tests/arbitraries/candid/constructed/variant_arb.ts
+++ property_tests/arbitraries/candid/constructed/variant_arb.ts
@@ -1,6 +1,6 @@
 import fc from 'fast-check';
 import { CandidMeta } from '../candid_arb';
-import { CandidType, CandidTypeArb } from '../candid_type_arb';
+import { CandidType } from '../candid_type_arb';
 import { UniqueIdentifierArb } from '../../unique_identifier_arb';
 import { JsFunctionNameArb } from '../../js_function_name_arb';
 import { BoolArb } from '../primitive/bool';
@@ -26,18 +26,9 @@ export type Variant = {
 };
 type Field = [string, CandidMeta<CandidType>];
 
-type RecursiveArb<T> = { base: T } | { nextLayer: RecursiveArb<T> };
-
-type RecursiveVariantArb<T> = {
-    nonVariantFields: CandidMeta<CandidType>;
-    variantFields: CandidMeta<Variant>;
-};
-
-type VariantField = [string, CandidMeta<CandidType>];
-
 function VariantFieldsArb(
     candidTypeArb: fc.Arbitrary<CandidMeta<CandidType>>
-): fc.Arbitrary<VariantField[]> {
+): fc.Arbitrary<Field[]> {
     return fc.uniqueArray(fc.tuple(JsFunctionNameArb, candidTypeArb), {
         selector: (entry) => entry[0],
         minLength: 1
@@ -47,9 +38,9 @@ function VariantFieldsArb(
     });
 }
 
-const ComplexCandidTypeArb: fc.Arbitrary<CandidMeta<CandidType>> = fc.letrec(
+export const CandidTypeArb: fc.Arbitrary<CandidMeta<CandidType>> = fc.letrec(
     (tie) => ({
-        Thing: fc.oneof(
+        CandidType: fc.oneof(
             Float32Arb,
             Float64Arb,
             IntArb,
@@ -67,52 +58,14 @@ const ComplexCandidTypeArb: fc.Arbitrary<CandidMeta<CandidType>> = fc.letrec(
             TextArb,
             PrincipalArb,
             BlobArb,
-            tie('VariantThing').map((sample) => sample as CandidMeta<Variant>)
+            tie('Variant').map((sample) => sample as CandidMeta<Variant>)
             // tie('RecordThing').map((sample) => sample as CandidMeta<Record>)
         ),
-        VariantThing: fc
-            .tuple(
-                UniqueIdentifierArb('typeDeclaration'),
-                VariantFieldsArb(
-                    tie('Thing') as fc.Arbitrary<CandidMeta<CandidType>>
-                )
-                // fc
-                //     .uniqueArray(fc.tuple(JsFunctionNameArb, tie('Thing')), {
-                //         selector: (entry) => entry[0],
-                //         minLength: 1
-                //         // Although no minLength is technically required (according to the
-                //         // spec), the DFX CLI itself currently errors out trying to pass
-                //         // an empty object.
-                //     })
-                //     .map(
-                //         (sample) => sample as [string, CandidMeta<CandidType>][]
-                //     )
-            )
-            .map(([name, fields]): CandidMeta<Variant> => {
-                const randomIndex = Math.floor(Math.random() * fields.length);
-
-                const candidType = generateCandidType(fields);
-
-                const typeDeclaration = generateTypeDeclaration(name, fields);
-
-                const imports = generateImports(fields);
-
-                const valueLiteral = generateValueLiteral(randomIndex, fields);
-
-                const value = generateValue(randomIndex, fields);
-
-                return {
-                    src: {
-                        candidType,
-                        typeDeclaration,
-                        imports,
-                        valueLiteral
-                    },
-                    value
-                };
-            })
+        Variant: BaseVariantArb(
+            tie('CandidType') as fc.Arbitrary<CandidMeta<CandidType>>
+        )
     })
-).Thing;
+).CandidType;
 
 export function BaseVariantArb(
     candidTypeArb: fc.Arbitrary<CandidMeta<CandidType>>
@@ -147,101 +100,7 @@ export function BaseVariantArb(
         });
 }
 
-export const RecursiveArb = <CType extends CandidType, Base>(
-    BaseArb: fc.Arbitrary<Base>,
-    generateCandidType: (recursiveThing: RecursiveArb<Base>) => string,
-    generateImports: (recursiveThing: RecursiveArb<Base>) => Set<string>,
-    generateValueLiteral: (recursiveThing: RecursiveArb<Base>) => string,
-    generateValue: (recursiveThing: RecursiveArb<Base>) => CType,
-    generateTypeDeclaration: (
-        recursiveThing: RecursiveArb<Base>
-    ) => string | undefined = (r) => undefined
-) =>
-    fc
-        .letrec((tie) => ({
-            RecursiveArb: fc.oneof(
-                fc.record({ base: BaseArb }),
-                fc.record({
-                    nextLayer: tie('RecursiveArb').map(
-                        (sample) => sample as RecursiveArb<Base>
-                    )
-                })
-            )
-        }))
-        .RecursiveArb.map((recursiveArb): CandidMeta<CType> => {
-            return {
-                src: {
-                    candidType: generateCandidType(recursiveArb),
-                    imports: generateImports(recursiveArb),
-                    valueLiteral: generateValueLiteral(recursiveArb),
-                    typeDeclaration: generateTypeDeclaration(recursiveArb)
-                },
-                value: generateValue(recursiveArb)
-            };
-        });
-
-// export const VariantArb = RecursiveArb(
-//     BaseVariantArb,
-//     doGenerateCandidType,
-//     doGenerateImports,
-//     doGenerateValueLiteral,
-//     doGenerateValue,
-//     doGenerateTypeDeclaration
-// );
-
-export const VariantArb = BaseVariantArb(ComplexCandidTypeArb);
-
-function doGenerateCandidType(
-    recursiveVariant: RecursiveArb<CandidMeta<Variant>>
-): string {
-    if ('base' in recursiveVariant) {
-        return recursiveVariant.base.src.candidType;
-    } else {
-        return `Variant({Name: ${doGenerateCandidType(
-            recursiveVariant.nextLayer
-        )}})`;
-    }
-}
-
-function doGenerateValueLiteral(
-    recursiveVariant: RecursiveArb<CandidMeta<Variant>>
-): string {
-    if ('base' in recursiveVariant) {
-        return recursiveVariant.base.src.valueLiteral;
-    } else {
-        return `{Name: ${doGenerateValueLiteral(recursiveVariant.nextLayer)}}`;
-    }
-}
-
-function doGenerateTypeDeclaration(
-    recursiveVariant: RecursiveArb<CandidMeta<Variant>>
-): string | undefined {
-    if ('base' in recursiveVariant) {
-        return recursiveVariant.base.src.typeDeclaration;
-    } else {
-        return doGenerateTypeDeclaration(recursiveVariant.nextLayer);
-    }
-}
-
-function doGenerateImports(
-    recursiveVariant: RecursiveArb<CandidMeta<Variant>>
-): Set<string> {
-    if ('base' in recursiveVariant) {
-        return new Set([...recursiveVariant.base.src.imports]);
-    } else {
-        return doGenerateImports(recursiveVariant.nextLayer);
-    }
-}
-
-function doGenerateValue(
-    recursiveVariant: RecursiveArb<CandidMeta<Variant>>
-): Variant {
-    if ('base' in recursiveVariant) {
-        return recursiveVariant.base.value;
-    } else {
-        return { Name: doGenerateValue(recursiveVariant.nextLayer) };
-    }
-}
+export const VariantArb = BaseVariantArb(CandidTypeArb);
 
 function generateImports(fields: Field[]): Set<string> {
     const fieldImports = fields.flatMap((field) => [...field[1].src.imports]);
