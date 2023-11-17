6e7b2c3296e48a20250f99763af13660b86f56e7
Benjamin DeMann <benjamin.demann@gmail.com>
2023-11-17 14:38:53 -0700

    start vec

--- property_tests/arbitraries/candid/constructed/vec_arb/base.ts
+++ property_tests/arbitraries/candid/constructed/vec_arb/base.ts
@@ -1,36 +1,9 @@
 import fc from 'fast-check';
-import { IntArb } from '../primitive/ints/int_arb';
-import { Int8Arb } from '../primitive/ints/int8_arb';
-import { Int16Arb } from '../primitive/ints/int16_arb';
-import { Int32Arb } from '../primitive/ints/int32_arb';
-import { Int64Arb } from '../primitive/ints/int64_arb';
-import { NatArb } from '../primitive/nats/nat_arb';
-import { Nat8Arb } from '../primitive/nats/nat8_arb';
-import { Nat16Arb } from '../primitive/nats/nat16_arb';
-import { Nat32Arb } from '../primitive/nats/nat32_arb';
-import { Nat64Arb } from '../primitive/nats/nat64_arb';
-import { PrincipalArb } from '../reference/principal_arb';
-import { CandidMeta } from '../candid_arb';
-import { BoolArb } from '../primitive/bool';
-import { Float32Arb } from '../primitive/floats/float32_arb';
-import { Float64Arb } from '../primitive/floats/float64_arb';
-import { NullArb } from '../primitive/null';
-import { TextArb } from '../primitive/text';
-import { BlobArb } from './blob_arb';
-import { CandidType } from '../candid_type_arb';
+import { CandidMeta } from '../../candid_arb';
+import { CandidType } from '../../candid_type_arb';
+import { Vec } from './index';
 
-type Vec<T> =
-    | T[]
-    | Uint16Array
-    | Uint32Array
-    | Uint8Array
-    | Int16Array
-    | Int32Array
-    | Int8Array
-    | BigUint64Array
-    | BigInt64Array;
-
-const VecInnerArb = <T extends CandidType>(
+export const VecInnerArb = <T extends CandidType>(
     arb: fc.Arbitrary<CandidMeta<T>>
 ) => {
     return fc
@@ -131,23 +104,3 @@ function generateValueLiteral<T extends CandidType>(
 
     return valueLiteral;
 }
-
-export const VecArb = fc.oneof(
-    VecInnerArb(Float32Arb),
-    VecInnerArb(Float64Arb),
-    VecInnerArb(IntArb),
-    VecInnerArb(Int8Arb),
-    VecInnerArb(Int16Arb),
-    VecInnerArb(Int32Arb),
-    VecInnerArb(Int64Arb),
-    VecInnerArb(NatArb),
-    VecInnerArb(Nat8Arb),
-    VecInnerArb(Nat16Arb),
-    VecInnerArb(Nat32Arb),
-    VecInnerArb(Nat64Arb),
-    VecInnerArb(BoolArb),
-    VecInnerArb(TextArb),
-    VecInnerArb(PrincipalArb),
-    VecInnerArb(BlobArb)
-    // VecInnerArb(NullArb)
-);
