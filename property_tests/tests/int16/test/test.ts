960f1fd87bbb25928dd050c5438ece1c85e5836b
Dan Steren <dan@demergentlabs.org>
2023-11-21 14:37:10 -0700

    Fixup int16 and int32 tests

--- property_tests/tests/int16/test/test.ts
+++ property_tests/tests/int16/test/test.ts
@@ -3,79 +3,35 @@ import { deepEqual } from 'fast-equals';
 
 import { CanisterArb } from '../../../arbitraries/canister_arb';
 import { Int16Arb } from '../../../arbitraries/candid/primitive/ints/int16_arb';
-import { JsFunctionNameArb } from '../../../arbitraries/js_function_name_arb';
-import { QueryMethodBlueprint } from '../../../arbitraries/test_sample_arb';
-import { createUniquePrimitiveArb } from '../../../arbitraries/unique_primitive_arb';
 import { getActor, runPropTests } from '../../../../property_tests';
 import { CandidMeta } from '../../../arbitraries/candid/candid_arb';
 import { Test } from '../../../../test';
 import { areParamsCorrectlyOrdered } from '../../../are_params_correctly_ordered';
+import { Named, QueryMethodArb } from '../../../arbitraries/query_method_arb';
 
-const Int16TestArb = fc
-    .tuple(
-        createUniquePrimitiveArb(JsFunctionNameArb),
-        fc.array(Int16Arb),
-        Int16Arb
-    )
-    .map(
-        ([
-            functionName,
-            paramInt16s,
-            defaultReturnInt16
-        ]): QueryMethodBlueprint => {
-            const imports = defaultReturnInt16.src.imports;
+const AllInt16sQueryMethod = QueryMethodArb(fc.array(Int16Arb), Int16Arb, {
+    generateBody,
+    generateTests
+});
 
-            const paramNames = paramInt16s.map((_, index) => `param${index}`);
-            const paramCandidTypes = paramInt16s
-                .map((int16) => int16.src.candidType)
-                .join(', ');
-
-            const returnCandidType = defaultReturnInt16.src.candidType;
-
-            const body = generateBody(
-                paramNames,
-                paramInt16s,
-                defaultReturnInt16
-            );
-
-            const tests = [
-                generateTest(functionName, paramInt16s, defaultReturnInt16)
-            ];
-
-            return {
-                imports,
-                functionName,
-                paramNames,
-                paramCandidTypes,
-                returnCandidType,
-                body,
-                tests
-            };
-        }
-    );
-
-runPropTests(CanisterArb(Int16TestArb));
+runPropTests(CanisterArb(AllInt16sQueryMethod));
 
 function generateBody(
-    paramNames: string[],
-    paramInt16s: CandidMeta<number>[],
+    namedParamInt16s: Named<CandidMeta<number>>[],
     returnInt16: CandidMeta<number>
 ): string {
-    const paramsAreNumbers = paramNames
-        .map((paramName) => {
-            return `if (typeof ${paramName} !== 'number') throw new Error('${paramName} must be a number');`;
+    const paramsAreNumbers = namedParamInt16s
+        .map((param) => {
+            return `if (typeof ${param.name} !== 'number') throw new Error('${param.name} must be a number');`;
         })
         .join('\n');
 
-    const paramsCorrectlyOrdered = areParamsCorrectlyOrdered(
-        paramNames,
-        paramInt16s
-    );
+    const paramsCorrectlyOrdered = areParamsCorrectlyOrdered(namedParamInt16s);
 
-    const sum = paramNames.reduce((acc, paramName) => {
-        return `${acc} + ${paramName}`;
+    const sum = namedParamInt16s.reduce((acc, { name }) => {
+        return `${acc} + ${name}`;
     }, returnInt16.src.valueLiteral);
-    const count = paramInt16s.length + 1;
+    const count = namedParamInt16s.length + 1;
     const average = `Math.floor((${sum}) / ${count})`;
 
     return `
@@ -87,30 +43,34 @@ function generateBody(
     `;
 }
 
-function generateTest(
+function generateTests(
     functionName: string,
-    paramInt16s: CandidMeta<number>[],
+    namedParamInt16s: Named<CandidMeta<number>>[],
     returnInt16: CandidMeta<number>
-): Test {
-    const count = paramInt16s.length + 1;
+): Test[] {
+    const count = namedParamInt16s.length + 1;
     const expectedResult = Math.floor(
-        paramInt16s.reduce(
-            (acc, int16) => acc + int16.agentResponseValue,
+        namedParamInt16s.reduce(
+            (acc, param) => acc + param.el.agentResponseValue,
             returnInt16.agentResponseValue
         ) / count
     );
-    const paramValues = paramInt16s.map((sample) => sample.agentArgumentValue);
+    const paramValues = namedParamInt16s.map(
+        (param) => param.el.agentArgumentValue
+    );
 
-    return {
-        name: `int16 ${functionName}`,
-        test: async () => {
-            const actor = getActor('./tests/int16/test');
+    return [
+        {
+            name: `int16 ${functionName}`,
+            test: async () => {
+                const actor = getActor('./tests/int16/test');
 
-            const result = await actor[functionName](...paramValues);
+                const result = await actor[functionName](...paramValues);
 
-            return {
-                Ok: deepEqual(result, expectedResult)
-            };
+                return {
+                    Ok: deepEqual(result, expectedResult)
+                };
+            }
         }
-    };
+    ];
 }
--- property_tests/tests/int32/test/test.ts
+++ property_tests/tests/int32/test/test.ts
@@ -3,80 +3,36 @@ import { deepEqual } from 'fast-equals';
 
 import { CanisterArb } from '../../../arbitraries/canister_arb';
 import { Int32Arb } from '../../../arbitraries/candid/primitive/ints/int32_arb';
-import { JsFunctionNameArb } from '../../../arbitraries/js_function_name_arb';
-import { QueryMethodBlueprint } from '../../../arbitraries/test_sample_arb';
-import { createUniquePrimitiveArb } from '../../../arbitraries/unique_primitive_arb';
 import { getActor, runPropTests } from '../../../../property_tests';
 import { CandidMeta } from '../../../arbitraries/candid/candid_arb';
 import { Test } from '../../../../test';
 import { areParamsCorrectlyOrdered } from '../../../are_params_correctly_ordered';
+import { Named, QueryMethodArb } from '../../../arbitraries/query_method_arb';
 
-const Int32TestArb = fc
-    .tuple(
-        createUniquePrimitiveArb(JsFunctionNameArb),
-        fc.array(Int32Arb),
-        Int32Arb
-    )
-    .map(
-        ([
-            functionName,
-            paramInt32s,
-            defaultReturnInt32
-        ]): QueryMethodBlueprint => {
-            const imports = defaultReturnInt32.src.imports;
+const AllInt32sQueryMethod = QueryMethodArb(fc.array(Int32Arb), Int32Arb, {
+    generateBody,
+    generateTests
+});
 
-            const paramNames = paramInt32s.map((_, index) => `param${index}`);
-            const paramCandidTypes = paramInt32s
-                .map((int32) => int32.src.candidType)
-                .join(', ');
-
-            const returnCandidType = defaultReturnInt32.src.candidType;
-
-            const body = generateBody(
-                paramNames,
-                paramInt32s,
-                defaultReturnInt32
-            );
-
-            const tests = [
-                generateTest(functionName, paramInt32s, defaultReturnInt32)
-            ];
-
-            return {
-                imports,
-                functionName,
-                paramNames,
-                paramCandidTypes,
-                returnCandidType,
-                body,
-                tests
-            };
-        }
-    );
-
-runPropTests(CanisterArb(Int32TestArb));
+runPropTests(CanisterArb(AllInt32sQueryMethod));
 
 function generateBody(
-    paramNames: string[],
-    paramInt32s: CandidMeta<number>[],
+    namedParamInt32s: Named<CandidMeta<number>>[],
     returnInt32: CandidMeta<number>
 ): string {
-    const paramsAreNumbers = paramNames
-        .map((paramName) => {
-            return `if (typeof ${paramName} !== 'number') throw new Error('${paramName} must be a number');`;
+    const paramsAreNumbers = namedParamInt32s
+        .map((param) => {
+            return `if (typeof ${param.name} !== 'number') throw new Error('${param.name} must be a number');`;
         })
         .join('\n');
 
-    const sum = paramNames.reduce((acc, paramName) => {
-        return `${acc} + ${paramName}`;
+    const sum = namedParamInt32s.reduce((acc, { name }) => {
+        return `${acc} + ${name}`;
     }, returnInt32.src.valueLiteral);
-    const count = paramInt32s.length + 1;
+    const count = namedParamInt32s.length + 1;
     const average = `Math.floor((${sum}) / ${count})`;
 
-    const paramsCorrectlyOrdered = areParamsCorrectlyOrdered(
-        paramNames,
-        paramInt32s
-    );
+    const paramsCorrectlyOrdered = areParamsCorrectlyOrdered(namedParamInt32s);
 
     return `
         ${paramsAreNumbers}
@@ -87,30 +43,34 @@ function generateBody(
     `;
 }
 
-function generateTest(
+function generateTests(
     functionName: string,
-    paramInt32s: CandidMeta<number>[],
+    namedParamInt32s: Named<CandidMeta<number>>[],
     returnInt32: CandidMeta<number>
-): Test {
-    const count = paramInt32s.length + 1;
+): Test[] {
+    const count = namedParamInt32s.length + 1;
     const expectedResult = Math.floor(
-        paramInt32s.reduce(
-            (acc, int32) => acc + int32.agentResponseValue,
+        namedParamInt32s.reduce(
+            (acc, param) => acc + param.el.agentResponseValue,
             returnInt32.agentResponseValue
         ) / count
     );
-    const paramValues = paramInt32s.map((sample) => sample.agentArgumentValue);
+    const paramValues = namedParamInt32s.map(
+        (param) => param.el.agentArgumentValue
+    );
 
-    return {
-        name: `int32 ${functionName}`,
-        test: async () => {
-            const actor = getActor('./tests/int32/test');
+    return [
+        {
+            name: `int32 ${functionName}`,
+            test: async () => {
+                const actor = getActor('./tests/int32/test');
 
-            const result = await actor[functionName](...paramValues);
+                const result = await actor[functionName](...paramValues);
 
-            return {
-                Ok: deepEqual(result, expectedResult)
-            };
+                return {
+                    Ok: deepEqual(result, expectedResult)
+                };
+            }
         }
-    };
+    ];
 }
