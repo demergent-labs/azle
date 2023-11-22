a3fca1c9e8e4d0eda08e903f516c569ac7349c23
Dan Steren <dan@demergentlabs.org>
2023-11-22 15:38:39 -0700

    Fixup nat16 test

--- property_tests/tests/nat16/test/test.ts
+++ property_tests/tests/nat16/test/test.ts
@@ -5,12 +5,12 @@ import { Nat16Arb } from 'azle/property_tests/arbitraries/candid/primitive/nats/
 import { CanisterArb } from 'azle/property_tests/arbitraries/canister_arb';
 import { QueryMethodArb } from 'azle/property_tests/arbitraries/query_method_arb';
 
+import { generateBody } from './generate_body';
+import { generateTests } from './generate_tests';
+
 const AllNat16sQueryMethod = QueryMethodArb(fc.array(Nat16Arb), Nat16Arb, {
     generateBody,
     generateTests
 });
-
-import { generateBody } from './generate_body';
-import { generateTests } from './generate_tests';
 
 runPropTests(CanisterArb(AllNat16sQueryMethod));
