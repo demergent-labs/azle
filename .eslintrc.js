d8ee26110e9f4fb60bbe394e65bd77ff2856704e
Dan Steren <dan@demergentlabs.org>
2023-11-27 11:49:45 -0700

    Add eslint
    These changes were all the result of running `npm init @eslint/config`.

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    env: {
+        es2021: true,
+        node: true
+    },
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    overrides: [
+        {
+            env: {
+                node: true
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script'
+            }
+        }
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    plugins: ['@typescript-eslint'],
+    rules: {
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'always']
+    }
+};
