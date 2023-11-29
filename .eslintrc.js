d94873da70d9d9205629a52392491634ef769c7b
Dan Steren <dan@demergentlabs.org>
2023-11-29 13:28:05 -0700

    Enable no-loss-of-precision and no-namespace

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,17 @@
 // TODO: These rules should be enabled, but we had offenses when we enabled ESLint.
 // This is tech-debt. We should go through and re-enable these at some point.
 const temporarilyDisabledRules = {
-    '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-loss-of-precision': 'off',
-    '@typescript-eslint/no-namespace': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/no-var-requires': 'off',
-    '@typescript-eslint/prefer-as-const': 'off',
-    'no-constant-condition': 'off',
-    'no-empty': 'off',
-    'no-prototype-builtins': 'off',
-    'no-undef': 'off',
-    'no-var': 'off',
-    'prefer-const': 'off'
+    '@typescript-eslint/ban-ts-comment': 'off', // 39 problems
+    '@typescript-eslint/no-explicit-any': 'off', // 321 problems
+    '@typescript-eslint/no-unused-vars': 'off', // 33 problems
+    '@typescript-eslint/no-var-requires': 'off', // 12 problems
+    '@typescript-eslint/prefer-as-const': 'off', // 45 problems
+    'no-constant-condition': 'off', // 1 problem
+    'no-empty': 'off', // 9 problems
+    'no-prototype-builtins': 'off', // 2 problems
+    'no-undef': 'off', // 11 problems
+    'no-var': 'off', // 6 problems
+    'prefer-const': 'off' // 137 problems
 };
 
 module.exports = {
@@ -46,6 +44,10 @@ module.exports = {
     rules: {
         'simple-import-sort/exports': 'error',
         'simple-import-sort/imports': 'error',
+        '@typescript-eslint/no-unused-vars': [
+            'error',
+            { argsIgnorePattern: '^_' }
+        ],
         ...temporarilyDisabledRules
     }
 };
