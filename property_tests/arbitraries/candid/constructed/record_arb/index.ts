e63b88434e48ba491a33f254740b96da1360f0a3
Benjamin DeMann <benjamin.demann@gmail.com>
2023-11-28 21:40:16 -0700

    WIP

--- property_tests/arbitraries/candid/constructed/record_arb/index.ts
+++ property_tests/arbitraries/candid/constructed/record_arb/index.ts
@@ -1,8 +1,8 @@
-import { CandidType, CandidTypeArb } from '../../candid_type_arb';
+import { CandidCoolTypeArb, CandidType } from '../../candid_type_arb';
 import { RecordArb as Base } from './base';
 
 export type Record = {
     [x: string]: CandidType;
 };
 
-export const RecordArb = Base(CandidTypeArb);
+export const RecordArb = Base(CandidCoolTypeArb);
