21d063157aa8229b3a7af6ffa3ea4c7b914ddf0a
Benjamin DeMann <benjamin.demann@gmail.com>
2023-12-01 09:48:54 -0700

    move service and func def and values to own files

--- property_tests/arbitraries/candid/reference/func_arb/base.ts
+++ property_tests/arbitraries/candid/reference/func_arb/base.ts
@@ -1,151 +1,18 @@
 import fc from 'fast-check';
 
-import { PrincipalArb } from '../principal_arb';
-import { VoidDefinitionArb } from '../../primitive/void';
 import { CandidValueAndMeta } from '../../value_and_meta_arb';
-import { UniqueIdentifierArb } from '../../../unique_identifier_arb';
 import { Func } from './index';
-import {
-    CandidDefinition,
-    FuncCandidDefinition
-} from '../../definition_arb/types';
-import { CandidType } from '../../candid_type';
-import { TextArb } from '../../primitive/text';
-import { CandidValues } from '../../values';
-
-type Mode = 'query' | 'update' | 'oneway';
-
-export function FuncDefinitionArb(
-    candidDefArb: fc.Arbitrary<CandidDefinition>
-): fc.Arbitrary<FuncCandidDefinition> {
-    return (fc.constantFrom('query', 'update', 'oneway') as fc.Arbitrary<Mode>)
-        .chain((mode) => {
-            const returnType =
-                mode === 'oneway' ? VoidDefinitionArb : candidDefArb;
-
-            return fc.tuple(
-                UniqueIdentifierArb('typeDeclaration'),
-                fc.array(candidDefArb),
-                returnType,
-                fc.constant(mode),
-                fc.boolean()
-            );
-        })
-        .map(
-            ([
-                name,
-                params,
-                returnFunc,
-                mode,
-                useTypeDeclaration
-            ]): FuncCandidDefinition => {
-                const typeAliasDeclarations = generateTypeAliasDeclarations(
-                    name,
-                    params,
-                    returnFunc,
-                    mode,
-                    useTypeDeclaration
-                );
-
-                const typeAnnotation = useTypeDeclaration
-                    ? name
-                    : generateTypeAnnotation(params, returnFunc, mode);
-
-                const imports = new Set([
-                    ...params.flatMap((param) => [...param.candidMeta.imports]),
-                    ...returnFunc.candidMeta.imports,
-                    'Func'
-                ]);
-
-                return {
-                    candidMeta: {
-                        typeAnnotation,
-                        typeAliasDeclarations,
-                        imports,
-                        candidType: CandidType.Func
-                    },
-                    paramCandidMeta: params,
-                    returnCandidMeta: returnFunc
-                };
-            }
-        );
-}
+import { CandidDefinition } from '../../definition_arb/types';
+import { FuncDefinitionArb } from './definition_arb';
+import { FuncValueArb } from './values_arb';
+import { CandidArb } from '../../complex_type_arb';
 
 export function FuncArb(
     candidDefinitionArb: fc.Arbitrary<CandidDefinition>
 ): fc.Arbitrary<CandidValueAndMeta<Func>> {
-    return fc.tuple(FuncDefinitionArb(candidDefinitionArb), FuncValueArb).map(
-        ([
-            {
-                candidMeta: { typeAnnotation, typeAliasDeclarations, imports }
-            },
-            { agentArgumentValue, agentResponseValue, valueLiteral }
-        ]) => {
-            return {
-                src: {
-                    typeAnnotation,
-                    typeAliasDeclarations,
-                    imports,
-                    valueLiteral
-                },
-                agentArgumentValue,
-                agentResponseValue
-            };
-        }
+    return CandidArb(
+        FuncDefinitionArb,
+        () => FuncValueArb,
+        candidDefinitionArb
     );
-}
-
-export const FuncValueArb: fc.Arbitrary<CandidValues<Func>> = fc
-    .tuple(TextArb, PrincipalArb)
-    .map(([name, principal]) => {
-        const value: Func = [
-            principal.agentArgumentValue,
-            name.agentArgumentValue
-        ];
-
-        const valueLiteral = `[${principal.src.valueLiteral}, ${name.src.valueLiteral}]`;
-
-        return {
-            valueLiteral,
-            agentArgumentValue: value,
-            agentResponseValue: value
-        };
-    });
-
-function generateTypeAliasDeclarations(
-    name: string,
-    paramCandids: CandidDefinition[],
-    returnCandid: CandidDefinition,
-    mode: Mode,
-    useTypeDeclaration: boolean
-): string[] {
-    const paramTypeDeclarations = paramCandids.flatMap(
-        (param) => param.candidMeta.typeAliasDeclarations
-    );
-    const returnTypeDeclaration = returnCandid.candidMeta.typeAliasDeclarations;
-
-    if (useTypeDeclaration) {
-        return [
-            ...paramTypeDeclarations,
-            ...returnTypeDeclaration,
-            `const ${name} = ${generateTypeAnnotation(
-                paramCandids,
-                returnCandid,
-                mode
-            )}`
-        ];
-    }
-
-    return [...paramTypeDeclarations, ...returnTypeDeclaration];
-}
-
-function generateTypeAnnotation(
-    paramCandids: CandidDefinition[],
-    returnCandid: CandidDefinition,
-    mode: Mode
-): string {
-    const params = paramCandids
-        .map((param) => param.candidMeta.typeAnnotation)
-        .join(', ');
-    return `Func([${params}], ${returnCandid.candidMeta.typeAnnotation}, '${mode}')`;
 }
--- property_tests/arbitraries/candid/reference/service_arb/base.ts
+++ property_tests/arbitraries/candid/reference/service_arb/base.ts
@@ -1,18 +1,11 @@
 import fc from 'fast-check';
 import { Principal } from '@dfinity/principal';
 
-import { PrincipalValueArb } from '../principal_arb';
-import { VoidDefinitionArb } from '../../primitive/void';
 import { CandidValueAndMeta } from '../../value_and_meta_arb';
-import { UniqueIdentifierArb } from '../../../unique_identifier_arb';
-import { JsFunctionNameArb } from '../../../js_function_name_arb';
-import {
-    CandidDefinition,
-    ServiceCandidDefinition,
-    ServiceMethodDefinition
-} from '../../definition_arb/types';
-import { CandidType } from '../../candid_type';
-import { CandidValues } from '../../values';
+import { CandidDefinition } from '../../definition_arb/types';
+import { ServiceValueArb } from './values_arb';
+import { ServiceDefinitionArb } from './definition_arb';
+import { CandidArb } from '../../complex_type_arb';
 
 // TODO:
 // - services that are more than type-definitions, i.e. have functionality
@@ -26,168 +19,12 @@ import { CandidValues } from '../../values';
 //     method2: query([text, text, nat64], nat64),
 // });
 
-function ServiceMethodArb(
-    candidDefArb: fc.Arbitrary<CandidDefinition>
-): fc.Arbitrary<ServiceMethodDefinition> {
-    return fc
-        .tuple(
-            JsFunctionNameArb,
-            fc.constantFrom('query', 'update'),
-            fc.array(candidDefArb),
-            fc.oneof(candidDefArb, VoidDefinitionArb)
-        )
-        .map(([name, mode, params, returnType]): ServiceMethodDefinition => {
-            const paramCandidTypes = params.map(
-                (param) => param.candidMeta.typeAnnotation
-            );
-
-            const typeAliasDeclarations = params.reduce(
-                (acc, { candidMeta: { typeAliasDeclarations } }): string[] => {
-                    return [...acc, ...typeAliasDeclarations];
-                },
-                returnType.candidMeta.typeAliasDeclarations
-            );
-
-            const src = `${name}: ${mode}([${paramCandidTypes}], ${returnType.candidMeta.typeAnnotation})`;
-
-            const imports = params.reduce(
-                (acc, param) => {
-                    return new Set([...acc, ...param.candidMeta.imports]);
-                },
-                new Set([mode, ...returnType.candidMeta.imports])
-            );
-
-            return {
-                name,
-                imports,
-                typeAliasDeclarations,
-                src
-            };
-        });
-}
-
-export function ServiceDefinitionArb(
-    fieldCandidDefArb: fc.Arbitrary<CandidDefinition>
-): fc.Arbitrary<ServiceCandidDefinition> {
-    return fc
-        .tuple(
-            UniqueIdentifierArb('typeDeclaration'),
-            fc.uniqueArray(ServiceMethodArb(fieldCandidDefArb), {
-                selector: (entry) => entry.name
-            }),
-            fc.boolean()
-        )
-        .map(([name, fields, useTypeDeclaration]): ServiceCandidDefinition => {
-            useTypeDeclaration = true;
-            const typeAnnotation = useTypeDeclaration
-                ? name
-                : generateTypeAnnotation(fields);
-
-            const typeAliasDeclarations = generateTypeAliasDeclarations(
-                name,
-                fields,
-                useTypeDeclaration
-            );
-
-            const imports = generateImports(fields);
-
-            return {
-                name,
-                candidMeta: {
-                    typeAnnotation,
-                    typeAliasDeclarations,
-                    imports,
-                    candidType: CandidType.Service
-                },
-                funcs: fields
-            };
-        });
-}
-
 export function ServiceArb(
     candidDefinitionArb: fc.Arbitrary<CandidDefinition>
 ): fc.Arbitrary<CandidValueAndMeta<Principal>> {
-    return ServiceDefinitionArb(candidDefinitionArb)
-        .chain((serviceDefinition) =>
-            fc.tuple(
-                fc.constant(serviceDefinition),
-                ServiceValueArb(serviceDefinition)
-            )
-        )
-        .map(
-            ([
-                {
-                    candidMeta: {
-                        typeAnnotation,
-                        typeAliasDeclarations,
-                        imports
-                    }
-                },
-                { agentArgumentValue, agentResponseValue, valueLiteral }
-            ]) => {
-                return {
-                    src: {
-                        typeAnnotation,
-                        typeAliasDeclarations,
-                        imports,
-                        valueLiteral
-                    },
-                    agentArgumentValue,
-                    agentResponseValue
-                };
-            }
-        );
-}
-
-export function ServiceValueArb(
-    serviceDefinition: ServiceCandidDefinition
-): fc.Arbitrary<CandidValues<Principal>> {
-    return PrincipalValueArb.map((principal) => {
-        const valueLiteral = `${serviceDefinition.name}(${principal.valueLiteral})`;
-        const value = principal.agentArgumentValue;
-
-        return {
-            valueLiteral,
-            agentArgumentValue: value,
-            agentResponseValue: value
-        };
-    });
-}
-
-function generateTypeAliasDeclarations(
-    name: string,
-    serviceMethods: ServiceMethodDefinition[],
-    useTypeDeclaration: boolean
-): string[] {
-    const serviceMethodTypeAliasDecls = serviceMethods.flatMap(
-        (serviceMethod) => serviceMethod.typeAliasDeclarations
+    return CandidArb(
+        ServiceDefinitionArb,
+        ServiceValueArb,
+        candidDefinitionArb
     );
-    if (useTypeDeclaration) {
-        return [
-            ...serviceMethodTypeAliasDecls,
-            `const ${name} = ${generateTypeAnnotation(serviceMethods)};`
-        ];
-    }
-    return serviceMethodTypeAliasDecls;
-}
-
-function generateTypeAnnotation(serviceMethods: ServiceMethodDefinition[]) {
-    const methods = serviceMethods
-        .map((serviceMethod) => serviceMethod.src)
-        .filter((typeDeclaration) => typeDeclaration)
-        .join(',\n');
-
-    return `Canister({${methods}})`;
-}
-
-function generateImports(
-    serviceMethods: ServiceMethodDefinition[]
-): Set<string> {
-    return new Set([
-        ...serviceMethods.flatMap((serviceMethod) =>
-            Array.from(serviceMethod.imports)
-        ),
-        'Canister',
-        'query'
-    ]);
 }
