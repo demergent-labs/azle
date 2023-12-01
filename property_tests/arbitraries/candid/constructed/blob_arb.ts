4d915dd7f72ade34d5a465bd44a60e0177e58c07
Benjamin DeMann <benjamin.demann@gmail.com>
2023-11-30 21:36:06 -0700

    reorder blob arb

--- property_tests/arbitraries/candid/constructed/blob_arb.ts
+++ property_tests/arbitraries/candid/constructed/blob_arb.ts
@@ -9,49 +9,8 @@ import { SimpleCandidValuesArb } from '../simple_type_arbs/values_arb';
 import { UniqueIdentifierArb } from '../../unique_identifier_arb';
 import { CandidValueAndMeta } from '../value_and_meta_arb';
 
-const VecNat8DefinitionArb: fc.Arbitrary<BlobCandidDefinition> = fc
-    .tuple(UniqueIdentifierArb('typeDeclaration'), fc.boolean())
-    .map(([name, useTypeDeclaration]): BlobCandidDefinition => {
-        const typeAnnotation = 'Vec(nat8)';
-        const typeAliasDeclarations = useTypeDeclaration
-            ? [`const ${name} = ${typeAnnotation}`]
-            : [];
-        const imports = new Set(['Vec', 'nat8']);
-        return {
-            candidMeta: {
-                typeAnnotation,
-                typeAliasDeclarations,
-                imports,
-                candidType: CandidType.Blob
-            }
-        };
-    });
-
-export const BlobValueArb: fc.Arbitrary<CandidValues<Uint8Array>> =
-    SimpleCandidValuesArb(fc.uint8Array(), blobToSrcLiteral);
-
-const VecNat8CandidValueAndMetaArb: fc.Arbitrary<
-    CandidValueAndMeta<Uint8Array>
-> = fc.tuple(VecNat8DefinitionArb, BlobValueArb).map(
-    ([
-        {
-            candidMeta: { typeAnnotation, typeAliasDeclarations, imports }
-        },
-        { agentArgumentValue, agentResponseValue, valueLiteral }
-    ]): CandidValueAndMeta<Uint8Array> => ({
-        src: {
-            typeAnnotation,
-            imports,
-            typeAliasDeclarations,
-            valueLiteral
-        },
-        agentArgumentValue,
-        agentResponseValue
-    })
-);
-
 export const BlobArb = fc.oneof(
-    VecNat8CandidValueAndMetaArb,
+    VecNat8CandidValueAndMetaArb(),
     SimpleCandidValueAndMetaArb(
         fc.uint8Array(),
         CandidType.Blob,
@@ -61,5 +20,50 @@ export const BlobArb = fc.oneof(
 
 export const BlobDefinitionArb: fc.Arbitrary<BlobCandidDefinition> = fc.oneof(
     SimpleCandidDefinitionArb(CandidType.Blob),
-    VecNat8DefinitionArb
+    VecNat8DefinitionArb()
 );
+
+export const BlobValueArb: fc.Arbitrary<CandidValues<Uint8Array>> =
+    SimpleCandidValuesArb(fc.uint8Array(), blobToSrcLiteral);
+
+function VecNat8DefinitionArb(): fc.Arbitrary<BlobCandidDefinition> {
+    return fc
+        .tuple(UniqueIdentifierArb('typeDeclaration'), fc.boolean())
+        .map(([name, useTypeDeclaration]): BlobCandidDefinition => {
+            const typeAnnotation = 'Vec(nat8)';
+            const typeAliasDeclarations = useTypeDeclaration
+                ? [`const ${name} = ${typeAnnotation}`]
+                : [];
+            const imports = new Set(['Vec', 'nat8']);
+            return {
+                candidMeta: {
+                    typeAnnotation,
+                    typeAliasDeclarations,
+                    imports,
+                    candidType: CandidType.Blob
+                }
+            };
+        });
+}
+
+function VecNat8CandidValueAndMetaArb(): fc.Arbitrary<
+    CandidValueAndMeta<Uint8Array>
+> {
+    return fc.tuple(VecNat8DefinitionArb(), BlobValueArb).map(
+        ([
+            {
+                candidMeta: { typeAnnotation, typeAliasDeclarations, imports }
+            },
+            { agentArgumentValue, agentResponseValue, valueLiteral }
+        ]): CandidValueAndMeta<Uint8Array> => ({
+            src: {
+                typeAnnotation,
+                imports,
+                typeAliasDeclarations,
+                valueLiteral
+            },
+            agentArgumentValue,
+            agentResponseValue
+        })
+    );
+}
