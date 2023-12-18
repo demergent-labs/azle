9441406b0194c2c1c06fc4d3a6e88adba1d43a17
Dan Steren <dan@demergentlabs.org>
2023-12-18 13:24:00 -0700

    Revert accidental change
    This was accidentally updated in 3b9e879a9615d03c2a1ac112377f56934febc999

--- examples/query/src/index.ts
+++ examples/query/src/index.ts
@@ -1,37 +1,7 @@
-import {
-    Canister,
-    ic,
-    Opt,
-    query,
-    Record,
-    StableBTreeMap,
-    text,
-    update,
-    Vec,
-    Void
-} from 'azle';
-
-const Entry = Record({
-    key: text,
-    value: text
-});
-
-let db = StableBTreeMap<text, text>(0);
+import { Canister, query, text } from 'azle';
 
 export default Canister({
-    get: query([text], Opt(text), (key) => {
-        return db.get(key);
-    }),
-    set: update([text, text], Void, (key, value) => {
-        db.insert(key, value);
-    }),
-    setMany: update([Vec(Entry)], Void, (entries) => {
-        entries.forEach((entry) => {
-            if (entry.key === 'trap') {
-                ic.trap('explicit trap');
-            }
-
-            db.insert(entry.key, entry.value);
-        });
+    simpleQuery: query([], text, () => {
+        return 'This is a query function';
     })
 });
