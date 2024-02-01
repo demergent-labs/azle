e07770ced533be4479c1012fdc6994c183149c7f
Benjamin DeMann <benjamin.demann@gmail.com>
2024-02-01 14:07:23 -0700

    apply constraints all the way down

--- property_tests/arbitraries/candid/constructed/variant_arb/index.ts
+++ property_tests/arbitraries/candid/constructed/variant_arb/index.ts
@@ -7,16 +7,20 @@ import { VariantDefinitionArb } from './definition_arbs';
 import { VariantValuesArb } from './values_arb';
 import { CandidValueAndMetaArbGenerator } from '../../candid_value_and_meta_arb_generator';
 import { DEFAULT_DEFINITION_MAX_DEPTH } from '../../../config';
+import { CandidValueConstraints } from '../../candid_values_arb';
 
 export type Variant = {
     [x: string]: CorrespondingJSType;
 };
 
-export function VariantArb(): fc.Arbitrary<CandidValueAndMeta<Variant>> {
+export function VariantArb(
+    constraints?: CandidValueConstraints
+): fc.Arbitrary<CandidValueAndMeta<Variant>> {
     return CandidValueAndMetaArbGenerator(
         VariantDefinitionArb(candidDefinitionMemo, [], {
             depthLevel: DEFAULT_DEFINITION_MAX_DEPTH - 1
         }),
-        VariantValuesArb
+        VariantValuesArb,
+        constraints
     );
 }
