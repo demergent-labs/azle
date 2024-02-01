0c9ea480869bbaef19d18c9903c93230aa3f5d4b
Benjamin DeMann <benjamin.demann@gmail.com>
2024-02-01 16:21:06 -0700

    add fetch to ethereum json rpc

--- examples/ethereum_json_rpc/src/index.ts
+++ examples/ethereum_json_rpc/src/index.ts
@@ -5,6 +5,7 @@ import {
     nat32,
     Principal,
     query,
+    serialize,
     Some,
     StableBTreeMap,
     text,
@@ -31,37 +32,64 @@ export default Canister({
 
         const url = urlOpt.Some;
 
-        const httpResponse = await ic.call(managementCanister.http_request, {
-            args: [
-                {
-                    url,
-                    max_response_bytes: Some(2_000n),
-                    method: {
-                        post: null
-                    },
-                    headers: [],
-                    body: Some(
-                        Buffer.from(
-                            JSON.stringify({
-                                jsonrpc: '2.0',
-                                method: 'eth_getBalance',
-                                params: [ethereumAddress, 'earliest'],
-                                id: 1
-                            }),
-                            'utf-8'
-                        )
-                    ),
-                    transform: Some({
+        const azleArgs = [
+            {
+                url,
+                max_response_bytes: Some(2_000n),
+                method: {
+                    post: null
+                },
+                headers: [],
+                body: Some(
+                    Buffer.from(
+                        JSON.stringify({
+                            jsonrpc: '2.0',
+                            method: 'eth_getBalance',
+                            params: [ethereumAddress, 'earliest'],
+                            id: 1
+                        }),
+                        'utf-8'
+                    )
+                ),
+                transform: Some({
+                    function: [ic.id(), 'ethTransform'] as [Principal, string],
+                    context: Uint8Array.from([])
+                })
+            }
+        ];
+        const agentArgs = [
+            {
+                url,
+                max_response_bytes: [2_000n],
+                method: {
+                    post: null
+                },
+                headers: [],
+                body: [
+                    Buffer.from(
+                        JSON.stringify({
+                            jsonrpc: '2.0',
+                            method: 'eth_getBalance',
+                            params: [ethereumAddress, 'earliest'],
+                            id: 1
+                        }),
+                        'utf-8'
+                    )
+                ],
+                transform: [
+                    {
                         function: [ic.id(), 'ethTransform'] as [
                             Principal,
                             string
                         ],
                         context: Uint8Array.from([])
-                    })
-                }
-            ],
-            cycles: 50_000_000n
-        });
+                    }
+                ]
+            }
+        ];
+        const cycles = 50_000_000n;
+
+        const httpResponse = await getHttpResponse(azleArgs, agentArgs, cycles);
 
         return Buffer.from(httpResponse.body.buffer).toString('utf-8');
     }),
@@ -74,37 +102,64 @@ export default Canister({
 
         const url = urlOpt.Some;
 
-        const httpResponse = await ic.call(managementCanister.http_request, {
-            args: [
-                {
-                    url,
-                    max_response_bytes: Some(2_000n),
-                    method: {
-                        post: null
-                    },
-                    headers: [],
-                    body: Some(
-                        Buffer.from(
-                            JSON.stringify({
-                                jsonrpc: '2.0',
-                                method: 'eth_getBlockByNumber',
-                                params: [`0x${number.toString(16)}`, false],
-                                id: 1
-                            }),
-                            'utf-8'
-                        )
-                    ),
-                    transform: Some({
+        const azleArgs = [
+            {
+                url,
+                max_response_bytes: Some(2_000n),
+                method: {
+                    post: null
+                },
+                headers: [],
+                body: Some(
+                    Buffer.from(
+                        JSON.stringify({
+                            jsonrpc: '2.0',
+                            method: 'eth_getBlockByNumber',
+                            params: [`0x${number.toString(16)}`, false],
+                            id: 1
+                        }),
+                        'utf-8'
+                    )
+                ),
+                transform: Some({
+                    function: [ic.id(), 'ethTransform'] as [Principal, string],
+                    context: Uint8Array.from([])
+                })
+            }
+        ];
+        const agentArgs = [
+            {
+                url,
+                max_response_bytes: [2_000n],
+                method: {
+                    post: null
+                },
+                headers: [],
+                body: [
+                    Buffer.from(
+                        JSON.stringify({
+                            jsonrpc: '2.0',
+                            method: 'eth_getBlockByNumber',
+                            params: [`0x${number.toString(16)}`, false],
+                            id: 1
+                        }),
+                        'utf-8'
+                    )
+                ],
+                transform: [
+                    {
                         function: [ic.id(), 'ethTransform'] as [
                             Principal,
                             string
                         ],
                         context: Uint8Array.from([])
-                    })
-                }
-            ],
-            cycles: 50_000_000n
-        });
+                    }
+                ]
+            }
+        ];
+        const cycles = 50_000_000n;
+
+        const httpResponse = await getHttpResponse(azleArgs, agentArgs, cycles);
 
         return Buffer.from(httpResponse.body.buffer).toString('utf-8');
     }),
@@ -115,3 +170,23 @@ export default Canister({
         };
     })
 });
+
+async function getHttpResponse(azleArgs: any, agentArgs: any, cycles: bigint) {
+    if (process.env.AZLE_TEST_FETCH === 'true') {
+        const response = await fetch(`icp://aaaaa-aa/http_request`, {
+            body: serialize({
+                candidPath: '/candid/management.did',
+                args: agentArgs,
+                cycles
+            })
+        });
+        const responseJson = await response.json();
+
+        return responseJson;
+    } else {
+        return await ic.call(managementCanister.http_request, {
+            args: azleArgs,
+            cycles: 50_000_000n
+        });
+    }
+}
