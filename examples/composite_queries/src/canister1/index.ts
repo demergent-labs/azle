72f2e915b96b83c7809671c2c47fcf35b6fc4a17
Benjamin DeMann <benjamin.demann@gmail.com>
2024-02-02 11:41:04 -0700

    add fetch to composite query

--- examples/composite_queries/src/canister1/index.ts
+++ examples/composite_queries/src/canister1/index.ts
@@ -6,6 +6,7 @@ import {
     nat,
     Principal,
     query,
+    serialize,
     text,
     update
 } from 'azle';
@@ -16,45 +17,138 @@ let counter: nat = 0n;
 
 const CompQueryCanister = Canister({
     init: init([], () => {
-        canister2 = Canister2(
-            Principal.fromText(
-                process.env.CANISTER2_PRINCIPAL ??
-                    ic.trap('process.env.CANISTER2_PRINCIPAL is undefined')
-            )
-        );
+        canister2 = Canister2(Principal.fromText(getCanister2Principal()));
     }),
     // Composite query calling a query
     simpleCompositeQuery: query([], text, async () => {
-        return await ic.call(canister2.simpleQuery);
+        if (process.env.AZLE_TEST_FETCH === 'true') {
+            const response = await fetch(
+                `icp://${getCanister2Principal()}/simpleQuery`,
+                {
+                    body: serialize({
+                        candidPath: `/src/canister2.did`
+                    })
+                }
+            );
+            const responseJson = await response.json();
+
+            return responseJson;
+        } else {
+            return await ic.call(canister2.simpleQuery);
+        }
     }),
     // Composite query calling a manual query
     manualQuery: query([], text, async () => {
-        return (await ic.call(canister2.manualQuery)) as unknown as string; // TODO is this the best we can do for the types in this situation?
+        if (process.env.AZLE_TEST_FETCH === 'true') {
+            const response = await fetch(
+                `icp://${getCanister2Principal()}/manualQuery`,
+                {
+                    body: serialize({
+                        candidPath: `/src/canister2.did`
+                    })
+                }
+            );
+            const responseJson = await response.json();
+
+            return responseJson;
+        } else {
+            return (await ic.call(canister2.manualQuery)) as unknown as string; // TODO is this the best we can do for the types in this situation?
+        }
     }),
     // Manual composite query calling a manual query
     totallyManualQuery: query(
         [],
         Manual(text),
         async () => {
-            ic.reply(await ic.call(canister2.manualQuery), text);
+            if (process.env.AZLE_TEST_FETCH === 'true') {
+                const response = await fetch(
+                    `icp://${getCanister2Principal()}/manualQuery`,
+                    {
+                        body: serialize({
+                            candidPath: `/src/canister2.did`
+                        })
+                    }
+                );
+                const responseJson = await response.json();
+
+                ic.reply(responseJson, text);
+            } else {
+                ic.reply(await ic.call(canister2.manualQuery), text);
+            }
         },
         { manual: true }
     ),
     // Composite query calling another composite query
     deepQuery: query([], text, async () => {
-        return await ic.call(canister2.deepQuery);
+        if (process.env.AZLE_TEST_FETCH === 'true') {
+            const response = await fetch(
+                `icp://${getCanister2Principal()}/deepQuery`,
+                {
+                    body: serialize({
+                        candidPath: `/src/canister2.did`
+                    })
+                }
+            );
+            const responseJson = await response.json();
+
+            return responseJson;
+        } else {
+            return await ic.call(canister2.deepQuery);
+        }
     }),
     // Composite query calling an update method. SHOULDN'T WORK
     updateQuery: query([], text, async () => {
-        return await ic.call(canister2.updateQuery);
+        if (process.env.AZLE_TEST_FETCH === 'true') {
+            const response = await fetch(
+                `icp://${getCanister2Principal()}/updateQuery`,
+                {
+                    body: serialize({
+                        candidPath: `/src/canister2.did`
+                    })
+                }
+            );
+            const responseJson = await response.json();
+
+            return responseJson;
+        } else {
+            return await ic.call(canister2.updateQuery);
+        }
     }),
     // Composite query being called by a query method. SHOULDN'T WORK
     simpleQuery: query([], text, async () => {
-        return await ic.call(canister2.simpleQuery);
+        if (process.env.AZLE_TEST_FETCH === 'true') {
+            const response = await fetch(
+                `icp://${getCanister2Principal()}/simpleQuery`,
+                {
+                    body: serialize({
+                        candidPath: `/src/canister2.did`
+                    })
+                }
+            );
+            const responseJson = await response.json();
+
+            return responseJson;
+        } else {
+            return await ic.call(canister2.simpleQuery);
+        }
     }),
     // Composite query being called by an update method. SHOULDN'T WORK
     simpleUpdate: update([], text, async () => {
-        return await ic.call(canister2.deepQuery);
+        if (process.env.AZLE_TEST_FETCH === 'true') {
+            const response = await fetch(
+                `icp://${getCanister2Principal()}/deepQuery`,
+                {
+                    body: serialize({
+                        candidPath: `/src/canister2.did`
+                    })
+                }
+            );
+            const responseJson = await response.json();
+
+            return responseJson;
+        } else {
+            return await ic.call(canister2.deepQuery);
+        }
     }),
     // Composite query that modifies the state. Should revert after the call is done
     incCounter: query([], nat, async () => {
@@ -68,8 +162,8 @@ const CompQueryCanister = Canister({
 
         counter += 1n;
 
-        const canister1AResult = await ic.call(self.incCounter);
-        const canister1BResult = await ic.call(self.incCounter);
+        const canister1AResult = await incCanister1(self);
+        const canister1BResult = await incCanister1(self);
 
         return counter + canister1AResult + canister1BResult;
     }),
@@ -77,11 +171,55 @@ const CompQueryCanister = Canister({
     incCanister2: query([], nat, async () => {
         counter += 1n;
 
-        const canister2AResult = await ic.call(canister2.incCounter);
-        const canister2BResult = await ic.call(canister2.incCounter);
+        const canister2AResult = await incCanister2();
+        const canister2BResult = await incCanister2();
 
         return counter + canister2AResult + canister2BResult;
     })
 });
 
 export default CompQueryCanister;
+
+function getCanister2Principal(): string {
+    if (process.env.CANISTER2_PRINCIPAL !== undefined) {
+        return process.env.CANISTER2_PRINCIPAL;
+    }
+
+    throw new Error(`process.env.CANISTER2_PRINCIPAL is not defined`);
+}
+
+async function incCanister1(canister1: any) {
+    if (process.env.AZLE_TEST_FETCH === 'true') {
+        const response = await fetch(
+            `icp://${canister1.principal.toText()}/incCounter`,
+            {
+                body: serialize({
+                    candidPath: `/src/canister1.did`
+                })
+            }
+        );
+        const responseJson = await response.json();
+
+        return responseJson;
+    } else {
+        return await ic.call(canister1.incCounter);
+    }
+}
+
+async function incCanister2() {
+    if (process.env.AZLE_TEST_FETCH === 'true') {
+        const response = await fetch(
+            `icp://${getCanister2Principal()}/incCounter`,
+            {
+                body: serialize({
+                    candidPath: `/src/canister2.did`
+                })
+            }
+        );
+        const responseJson = await response.json();
+
+        return responseJson;
+    } else {
+        return await ic.call(canister2.incCounter);
+    }
+}
