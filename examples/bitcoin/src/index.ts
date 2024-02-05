368d038f9f61a8df28f7898746aeedfc99cd3a55
Benjamin DeMann <benjamin.demann@gmail.com>
2024-02-05 14:43:41 -0700

    add fetch to bitcoin example

--- examples/bitcoin/src/index.ts
+++ examples/bitcoin/src/index.ts
@@ -1,4 +1,14 @@
-import { blob, bool, Canister, ic, None, text, update, Vec } from 'azle';
+import {
+    blob,
+    bool,
+    Canister,
+    ic,
+    None,
+    serialize,
+    text,
+    update,
+    Vec
+} from 'azle';
 import {
     GetUtxosResult,
     managementCanister,
@@ -9,44 +19,111 @@ import {
 const BITCOIN_API_CYCLE_COST = 100_000_000n;
 const BITCOIN_BASE_TRANSACTION_COST = 5_000_000_000n;
 const BITCOIN_CYCLE_COST_PER_TRANSACTION_BYTE = 20_000_000n;
+/*
+
+    if (process.env.AZLE_TEST_FETCH === 'true') {
+        const response = await fetch(`icp://aaaaa-aa/raw_rand`);
+        const responseJson = await response.json();
+
+        return responseJson;
+    } else {
+        return await ic.call(managementCanister.raw_rand);
+    }
+
+    */
 
 export default Canister({
     getBalance: update([text], Satoshi, async (address) => {
-        return await ic.call(managementCanister.bitcoin_get_balance, {
-            args: [
-                {
-                    address,
-                    min_confirmations: None,
-                    network: { Regtest: null }
-                }
-            ],
-            cycles: BITCOIN_API_CYCLE_COST
-        });
-    }),
-    getCurrentFeePercentiles: update([], Vec(MillisatoshiPerByte), async () => {
-        return await ic.call(
-            managementCanister.bitcoin_get_current_fee_percentiles,
-            {
+        if (process.env.AZLE_TEST_FETCH === 'true' || true) {
+            const response = await fetch(`icp://aaaaa-aa/bitcoin_get_balance`, {
+                body: serialize({
+                    args: [
+                        {
+                            address,
+                            min_confirmations: None,
+                            network: { Regtest: null }
+                        }
+                    ],
+                    cycles: BITCOIN_API_CYCLE_COST
+                })
+            });
+            const responseJson = await response.json();
+
+            return responseJson;
+        } else {
+            return await ic.call(managementCanister.bitcoin_get_balance, {
                 args: [
                     {
+                        address,
+                        min_confirmations: None,
                         network: { Regtest: null }
                     }
                 ],
                 cycles: BITCOIN_API_CYCLE_COST
-            }
-        );
+            });
+        }
+    }),
+    getCurrentFeePercentiles: update([], Vec(MillisatoshiPerByte), async () => {
+        if (process.env.AZLE_TEST_FETCH === 'true' || true) {
+            const response = await fetch(
+                `icp://aaaaa-aa/bitcoin_get_current_fee_percentiles`,
+                {
+                    body: serialize({
+                        args: [
+                            {
+                                network: { Regtest: null }
+                            }
+                        ],
+                        cycles: BITCOIN_API_CYCLE_COST
+                    })
+                }
+            );
+            const responseJson = await response.json();
+
+            return responseJson;
+        } else {
+            return await ic.call(
+                managementCanister.bitcoin_get_current_fee_percentiles,
+                {
+                    args: [
+                        {
+                            network: { Regtest: null }
+                        }
+                    ],
+                    cycles: BITCOIN_API_CYCLE_COST
+                }
+            );
+        }
     }),
     getUtxos: update([text], GetUtxosResult, async (address) => {
-        return await ic.call(managementCanister.bitcoin_get_utxos, {
-            args: [
-                {
-                    address,
-                    filter: None,
-                    network: { Regtest: null }
-                }
-            ],
-            cycles: BITCOIN_API_CYCLE_COST
-        });
+        if (process.env.AZLE_TEST_FETCH === 'true' || true) {
+            const response = await fetch(`icp://aaaaa-aa/bitcoin_get_utxos`, {
+                body: serialize({
+                    args: [
+                        {
+                            address,
+                            filter: None,
+                            network: { Regtest: null }
+                        }
+                    ],
+                    cycles: BITCOIN_API_CYCLE_COST
+                })
+            });
+            const responseJson = await response.json();
+
+            return responseJson;
+        } else {
+            return await ic.call(managementCanister.bitcoin_get_utxos, {
+                args: [
+                    {
+                        address,
+                        filter: None,
+                        network: { Regtest: null }
+                    }
+                ],
+                cycles: BITCOIN_API_CYCLE_COST
+            });
+        }
     }),
     sendTransaction: update([blob], bool, async (transaction) => {
         const transactionFee =
@@ -54,15 +131,33 @@ export default Canister({
             BigInt(transaction.length) *
                 BITCOIN_CYCLE_COST_PER_TRANSACTION_BYTE;
 
-        await ic.call(managementCanister.bitcoin_send_transaction, {
-            args: [
+        if (process.env.AZLE_TEST_FETCH === 'true' || true) {
+            const response = await fetch(
+                `icp://aaaaa-aa/bitcoin_send_transaction`,
                 {
-                    transaction,
-                    network: { Regtest: null }
+                    body: serialize({
+                        args: [
+                            {
+                                transaction,
+                                network: { Regtest: null }
+                            }
+                        ],
+                        cycles: transactionFee
+                    })
                 }
-            ],
-            cycles: transactionFee
-        });
+            );
+            await response.json();
+        } else {
+            await ic.call(managementCanister.bitcoin_send_transaction, {
+                args: [
+                    {
+                        transaction,
+                        network: { Regtest: null }
+                    }
+                ],
+                cycles: transactionFee
+            });
+        }
 
         return true;
     })
