91684a4cc9d1e9288e7a98c064e919718e93559c
Jordan Last <jordan.michael.last@gmail.com>
2024-02-07 18:45:11 -0600

    release 0.20.2-rc.6

--- publish-github-action.sh
+++ publish-github-action.sh
@@ -23,8 +23,7 @@ fi
 # TODO loop through checking for the status instead of sleeping
 echo -e "sleeping for 30 seconds to ensure azle@$VERSION is fully registered on npm"
 
-# TODO put this all back of course
-# sleep 30
+sleep 30
 
 for directory in ${directories[@]}
 do
@@ -36,8 +35,6 @@ do
     rm -rf node_modules
 
     cd $root_dir
-
-    break
 done
 
 git add --all
