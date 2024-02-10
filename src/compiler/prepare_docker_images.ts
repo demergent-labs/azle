98a700cfb9cd9666edc9a8e0ec323fa3ecc9d239
Jordan Last <jordan.michael.last@gmail.com>
2024-02-10 14:58:08 -0600

    refactor the docker stuff

--- src/compiler/prepare_docker_images.ts
+++ src/compiler/prepare_docker_images.ts
@@ -12,91 +12,144 @@ export function prepareDockerImage(
     dockerContainerName: string,
     wasmedgeQuickJsPath: string
 ) {
-    const imageHasBeenLoaded = hasImageBeenLoaded(dockerImageName, stdioType);
+    buildAndLoadImage(
+        stdioType,
+        dockerImageName,
+        dockerImagePathTar,
+        dockerImagePathTarGz
+    );
 
-    if (process.env.AZLE_USE_DOCKERFILE === 'true') {
-        try {
-            if (!imageHasBeenLoaded) {
-                if (existsSync(dockerImagePathTar)) {
-                    console.info(yellow(`\nLoading image...\n`));
+    createAndStartContainer(
+        stdioType,
+        dockerImageName,
+        dockerContainerName,
+        wasmedgeQuickJsPath
+    );
+}
 
-                    execSync(`podman load -i ${dockerImagePathTar}`, {
-                        stdio: 'inherit'
-                    });
-                } else if (existsSync(dockerImagePathTarGz)) {
-                    console.info(yellow(`\nLoading image...\n`));
+function buildAndLoadImage(
+    stdioType: IOType,
+    dockerImageName: string,
+    dockerImagePathTar: string,
+    dockerImagePathTarGz: string
+) {
+    const imageHasAlreadyBeenLoaded = hasImageAlreadyBeenLoaded(
+        stdioType,
+        dockerImageName
+    );
 
-                    execSync(`podman load -i ${dockerImagePathTarGz}`, {
-                        stdio: 'inherit'
-                    });
-                } else {
-                    throw new Error(
-                        `${dockerImagePathTar} or ${dockerImagePathTarGz} does not exist`
-                    );
-                }
-            }
-        } catch (error) {
-            console.info(yellow(`\nBuilding image...\n`));
-
-            execSync(
-                `podman build -f ${__dirname}/Dockerfile -t ${dockerImageName} ${__dirname}`,
-                {
-                    stdio: 'inherit'
-                }
-            );
-
-            console.info(yellow(`\nSaving image...\n`));
-
-            execSync(
-                `podman save -o ${dockerImagePathTar} ${dockerImageName}`,
-                {
-                    stdio: 'inherit'
-                }
-            );
-
-            console.info(yellow(`\nCompiling...`));
-        }
-    } else {
-        try {
-            if (!imageHasBeenLoaded) {
-                if (existsSync(dockerImagePathTar)) {
-                    console.info(yellow(`\nLoading image...\n`));
-
-                    execSync(`podman load -i ${dockerImagePathTar}`, {
-                        stdio: 'inherit'
-                    });
-                } else if (existsSync(dockerImagePathTarGz)) {
-                    console.info(yellow(`\nLoading image...\n`));
-
-                    execSync(`podman load -i ${dockerImagePathTarGz}`, {
-                        stdio: 'inherit'
-                    });
-                } else {
-                    throw new Error(
-                        `${dockerImagePathTar} or ${dockerImagePathTarGz} does not exist`
-                    );
-                }
-            }
-        } catch (error) {
-            console.info(yellow(`\nDownloading image...\n`));
-
-            execSync(
-                `curl -L https://github.com/demergent-labs/azle/releases/download/${azleVersion}/${dockerImageName}.tar.gz -o ${dockerImagePathTarGz}`,
-                {
-                    stdio: 'inherit'
-                }
-            );
-
-            console.info(yellow(`\nLoading image...\n`));
-
-            execSync(`podman load -i ${dockerImagePathTarGz}`, {
-                stdio: 'inherit'
-            });
-
-            console.info(yellow(`\nCompiling...`));
-        }
+    if (imageHasAlreadyBeenLoaded === true) {
+        return;
     }
 
+    const existingLocalImageLoaded = loadExistingLocalImage(
+        dockerImagePathTar,
+        dockerImagePathTarGz
+    );
+
+    if (existingLocalImageLoaded === true) {
+        return;
+    }
+
+    if (process.env.AZLE_USE_DOCKERFILE === 'true') {
+        buildAndLoadImageWithDockerfile(dockerImageName, dockerImagePathTar);
+    } else {
+        buildAndLoadRemoteImage(dockerImageName, dockerImagePathTarGz);
+    }
+}
+
+function hasImageAlreadyBeenLoaded(
+    stdioType: IOType,
+    dockerImageName: string
+): boolean {
+    try {
+        execSync(`podman image inspect ${dockerImageName}`, {
+            stdio: stdioType
+        });
+
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
+function loadExistingLocalImage(
+    dockerImagePathTar: string,
+    dockerImagePathTarGz: string
+): boolean {
+    if (existsSync(dockerImagePathTar)) {
+        console.info(yellow(`\nLoading image...\n`));
+
+        execSync(`podman load -i ${dockerImagePathTar}`, {
+            stdio: 'inherit'
+        });
+
+        return true;
+    }
+
+    if (existsSync(dockerImagePathTarGz)) {
+        console.info(yellow(`\nLoading image...\n`));
+
+        execSync(`podman load -i ${dockerImagePathTarGz}`, {
+            stdio: 'inherit'
+        });
+
+        return true;
+    }
+
+    return false;
+}
+
+function buildAndLoadImageWithDockerfile(
+    dockerImageName: string,
+    dockerImagePathTar: string
+) {
+    console.info(yellow(`\nBuilding image...\n`));
+
+    execSync(
+        `podman build -f ${__dirname}/Dockerfile -t ${dockerImageName} ${__dirname}`,
+        {
+            stdio: 'inherit'
+        }
+    );
+
+    console.info(yellow(`\nSaving image...\n`));
+
+    execSync(`podman save -o ${dockerImagePathTar} ${dockerImageName}`, {
+        stdio: 'inherit'
+    });
+
+    console.info(yellow(`\nCompiling...`));
+}
+
+function buildAndLoadRemoteImage(
+    dockerImageName: string,
+    dockerImagePathTarGz: string
+) {
+    console.info(yellow(`\nDownloading image...\n`));
+
+    execSync(
+        `curl -L https://github.com/demergent-labs/azle/releases/download/${azleVersion}/${dockerImageName}.tar.gz -o ${dockerImagePathTarGz}`,
+        {
+            stdio: 'inherit'
+        }
+    );
+
+    console.info(yellow(`\nLoading image...\n`));
+
+    execSync(`podman load -i ${dockerImagePathTarGz}`, {
+        stdio: 'inherit'
+    });
+
+    console.info(yellow(`\nCompiling...`));
+}
+
+function createAndStartContainer(
+    stdioType: IOType,
+    dockerImageName: string,
+    dockerContainerName: string,
+    wasmedgeQuickJsPath: string
+) {
     execSync(
         `podman inspect ${dockerContainerName} || podman create --name ${dockerContainerName} ${dockerImageName} tail -f /dev/null`,
         { stdio: stdioType }
@@ -110,19 +163,4 @@ export function prepareDockerImage(
         `podman cp ${dockerContainerName}:/wasmedge-quickjs ${wasmedgeQuickJsPath}`,
         { stdio: stdioType }
     );
-}
-
-function hasImageBeenLoaded(
-    dockerImageName: string,
-    stdioType: IOType
-): boolean {
-    try {
-        execSync(`podman image inspect ${dockerImageName}`, {
-            stdio: stdioType
-        });
-
-        return true;
-    } catch (error) {
-        return false;
-    }
 }
