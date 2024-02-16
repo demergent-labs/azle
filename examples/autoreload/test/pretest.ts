adc403f4ac8486a1e78429dcad303b4040c8a50e
Jordan Last <jordan.michael.last@gmail.com>
2024-02-16 15:51:16 -0600

    fix server.ts error, write to the file in pretest

--- examples/autoreload/test/pretest.ts
+++ examples/autoreload/test/pretest.ts
@@ -1,7 +1,11 @@
 import { execSync } from 'child_process';
+import { writeFileSync } from 'fs';
+import { originalServerTs } from './tests';
 
 async function pretest() {
     await new Promise((resolve) => setTimeout(resolve, 5000));
+
+    writeFileSync('./src/server.ts', originalServerTs);
 
     execSync(`dfx canister uninstall-code autoreload || true`, {
         stdio: 'inherit'
--- examples/autoreload/test/tests.ts
+++ examples/autoreload/test/tests.ts
@@ -1,35 +1,29 @@
+// TODO add some tests where the file is written to rapidly
+
 import * as dns from 'node:dns';
 dns.setDefaultResultOrder('ipv4first');
 
 import { Test } from 'azle/test';
 import { writeFileSync } from 'fs';
 
-export function getTests(canisterId: string): Test[] {
-    const origin = `http://${canisterId}.localhost:8000`;
-
-    const originalServerTs = `import { Server } from 'azle';
+export const originalServerTs = `import { Server } from 'azle';
 import express from 'express';
 
 export default Server(() => {
-    const app = express();
+const app = express();
 
-    app.get('/test', (req, res) => {
-        res.send('test');
-    });
+app.get('/test', (req, res) => {
+    res.send('test');
+});
 
-    return app.listen();
+return app.listen();
 });
 `;
 
+export function getTests(canisterId: string): Test[] {
+    const origin = `http://${canisterId}.localhost:8000`;
+
     return [
-        {
-            name: 'write original file contents',
-            prep: async () => {
-                writeFileSync('./src/server.ts', originalServerTs);
-            }
-        },
-        // TODO without this wait we have things messed up...we should fix and remove the waits perhaps
-        // TODO or write tests to ensure that it doesn't matter how quickly you write to the file
         {
             name: 'waiting for Azle to reload',
             wait: 5_000
