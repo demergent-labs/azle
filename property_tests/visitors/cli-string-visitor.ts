ffb952f26564f05a2eda8732eeed948ea9e66a79
Benjamin DeMann <benjamin.demann@gmail.com>
2024-02-16 14:46:16 -0700

    pr fixes

--- property_tests/visitors/cli-string-visitor.ts
+++ property_tests/visitors/cli-string-visitor.ts
@@ -10,8 +10,8 @@ export type VisitorData = { value: any };
 export class CliStringVisitor extends IDL.Visitor<VisitorData, string> {
     visitFloat(_t: IDL.FloatClass, data: VisitorData) {
         /**
-         * If a float doesn't have a decimal it won't serialize properly, so 10 while
-         * is a float won't serialize unless it's 10.0
+         * If a float doesn't have a decimal it won't serialize properly, so
+         * while 10 is a float it won't serialize unless it's 10.0
          */
         const floatString = data.value.toString();
         if (floatString.includes('.') || floatString.includes('e')) {
@@ -71,7 +71,6 @@ export class CliStringVisitor extends IDL.Visitor<VisitorData, string> {
         data: VisitorData
     ): string {
         for (const [name, type] of fields) {
-            // eslint-disable-next-line
             if (data.value.hasOwnProperty(name)) {
                 const value = type.accept(this, { value: data.value[name] });
                 if (value === 'null') {
@@ -88,7 +87,6 @@ export class CliStringVisitor extends IDL.Visitor<VisitorData, string> {
         ty: IDL.Type<T>,
         data: VisitorData
     ): string {
-        const arr = asAnyArray(data.value); // TO REVIEW Do you like this or the any down bellow?
         const elements = data.value.map((e: any) => {
             return ty.accept(this, { value: e });
         });
@@ -97,41 +95,6 @@ export class CliStringVisitor extends IDL.Visitor<VisitorData, string> {
     visitType<T>(t: IDL.Type<T>, data: VisitorData): string {
         return t.valueToString(data.value);
     }
-}
-
-type AnyArray =
-    | Array<any>
-    | ReadonlyArray<any>
-    | Uint8Array
-    | Int8Array
-    | Uint8ClampedArray
-    | Uint16Array
-    | Int16Array
-    | Uint32Array
-    | Int32Array
-    | Float32Array
-    | Float64Array
-    | BigInt64Array
-    | BigUint64Array;
-
-function asAnyArray(input: any): AnyArray {
-    if (
-        Array.isArray(input) ||
-        input instanceof Int8Array ||
-        input instanceof Uint8Array ||
-        input instanceof Uint8ClampedArray ||
-        input instanceof Int16Array ||
-        input instanceof Uint16Array ||
-        input instanceof Int32Array ||
-        input instanceof Uint32Array ||
-        input instanceof Float32Array ||
-        input instanceof Float64Array ||
-        input instanceof BigInt64Array ||
-        input instanceof BigUint64Array
-    ) {
-        return input;
-    }
-    throw new Error("It wasn't an array");
 }
 
 function escapeForBash(input: string) {
