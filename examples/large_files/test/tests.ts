ec5229081224a9d9c7d295a6a98fe99e933fcd5d
Benjamin DeMann <benjamin.demann@gmail.com>
2024-02-20 21:46:17 -0700

    add large files example

---
+++ examples/large_files/test/tests.ts
@@ -0,0 +1,17 @@
+import * as dns from 'node:dns';
+dns.setDefaultResultOrder('ipv4first');
+
+import { Test } from 'azle/test';
+
+export function getTests(canisterId: string): Test[] {
+    const origin = `http://${canisterId}.localhost:8000`;
+
+    return [
+        {
+            name: '/',
+            test: async () => {
+                return { Ok: true };
+            }
+        }
+    ];
+}
