5c8bf25ee2c5e5136e2d6fc8274cf25cd9c3e946
Jordan Last <jordan.michael.last@gmail.com>
2024-03-08 12:12:50 -0600

    attempt to make more nodejs compatible

--- http_client/fetch_ic/host.ts
+++ http_client/fetch_ic/host.ts
@@ -1,10 +1,26 @@
-export function getHostFromBrowser(): string {
-    const runningLocally =
-        window.location.host.includes(`localhost:`) ||
-        window.location.host.includes(`127.0.0.1:`);
+export function getHost(input: RequestInfo | URL): string {
+    const hostString = getHostString(input);
 
-    const host =
-        runningLocally === true ? window.location.host : 'http://icp-api.io';
+    const runningLocally =
+        hostString.includes(`localhost:`) || hostString.includes(`127.0.0.1:`);
+
+    const host = runningLocally === true ? hostString : 'http://icp-api.io';
 
     return host;
+}
+
+function getHostString(input: RequestInfo | URL): string {
+    if (typeof input === 'string') {
+        return new URL(input).host;
+    }
+
+    if (input instanceof URL) {
+        return input.host;
+    }
+
+    if (input instanceof Request) {
+        return getHostString(input.url);
+    }
+
+    throw new Error(`fetchIc: input not supported`);
 }
--- http_client/fetch_ic/index.ts
+++ http_client/fetch_ic/index.ts
@@ -1,19 +1,17 @@
-// TODO document that the following will not work: cache, keepalive
-
-// TODO should we ensure that this works in Node.js as well? Or just focus on the browser?
+// TODO should we write tests for nodejs as well then?
 
 import { createActor } from './actor';
 import { call } from './call';
 import { getCanisterId } from './get_canister_id';
-import { getHostFromBrowser } from './host';
+import { getHost } from './host';
 import { getIdentity } from './identity';
 import { createResponse } from './response';
 
-const originalFetch = window.fetch;
+const originalFetch = globalThis.fetch;
 
 // TODO it would be nice to get rid of the any cast
 // TODO and to ensure that we really are using the correct types for fetch
-(window as any).fetch = fetchIc;
+(globalThis as any).fetch = fetchIc;
 
 // TODO implement credentials
 // TODO implement integrity
@@ -33,7 +31,7 @@ export async function fetchIc(
     }
 
     const canisterId = getCanisterId(input);
-    const host = getHostFromBrowser();
+    const host = getHost(input);
     const actor = await createActor(identity, canisterId, host);
 
     const callResult = await call(input, init, actor);
@@ -45,4 +43,4 @@ export async function fetchIc(
 export { createActor } from './actor';
 export { createAgent } from './agent';
 export { getCanisterId };
-export { getHostFromBrowser } from './host';
+export { getHost } from './host';
