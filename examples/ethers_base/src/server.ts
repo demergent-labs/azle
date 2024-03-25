4f5f1f1bb48d13ba2b9fe98665d15406f48bd405
Jordan Last <jordan.michael.last@gmail.com>
2024-03-25 14:49:46 -0500

    remove Server export from ethers_base example

--- examples/ethers_base/src/server.ts
+++ examples/ethers_base/src/server.ts
@@ -1,37 +1,70 @@
-import { ic, jsonStringify, Server, ThresholdWallet } from 'azle';
+import { ic, jsonStringify, ThresholdWallet } from 'azle';
 import { ethers } from 'ethers';
 import express, { Request } from 'express';
 
-// TODO remember to remove the need for Server here
-export default Server(() => {
-    const app = express();
+const app = express();
 
-    app.use(express.json());
+app.use(express.json());
 
-    // TODO I would like to test usdc, maybe this is for a different example though?
-    // app.post('/test-usdc', (req, res) => {
-    //     const wallet = new ThresholdWallet(
-    //         {
-    //             derivationPath: [ic.caller().toUint8Array()]
-    //         },
-    //         ethers.getDefaultProvider('https://sepolia.base.org')
-    //     );
+app.post('/caller-address', async (_req, res) => {
+    const wallet = new ThresholdWallet(
+        {
+            derivationPath: [ic.caller().toUint8Array()]
+        },
+        ethers.getDefaultProvider('https://sepolia.base.org')
+    );
 
-    //     // const result = await wallet.call
-    // });
+    res.send(await wallet.getAddress());
+});
 
-    app.post('/caller-address', async (_req, res) => {
-        const wallet = new ThresholdWallet(
-            {
-                derivationPath: [ic.caller().toUint8Array()]
-            },
+app.post('/canister-address', async (_req, res) => {
+    const wallet = new ThresholdWallet(
+        {
+            derivationPath: [ic.id().toUint8Array()]
+        },
+        ethers.getDefaultProvider('https://sepolia.base.org')
+    );
+
+    res.send(await wallet.getAddress());
+});
+
+app.post(
+    '/address-balance',
+    async (req: Request<any, any, { address: string }>, res) => {
+        const balance = await ethers
+            .getDefaultProvider('https://sepolia.base.org')
+            .getBalance(req.body.address);
+
+        res.send(jsonStringify(balance));
+    }
+);
+
+app.post(
+    '/transfer-from-sepolia-faucet-wallet',
+    async (req: Request<any, any, { to: string; value: string }>, res) => {
+        // address: 0x9Ac70EE21bE697173b74aF64399d850038697FD3
+        const wallet = new ethers.Wallet(
+            '0x6f784763681eb712dc16714b8ade23f6c982a5872d054059dd64d0ec4e52be33',
             ethers.getDefaultProvider('https://sepolia.base.org')
         );
 
-        res.send(await wallet.getAddress());
-    });
+        const to = req.body.to;
+        const value = ethers.parseEther(req.body.value);
+        const gasLimit = 21_000n;
 
-    app.post('/canister-address', async (_req, res) => {
+        const tx = await wallet.sendTransaction({
+            to,
+            value,
+            gasLimit
+        });
+
+        res.send(`transaction sent with hash: ${tx.hash}`);
+    }
+);
+
+app.post(
+    '/transfer-from-canister',
+    async (req: Request<any, any, { to: string; value: string }>, res) => {
         const wallet = new ThresholdWallet(
             {
                 derivationPath: [ic.id().toUint8Array()]
@@ -39,66 +72,18 @@ export default Server(() => {
             ethers.getDefaultProvider('https://sepolia.base.org')
         );
 
-        res.send(await wallet.getAddress());
-    });
+        const to = req.body.to;
+        const value = ethers.parseEther(req.body.value);
+        const gasLimit = 21_000n;
 
-    app.post(
-        '/address-balance',
-        async (req: Request<any, any, { address: string }>, res) => {
-            const balance = await ethers
-                .getDefaultProvider('https://sepolia.base.org')
-                .getBalance(req.body.address);
+        const tx = await wallet.sendTransaction({
+            to,
+            value,
+            gasLimit
+        });
 
-            res.send(jsonStringify(balance));
-        }
-    );
+        res.send(`transaction sent with hash: ${tx.hash}`);
+    }
+);
 
-    app.post(
-        '/transfer-from-sepolia-faucet-wallet',
-        async (req: Request<any, any, { to: string; value: string }>, res) => {
-            // address: 0x9Ac70EE21bE697173b74aF64399d850038697FD3
-            const wallet = new ethers.Wallet(
-                '0x6f784763681eb712dc16714b8ade23f6c982a5872d054059dd64d0ec4e52be33',
-                ethers.getDefaultProvider('https://sepolia.base.org')
-            );
-
-            const to = req.body.to;
-            const value = ethers.parseEther(req.body.value);
-            const gasLimit = 21_000n;
-
-            const tx = await wallet.sendTransaction({
-                to,
-                value,
-                gasLimit
-            });
-
-            res.send(`transaction sent with hash: ${tx.hash}`);
-        }
-    );
-
-    app.post(
-        '/transfer-from-canister',
-        async (req: Request<any, any, { to: string; value: string }>, res) => {
-            const wallet = new ThresholdWallet(
-                {
-                    derivationPath: [ic.id().toUint8Array()]
-                },
-                ethers.getDefaultProvider('https://sepolia.base.org')
-            );
-
-            const to = req.body.to;
-            const value = ethers.parseEther(req.body.value);
-            const gasLimit = 21_000n;
-
-            const tx = await wallet.sendTransaction({
-                to,
-                value,
-                gasLimit
-            });
-
-            res.send(`transaction sent with hash: ${tx.hash}`);
-        }
-    );
-
-    return app.listen();
-});
+app.listen();
