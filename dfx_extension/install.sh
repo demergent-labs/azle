4d08dca40faf9007205c4e54c40d3cffe1bc5847
Jordan Last <jordan.michael.last@gmail.com>
2024-04-25 06:59:54 -0500

    only run dfx cache install if the cache directory does not yet exist

--- dfx_extension/install.sh
+++ dfx_extension/install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 DFX_CACHE_DIR="$(dfx cache show)"
-dfx cache install
+
+if [ ! -d "$DFX_CACHE_DIR" ]; then
+    dfx cache install
+fi
+
 mkdir -p "$DFX_CACHE_DIR/extensions/azle"
 cp extension.json "$DFX_CACHE_DIR/extensions/azle/extension.json"
