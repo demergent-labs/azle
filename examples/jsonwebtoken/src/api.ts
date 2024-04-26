cca2f27520105deb4f3fda813a64b153be13caa0
Jordan Last <jordan.michael.last@gmail.com>
2024-04-26 07:12:48 -0500

    create jsonwebtoken example using ICP randomness as the secret

---
+++ examples/jsonwebtoken/src/api.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import jwt from 'jsonwebtoken';
+
+const app = express();
+
+app.post('/create-token', async (req, res) => {
+    console.log(0);
+    const response = await fetch('icp://aaaaa-aa/raw_rand');
+    const randomness = Buffer.from(await response.json());
+
+    console.log('randomness', randomness);
+
+    const payload = {
+        userId: '123456',
+        username: 'exampleUser'
+    };
+
+    console.log('payload done');
+
+    // TODO it breaks here
+    const token = jwt.sign(payload, randomness, {
+        algorithm: 'HS256',
+        expiresIn: '1h'
+    });
+
+    console.log('token done');
+
+    res.send(token);
+});
+
+app.listen();
