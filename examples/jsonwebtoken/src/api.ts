7aeec1deb6dfe35aac99e5cc86ec793c2119b2d1
Jordan Last <jordan.michael.last@gmail.com>
2024-04-26 07:54:23 -0500

    attempting to track down jsonwebtoken dependency problems, looks like we just do not have the crypto module implemented like we need it

--- examples/jsonwebtoken/src/api.ts
+++ examples/jsonwebtoken/src/api.ts
@@ -4,28 +4,32 @@ import jwt from 'jsonwebtoken';
 const app = express();
 
 app.post('/create-token', async (req, res) => {
-    console.log(0);
-    const response = await fetch('icp://aaaaa-aa/raw_rand');
-    const randomness = Buffer.from(await response.json());
+    try {
+        console.log(0);
+        const response = await fetch('icp://aaaaa-aa/raw_rand');
+        const randomness = Buffer.from(await response.json());
 
-    console.log('randomness', randomness);
+        console.log('randomness', randomness);
 
-    const payload = {
-        userId: '123456',
-        username: 'exampleUser'
-    };
+        const payload = {
+            userId: '123456',
+            username: 'exampleUser'
+        };
 
-    console.log('payload done');
+        console.log('payload done');
 
-    // TODO it breaks here
-    const token = jwt.sign(payload, randomness, {
-        algorithm: 'HS256',
-        expiresIn: '1h'
-    });
+        // TODO it breaks here
+        const token = jwt.sign(payload, randomness, {
+            algorithm: 'HS256',
+            expiresIn: '1h'
+        });
 
-    console.log('token done');
+        console.log('token done');
 
-    res.send(token);
+        res.send(token);
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 app.listen();
