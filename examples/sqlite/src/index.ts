6b211dff76bde4aca54471e1fdeba5a21d37f74f
Jordan Last <jordan.michael.last@gmail.com>
2024-04-29 08:29:27 -0500

    improving the sqlite example, create and read and batch create in place

--- examples/sqlite/src/index.ts
+++ examples/sqlite/src/index.ts
@@ -1,22 +1,108 @@
-import express from 'express';
+// TODO let's make routes for each of the User and BlogPost
+// TODO do all crud
+
+// TODO let's see if we can get init and post upgrade to work well
+// TODO implement stable storage and push it as far as possible
+
+import { ic, jsonStringify } from 'azle';
+import express, { Request } from 'express';
 import initSqlJs from 'sql.js/dist/sql-asm.js';
+import { v4 } from 'uuid';
 
-const app = express();
+import { createUser, getUser, getUsers } from './users';
 
-app.post('/', async (_req, res) => {
+async function init() {
     const SQL = await initSqlJs({});
 
-    const db = new SQL.Database();
+    let db = new SQL.Database();
+
+    // TODO we want to do this to save the db across upgrades
+    // TODO test how big the db can get like this
+    // db.export
 
     db.run(`
         CREATE TABLE users
             (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                username TEXT NOT NULL UNIQUE
+                username TEXT NOT NULL UNIQUE,
+                age INTEGER NOT NULL
             );
     `);
 
-    res.send('Hello world');
-});
+    let app = express();
 
-app.listen();
+    app.use(express.json());
+
+    app.get(
+        '/users',
+        (
+            req: Request<any, any, any, { limit?: string; offset?: string }>,
+            res
+        ) => {
+            try {
+                const limit = Number(req.query.limit ?? -1);
+                const offset = Number(req.query.offset ?? 0);
+
+                const users = getUsers(db, limit, offset);
+
+                res.json(users);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    );
+
+    app.get('/users/:id', (req, res) => {
+        const { id } = req.params;
+
+        const user = getUser(db, Number(id));
+
+        res.json(user);
+    });
+
+    app.post(
+        '/users',
+        (req: Request<any, any, { username: string; age: number }>, res) => {
+            const { username, age } = req.body;
+
+            const id = createUser(db, {
+                username,
+                age
+            });
+
+            res.json(id);
+        }
+    );
+
+    app.post('/users/batch/:num', (req, res) => {
+        try {
+            const start = ic.instructionCounter();
+
+            const num = Number(req.params.num);
+
+            for (let i = 0; i < Number(req.params.num); i++) {
+                createUser(db, {
+                    username: `lastmjs${v4()}`,
+                    age: i
+                });
+            }
+
+            const end = ic.instructionCounter();
+
+            res.send(
+                jsonStringify({
+                    Success: `${num} users created`,
+                    instructions: end - start
+                })
+            );
+        } catch (error) {
+            console.log('error', error);
+        }
+    });
+
+    // TODO also do put and patch
+
+    app.listen();
+}
+
+init();
---
+++ examples/sqlite/src/users.ts
@@ -0,0 +1,60 @@
+import { Database, QueryExecResult, SqlValue } from 'sql.js/dist/sql-asm.js';
+
+type User = {
+    id: number;
+    username: string;
+    age: number;
+};
+
+type UserCreate = Pick<User, 'username' | 'age'>;
+
+export function getUsers(db: Database, limit: number, offset: number): User[] {
+    const queryExecResults = db.exec(
+        `SELECT * FROM users ORDER BY id LIMIT :limit OFFSET :offset`,
+        {
+            ':limit': limit,
+            ':offset': offset
+        }
+    );
+    const queryExecResult = queryExecResults[0] as QueryExecResult | undefined;
+
+    return (
+        queryExecResult?.values.map((sqlValues) => {
+            return convertQueryExecResultToUser(sqlValues);
+        }) ?? []
+    );
+}
+
+export function getUser(db: Database, id: number): User | null {
+    const queryExecResults = db.exec('SELECT * FROM users WHERE id=:id', {
+        ':id': id
+    });
+    const queryExecResult = queryExecResults[0] as QueryExecResult | undefined;
+
+    if (queryExecResult === undefined) {
+        return null;
+    } else {
+        return queryExecResult.values.map((sqlValues) => {
+            return convertQueryExecResultToUser(sqlValues);
+        })[0];
+    }
+}
+
+export function createUser(db: Database, userCreate: UserCreate): number {
+    db.run('INSERT INTO users (username, age) VALUES (:username, :age)', {
+        ':username': userCreate.username,
+        ':age': userCreate.age
+    });
+
+    const id = db.exec('SELECT last_insert_rowid()')[0].values[0][0] as number;
+
+    return id;
+}
+
+export function convertQueryExecResultToUser(sqlValues: SqlValue[]): User {
+    return {
+        id: sqlValues[0] as number,
+        username: sqlValues[1] as string,
+        age: sqlValues[2] as number
+    };
+}
