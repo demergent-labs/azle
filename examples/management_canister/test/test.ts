c8f511e3169529afb1914a4fc2627d5992fb3d98
Benjamin DeMann <benjamin.demann@gmail.com>
2024-06-03 11:04:07 -0600

    update management canister to use jest

--- examples/management_canister/test/test.ts
+++ examples/management_canister/test/test.ts
@@ -1,13 +1,15 @@
 import { getCanisterId } from 'azle/dfx';
-import { runTests } from 'azle/test';
+import { runTests } from 'azle/test/jest';
 
 import { createActor } from '../test/dfx_generated/management_canister';
 import { getTests } from './tests';
 
-const managementCanister = createActor(getCanisterId('management_canister'), {
+const canisterName = 'management_canister';
+
+const managementCanister = createActor(getCanisterId(canisterName), {
     agentOptions: {
         host: 'http://127.0.0.1:8000'
     }
 });
 
-runTests(getTests(managementCanister));
+runTests(canisterName, getTests(managementCanister));
