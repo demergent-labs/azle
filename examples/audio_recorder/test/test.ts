78410c96613a6aa705a8567c2e3046a4b07753f0
Benjamin DeMann <benjamin.demann@gmail.com>
2024-06-03 21:16:04 -0600

    update audio recorder to use jest

--- examples/audio_recorder/test/test.ts
+++ examples/audio_recorder/test/test.ts
@@ -1,5 +1,5 @@
 import { getCanisterId } from 'azle/dfx';
-import { runTests } from 'azle/test';
+import { runTests } from 'azle/test/jest';
 
 import { createActor } from '../test/dfx_generated/audio_recorder';
 import { get_tests } from './tests';
