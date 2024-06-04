aceb9515a362d8803ccd0069719b72c99d3bb86b
Benjamin DeMann <benjamin.demann@gmail.com>
2024-06-03 21:16:04 -0600

    update audio recorder to use jest

--- examples/audio_recorder/test/test.ts
+++ examples/audio_recorder/test/test.ts
@@ -1,13 +1,15 @@
 import { getCanisterId } from 'azle/dfx';
-import { runTests } from 'azle/test';
+import { runTests } from 'azle/test/jest';
 
 import { createActor } from '../test/dfx_generated/audio_recorder';
 import { get_tests } from './tests';
 
-const audio_recorder_canister = createActor(getCanisterId('audio_recorder'), {
+const canisterName = 'audio_recorder';
+
+const audio_recorder_canister = createActor(getCanisterId(canisterName), {
     agentOptions: {
         host: 'http://127.0.0.1:8000'
     }
 });
 
-runTests(get_tests(audio_recorder_canister));
+runTests(canisterName, get_tests(audio_recorder_canister));
