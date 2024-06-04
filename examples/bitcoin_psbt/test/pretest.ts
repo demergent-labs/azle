5140ae6cf82878ccbb5ede89636234287b1d86b1
Benjamin DeMann <benjamin.demann@gmail.com>
2024-06-04 13:47:53 -0600

    add env varaible to control linking azle

--- examples/bitcoin_psbt/test/pretest.ts
+++ examples/bitcoin_psbt/test/pretest.ts
@@ -1,6 +1,13 @@
 import { execSync } from 'child_process';
 
 async function pretest() {
+    // TODO remove basic_bitcoin install and link after https://github.com/demergent-labs/azle/issues/1807 is resolved
+    execSync(`cd ../basic_bitcoin && npm install`);
+
+    if (process.env.AZLE_INTEGRATION_TEST_LINK_AZLE === 'true') {
+        execSync(`cd ../basic_bitcoin && npm link azle`);
+    }
+
     execSync(`dfx canister uninstall-code bitcoin_psbt || true`, {
         stdio: 'inherit'
     });
