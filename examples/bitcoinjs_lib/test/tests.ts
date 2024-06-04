dcdd6d05bd91aed1ce2924eaa1c2add19d832c36
Benjamin DeMann <benjamin.demann@gmail.com>
2024-06-04 10:36:32 -0600

    rename bitcore-lib and bitcoinjs-lib examples

--- examples/bitcoinjs_lib/test/tests.ts
+++ examples/bitcoinjs_lib/test/tests.ts
@@ -2,7 +2,7 @@ import * as dns from 'node:dns';
 dns.setDefaultResultOrder('ipv4first');
 
 import { Test } from 'azle/test';
-import { getTests as getBitcoinTests } from 'bitcore-lib-example/test/tests';
+import { getTests as getBitcoinTests } from 'bitcore_lib_example/test/tests';
 
 export function getTests(canisterId: string): Test[] {
     const origin = `http://${canisterId}.localhost:8000`;
