9d0459d2a2106b58f86cd5c6b146abc6f986148e
Benjamin DeMann <benjamin.demann@gmail.com>
2024-06-04 11:33:48 -0600

    update query to use jest

--- examples/query/test/test.ts
+++ examples/query/test/test.ts
@@ -1,13 +1,15 @@
 import { getCanisterId } from 'azle/dfx';
-import { runTests } from 'azle/test';
+import { runTests } from 'azle/test/jest';
 
 import { createActor } from './dfx_generated/query';
 import { getTests } from './tests';
 
-const queryCanister = createActor(getCanisterId('query'), {
+const canisterName = 'query';
+
+const queryCanister = createActor(getCanisterId(canisterName), {
     agentOptions: {
         host: 'http://127.0.0.1:8000'
     }
 });
 
-runTests(getTests(queryCanister));
+runTests(canisterName, getTests(queryCanister));
