9fe2b34c29547c452d60ff0cde956f6cf5f2128b
Benjamin DeMann <benjamin.demann@gmail.com>
2024-06-05 11:04:01 -0600

    update call_raw to use jest

--- examples/call_raw/test/test.ts
+++ examples/call_raw/test/test.ts
@@ -1,13 +1,15 @@
 import { getCanisterId } from 'azle/dfx';
-import { runTests } from 'azle/test';
+import { runTests } from 'azle/test/jest';
 
 import { createActor } from '../test/dfx_generated/call_raw';
-import { get_tests } from './tests';
+import { getTests } from './tests';
 
-const callRawCanister = createActor(getCanisterId('call_raw'), {
+const canisterName = 'call_raw';
+
+const callRawCanister = createActor(getCanisterId(canisterName), {
     agentOptions: {
         host: 'http://127.0.0.1:8000'
     }
 });
 
-runTests(get_tests(callRawCanister));
+runTests(canisterName, getTests(callRawCanister));
