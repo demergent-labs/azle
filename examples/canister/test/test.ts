c9e5a6166cd9357bc2a3da66c796e94044478290
Benjamin DeMann <benjamin.demann@gmail.com>
2024-06-05 13:13:18 -0600

    update canister example to use jest

--- examples/canister/test/test.ts
+++ examples/canister/test/test.ts
@@ -1,13 +1,15 @@
 import { getCanisterId } from 'azle/dfx';
-import { runTests } from 'azle/test';
+import { runTests } from 'azle/test/jest';
 
 import { createActor } from './dfx_generated/canister';
 import { getTests } from './tests';
 
-const canister = createActor(getCanisterId('canister'), {
+const canisterName = 'canister';
+
+const canister = createActor(getCanisterId(canisterName), {
     agentOptions: {
         host: 'http://127.0.0.1:8000'
     }
 });
 
-runTests(getTests(canister));
+runTests(canisterName, getTests(canister));
