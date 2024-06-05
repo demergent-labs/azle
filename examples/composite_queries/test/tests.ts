6192d9e2798025cea2e31ee08f20ab54072e3a8e
Benjamin DeMann <benjamin.demann@gmail.com>
2024-06-05 15:42:44 -0600

    update composite queries to use jest

--- examples/composite_queries/test/tests.ts
+++ examples/composite_queries/test/tests.ts
@@ -1,105 +1,66 @@
 import { ActorSubclass } from '@dfinity/agent';
 import { getCanisterId } from 'azle/dfx';
-import { Test } from 'azle/test';
+import { expect, it, Test } from 'azle/test/jest';
 
 import { _SERVICE } from './dfx_generated/canister1/canister1.did';
 
-export function get_tests(canister1: ActorSubclass<_SERVICE>): Test[] {
-    return [
-        {
-            name: 'simple_composite_query test',
-            test: async () => {
-                const result = await canister1.simpleCompositeQuery();
-                return {
-                    Ok: result === 'Hello from Canister 2'
-                };
-            }
-        },
-        {
-            name: 'manual_query test',
-            test: async () => {
-                const result = await canister1.manualQuery();
-                return {
-                    Ok: result === 'Hello from Canister 2 manual query'
-                };
-            }
-        },
-        {
-            name: 'totally manual_query test',
-            test: async () => {
-                const result = await canister1.totallyManualQuery();
-                return {
-                    Ok: result === 'Hello from Canister 2 manual query'
-                };
-            }
-        },
-        {
-            name: 'deep_query test',
-            test: async () => {
-                const result = await canister1.deepQuery();
-                return {
-                    Ok: result === 'Hello from Canister 3'
-                };
-            }
-        },
-        {
-            name: 'update_query test',
-            test: async () => {
-                try {
-                    await canister1.updateQuery();
-                    return {
-                        Ok: false
-                    };
-                } catch (error: any) {
-                    return {
-                        Ok: error
-                            .toString()
-                            .includes(
-                                `Rejection code 5, Canister ${getCanisterId(
-                                    'canister2'
-                                )} has no query method`
-                            )
-                    };
-                }
-            }
-        },
-        {
-            name: 'simple_update test',
-            test: async () => {
-                try {
-                    await canister1.simpleUpdate();
-                    return {
-                        Ok: false
-                    };
-                } catch (error: any) {
-                    return {
-                        Ok: error
-                            .toString()
-                            .includes(
-                                'Rejection code 5, IC0527: Composite query cannot be called in replicated mode'
-                            )
-                    };
-                }
-            }
-        },
-        {
-            name: 'inc_canister1 test',
-            test: async () => {
-                const result = await canister1.incCanister1();
+export function getTests(canister1: ActorSubclass<_SERVICE>): Test {
+    return () => {
+        it('performs a simple composite query', async () => {
+            const result = await canister1.simpleCompositeQuery();
 
-                return {
-                    Ok: result === 3n
-                };
-            }
-        },
-        {
-            name: 'inc_canister2 test',
-            test: async () => {
-                const result = await canister1.incCanister2();
-                return {
-                    Ok: result === 3n
-                };
-            }
-        }
-    ];
+            expect(result).toBe('Hello from Canister 2');
+        });
+
+        it('performs a simple manual query', async () => {
+            const result = await canister1.manualQuery();
+
+            expect(result).toBe('Hello from Canister 2 manual query');
+        });
+
+        it('performs a manual query where all involved methods were manual', async () => {
+            const result = await canister1.totallyManualQuery();
+
+            expect(result).toBe('Hello from Canister 2 manual query');
+        });
+
+        it('performs a composite query that performs a composite query', async () => {
+            const result = await canister1.deepQuery();
+
+            expect(result).toBe('Hello from Canister 3');
+        });
+
+        it('fails to perform a composite query attempting to call an update method', async () => {
+            const canisterId = getCanisterId('canister2');
+            const partialErrorMessage = new RegExp(
+                `Rejection code 5, Canister ${canisterId} has no query method`
+            );
+
+            await expect(canister1.updateQuery()).rejects.toThrow(
+                partialErrorMessage
+            );
+        });
+
+        it('fails to perform a composite query from a regular query', async () => {
+            const partialErrorMessage = new RegExp(
+                `Rejection code 5, IC0527: Composite query cannot be called in replicated mode`
+            );
+
+            await expect(canister1.simpleUpdate()).rejects.toThrow(
+                partialErrorMessage
+            );
+        });
+
+        it('unwinds modified state of canister1 between sequential composite queries to itself', async () => {
+            const result = await canister1.incCanister1();
+
+            expect(result).toBe(3n);
+        });
+
+        it('unwinds modified state of canister2 between sequential composite queries from canister1', async () => {
+            const result = await canister1.incCanister2();
+
+            expect(result).toBe(3n);
+        });
+    };
 }
