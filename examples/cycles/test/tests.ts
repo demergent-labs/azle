328aa9788d790351673fd1d7f533f935a1c97021
Benjamin DeMann <benjamin.demann@gmail.com>
2024-06-06 10:51:16 -0600

    update cycles to use jest

--- examples/cycles/test/tests.ts
+++ examples/cycles/test/tests.ts
@@ -1,5 +1,5 @@
 import { ActorSubclass } from '@dfinity/agent';
-import { Test } from 'azle/test';
+import { expect, it, Test } from 'azle/test/jest';
 
 import { _SERVICE as CYCLESCANISTERSERVICE } from './dfx_generated/cycles/cycles.did';
 import { _SERVICE as INTERMEDIARYCANISTERSERVICE } from './dfx_generated/intermediary/intermediary.did';
@@ -7,155 +7,130 @@ import { _SERVICE as INTERMEDIARYCANISTERSERVICE } from './dfx_generated/interme
 export function getTests(
     cyclesCanister: ActorSubclass<CYCLESCANISTERSERVICE>,
     intermediaryCanister: ActorSubclass<INTERMEDIARYCANISTERSERVICE>
-): Test[] {
-    return [
-        {
-            name: 'initial getCanisterBalance',
-            test: async () => {
-                const intermediaryCallResult =
-                    await intermediaryCanister.getCanisterBalance();
-                const cyclesCallResult =
-                    await cyclesCanister.getCanisterBalance();
+): Test {
+    return () => {
+        it('gets the initial canister balance', async () => {
+            const intermediaryCallResult =
+                await intermediaryCanister.getCanisterBalance();
+            const cyclesCallResult = await cyclesCanister.getCanisterBalance();
 
-                const intermediaryCanister128Result =
-                    await intermediaryCanister.getCanisterBalance128();
-                const cyclesCanister128Result =
-                    await cyclesCanister.getCanisterBalance128();
+            const intermediaryCanister128Result =
+                await intermediaryCanister.getCanisterBalance128();
+            const cyclesCanister128Result =
+                await cyclesCanister.getCanisterBalance128();
 
-                return {
-                    Ok:
-                        intermediaryCallResult > 2_500_000_000_000n &&
-                        intermediaryCallResult < 4_000_000_000_000n &&
-                        cyclesCallResult > 2_500_000_000_000n &&
-                        cyclesCallResult < 4_000_000_000_000n &&
-                        intermediaryCanister128Result > 2_500_000_000_000n &&
-                        intermediaryCanister128Result < 4_000_000_000_000n &&
-                        cyclesCanister128Result > 2_500_000_000_000n &&
-                        cyclesCanister128Result < 4_000_000_000_000n
-                };
-            }
-        },
-        {
-            name: 'msgCyclesAvailable and msgCyclesAccept with 0 cycles sent',
-            test: async () => {
-                const result = await cyclesCanister.receiveCycles();
+            const twoAndAHalfTrillion = 2_500_000_000_000n;
+            const fourTrillion = 4_000_000_000_000n;
 
-                return {
-                    Ok: result === 0n
-                };
-            }
-        },
-        {
-            name: 'msgCyclesAvailable128 and msgCyclesAccept128 with 0 cycles sent',
-            test: async () => {
-                const result = await cyclesCanister.receiveCycles128();
+            expect(intermediaryCallResult).toBeGreaterThan(twoAndAHalfTrillion);
+            expect(intermediaryCallResult).toBeLessThan(fourTrillion);
+            expect(cyclesCallResult).toBeGreaterThan(twoAndAHalfTrillion);
+            expect(cyclesCallResult).toBeLessThan(fourTrillion);
+            expect(intermediaryCanister128Result).toBeGreaterThan(
+                twoAndAHalfTrillion
+            );
+            expect(intermediaryCanister128Result).toBeLessThan(fourTrillion);
+            expect(cyclesCanister128Result).toBeGreaterThan(
+                twoAndAHalfTrillion
+            );
+            expect(cyclesCanister128Result).toBeLessThan(fourTrillion);
+        });
 
-                return {
-                    Ok: result === 0n
-                };
-            }
-        },
-        {
-            name: 'msgCyclesRefunded',
-            test: async () => {
-                const refundResult = await intermediaryCanister.sendCycles();
+        it('checks msgCyclesAvailable and msgCyclesAccept with 0 cycles sent', async () => {
+            const result = await cyclesCanister.receiveCycles();
 
-                // TODO It would be a bit messy and difficult to try and do this
-                // const intermediaryCallResult =
-                //     await intermediaryCanister.getCanisterBalance();
-                // const cyclesCallResult =
-                //     await cyclesCanister.getCanisterBalance();
+            expect(result).toBe(0n);
+        });
 
-                // const intermediaryCanister128Result =
-                //     await intermediaryCanister.getCanisterBalance128();
-                // const cyclesCanister128Result =
-                //     await cyclesCanister.getCanisterBalance128();
+        it('checks msgCyclesAvailable128 and msgCyclesAccept128 with 0 cycles sent', async () => {
+            const result = await cyclesCanister.receiveCycles128();
 
-                return {
-                    Ok: refundResult === 500_000n
-                    // intermediaryCallResult === 3_999_999_500_000n &&
-                    // cyclesCallResult === 4_000_000_500_000n &&
-                    // intermediaryCanister128Result === 3_999_999_500_000n &&
-                    // cyclesCanister128Result === 4_000_000_500_000n
-                };
-            }
-        },
-        {
-            name: 'msgCyclesRefunded128',
-            test: async () => {
-                const refundResult = await intermediaryCanister.sendCycles128();
+            expect(result).toBe(0n);
+        });
 
-                // TODO It would be a bit messy and difficult to try and do this
-                // const intermediaryCallResult =
-                //     await intermediaryCanister.getCanisterBalance();
-                // const cyclesCallResult =
-                //     await cyclesCanister.getCanisterBalance();
+        it('refunds cycles with msgCyclesRefunded', async () => {
+            const refundResult = await intermediaryCanister.sendCycles();
 
-                // const intermediaryCanister128Result =
-                //     await intermediaryCanister.getCanisterBalance128();
-                // const cyclesCanister128Result =
-                //     await cyclesCanister.getCanisterBalance128();
+            // TODO It would be a bit messy and difficult to try and do this
+            // const intermediaryCallResult =
+            //     await intermediaryCanister.getCanisterBalance();
+            // const cyclesCallResult = await cyclesCanister.getCanisterBalance();
 
-                return {
-                    Ok: refundResult === 500_000n
-                    // intermediaryCallResult === 3_999_999_000_000n &&
-                    // cyclesCallResult === 4_000_001_000_000n &&
-                    // intermediaryCanister128Result === 3_999_999_000_000n &&
-                    // cyclesCanister128Result === 4_000_001_000_000n
-                };
-            }
-        },
-        {
-            name: 'send cycles with notify',
-            test: async () => {
-                const sendCyclesNotifyResult =
-                    await intermediaryCanister.sendCyclesNotify();
+            // const intermediaryCanister128Result =
+            //     await intermediaryCanister.getCanisterBalance128();
+            // const cyclesCanister128Result =
+            //     await cyclesCanister.getCanisterBalance128();
 
-                // TODO It would be a bit messy and difficult to try and do this
-                // const intermediaryCallResult =
-                //     await intermediaryCanister.getCanisterBalance();
-                // const cyclesCallResult =
-                //     await cyclesCanister.getCanisterBalance();
+            expect(refundResult).toBe(500_000n);
+            // expect(intermediaryCallResult).toBe(3_999_999_500_000n);
+            // expect(cyclesCallResult).toBe(4_000_000_500_000n);
+            // expect(intermediaryCanister128Result).toBe(3_999_999_500_000n);
+            // expect(cyclesCanister128Result).toBe(4_000_000_500_000n);
+        });
 
-                // const intermediaryCanister128Result =
-                //     await intermediaryCanister.getCanisterBalance128();
-                // const cyclesCanister128Result =
-                //     await cyclesCanister.getCanisterBalance128();
+        it('refunds cycles with msgCyclesRefunded128', async () => {
+            const refundResult = await intermediaryCanister.sendCycles128();
 
-                return {
-                    Ok: sendCyclesNotifyResult === undefined
-                    // intermediaryCallResult === 3_999_998_500_000n &&
-                    // cyclesCallResult === 4_000_001_500_000n &&
-                    // intermediaryCanister128Result === 3_999_998_500_000n &&
-                    // cyclesCanister128Result === 4_000_001_500_000n
-                };
-            }
-        },
-        {
-            name: 'send cycles128 with notify',
-            test: async () => {
-                const sendCycles128NotifyResult =
-                    await intermediaryCanister.sendCycles128Notify();
+            // TODO It would be a bit messy and difficult to try and do this
+            // const intermediaryCallResult =
+            //     await intermediaryCanister.getCanisterBalance();
+            // const cyclesCallResult =
+            //     await cyclesCanister.getCanisterBalance();
 
-                // TODO It would be a bit messy and difficult to try and do this
-                // const intermediaryCallResult =
-                //     await intermediaryCanister.getCanisterBalance();
-                // const cyclesCallResult =
-                //     await cyclesCanister.getCanisterBalance();
+            // const intermediaryCanister128Result =
+            //     await intermediaryCanister.getCanisterBalance128();
+            // const cyclesCanister128Result =
+            //     await cyclesCanister.getCanisterBalance128();
 
-                // const intermediaryCanister128Result =
-                //     await intermediaryCanister.getCanisterBalance128();
-                // const cyclesCanister128Result =
-                //     await cyclesCanister.getCanisterBalance128();
+            expect(refundResult).toBe(500_000n);
+            // expect(intermediaryCallResult).toBe(3_999_999_000_000n);
+            // expect(cyclesCallResult).toBe(4_000_001_000_000n);
+            // expect(intermediaryCanister128Result).toBe(3_999_999_000_000n);
+            // expect(cyclesCanister128Result).toBe(4_000_001_000_000n);
+        });
 
-                return {
-                    Ok: sendCycles128NotifyResult === undefined
-                    // intermediaryCallResult === 3_999_998_000_000n &&
-                    // cyclesCallResult === 4_000_002_000_000n &&
-                    // intermediaryCanister128Result === 3_999_998_000_000n &&
-                    // cyclesCanister128Result === 4_000_002_000_000n
-                };
-            }
-        }
-    ];
+        it('sends cycles with notify', async () => {
+            const sendCyclesNotifyResult =
+                await intermediaryCanister.sendCyclesNotify();
+
+            // TODO It would be a bit messy and difficult to try and do this
+            // const intermediaryCallResult =
+            //     await intermediaryCanister.getCanisterBalance();
+            // const cyclesCallResult =
+            //     await cyclesCanister.getCanisterBalance();
+
+            // const intermediaryCanister128Result =
+            //     await intermediaryCanister.getCanisterBalance128();
+            // const cyclesCanister128Result =
+            //     await cyclesCanister.getCanisterBalance128();
+
+            expect(sendCyclesNotifyResult).toBeUndefined();
+            // expect(intermediaryCallResult).toBe(3_999_998_500_000n);
+            // expect(cyclesCallResult).toBe(4_000_001_500_000n);
+            // expect(intermediaryCanister128Result).toBe(3_999_998_500_000n);
+            // expect(cyclesCanister128Result).toBe(4_000_001_500_000n);
+        });
+
+        it('sends cycles128 with notify', async () => {
+            const sendCycles128NotifyResult =
+                await intermediaryCanister.sendCycles128Notify();
+
+            // TODO It would be a bit messy and difficult to try and do this
+            // const intermediaryCallResult =
+            //     await intermediaryCanister.getCanisterBalance();
+            // const cyclesCallResult =
+            //     await cyclesCanister.getCanisterBalance();
+
+            // const intermediaryCanister128Result =
+            //     await intermediaryCanister.getCanisterBalance128();
+            // const cyclesCanister128Result =
+            //     await cyclesCanister.getCanisterBalance128();
+
+            expect(sendCycles128NotifyResult).toBeUndefined();
+            // expect(intermediaryCallResult).toBe(3_999_998_000_000n);
+            // expect(cyclesCallResult).toBe(4_000_002_000_000n);
+            // expect(intermediaryCanister128Result).toBe(3_999_998_000_000n);
+            // expect(cyclesCanister128Result).toBe(4_000_002_000_000n);
+        });
+    };
 }
