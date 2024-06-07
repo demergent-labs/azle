ee1245500338744490e865516a1921a5f849a7d1
Jordan Last <jordan.michael.last@gmail.com>
2024-06-07 15:59:19 -0600

    fix sqlite_drizzle npm type issues

--- examples/sqlite_drizzle/test/test.ts
+++ examples/sqlite_drizzle/test/test.ts
@@ -1,6 +1,6 @@
 import { getCanisterId } from 'azle/dfx';
-import { getTests } from 'azle/examples/sqlite/test/tests';
 import { runTests } from 'azle/test';
+import { getTests } from 'sqlite_example/test/tests';
 
 const canisterId = getCanisterId('sqlite_drizzle');
 
