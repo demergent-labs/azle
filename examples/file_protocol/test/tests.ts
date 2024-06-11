9f68a87e67df905270309c39d86fe96b084b8747
Benjamin DeMann <benjamin.demann@gmail.com>
2024-06-11 15:13:29 -0600

    update file_protocol to use jest

--- examples/file_protocol/test/tests.ts
+++ examples/file_protocol/test/tests.ts
@@ -1,99 +1,53 @@
-import * as dns from 'node:dns';
-dns.setDefaultResultOrder('ipv4first');
+import { expect, it, Test } from 'azle/test/jest';
 
-import { Test } from 'azle/test';
-
-export function getTests(canisterId: string): Test[] {
+export function getTests(canisterId: string): Test {
     const origin = `http://${canisterId}.localhost:8000`;
 
-    return [
-        {
-            name: '/read-test0',
-            test: async () => {
-                try {
-                    const response = await fetch(
-                        `${origin}/read-test0?filename=${'assets/test0.txt'}`
-                    );
-                    const responseText = await response.text();
+    return () => {
+        it('reads a file in the top level directory', async () => {
+            const response = await fetch(
+                `${origin}/read-test0?filename=${'assets/test0.txt'}`
+            );
+            const responseText = await response.text();
 
-                    return {
-                        Ok: responseText.trim() === 'test0'
-                    };
-                } catch (error: any) {
-                    return {
-                        Err: error
-                    };
-                }
-            }
-        },
-        {
-            name: '/read-test2',
-            test: async () => {
-                try {
-                    const response = await fetch(`${origin}/read-test2`, {
-                        method: 'POST',
-                        headers: [['Content-Type', 'application/json']],
-                        body: JSON.stringify({
-                            filename: 'assets/test1/test2.txt'
-                        })
-                    });
-                    const responseText = await response.text();
+            expect(responseText.trim()).toBe('test0');
+        });
 
-                    return {
-                        Ok: responseText.trim() === 'test2'
-                    };
-                } catch (error: any) {
-                    return {
-                        Err: error
-                    };
-                }
-            }
-        },
-        {
-            name: '/read-test3',
-            test: async () => {
-                try {
-                    const response = await fetch(`${origin}/read-test3`, {
-                        method: 'PUT',
-                        headers: [['Content-Type', 'application/json']],
-                        body: JSON.stringify({
-                            filename: 'assets/test1/test3.txt'
-                        })
-                    });
-                    const responseText = await response.text();
+        it('reads a file in a child directory', async () => {
+            const response = await fetch(`${origin}/read-test2`, {
+                method: 'POST',
+                headers: [['Content-Type', 'application/json']],
+                body: JSON.stringify({
+                    filename: 'assets/test1/test2.txt'
+                })
+            });
+            const responseText = await response.text();
 
-                    return {
-                        Ok: responseText.trim() === 'test3'
-                    };
-                } catch (error: any) {
-                    return {
-                        Err: error
-                    };
-                }
-            }
-        },
-        {
-            name: '/read-test5',
-            test: async () => {
-                try {
-                    const response = await fetch(`${origin}/read-test5`, {
-                        method: 'PATCH',
-                        headers: [['Content-Type', 'application/json']],
-                        body: JSON.stringify({
-                            filename: 'assets/test1/test4/test5.txt'
-                        })
-                    });
-                    const responseText = await response.text();
+            expect(responseText.trim()).toBe('test2');
+        });
+        it('reads a sibling file', async () => {
+            const response = await fetch(`${origin}/read-test3`, {
+                method: 'PUT',
+                headers: [['Content-Type', 'application/json']],
+                body: JSON.stringify({
+                    filename: 'assets/test1/test3.txt'
+                })
+            });
+            const responseText = await response.text();
 
-                    return {
-                        Ok: responseText.trim() === 'test5'
-                    };
-                } catch (error: any) {
-                    return {
-                        Err: error
-                    };
-                }
-            }
-        }
-    ];
+            expect(responseText.trim()).toBe('test3');
+        });
+        it('reads in a file in a grandchild directory', async () => {
+            const response = await fetch(`${origin}/read-test5`, {
+                method: 'PATCH',
+                headers: [['Content-Type', 'application/json']],
+                body: JSON.stringify({
+                    filename: 'assets/test1/test4/test5.txt'
+                })
+            });
+            const responseText = await response.text();
+
+            expect(responseText.trim()).toBe('test5');
+        });
+    };
 }
