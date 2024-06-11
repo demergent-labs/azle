b2bb25ecdb6e9b3a5383b68a7c0a656f922b0c0e
Benjamin DeMann <benjamin.demann@gmail.com>
2024-06-11 11:13:06 -0600

    update cross_canister_calls to use jest

--- examples/cross_canister_calls/test/test.ts
+++ examples/cross_canister_calls/test/test.ts
@@ -1,9 +1,9 @@
 import { getCanisterId } from 'azle/dfx';
-import { runTests } from 'azle/test';
+import { runTests } from 'azle/test/jest';
 
 import { createActor as createActorCanister1 } from './dfx_generated/canister1';
 import { createActor as createActorCanister2 } from './dfx_generated/canister2';
-import { get_tests } from './tests';
+import { getTests } from './tests';
 
 const canister1 = createActorCanister1(getCanisterId('canister1'), {
     agentOptions: {
@@ -17,4 +17,4 @@ const canister2 = createActorCanister2(getCanisterId('canister2'), {
     }
 });
 
-runTests(get_tests(canister1, canister2));
+runTests('cross_canister_calls', getTests(canister1, canister2));
--- examples/cross_canister_calls/test/tests.ts
+++ examples/cross_canister_calls/test/tests.ts
@@ -1,195 +1,128 @@
 import { ActorSubclass } from '@dfinity/agent';
-import { Test } from 'azle/test';
+import { expect, it, Test } from 'azle/test/jest';
 
+import { getCanisterId } from '../../../dfx';
 import { _SERVICE as CANISTER1_SERVICE } from './dfx_generated/canister1/canister1.did';
 import { _SERVICE as CANISTER2_SERVICE } from './dfx_generated/canister2/canister2.did';
 
-export function get_tests(
+export function getTests(
     canister1: ActorSubclass<CANISTER1_SERVICE>,
     canister2: ActorSubclass<CANISTER2_SERVICE>
-): Test[] {
-    return [
-        {
-            name: 'canister1 balance 0',
-            test: async () => {
-                const result = await canister1.balance('0');
+): Test {
+    return () => {
+        it('gets balance of account 0 via cross canister call', async () => {
+            const result = await canister1.balance('0');
 
-                return {
-                    Ok: result === 100n
-                };
-            }
-        },
-        {
-            name: 'canister1 account 0',
-            test: async () => {
-                const result = await canister1.account({
-                    id: '0'
-                });
+            expect(result).toBe(100n);
+        });
 
-                return {
-                    Ok:
-                        result.length === 1 &&
-                        result[0].id === '0' &&
-                        result[0].balance === 100n
-                };
-            }
-        },
-        {
-            name: 'canister1 balance 1',
-            test: async () => {
-                const result = await canister1.balance('1');
+        it('gets account 0 details via cross canister call', async () => {
+            const result = await canister1.account({
+                id: '0'
+            });
 
-                return {
-                    Ok: result === 0n
-                };
-            }
-        },
-        {
-            name: 'canister1 account 1',
-            test: async () => {
-                const result = await canister1.account({
-                    id: '1'
-                });
+            const expected = { id: '0', balance: 100n };
 
-                return {
-                    Ok: result.length === 0
-                };
-            }
-        },
-        {
-            name: 'canister1 accounts',
-            test: async () => {
-                const result = await canister1.accounts();
+            expect(result).toEqual([expected]);
+        });
 
-                return {
-                    Ok:
-                        result.length === 1 &&
-                        result[0].id === '0' &&
-                        result[0].balance === 100n
-                };
-            }
-        },
-        {
-            name: 'canister1 transfer',
-            test: async () => {
-                const result = await canister1.transfer('0', '1', 34n);
+        it('gets balance of account 1 via cross canister call', async () => {
+            const result = await canister1.balance('1');
 
-                return {
-                    Ok: result === 34n
-                };
-            }
-        },
-        {
-            name: 'canister1 balance 0',
-            test: async () => {
-                const result = await canister1.balance('0');
+            expect(result).toBe(0n);
+        });
 
-                return {
-                    Ok: result === 66n
-                };
-            }
-        },
-        {
-            name: 'canister1 account 0',
-            test: async () => {
-                const result = await canister1.account({
-                    id: '0'
-                });
+        it('fail to get non-existent account 1 via cross canister call', async () => {
+            const result = await canister1.account({
+                id: '1'
+            });
 
-                return {
-                    Ok:
-                        result.length === 1 &&
-                        result[0].id === '0' &&
-                        result[0].balance === 66n
-                };
-            }
-        },
-        {
-            name: 'canister1 balance 1',
-            test: async () => {
-                const result = await canister1.balance('1');
+            expect(result).toHaveLength(0);
+        });
 
-                return {
-                    Ok: result === 34n
-                };
-            }
-        },
-        {
-            name: 'canister1 account 1',
-            test: async () => {
-                const result = await canister1.account({
-                    id: '1'
-                });
+        it('gets all accounts via cross canister call', async () => {
+            const result = await canister1.accounts();
 
-                return {
-                    Ok:
-                        result.length === 1 &&
-                        result[0].id === '1' &&
-                        result[0].balance === 34n
-                };
-            }
-        },
-        {
-            name: 'canister1 accounts',
-            test: async () => {
-                const result = await canister1.accounts();
+            const expected = { id: '0', balance: 100n };
 
-                return {
-                    Ok:
-                        result[0].id === '0' &&
-                        result[0].balance === 66n &&
-                        result[1].id === '1' &&
-                        result[1].balance === 34n
-                };
-            }
-        },
-        {
-            name: 'canister1 trap',
-            test: async () => {
-                try {
-                    await canister1.trap();
+            expect(result).toEqual([expected]);
+        });
 
-                    return {
-                        Ok: false
-                    };
-                } catch (error) {
-                    return {
-                        Ok: (error as { message: string }).message.includes(
-                            'hahahaha'
-                        )
-                    };
-                }
-            }
-        },
-        {
-            name: 'canister2 get_notification empty',
-            test: async () => {
-                const result = await canister2.getNotification();
+        it('transfers from account 0 to account 1 via cross canister call', async () => {
+            const result = await canister1.transfer('0', '1', 34n);
 
-                return {
-                    Ok: result === ''
-                };
-            }
-        },
-        {
-            name: 'canister1 send_notification',
-            test: async () => {
-                const result = await canister1.sendNotification();
+            expect(result).toBe(34n);
+        });
 
-                return {
-                    Ok: result === undefined
-                };
-            }
-        },
-        {
-            name: 'canister2 get_notification',
-            test: async () => {
-                const result = await canister2.getNotification();
+        it('gets updated balance of account 0 via cross canister call', async () => {
+            const result = await canister1.balance('0');
 
-                return {
-                    Ok: result === 'This is the notification'
-                };
-            }
-        }
-    ];
+            expect(result).toBe(66n);
+        });
+
+        it('gets updated account 0 details via cross canister call', async () => {
+            const result = await canister1.account({
+                id: '0'
+            });
+
+            const expected = { id: '0', balance: 66n };
+
+            expect(result).toEqual([expected]);
+        });
+
+        it('gets update balance of account 1 via cross canister call', async () => {
+            const result = await canister1.balance('1');
+
+            expect(result).toBe(34n);
+        });
+
+        it('gets account 1 details after creation via cross canister call', async () => {
+            const result = await canister1.account({
+                id: '1'
+            });
+
+            const expected = { id: '1', balance: 34n };
+
+            expect(result).toEqual([expected]);
+        });
+
+        it('gets all updated account details via cross canister call', async () => {
+            const result = await canister1.accounts();
+
+            const expected = [
+                { id: '0', balance: 66n },
+                { id: '1', balance: 34n }
+            ];
+
+            expect(result).toEqual(expected);
+        });
+
+        it('propagates traps across cross canister calls', async () => {
+            const canister1Id = getCanisterId('canister1');
+            const canister2Id = getCanisterId('canister2');
+            const partialErrorMessage = new RegExp(
+                `Error from Canister ${canister1Id}: Canister trapped explicitly: Uncaught Error: Rejection code 5, IC0503: Error from Canister ${canister2Id}: Canister trapped explicitly: hahahaha`
+            );
+
+            await expect(canister1.trap()).rejects.toThrow(partialErrorMessage);
+        });
+
+        it('gets no notifications from canister2 before any are sent', async () => {
+            const result = await canister2.getNotification();
+
+            expect(result).toBe('');
+        });
+
+        it('sends a notification to canister 2 via cross canister call', async () => {
+            const result = await canister1.sendNotification();
+
+            expect(result).toBeUndefined();
+        });
+
+        it('gets sent notification from canister 2', async () => {
+            const result = await canister2.getNotification();
+
+            expect(result).toBe('This is the notification');
+        });
+    };
 }
