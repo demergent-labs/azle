703c884bd02751355a20cf1af0e97b65fed0d916
Benjamin DeMann <benjamin.demann@gmail.com>
2024-06-11 21:17:27 -0600

    update generics to use jest

--- examples/generics/test/test.ts
+++ examples/generics/test/test.ts
@@ -1,13 +1,16 @@
 import { getCanisterId } from 'azle/dfx';
-import { runTests } from 'azle/test';
+import { runTests } from 'azle/test/jest';
 
 import { createActor } from './dfx_generated/generics';
 import { getTests } from './tests';
 
-const genericsCanister = createActor(getCanisterId('generics'), {
+// TODO is it time to get rid of this example?
+const canisterName = 'generics';
+
+const genericsCanister = createActor(getCanisterId(canisterName), {
     agentOptions: {
         host: 'http://127.0.0.1:8000'
     }
 });
 
-runTests(getTests(genericsCanister));
+runTests(canisterName, getTests(genericsCanister));
