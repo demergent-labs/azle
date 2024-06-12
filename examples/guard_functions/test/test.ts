bdfde15edd93249920cb6275b311858d49cf821f
Benjamin DeMann <benjamin.demann@gmail.com>
2024-06-11 21:05:56 -0600

    update guard functions to use jest

--- examples/guard_functions/test/test.ts
+++ examples/guard_functions/test/test.ts
@@ -1,13 +1,15 @@
 import { getCanisterId } from 'azle/dfx';
-import { runTests } from 'azle/test';
+import { runTests } from 'azle/test/jest';
 
 import { createActor } from './dfx_generated/guard_functions';
 import { getTests } from './tests';
 
-const guardFunctionsCanister = createActor(getCanisterId('guard_functions'), {
+const canisterName = 'guard_functions';
+
+const guardFunctionsCanister = createActor(getCanisterId(canisterName), {
     agentOptions: {
         host: 'http://127.0.0.1:8000'
     }
 });
 
-runTests(getTests(guardFunctionsCanister));
+runTests(canisterName, getTests(guardFunctionsCanister));
