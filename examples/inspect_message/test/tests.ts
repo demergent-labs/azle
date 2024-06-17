6f7371cf4060f724d315867a7f7e9ed71fec9851
Benjamin DeMann <benjamin.demann@gmail.com>
2024-06-17 15:10:08 -0600

    update inspect message to use jest

--- examples/inspect_message/test/tests.ts
+++ examples/inspect_message/test/tests.ts
@@ -1,60 +1,27 @@
 import { ActorSubclass } from '@dfinity/agent';
-import { Test } from 'azle/test';
+import { expect, it, Test } from 'azle/test/jest';
 
 import { _SERVICE } from './dfx_generated/inspect_message/inspect_message.did';
 
 export function getTests(
     inspectMessageCanister: ActorSubclass<_SERVICE>
-): Test[] {
-    return [
-        {
-            name: 'calling `ic.acceptMessage` in inspectMessage',
-            test: async () => {
-                try {
-                    const result = await inspectMessageCanister.accessible();
+): Test {
+    return () => {
+        it('calls `ic.acceptMessage` in inspectMessage', async () => {
+            const result = await inspectMessageCanister.accessible();
+            expect(result).toBe(true);
+        });
 
-                    return {
-                        Ok: result === true
-                    };
-                } catch (error) {
-                    console.error(error);
-                    return {
-                        Ok: false
-                    };
-                }
-            }
-        },
-        {
-            name: 'not calling `ic.acceptMessage` in inspectMessage',
-            test: async () => {
-                try {
-                    await inspectMessageCanister.inaccessible();
+        it('rejects because `ic.acceptMessage` was not called in inspectMessage', async () => {
+            await expect(inspectMessageCanister.inaccessible()).rejects.toThrow(
+                /IC0406/
+            );
+        });
 
-                    return {
-                        Ok: false
-                    };
-                } catch (error: any) {
-                    return {
-                        Ok: error.message.includes('IC0406')
-                    };
-                }
-            }
-        },
-        {
-            name: 'throwing in `inspectMessage`',
-            test: async () => {
-                try {
-                    await inspectMessageCanister.alsoInaccessible();
-
-                    return {
-                        Ok: false
-                    };
-                } catch (error: any) {
-                    return {
-                        Ok: error.message.includes('IC0503')
-                    };
-                }
-            }
-        }
-    ];
+        it('rejects because `inspectMessage` threw', async () => {
+            await expect(
+                inspectMessageCanister.alsoInaccessible()
+            ).rejects.toThrow(/IC0503/);
+        });
+    };
 }
