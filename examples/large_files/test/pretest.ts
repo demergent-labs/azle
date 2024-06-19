e6c5ae6615b25542e9b6605628c42ec5dd3e3bd9
Benjamin DeMann <benjamin.demann@gmail.com>
2024-06-19 07:32:50 -0600

    pr fixes

--- examples/large_files/test/pretest.ts
+++ examples/large_files/test/pretest.ts
@@ -5,6 +5,10 @@ async function pretest() {
         stdio: 'inherit'
     });
 
+    // Since a lot of this test revolves around the upload process and the post
+    // install scripts the call to dfx deploy is inside the tests. Canister
+    // create is called here to make sure we have the right canister id for the
+    // origin
     execSync(`dfx canister create backend`);
 }
 
