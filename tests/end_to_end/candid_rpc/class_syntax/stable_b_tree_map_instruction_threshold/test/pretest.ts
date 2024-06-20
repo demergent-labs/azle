a8070d949a809c1f1d95dd62ac5334959d93a5a4
Benjamin DeMann <benjamin.demann@gmail.com>
2024-06-20 11:07:01 -0600

    properly install dependant packages

--- tests/end_to_end/candid_rpc/class_syntax/stable_b_tree_map_instruction_threshold/test/pretest.ts
+++ tests/end_to_end/candid_rpc/class_syntax/stable_b_tree_map_instruction_threshold/test/pretest.ts
@@ -1,6 +1,30 @@
 import { execSync } from 'child_process';
+import { join, resolve } from 'path';
 
 async function pretest() {
+    const azleDir = resolve(
+        __dirname,
+        join('..', '..', '..', '..', '..', '..')
+    );
+
+    execSync(
+        `cd ${join(
+            azleDir,
+            'examples',
+            'stable_b_tree_map_instruction_threshold'
+        )} && npm install`
+    );
+
+    if (process.env.AZLE_END_TO_END_TEST_LINK_AZLE !== 'false') {
+        execSync(
+            `cd ${join(
+                azleDir,
+                'examples',
+                'stable_b_tree_map_instruction_threshold'
+            )} && npm link azle`
+        );
+    }
+
     execSync(
         `dfx canister uninstall-code stable_b_tree_map_instruction_threshold || true`,
         {
--- tests/end_to_end/candid_rpc/class_syntax/update/test/pretest.ts
+++ tests/end_to_end/candid_rpc/class_syntax/update/test/pretest.ts
@@ -1,6 +1,18 @@
 import { execSync } from 'child_process';
+import { join, resolve } from 'path';
 
 async function pretest() {
+    const azleDir = resolve(
+        __dirname,
+        join('..', '..', '..', '..', '..', '..')
+    );
+
+    execSync(`cd ${join(azleDir, 'examples', 'update')} && npm install`);
+
+    if (process.env.AZLE_END_TO_END_TEST_LINK_AZLE !== 'false') {
+        execSync(`cd ${join(azleDir, 'examples', 'update')} && npm link azle`);
+    }
+
     execSync(`dfx canister uninstall-code update || true`, {
         stdio: 'inherit'
     });
