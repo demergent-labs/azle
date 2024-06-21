c9a471ce35f5f6d3251cd526528317867f9c682e
Benjamin DeMann <benjamin.demann@gmail.com>
2024-06-21 12:02:22 -0600

    add script to delete test/tests.ts

---
+++ scripts/class_migration/delete_tests_file.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fs = require('fs');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
+// Path to the tests.ts file
+const testsFilePath = path.join(process.cwd(), 'test', 'tests.ts');
+
+// Function to delete the file
+const deleteFile = (filePath) => {
+    fs.unlink(filePath, (err) => {
+        if (err && err.code === 'ENOENT') {
+            console.log(`File not found: ${filePath}`);
+        } else if (err) {
+            console.error(`Error deleting file ${filePath}: ${err}`);
+        } else {
+            console.log(`Successfully deleted file: ${filePath}`);
+        }
+    });
+};
+
+// Delete the tests.ts file
+deleteFile(testsFilePath);
