1ed607919ccff2170066464602e3041476f2ddf5
Benjamin DeMann <benjamin.demann@gmail.com>
2024-06-28 13:40:42 -0600

    update key value store

--- examples/key_value_store/test/tests.ts
+++ examples/key_value_store/test/tests.ts
@@ -1,72 +1,45 @@
 import { ActorSubclass } from '@dfinity/agent';
-import { Test } from 'azle/test';
+import { expect, it, Test } from 'azle/test';
 
 // @ts-ignore this path may not exist when these tests are imported into other test projects
 import { _SERVICE } from './dfx_generated/key_value_store/key_value_store.did';
 
-export function getTests(
-    keyValueStoreCanister: ActorSubclass<_SERVICE>
-): Test[] {
-    return [
-        {
-            name: 'get 0',
-            test: async () => {
-                const result = await keyValueStoreCanister.get('0');
+export function getTests(keyValueStoreCanister: ActorSubclass<_SERVICE>): Test {
+    return () => {
+        it('get initial value 0 from simple key value store', async () => {
+            const result = await keyValueStoreCanister.get('0');
 
-                return {
-                    Ok: result.length === 0
-                };
-            }
-        },
-        {
-            name: 'get 1',
-            test: async () => {
-                const result = await keyValueStoreCanister.get('1');
+            expect(result).toHaveLength(0);
+        });
 
-                return {
-                    Ok: result.length === 0
-                };
-            }
-        },
-        {
-            name: 'set 0',
-            test: async () => {
-                const result = await keyValueStoreCanister.set('0', 'zero');
+        it('get initial value 1 from simple key value store', async () => {
+            const result = await keyValueStoreCanister.get('1');
 
-                return {
-                    Ok: result === undefined
-                };
-            }
-        },
-        {
-            name: 'set 1',
-            test: async () => {
-                const result = await keyValueStoreCanister.set('1', 'one');
+            expect(result).toHaveLength(0);
+        });
 
-                return {
-                    Ok: result === undefined
-                };
-            }
-        },
-        {
-            name: 'get 0',
-            test: async () => {
-                const result = await keyValueStoreCanister.get('0');
+        it('set value 0 from a simple key value store', async () => {
+            const result = await keyValueStoreCanister.set('0', 'zero');
 
-                return {
-                    Ok: result.length === 1 && result[0] === 'zero'
-                };
-            }
-        },
-        {
-            name: 'get 1',
-            test: async () => {
-                const result = await keyValueStoreCanister.get('1');
+            expect(result).toBeUndefined();
+        });
 
-                return {
-                    Ok: result.length === 1 && result[0] === 'one'
-                };
-            }
-        }
-    ];
+        it('set value 1 from a simple key value store', async () => {
+            const result = await keyValueStoreCanister.set('1', 'one');
+
+            expect(result).toBeUndefined();
+        });
+
+        it('get value 0 from a simple key value store', async () => {
+            const result = await keyValueStoreCanister.get('0');
+
+            expect(result).toStrictEqual(['zero']);
+        });
+
+        it('get value 1 from a simple key value store', async () => {
+            const result = await keyValueStoreCanister.get('1');
+
+            expect(result).toStrictEqual(['one']);
+        });
+    };
 }
