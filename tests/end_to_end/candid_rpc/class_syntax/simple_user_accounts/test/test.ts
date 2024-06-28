9856cbfac04af43646be5f3c65996e23e7eb8325
Benjamin DeMann <benjamin.demann@gmail.com>
2024-06-28 15:31:36 -0600

    jest examples s

--- tests/end_to_end/candid_rpc/class_syntax/simple_user_accounts/test/test.ts
+++ tests/end_to_end/candid_rpc/class_syntax/simple_user_accounts/test/test.ts
@@ -2,7 +2,7 @@ import { getCanisterId } from 'azle/dfx';
 import { runTests } from 'azle/test';
 import { getTests } from 'simple_user_accounts_end_to_end_test_canister_syntax/test/tests';
 
-import { createActor } from '../test/dfx_generated/simple_user_accounts';
+import { createActor } from './dfx_generated/simple_user_accounts';
 
 const simpleUserAccountsCanister = createActor(
     getCanisterId('simple_user_accounts'),
