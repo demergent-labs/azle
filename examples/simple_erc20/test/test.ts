37c820e00bf9d75e19d7278ba626b4abb8f08c15
Benjamin DeMann <benjamin.demann@gmail.com>
2024-06-28 15:31:36 -0600

    jest examples s

--- examples/simple_erc20/test/test.ts
+++ examples/simple_erc20/test/test.ts
@@ -1,7 +1,7 @@
 import { getCanisterId } from 'azle/dfx';
 import { runTests } from 'azle/test';
 
-import { createActor } from '../test/dfx_generated/simple_erc20';
+import { createActor } from './dfx_generated/simple_erc20';
 import { getTests } from './tests';
 
 const simpleErc20Canister = createActor(getCanisterId('simple_erc20'), {
