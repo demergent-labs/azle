86f8507ce2185005e87bc980b20a009ac464a5c6
Benjamin DeMann <benjamin.demann@gmail.com>
2024-06-28 15:31:12 -0600

    jest examples sqlite

--- examples/sqlite_drizzle/test/pretest.ts
+++ examples/sqlite_drizzle/test/pretest.ts
@@ -1,6 +1,12 @@
 import { execSync } from 'child_process';
 
 async function pretest() {
+    // TODO replace with standard solution once class based is merged in
+    execSync(`cd ../sqlite && npm install`);
+    if (process.env.AZLE_END_TO_END_TEST_LINK_AZLE !== 'false') {
+        execSync(`cd ../sqlite && npm link azle`);
+    }
+
     execSync(`dfx canister uninstall-code sqlite_drizzle || true`, {
         stdio: 'inherit'
     });
