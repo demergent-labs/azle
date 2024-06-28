1565ed89b542997578f2cfb0e2666d9cfc92c1c0
Benjamin DeMann <benjamin.demann@gmail.com>
2024-06-28 15:30:27 -0600

    jest examples r

--- examples/run_time_errors/test/invalid_blobs.ts
+++ examples/run_time_errors/test/invalid_blobs.ts
@@ -1,10 +1,7 @@
-// @ts-nocheck
-
 import { ActorSubclass } from '@dfinity/agent';
-import { Test } from 'azle/test';
+import { expect, it, Test } from 'azle/test';
 // @ts-ignore this path may not exist when these tests are imported into other test projects
 import { _SERVICE } from './dfx_generated/run_time_errors/run_time_errors.did';
-import { expectError } from './tests';
 
 const valueIsNotOfTypeBlobErrorMessage =
     "TypeError: Value is not of type 'blob'";
@@ -14,17 +11,18 @@ const valueIsNotATypedArrayErrorMessage = `TypeError: Value is not of type 'blob
 
 export function getInvalidBlobTests(
     errorCanister: ActorSubclass<_SERVICE>
-): Test[] {
-    return [
-        expectError(
-            'return non-object as invalid blob',
-            errorCanister.returnNonObjectAsInvalidBlob,
-            valueIsNotOfTypeBlobErrorMessage
-        ),
-        expectError(
-            'return an empty object as invalid blob',
-            errorCanister.returnEmptyObjectAsInvalidBlob,
-            valueIsNotATypedArrayErrorMessage
-        )
-    ];
+): Test {
+    return () => {
+        it('return non-object as invalid blob', async () => {
+            await expect(
+                errorCanister.returnNonObjectAsInvalidBlob
+            ).rejects.toThrow(valueIsNotOfTypeBlobErrorMessage);
+        });
+
+        it('return an empty object as invalid blob', async () => {
+            await expect(
+                errorCanister.returnEmptyObjectAsInvalidBlob
+            ).rejects.toThrow(valueIsNotATypedArrayErrorMessage);
+        });
+    };
 }
