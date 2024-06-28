f07c9045841f36d336953a693fb64db4cb751eb6
Benjamin DeMann <benjamin.demann@gmail.com>
2024-06-28 15:30:07 -0600

    jest examples o-p

--- examples/plugins/test/tests.ts
+++ examples/plugins/test/tests.ts
@@ -1,85 +1,59 @@
 import { ActorSubclass } from '@dfinity/agent';
-import { Test } from 'azle/test';
+import { expect, it, Test } from 'azle/test';
 
 // @ts-ignore this path may not exist when these tests are imported into other test projects
 import { _SERVICE } from './dfx_generated/plugins/plugins.did';
 
-export function getTests(pluginsCanister: ActorSubclass<_SERVICE>): Test[] {
-    return [
-        {
-            name: 'execute - create table',
-            test: async () => {
-                const sql = `CREATE TABLE IF NOT EXISTS test_table (id INTEGER PRIMARY KEY, value TEXT);`;
-                const result = await pluginsCanister.execute(sql);
+export function getTests(pluginsCanister: ActorSubclass<_SERVICE>): Test {
+    return () => {
+        it('execute - create table', async () => {
+            const sql = `CREATE TABLE IF NOT EXISTS test_table (id INTEGER PRIMARY KEY, value TEXT);`;
+            const result = await pluginsCanister.execute(sql);
 
-                return 'Ok' in result ? { Ok: true } : { Ok: false };
-            }
-        },
-        {
-            name: 'execute - insert data',
-            test: async () => {
-                const sql = `INSERT INTO test_table (value) VALUES ('test_value');`;
-                const result = await pluginsCanister.execute(sql);
+            expect(result).toHaveProperty('Ok');
+        });
 
-                return 'Ok' in result ? { Ok: true } : { Ok: false };
-            }
-        },
-        {
-            name: 'query - select data',
-            test: async () => {
-                const sql = `SELECT * FROM test_table;`;
-                const result = await pluginsCanister.query(sql);
+        it('execute - insert data', async () => {
+            const sql = `INSERT INTO test_table (value) VALUES ('test_value');`;
+            const result = await pluginsCanister.execute(sql);
 
-                return 'Ok' in result &&
-                    result.Ok.length === 1 &&
-                    result.Ok[0][0] === '1' &&
-                    result.Ok[0][1] === 'test_value'
-                    ? { Ok: true }
-                    : { Ok: false };
-            }
-        },
-        {
-            name: 'execute - update data',
-            test: async () => {
-                const sql = `UPDATE test_table SET value = 'updated_value' WHERE id = 1;`;
-                const result = await pluginsCanister.execute(sql);
+            expect(result).toHaveProperty('Ok');
+        });
 
-                return 'Ok' in result ? { Ok: true } : { Ok: false };
-            }
-        },
-        {
-            name: 'query - select updated data',
-            test: async () => {
-                const sql = `SELECT * FROM test_table WHERE id = 1;`;
-                const result = await pluginsCanister.query(sql);
+        it('query - select data', async () => {
+            const sql = `SELECT * FROM test_table;`;
+            const result = await pluginsCanister.query(sql);
 
-                return 'Ok' in result &&
-                    result.Ok.length === 1 &&
-                    result.Ok[0][0] === '1' &&
-                    result.Ok[0][1] === 'updated_value'
-                    ? { Ok: true }
-                    : { Ok: false };
-            }
-        },
-        {
-            name: 'execute - delete data',
-            test: async () => {
-                const sql = `DELETE FROM test_table WHERE id = 1;`;
-                const result = await pluginsCanister.execute(sql);
+            expect(result).toStrictEqual({ Ok: [['1', 'test_value']] });
+        });
 
-                return 'Ok' in result ? { Ok: true } : { Ok: false };
-            }
-        },
-        {
-            name: 'query - select no data',
-            test: async () => {
-                const sql = `SELECT * FROM test_table;`;
-                const result = await pluginsCanister.query(sql);
+        it('execute - update data', async () => {
+            const sql = `UPDATE test_table SET value = 'updated_value' WHERE id = 1;`;
+            const result = await pluginsCanister.execute(sql);
 
-                return 'Ok' in result && result.Ok.length === 0
-                    ? { Ok: true }
-                    : { Ok: false };
-            }
-        }
-    ];
+            expect(result).toHaveProperty('Ok');
+        });
+
+        it('query - select updated data', async () => {
+            const sql = `SELECT * FROM test_table WHERE id = 1;`;
+            const result = await pluginsCanister.query(sql);
+
+            expect(result).toStrictEqual({ Ok: [['1', 'updated_value']] });
+        });
+
+        it('execute - delete data', async () => {
+            const sql = `DELETE FROM test_table WHERE id = 1;`;
+            const result = await pluginsCanister.execute(sql);
+
+            expect(result).toHaveProperty('Ok');
+        });
+
+        it('query - select no data', async () => {
+            const sql = `SELECT * FROM test_table;`;
+            const result = await pluginsCanister.query(sql);
+
+            expect(result).toHaveProperty('Ok');
+            expect(result.Ok).toHaveLength(0);
+        });
+    };
 }
