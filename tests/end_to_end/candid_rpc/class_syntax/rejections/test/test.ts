f8f0c585409f2d577f02039b9ca6dfe801f93ec7
Benjamin DeMann <benjamin.demann@gmail.com>
2024-07-01 17:14:50 -0600

    last of the easy fixes

--- tests/end_to_end/candid_rpc/class_syntax/rejections/test/test.ts
+++ tests/end_to_end/candid_rpc/class_syntax/rejections/test/test.ts
@@ -1,7 +1,7 @@
+import { getCanisterId } from 'azle/dfx';
 import { runTests } from 'azle/test';
 import { getTests } from 'rejections_end_to_end_test_functional_syntax/test/tests';
 
-import { getCanisterId } from '../../../dfx';
 import { createActor } from './dfx_generated/rejections';
 
 const rejectionsCanister = createActor(getCanisterId('rejections'), {
