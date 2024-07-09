0a1354170510ad93c1d4c62661bd90a5ffc9be5e
Benjamin DeMann <benjamin.demann@gmail.com>
2024-07-09 09:54:24 -0600

    pr fixes

--- examples/key_value_store/test/tests.ts
+++ examples/key_value_store/test/tests.ts
@@ -6,37 +6,37 @@ import { _SERVICE } from './dfx_generated/key_value_store/key_value_store.did';
 
 export function getTests(keyValueStoreCanister: ActorSubclass<_SERVICE>): Test {
     return () => {
-        it('get initial value 0 from simple key value store', async () => {
+        it('gets initial value 0 from simple key value store', async () => {
             const result = await keyValueStoreCanister.get('0');
 
             expect(result).toHaveLength(0);
         });
 
-        it('get initial value 1 from simple key value store', async () => {
+        it('gets initial value 1 from simple key value store', async () => {
             const result = await keyValueStoreCanister.get('1');
 
             expect(result).toHaveLength(0);
         });
 
-        it('set value 0 from a simple key value store', async () => {
+        it('sets value 0 from a simple key value store', async () => {
             const result = await keyValueStoreCanister.set('0', 'zero');
 
             expect(result).toBeUndefined();
         });
 
-        it('set value 1 from a simple key value store', async () => {
+        it('sets value 1 from a simple key value store', async () => {
             const result = await keyValueStoreCanister.set('1', 'one');
 
             expect(result).toBeUndefined();
         });
 
-        it('get value 0 from a simple key value store', async () => {
+        it('gets value 0 from a simple key value store', async () => {
             const result = await keyValueStoreCanister.get('0');
 
             expect(result).toStrictEqual(['zero']);
         });
 
-        it('get value 1 from a simple key value store', async () => {
+        it('gets value 1 from a simple key value store', async () => {
             const result = await keyValueStoreCanister.get('1');
 
             expect(result).toStrictEqual(['one']);
