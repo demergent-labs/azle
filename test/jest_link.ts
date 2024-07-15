ab62aff97956dbfbd31e88a2319b39fccf58d65c
Jordan Last <jordan.michael.last@gmail.com>
2024-07-15 14:25:02 -0500

    test linkAndInstallPatch

--- test/jest_link.ts
+++ test/jest_link.ts
@@ -1,17 +1,29 @@
 import { execSync } from 'child_process';
-import { join, resolve } from 'path';
+import { join } from 'path';
 
+import { AZLE_PACKAGE_PATH } from '../src/compiler/utils/global_paths';
+
+// TODO remove install and link after https://github.com/demergent-labs/azle/issues/1807 is resolved
 export function linkAndInstallPatch(pathRelativeToAzle: string): void {
-    // TODO remove install and link after https://github.com/demergent-labs/azle/issues/1807 is resolved
-    const azleDir = resolve(__dirname, '..');
+    console.log(
+        'process.env.AZLE_END_TO_END_TEST_LINK_AZLE',
+        process.env.AZLE_END_TO_END_TEST_LINK_AZLE
+    );
 
-    execSync(`cd ${join(azleDir, pathRelativeToAzle)} && npm install`);
+    console.log('AZLE_PACKAGE_PATH', AZLE_PACKAGE_PATH);
+
+    // TODO do we even need to do this on release?
+    execSync(
+        `cd ${join(AZLE_PACKAGE_PATH, pathRelativeToAzle)} && npm install`
+    );
 
     if (process.env.AZLE_END_TO_END_TEST_LINK_AZLE !== 'false') {
         // TODO remove logs once we are satisfied that the link doesn't happen during release candidate tests
         console.log('--------------------------------------------------------');
         console.log("--- We're linking!!! -----------------------------------");
         console.log('--------------------------------------------------------');
-        execSync(`cd ${join(azleDir, pathRelativeToAzle)} && npm link azle`);
+        execSync(
+            `cd ${join(AZLE_PACKAGE_PATH, pathRelativeToAzle)} && npm link azle`
+        );
     }
 }
