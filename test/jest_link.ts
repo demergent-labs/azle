60a5c13157092d2821e40b6301c6d755926f7edd
Jordan Last <jordan.michael.last@gmail.com>
2024-07-15 14:06:10 -0500

    azle-bot automated release 0.23.0-rc.4

--- test/jest_link.ts
+++ test/jest_link.ts
@@ -3,11 +3,10 @@ import { join, resolve } from 'path';
 
 export function linkAndInstallPatch(pathRelativeToAzle: string): void {
     // TODO remove install and link after https://github.com/demergent-labs/azle/issues/1807 is resolved
-    const azleDir = resolve(__dirname, '..');
-
-    execSync(`cd ${join(azleDir, pathRelativeToAzle)} && npm install`);
 
     if (process.env.AZLE_END_TO_END_TEST_LINK_AZLE !== 'false') {
+        const azleDir = resolve(__dirname, '..');
+        execSync(`cd ${join(azleDir, pathRelativeToAzle)} && npm install`);
         // TODO remove logs once we are satisfied that the link doesn't happen during release candidate tests
         console.log('--------------------------------------------------------');
         console.log("--- We're linking!!! -----------------------------------");
