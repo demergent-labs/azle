04ba4303f84dbbc8e9125005a0e953284d1cab09
Jordan Last <jordan.michael.last@gmail.com>
2024-07-16 14:44:10 -0500

    final cleanup of jest_link

--- test/jest_link.ts
+++ test/jest_link.ts
@@ -1,21 +1,14 @@
 import { execSync } from 'child_process';
 import { join, resolve } from 'path';
 
-// TODO remove install and link after https://github.com/demergent-labs/azle/issues/1807 is resolved
+// TODO remove linkAndInstallPatch after https://github.com/demergent-labs/azle/issues/1807 is resolved
 export function linkAndInstallPatch(pathRelativeToAzle: string): void {
     const examplesDir =
         process.env.GITHUB_WORKSPACE ?? resolve(__dirname, '..');
 
-    console.log('examplesDir', examplesDir);
-
-    console.log(
-        'process.env.AZLE_END_TO_END_TEST_LINK_AZLE',
-        process.env.AZLE_END_TO_END_TEST_LINK_AZLE
-    );
+    execSync(`cd ${join(examplesDir, pathRelativeToAzle)} && npm install`);
 
     if (process.env.AZLE_END_TO_END_TEST_LINK_AZLE !== 'false') {
-        // TODO do we even need to do this on release?
-        execSync(`cd ${join(examplesDir, pathRelativeToAzle)} && npm install`);
         execSync(
             `cd ${join(examplesDir, pathRelativeToAzle)} && npm link azle`
         );
