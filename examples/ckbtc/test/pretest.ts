4dae4f73a496559f2d9223823704b093c86f15d9
Jordan Last <jordan.michael.last@gmail.com>
2024-07-17 10:12:20 -0500

    fix ckbtc link path

--- examples/ckbtc/test/pretest.ts
+++ examples/ckbtc/test/pretest.ts
@@ -3,7 +3,9 @@ import { execSync } from 'child_process';
 import { join } from 'path';
 
 function pretest(): void {
-    linkAndInstallPatch(join('examples', 'ckbtc'));
+    linkAndInstallPatch(
+        join('tests', 'end_to_end', 'candid_rpc', 'functional_syntax', 'ckbtc')
+    );
 
     uninstall(
         'ckbtc',
