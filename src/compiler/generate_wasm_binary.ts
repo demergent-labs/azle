cee8c95e3fa778b74186a07a076ad8ad755ea617
Benjamin DeMann <benjamin.demann@gmail.com>
2024-07-22 15:36:49 -0600

    move log global_dependencies to its own file

--- src/compiler/generate_wasm_binary.ts
+++ src/compiler/generate_wasm_binary.ts
@@ -1,14 +1,10 @@
 import { IOType } from 'child_process';
-import { writeFile } from 'fs/promises';
-import { join } from 'path';
 
+import { logGlobalDependencies } from './log_global_dependencies';
 import { manipulateWasmBinary } from './manipulate_wasm_binary';
 import { prepareRustStagingArea } from './prepare_rust_staging_area';
 import { execSyncPretty } from './utils/exec_sync_pretty';
-import {
-    AZLE_PACKAGE_PATH,
-    STATIC_CANISTER_TEMPLATE_PATH
-} from './utils/global_paths';
+import { STATIC_CANISTER_TEMPLATE_PATH } from './utils/global_paths';
 import { CanisterConfig, CompilerInfo } from './utils/types';
 
 export async function generateWasmBinary(
@@ -48,21 +44,4 @@ function compileRustCodeNatively(
         `wasi2ic target/wasm32-wasi/release/canister.wasm ${wasmDest}`,
         stdio
     );
-}
-
-async function logGlobalDependencies(): Promise<void> {
-    const wasiVersion = execSyncPretty('cargo install --list | grep wasi2ic');
-    const nodeVersion = execSyncPretty('node --version');
-    const rustVersion = execSyncPretty('rustc --version');
-
-    const globalDependencies = `WASI2IC Version: ${wasiVersion}
-Node Version: ${nodeVersion}
-Rust Version: ${rustVersion}`;
-
-    await writeFile(
-        join(AZLE_PACKAGE_PATH, 'global_dependencies'),
-        globalDependencies,
-        'utf-8'
-    );
-    console.log('Global dependencies written to global_dependencies file.');
 }
---
+++ src/compiler/log_global_dependencies.ts
@@ -0,0 +1,22 @@
+import { writeFile } from 'fs/promises';
+import { join } from 'path';
+
+import { execSyncPretty } from './utils/exec_sync_pretty';
+import { AZLE_PACKAGE_PATH } from './utils/global_paths';
+
+export async function logGlobalDependencies(): Promise<void> {
+    const wasiVersion = execSyncPretty('cargo install --list | grep wasi2ic');
+    const nodeVersion = execSyncPretty('node --version');
+    const rustVersion = execSyncPretty('rustc --version');
+
+    const globalDependencies = `WASI2IC Version: ${wasiVersion}
+Node Version: ${nodeVersion}
+Rust Version: ${rustVersion}`;
+
+    await writeFile(
+        join(AZLE_PACKAGE_PATH, 'global_dependencies'),
+        globalDependencies,
+        'utf-8'
+    );
+    console.log('Global dependencies written to global_dependencies file.');
+}
