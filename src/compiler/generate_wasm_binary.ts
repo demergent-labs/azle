881d0b1997d68ba15f48e6c2e955baa94df38bde
Benjamin DeMann <benjamin.demann@gmail.com>
2024-07-22 15:30:05 -0600

    update ic-wasi-polyfill

--- src/compiler/generate_wasm_binary.ts
+++ src/compiler/generate_wasm_binary.ts
@@ -1,9 +1,14 @@
 import { IOType } from 'child_process';
+import { writeFile } from 'fs/promises';
+import { join } from 'path';
 
 import { manipulateWasmBinary } from './manipulate_wasm_binary';
 import { prepareRustStagingArea } from './prepare_rust_staging_area';
 import { execSyncPretty } from './utils/exec_sync_pretty';
-import { STATIC_CANISTER_TEMPLATE_PATH } from './utils/global_paths';
+import {
+    AZLE_PACKAGE_PATH,
+    STATIC_CANISTER_TEMPLATE_PATH
+} from './utils/global_paths';
 import { CanisterConfig, CompilerInfo } from './utils/types';
 
 export async function generateWasmBinary(
@@ -15,6 +20,8 @@ export async function generateWasmBinary(
     canisterPath: string
 ): Promise<void> {
     if (process.env.AZLE_GEN_WASM === 'true') {
+        await logGlobalDependencies();
+
         await prepareRustStagingArea(canisterConfig, canisterPath);
 
         compileRustCodeNatively(
@@ -41,4 +48,21 @@ function compileRustCodeNatively(
         `wasi2ic target/wasm32-wasi/release/canister.wasm ${wasmDest}`,
         stdio
     );
+}
+
+async function logGlobalDependencies(): Promise<void> {
+    const wasiVersion = execSyncPretty('cargo install --list | grep wasi2ic');
+    const nodeVersion = execSyncPretty('node --version');
+    const rustVersion = execSyncPretty('rustc --version');
+
+    const globalDependencies = `WASI2IC Version: ${wasiVersion}
+Node Version: ${nodeVersion}
+Rust Version: ${rustVersion}`;
+
+    await writeFile(
+        join(AZLE_PACKAGE_PATH, 'global_dependencies'),
+        globalDependencies,
+        'utf-8'
+    );
+    console.log('Global dependencies written to global_dependencies file.');
 }
