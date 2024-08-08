d5fa9bb2c8763e4b8e21ea5e544661509ee38547
Jordan Last <jordan.michael.last@gmail.com>
2024-08-08 11:58:06 -0500

    move candid into its own file

--- src/compiler/rust/canister/src/lib.rs
+++ src/compiler/rust/canister/src/lib.rs
@@ -17,6 +17,7 @@ use wasmedge_quickjs::AsObject;
 
 #[cfg(feature = "experimental")]
 mod autoreload;
+mod candid;
 mod chunk;
 mod guards;
 mod ic;
@@ -93,76 +94,7 @@ pub fn execute_js(function_index: i32, pass_arg_data: i32) {
     });
 }
 
-// Heavily inspired by https://stackoverflow.com/a/47676844
-#[no_mangle]
-pub fn get_candid_pointer(experimental: i32) -> *mut std::os::raw::c_char {
-    std::panic::set_hook(Box::new(|panic_info| {
-        let msg = match panic_info.payload().downcast_ref::<&str>() {
-            Some(s) => *s,
-            None => "Unknown panic message",
-        };
-        let location = if let Some(location) = panic_info.location() {
-            format!(" at {}:{}", location.file(), location.line())
-        } else {
-            " (unknown location)".to_string()
-        };
-
-        let message = &format!("Panic occurred: {}{}", msg, location);
-
-        ic_cdk::println!("{}", message);
-    }));
-
-    RUNTIME.with(|runtime| {
-        let mut runtime = wasmedge_quickjs::Runtime::new();
-
-        runtime.run_with_context(|context| {
-            context.get_global().set(
-                "_azleWasmtimeCandidEnvironment",
-                wasmedge_quickjs::JsValue::Bool(true),
-            );
-
-            ic::register(context);
-
-            let js = get_js_code();
-
-            // TODO what do we do if there is an error in here?
-            context.eval_global_str("globalThis.exports = {};".to_string());
-            context.eval_global_str(format!(
-                "globalThis._azleExperimental = {};",
-                if experimental == 1 { "true" } else { "false" }
-            ));
-            context.eval_module_str(std::str::from_utf8(&js).unwrap().to_string(), "azle_main");
-
-            run_event_loop(context);
-
-            let global = context.get_global();
-
-            let candid_info_function = global.get("candidInfoFunction").to_function().unwrap();
-
-            let candid_info = candid_info_function.call(&[]);
-
-            // TODO error handling is mostly done in JS right now
-            // TODO we would really like wasmedge-quickjs to add
-            // TODO good error info to JsException and move error handling
-            // TODO out of our own code
-            match &candid_info {
-                wasmedge_quickjs::JsValue::Exception(js_exception) => {
-                    js_exception.dump_error();
-                    panic!("TODO needs error info");
-                }
-                _ => run_event_loop(context),
-            };
-
-            let candid_info_string = candid_info.to_string().unwrap().to_string();
-
-            let c_string = std::ffi::CString::new(candid_info_string).unwrap();
-
-            c_string.into_raw()
-        })
-    })
-}
-
-fn run_event_loop(context: &mut wasmedge_quickjs::Context) {
+pub fn run_event_loop(context: &mut wasmedge_quickjs::Context) {
     context.promise_loop_poll();
 
     loop {
