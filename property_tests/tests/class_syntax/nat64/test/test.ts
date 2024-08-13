275567fb91273b437c2d57af9e4e09711ad35ffe
Benjamin DeMann <benjamin.demann@gmail.com>
2024-08-13 15:59:55 -0600

    add nat64 class syntax prop tests

---
+++ property_tests/tests/class_syntax/nat64/test/test.ts
@@ -0,0 +1,31 @@
+import { defaultArrayConstraints, runPropTests } from 'azle/property_tests';
+import { Nat64Arb } from 'azle/property_tests/arbitraries/candid/primitive/nats/nat64_arb';
+import {
+    CanisterArb,
+    CanisterConfig
+} from 'azle/property_tests/arbitraries/canister_arb';
+import { QueryMethodArb } from 'azle/property_tests/arbitraries/canister_methods/query_method_arb';
+import fc from 'fast-check';
+
+import { generateBody } from './generate_body';
+import { generateTests } from './generate_tests';
+
+const syntax = 'class';
+
+const AllNat64sQueryMethodArb = QueryMethodArb(
+    fc.array(Nat64Arb(syntax)),
+    Nat64Arb(syntax),
+    {
+        generateBody,
+        generateTests,
+        syntax
+    }
+);
+
+const CanisterConfigArb = fc
+    .array(AllNat64sQueryMethodArb, defaultArrayConstraints)
+    .map((queryMethods): CanisterConfig => {
+        return { queryMethods };
+    });
+
+runPropTests(CanisterArb(CanisterConfigArb, syntax));
