dbb144ab0f439b93dc28db75a12ca7fd2a36f108
Benjamin DeMann <benjamin.demann@gmail.com>
2024-08-13 15:54:34 -0600

    add nat class syntax prop tests

---
+++ property_tests/tests/class_syntax/nat/test/test.ts
@@ -0,0 +1,31 @@
+import { defaultArrayConstraints, runPropTests } from 'azle/property_tests';
+import { NatArb } from 'azle/property_tests/arbitraries/candid/primitive/nats/nat_arb';
+import {
+    CanisterArb,
+    CanisterConfig
+} from 'azle/property_tests/arbitraries/canister_arb';
+import { QueryMethodArb } from 'azle/property_tests/arbitraries/canister_methods/query_method_arb';
+import fc from 'fast-check';
+
+import { generateBody } from './generate_body';
+import { generateTests } from './generate_tests';
+
+const syntax = 'class';
+
+const AllNatsQueryMethodArb = QueryMethodArb(
+    fc.array(NatArb(syntax)),
+    NatArb(syntax),
+    {
+        generateBody,
+        generateTests,
+        syntax
+    }
+);
+
+const CanisterConfigArb = fc
+    .array(AllNatsQueryMethodArb, defaultArrayConstraints)
+    .map((queryMethods): CanisterConfig => {
+        return { queryMethods };
+    });
+
+runPropTests(CanisterArb(CanisterConfigArb, syntax));
