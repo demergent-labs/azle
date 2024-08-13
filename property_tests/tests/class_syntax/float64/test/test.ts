a4a2b9acf5f4bacd96833a4b1b019d639d1bc28a
Benjamin DeMann <benjamin.demann@gmail.com>
2024-08-13 15:52:13 -0600

    add float64 class syntax prop tests

---
+++ property_tests/tests/class_syntax/float64/test/test.ts
@@ -0,0 +1,31 @@
+import { defaultArrayConstraints, runPropTests } from 'azle/property_tests';
+import { Float64Arb } from 'azle/property_tests/arbitraries/candid/primitive/floats/float64_arb';
+import {
+    CanisterArb,
+    CanisterConfig
+} from 'azle/property_tests/arbitraries/canister_arb';
+import { QueryMethodArb } from 'azle/property_tests/arbitraries/canister_methods/query_method_arb';
+import fc from 'fast-check';
+
+import { generateBody } from './generate_body';
+import { generateTests } from './generate_tests';
+
+const syntax = 'class';
+
+const AllFloat64sQueryMethodArb = QueryMethodArb(
+    fc.array(Float64Arb(syntax)),
+    Float64Arb(syntax),
+    {
+        generateBody,
+        generateTests,
+        syntax
+    }
+);
+
+const CanisterConfigArb = fc
+    .array(AllFloat64sQueryMethodArb, defaultArrayConstraints)
+    .map((queryMethods): CanisterConfig => {
+        return { queryMethods };
+    });
+
+runPropTests(CanisterArb(CanisterConfigArb, syntax));
