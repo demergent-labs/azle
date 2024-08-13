88bfdfe8b3760049132da3101c8db95d96cc86e2
Benjamin DeMann <benjamin.demann@gmail.com>
2024-08-13 15:55:52 -0600

    add nat16 class syntax prop tests

---
+++ property_tests/tests/class_syntax/nat16/test/generate_body.ts
@@ -0,0 +1,30 @@
+import { Named } from 'azle/property_tests';
+import { CandidValueAndMeta } from 'azle/property_tests/arbitraries/candid/candid_value_and_meta_arb';
+import { areParamsCorrectlyOrdered } from 'azle/property_tests/are_params_correctly_ordered';
+
+export function generateBody(
+    namedParamNat16s: Named<CandidValueAndMeta<number>>[],
+    returnNat16: CandidValueAndMeta<number>
+): string {
+    const paramsAreNumbers = namedParamNat16s
+        .map((param) => {
+            return `if (typeof ${param.name} !== 'number') throw new Error('${param.name} must be a number');`;
+        })
+        .join('\n');
+
+    const sum = namedParamNat16s.reduce((acc, { name }) => {
+        return `${acc} + ${name}`;
+    }, returnNat16.src.valueLiteral);
+    const count = namedParamNat16s.length + 1;
+    const average = `Math.floor((${sum}) / ${count})`;
+
+    const paramsCorrectlyOrdered = areParamsCorrectlyOrdered(namedParamNat16s);
+
+    return `
+        ${paramsAreNumbers}
+
+        ${paramsCorrectlyOrdered}
+
+        return ${average};
+    `;
+}
