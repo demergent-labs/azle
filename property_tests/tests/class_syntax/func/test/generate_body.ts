a98a2ec8ec76f78c58f2ffd7bff36568c67cc32f
Benjamin DeMann <benjamin.demann@gmail.com>
2024-08-13 20:40:15 -0600

    add func class syntax prop tests

---
+++ property_tests/tests/class_syntax/func/test/generate_body.ts
@@ -0,0 +1,41 @@
+import { Named } from 'azle/property_tests';
+import { CandidValueAndMeta } from 'azle/property_tests/arbitraries/candid/candid_value_and_meta_arb';
+import { Func } from 'azle/property_tests/arbitraries/candid/reference/func_arb';
+import { areParamsCorrectlyOrdered } from 'azle/property_tests/are_params_correctly_ordered';
+
+export function generateBody(
+    namedParamFuncs: Named<CandidValueAndMeta<Func>>[],
+    returnFunc: CandidValueAndMeta<Func>
+): string {
+    const paramsAreFuncs = namedParamFuncs
+        .map(({ name }) => {
+            const paramIsArray = `Array.isArray(${name})`;
+            const paramHas2Fields = `${name}.length === 2`;
+            const field0IsAPrincipal = `${name}[0]._isPrincipal === true`;
+            const field1IsAString = `typeof ${name}[1] === 'string'`;
+
+            const paramIsAFunc = [
+                paramIsArray,
+                paramHas2Fields,
+                field0IsAPrincipal,
+                field1IsAString
+            ].join(' && ');
+
+            const throwError = `throw new Error('${name} must be a Func');`;
+
+            return `if (!(${paramIsAFunc})) ${throwError}`;
+        })
+        .join('\n');
+
+    const paramsCorrectlyOrdered = areParamsCorrectlyOrdered(namedParamFuncs);
+
+    const returnStatement = returnFunc.src.valueLiteral;
+
+    return `
+        ${paramsAreFuncs}
+
+        ${paramsCorrectlyOrdered}
+
+        return ${returnStatement};
+    `;
+}
---
+++ property_tests/tests/class_syntax/func/test/test.ts
@@ -0,0 +1,33 @@
+import { defaultArrayConstraints, runPropTests } from 'azle/property_tests';
+import { FuncArb } from 'azle/property_tests/arbitraries/candid/reference/func_arb';
+import {
+    CanisterArb,
+    CanisterConfig
+} from 'azle/property_tests/arbitraries/canister_arb';
+import { QueryMethodArb } from 'azle/property_tests/arbitraries/canister_methods/query_method_arb';
+import fc from 'fast-check';
+
+import { generateBody } from './generate_body';
+import { generateTests } from './generate_tests';
+
+const syntax = 'class';
+
+const AllFuncsQueryMethodArb = QueryMethodArb(
+    fc.uniqueArray(FuncArb(syntax), {
+        selector: (entry) => entry.src.candidTypeAnnotation
+    }),
+    FuncArb(syntax),
+    {
+        generateBody,
+        generateTests,
+        syntax
+    }
+);
+
+const CanisterConfigArb = fc
+    .array(AllFuncsQueryMethodArb, defaultArrayConstraints)
+    .map((queryMethods): CanisterConfig => {
+        return { queryMethods };
+    });
+
+runPropTests(CanisterArb(CanisterConfigArb, syntax));
