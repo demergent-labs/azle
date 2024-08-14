81130b512b0893740cbf28845251a4f87e01ef92
Benjamin DeMann <benjamin.demann@gmail.com>
2024-08-13 20:38:37 -0600

    add record class syntax prop tests

---
+++ property_tests/tests/class_syntax/record/test/generate_body.ts
@@ -0,0 +1,35 @@
+import { Named } from 'azle/property_tests';
+import { CandidValueAndMeta } from 'azle/property_tests/arbitraries/candid/candid_value_and_meta_arb';
+import { Record } from 'azle/property_tests/arbitraries/candid/constructed/record_arb';
+import { areParamsCorrectlyOrdered } from 'azle/property_tests/are_params_correctly_ordered';
+
+export function generateBody(
+    namedParamRecords: Named<CandidValueAndMeta<Record>>[],
+    returnRecord: CandidValueAndMeta<Record>
+): string {
+    const paramsAreRecords = namedParamRecords
+        .map((param) => {
+            const fieldsCount = Object.keys(
+                param.value.value.agentArgumentValue
+            ).length;
+
+            const paramIsObject = `typeof ${param.name} === 'object'`;
+            const paramHasCorrectNumberOfFields = `Object.keys(${param.name}).length === ${fieldsCount}`;
+            const throwError = `throw new Error('${param.name} must be a Record');`;
+
+            return `if (!(${paramIsObject} && ${paramHasCorrectNumberOfFields})) ${throwError}`;
+        })
+        .join('\n');
+
+    const paramsCorrectlyOrdered = areParamsCorrectlyOrdered(namedParamRecords);
+
+    const returnStatement = returnRecord.src.valueLiteral;
+
+    return `
+        ${paramsAreRecords}
+
+        ${paramsCorrectlyOrdered}
+
+        return ${returnStatement};
+    `;
+}
---
+++ property_tests/tests/class_syntax/record/test/generate_tests.ts
@@ -0,0 +1,32 @@
+import { getActor, Named } from 'azle/property_tests';
+import { CandidValueAndMeta } from 'azle/property_tests/arbitraries/candid/candid_value_and_meta_arb';
+import { Record } from 'azle/property_tests/arbitraries/candid/constructed/record_arb';
+import { AzleResult, Test, testEquality } from 'azle/property_tests/test';
+
+export function generateTests(
+    functionName: string,
+    namedParamRecords: Named<CandidValueAndMeta<Record>>[],
+    returnRecord: CandidValueAndMeta<Record>
+): Test[][] {
+    return [
+        [
+            {
+                name: `record ${functionName}`,
+                test: async (): Promise<AzleResult> => {
+                    const actor = getActor(__dirname);
+
+                    const result = await actor[functionName](
+                        ...namedParamRecords.map(
+                            (param) => param.value.value.agentArgumentValue
+                        )
+                    );
+
+                    return testEquality(
+                        result,
+                        returnRecord.value.agentResponseValue
+                    );
+                }
+            }
+        ]
+    ];
+}
