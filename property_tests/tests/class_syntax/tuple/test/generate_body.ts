01eec74b389e6522b182751b3d27567ceabe14d3
Benjamin DeMann <benjamin.demann@gmail.com>
2024-08-13 20:41:42 -0600

    add tuple class syntax prop tests

---
+++ property_tests/tests/class_syntax/tuple/test/generate_body.ts
@@ -0,0 +1,36 @@
+import { Named } from 'azle/property_tests';
+import { CandidValueAndMeta } from 'azle/property_tests/arbitraries/candid/candid_value_and_meta_arb';
+import {
+    ReturnTuple,
+    Tuple
+} from 'azle/property_tests/arbitraries/candid/constructed/tuple_arb';
+import { areParamsCorrectlyOrdered } from 'azle/property_tests/are_params_correctly_ordered';
+
+export function generateBody(
+    namedParamTuples: Named<CandidValueAndMeta<Tuple, ReturnTuple>>[],
+    returnTuple: CandidValueAndMeta<Tuple, ReturnTuple>
+): string {
+    const paramsAreTuples = namedParamTuples
+        .map((param) => {
+            const fieldsCount = param.value.value.agentArgumentValue.length;
+
+            const paramIsArray = `Array.isArray(${param.name})`;
+            const paramHasCorrectNumberOfFields = `${param.name}.length === ${fieldsCount}`;
+            const throwError = `throw new Error('${param.name} must be a Tuple');`;
+
+            return `if (!(${paramIsArray} && ${paramHasCorrectNumberOfFields})) ${throwError}`;
+        })
+        .join('\n');
+
+    const paramsCorrectlyOrdered = areParamsCorrectlyOrdered(namedParamTuples);
+
+    const returnStatement = returnTuple.src.valueLiteral;
+
+    return `
+        ${paramsAreTuples}
+
+        ${paramsCorrectlyOrdered}
+
+        return ${returnStatement};
+    `;
+}
