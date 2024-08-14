3120da4c3b7c2016ddb34bfefe993358f59cd69b
Benjamin DeMann <benjamin.demann@gmail.com>
2024-08-13 20:42:16 -0600

    add vec class syntax prop tests

---
+++ property_tests/tests/class_syntax/vec/test/generate_body.ts
@@ -0,0 +1,26 @@
+import { Named } from 'azle/property_tests';
+import { CandidValueAndMeta } from 'azle/property_tests/arbitraries/candid/candid_value_and_meta_arb';
+import { areParamsCorrectlyOrdered } from 'azle/property_tests/are_params_correctly_ordered';
+
+export function generateBody(
+    namedParamVecs: Named<CandidValueAndMeta<any>>[],
+    returnVec: CandidValueAndMeta<any>
+): string {
+    const paramsAreArrays = namedParamVecs
+        .map((param) => {
+            return `if (!Array.isArray(${param.name}) && !ArrayBuffer.isView(${param.name})) throw new Error('${param.name} must be an array');`;
+        })
+        .join('\n');
+
+    const paramsCorrectlyOrdered = areParamsCorrectlyOrdered(namedParamVecs);
+
+    const returnValue = returnVec.src.valueLiteral;
+
+    return `
+        ${paramsAreArrays}
+
+        ${paramsCorrectlyOrdered}
+
+        return ${returnValue};
+    `;
+}
---
+++ property_tests/tests/class_syntax/vec/test/test.ts
@@ -0,0 +1,31 @@
+import { defaultArrayConstraints, runPropTests } from 'azle/property_tests';
+import { VecArb } from 'azle/property_tests/arbitraries/candid/constructed/vec_arb';
+import {
+    CanisterArb,
+    CanisterConfig
+} from 'azle/property_tests/arbitraries/canister_arb';
+import { QueryMethodArb } from 'azle/property_tests/arbitraries/canister_methods/query_method_arb';
+import fc from 'fast-check';
+
+import { generateBody } from './generate_body';
+import { generateTests } from './generate_tests';
+
+const syntax = 'class';
+
+const AllVecsQueryMethodArb = QueryMethodArb(
+    fc.array(VecArb(syntax)),
+    VecArb(syntax),
+    {
+        generateBody,
+        generateTests,
+        syntax
+    }
+);
+
+const CanisterConfigArb = fc
+    .array(AllVecsQueryMethodArb, defaultArrayConstraints)
+    .map((queryMethods): CanisterConfig => {
+        return { queryMethods };
+    });
+
+runPropTests(CanisterArb(CanisterConfigArb, syntax));
