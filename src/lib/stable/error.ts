bc85d1138ef83ae5155bc056077ae54e9efcf44f
Benjamin DeMann <benjamin.demann@gmail.com>
2024-08-19 15:19:37 -0600

    pr fix

--- src/lib/stable/error.ts
+++ src/lib/stable/error.ts
@@ -1,4 +1,4 @@
-// import { trap } from './ic_apis/trap'; // TODO why does this break bitcoin_psbt examples?
+// import { trap } from './ic_apis/trap'; // TODO why does this break bitcoin_psbt examples? https://github.com/demergent-labs/azle/issues/2003
 import { trap } from '.';
 
 export function handleUncaughtError(rawError: any): never {
