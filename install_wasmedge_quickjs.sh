1b3ba12cfc788933e6ea12fbae75410d05335d48
Jordan Last <jordan.michael.last@gmail.com>
2024-08-28 16:23:20 -0500

    use add instead of set for sparse checkout

--- install_wasmedge_quickjs.sh
+++ install_wasmedge_quickjs.sh
@@ -4,5 +4,7 @@ rm -rf "$WASMEDGE_REPO_PATH"
 
 git clone --depth 1 --filter=blob:none --sparse https://github.com/demergent-labs/wasmedge-quickjs "$WASMEDGE_REPO_PATH"
 cd "$WASMEDGE_REPO_PATH"
-git sparse-checkout set modules LICENSE LICENSE-QuickJS.txt
+git sparse-checkout set modules
+git sparse-checkout add LICENSE
+git sparse-checkout add LICENSE-QuickJS.txt
 git checkout c21ff69f442998e4cda4619166e23a9bc91418be
