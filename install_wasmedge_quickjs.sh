537699c224cb4492498fcf2f8286bed3348c55e2
Jordan Last <jordan.michael.last@gmail.com>
2024-08-29 12:35:39 -0500

    add dependencies.json and switch back to http for wasmedge-quickjs installation

--- install_wasmedge_quickjs.sh
+++ install_wasmedge_quickjs.sh
@@ -2,11 +2,7 @@ WASMEDGE_REPO_PATH="./wasmedge_quickjs"
 
 rm -rf "$WASMEDGE_REPO_PATH"
 
-git clone --depth 1 --filter=blob:none --sparse git@github.com:demergent-labs/wasmedge-quickjs.git "$WASMEDGE_REPO_PATH"
+git clone --depth 1 --filter=blob:none --sparse https://github.com/demergent-labs/wasmedge-quickjs.git "$WASMEDGE_REPO_PATH"
 cd "$WASMEDGE_REPO_PATH"
 git sparse-checkout set modules LICENSE LICENSE-QuickJS.txt
-# git sparse-checkout init
-# git sparse-checkout add modules
-# git sparse-checkout add LICENSE
-# git sparse-checkout add LICENSE-QuickJS.txt
 git checkout c21ff69f442998e4cda4619166e23a9bc91418be
