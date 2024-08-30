3ff7ef183805bafa0a5139f7c3596fa2e1d2e425
Benjamin DeMann <benjamin.demann@gmail.com>
2024-08-30 09:18:57 -0600

    make sure empty all_directories leads to empty result

--- .github/scripts/generate_tests.sh
+++ .github/scripts/generate_tests.sh
@@ -79,13 +79,21 @@ done
 # Initialize an empty variable to store the JSON result
 json_result="["
 
-# Sort, filter, and generate JSON objects
-while read -r dir; do
-    if ! is_excluded "$dir"; then
-        json_result+=$(generate_json "$dir")
-        json_result+=","
-    fi
-done <<< "$(echo "$all_directories" | sort)"  # Feed sorted directories into the loop
+if [[ -n "$all_directories" ]]; then  # Check if all_directories is not an empty string
+    while read -r dir; do
+        if [[ -n "$dir" ]]; then  # Check if dir is not an empty string
+            echo "This is the dir" >&2
+            echo "$dir" >&2
+            if ! is_excluded "$dir"; then
+                json_result+=$(generate_json "$dir")
+                json_result+=","
+            fi
+        fi
+    done <<< "$(echo "$all_directories" | sort)"  # Feed sorted directories into the loop
+
+    # Remove the trailing comma from the json_result, if necessary
+    json_result="${json_result%,}"
+fi
 
 # Remove the last comma and close the JSON array
 json_result=$(echo "$json_result" | sed '$ s/,$//')
