442dea9f87cc99f86965865e5e61f9ffe626f312
Benjamin DeMann <benjamin.demann@gmail.com>
2024-09-13 15:15:30 -0600

    update test names

--- .github/actions/get_test_infos/get_test_infos.js
+++ .github/actions/get_test_infos/get_test_infos.js
@@ -2,25 +2,21 @@ import { discoverTestDirs } from './discover_test_dirs.js';
 import { dirToTestInfo } from './dir_to_test_info.js';
 
 export async function getTestInfos(dirs, excludeDirs) {
-    // Use reduce to accumulate all discovered directories
     const allDirs = await dirs.reduce(async (accPromise, dir) => {
         const acc = await accPromise;
         const discoveredDirs = await discoverTestDirs(dir);
         return [...acc, ...discoveredDirs];
     }, Promise.resolve([]));
 
-    // Sort the directories and remove duplicates
     const testDirs = [...new Set(allDirs)] // Remove duplicates if any
         .sort()
         .filter((dir) => dir && !isExcluded(dir, excludeDirs));
 
     const testInfos = testDirs.map((dir) => dirToTestInfo(dir));
 
-    // Log the result (or do further processing)
     return testInfos;
 }
 
-// Function to check if a directory is excluded
 function isExcluded(dir, excludeDirs) {
     return excludeDirs.some((exclude) => dir.includes(exclude));
 }
