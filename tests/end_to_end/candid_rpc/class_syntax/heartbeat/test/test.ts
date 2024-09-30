5d21b6dcbf3196db10b6c488ee687740302af37f
Jordan Last <jordan.michael.last@gmail.com>
2024-09-30 09:45:17 -0600

    cleanup some mistakes

--- tests/end_to_end/candid_rpc/class_syntax/heartbeat/test/test.ts
+++ tests/end_to_end/candid_rpc/class_syntax/heartbeat/test/test.ts
@@ -5,10 +5,10 @@ import { getTests } from 'heartbeat_end_to_end_test_functional_syntax/test/tests
 import { createActor as createActorHeartbeatAsync } from './dfx_generated/heartbeat_async';
 import { createActor as createActorHeartbeatSync } from './dfx_generated/heartbeat_sync';
 
-const canisterName = 'heartbeat';
+const canisterName = 'heartbeat_async';
 
 const heartbeatAsyncCanister = createActorHeartbeatAsync(
-    getCanisterId('heartbeat_async'),
+    getCanisterId(canisterName),
     {
         agentOptions: {
             host: 'http://127.0.0.1:8000'
