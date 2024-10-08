f48925c684ff1faf702df7e32a880675cfebb21a
Jordan Last <jordan.michael.last@gmail.com>
2024-10-07 18:49:31 -0600

    refactor call_raw very nicely

--- src/build/rust/stable/src/ic/call_raw.rs
+++ src/build/rust/stable/src/ic/call_raw.rs
@@ -1,14 +1,14 @@
 use crate::{run_event_loop, CONTEXT};
 use rquickjs::{Ctx, Exception, Function, IntoJs, TypedArray};
 
-pub fn get_function(context: Ctx) -> Function {
+pub fn get_function(ctx: Ctx) -> Function {
     Function::new(
-        context.clone(),
-        move |promise_id: String,
-              canister_id_bytes: TypedArray<u8>,
-              method: String,
-              args_raw: TypedArray<u8>,
-              payment_string: String| {
+        ctx,
+        |promise_id: String,
+         canister_id_bytes: TypedArray<u8>,
+         method: String,
+         args_raw: TypedArray<u8>,
+         payment_string: String| {
             let canister_id = candid::Principal::from_slice(canister_id_bytes.as_ref());
             let args_raw = args_raw.as_bytes().unwrap().to_vec();
             let payment: u128 = payment_string.parse().unwrap();
@@ -17,14 +17,8 @@ pub fn get_function(context: Ctx) -> Function {
                 let call_result =
                     ic_cdk::api::call::call_raw128(canister_id, &method, args_raw, payment).await;
 
-                execute_in_context(move |ctx| {
-                    let (should_resolve, js_value) =
-                        prepare_final_js_value(ctx.clone(), &call_result);
-                    let resolve_or_reject =
-                        get_resolve_or_reject_object(ctx.clone(), should_resolve);
-                    let function_name = get_function_name(&promise_id, should_resolve);
-                    let callback = get_callback(resolve_or_reject, &function_name);
-                    execute_callback(callback, js_value);
+                quickjs_with_ctx(move |ctx| {
+                    resolve_or_reject(ctx.clone(), &call_result, &promise_id);
                     run_event_loop(ctx.clone());
                 });
             });
@@ -35,7 +29,7 @@ pub fn get_function(context: Ctx) -> Function {
     .unwrap()
 }
 
-fn execute_in_context<F>(f: F)
+fn quickjs_with_ctx<F>(f: F)
 where
     F: FnOnce(Ctx) + 'static,
 {
@@ -46,8 +40,20 @@ where
     });
 }
 
-// TODO get rid of lifetimes if possible
-fn prepare_final_js_value<'a>(
+fn resolve_or_reject<'a>(
+    ctx: Ctx<'a>,
+    call_result: &Result<Vec<u8>, (ic_cdk::api::call::RejectionCode, String)>,
+    promise_id: &str,
+) {
+    let (should_resolve, js_value) = prepare_js_value(ctx.clone(), &call_result);
+    let callback = get_callback(ctx.clone(), &promise_id, should_resolve);
+
+    callback
+        .call::<_, rquickjs::Undefined>((js_value,))
+        .unwrap();
+}
+
+fn prepare_js_value<'a>(
     ctx: Ctx<'a>,
     call_result: &Result<Vec<u8>, (ic_cdk::api::call::RejectionCode, String)>,
 ) -> (bool, rquickjs::Value<'a>) {
@@ -56,6 +62,7 @@ fn prepare_final_js_value<'a>(
             let candid_bytes_js_value = TypedArray::<u8>::new(ctx.clone(), candid_bytes.clone())
                 .into_js(&ctx)
                 .unwrap();
+
             (true, candid_bytes_js_value)
         }
         Err(err) => {
@@ -65,39 +72,36 @@ fn prepare_final_js_value<'a>(
             )
             .into_js(&ctx)
             .unwrap();
+
             (false, err_js_value)
         }
     }
 }
 
-fn get_resolve_or_reject_object(ctx: Ctx, should_resolve: bool) -> rquickjs::Object {
+fn get_callback<'a>(
+    ctx: Ctx<'a>,
+    promise_id: &str,
+    should_resolve: bool,
+) -> rquickjs::Function<'a> {
+    let global_object = get_resolve_or_reject_global_object(ctx.clone(), should_resolve);
+    let callback_name = get_resolve_or_reject_callback_name(&promise_id, should_resolve);
+
+    global_object.get(callback_name).unwrap()
+}
+
+fn get_resolve_or_reject_global_object(ctx: Ctx, should_resolve: bool) -> rquickjs::Object {
     let global = ctx.globals();
     if should_resolve {
-        global
-            .get::<_, rquickjs::Object>("_azleResolveIds")
-            .unwrap()
+        global.get("_azleResolveIds").unwrap()
     } else {
-        global.get::<_, rquickjs::Object>("_azleRejectIds").unwrap()
+        global.get("_azleRejectIds").unwrap()
     }
 }
 
-fn get_function_name(promise_id: &str, should_resolve: bool) -> String {
+fn get_resolve_or_reject_callback_name(promise_id: &str, should_resolve: bool) -> String {
     if should_resolve {
         format!("_resolve_{promise_id}")
     } else {
         format!("_reject_{promise_id}")
     }
-}
-
-fn get_callback<'a>(
-    resolve_or_reject: rquickjs::Object<'a>,
-    function_name: &str,
-) -> rquickjs::Function<'a> {
-    resolve_or_reject.get(function_name).unwrap()
-}
-
-fn execute_callback<'a>(callback: rquickjs::Function<'a>, js_value: rquickjs::Value<'a>) {
-    callback
-        .call::<_, rquickjs::Undefined>((js_value,))
-        .unwrap();
 }
