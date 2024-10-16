5949797f40f9b5e83c1ec402a5ec86dcf2a9415a
Benjamin DeMann <benjamin.demann@gmail.com>
2024-10-16 16:40:52 -0600

    first pass at calling extra tests

--- tests/property/ic_api/certified_data/test/tests.ts
+++ tests/property/ic_api/certified_data/test/tests.ts
@@ -1,5 +1,3 @@
-import {} from '@dfinity/agent';
-import {} from '@dfinity/candid';
 import {
     defaultPropTestParams,
     expect,
@@ -7,6 +5,7 @@ import {
     it,
     Test
 } from 'azle/test';
+import { execSync } from 'child_process';
 import fc from 'fast-check';
 
 import { _SERVICE as Actor } from './dfx_generated/canister/canister.did';
@@ -17,12 +16,36 @@ export function getTests(): Test {
             await fc.assert(
                 fc.asyncProperty(
                     fc.uint8Array({ minLength: 1, maxLength: 32 }),
-                    async (arbitraryData) => {
+                    fc.boolean(),
+                    fc.boolean(),
+                    async (
+                        arbitraryData,
+                        setDataInInit,
+                        setDataInPostUpgrade
+                    ) => {
                         const actor = await getCanisterActor<Actor>('canister');
+                        execSync(
+                            `dfx canister uninstall-code canister || true`,
+                            {
+                                stdio: 'inherit'
+                            }
+                        );
+                        execSync(
+                            `dfx deploy canister --argument '(${setDataInInit})'`,
+                            {
+                                stdio: 'inherit'
+                            }
+                        );
 
                         // Check that getCertificate returns an empty array initially
                         const initialCertificate = await actor.getCertificate();
-                        expect(initialCertificate).toEqual([]);
+                        if (setDataInInit) {
+                            expect(initialCertificate).toEqual([
+                                new Uint8Array([0])
+                            ]);
+                        } else {
+                            expect(initialCertificate).toEqual([]);
+                        }
 
                         // Set the certified data
                         await actor.setData(arbitraryData);
@@ -30,6 +53,38 @@ export function getTests(): Test {
                         // Check that getCertificate now returns the set data
                         const updatedCertificate = await actor.getCertificate();
                         expect(updatedCertificate).toEqual([arbitraryData]);
+
+                        execSync(
+                            `dfx deploy canister --argument '(${setDataInPostUpgrade})' --upgrade-unchanged`,
+                            {
+                                stdio: 'inherit'
+                            }
+                        );
+
+                        // Check that getCertificate returns an empty array after reinstalling
+                        const reinstalledCertificate =
+                            await actor.getCertificate();
+                        if (setDataInPostUpgrade) {
+                            expect(reinstalledCertificate).toEqual([
+                                new Uint8Array([0])
+                            ]);
+                        } else {
+                            expect(initialCertificate).toEqual([]);
+                        }
+
+                        // Check that setData works in preUpgrade
+                        execSync(
+                            `dfx deploy canister --argument '(false)' --upgrade-unchanged`,
+                            {
+                                stdio: 'inherit'
+                            }
+                        );
+
+                        const preUpgradeCertificate =
+                            await actor.getCertificate();
+                        expect(preUpgradeCertificate).toEqual([
+                            new Uint8Array([0])
+                        ]);
                     }
                 ),
                 defaultPropTestParams
