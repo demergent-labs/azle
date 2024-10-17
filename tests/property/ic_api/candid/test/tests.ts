25bc91570223187352d9377056c1272917f82db4
Benjamin DeMann <benjamin.demann@gmail.com>
2024-10-17 15:47:00 -0600

    add utf8 double checker

--- tests/property/ic_api/candid/test/tests.ts
+++ tests/property/ic_api/candid/test/tests.ts
@@ -1,38 +1,48 @@
 import { ActorSubclass } from '@dfinity/agent';
 import { defaultPropTestParams, expect, it, Test } from 'azle/test';
 import fc from 'fast-check';
+import { TextDecoder, TextEncoder } from 'util';
 
 // @ts-ignore this path may not exist when these tests are imported into other test projects
 import { _SERVICE } from './dfx_generated/canister/canister.did';
 
 export function getTests(canister: ActorSubclass<_SERVICE>): Test {
     return () => {
-        it('should encode and decode Candid strings correctly', async () => {
+        it('should encode and decode Candid strings correctly, with UTF-8 verification', async () => {
             await fc.assert(
-                fc.asyncProperty(
-                    fc.string().filter((s) => !s.includes("'")), // TODO figure out how to escape single quotes in candid strings
-                    async (arbitraryString) => {
-                        const candidString = `("${escapeCandidString(
-                            arbitraryString
-                        )}")`;
+                fc.asyncProperty(fc.string(), async (arbitraryString) => {
+                    const candidString = `("${escapeCandidString(
+                        arbitraryString
+                    )}")`;
 
-                        // Encode the Candid string
-                        const encodedBytes =
-                            await canister.candidEncodeQuery(candidString);
+                    // Encode the Candid string
+                    const encodedBytes =
+                        await canister.candidEncodeQuery(candidString);
 
-                        // Decode the encoded bytes
-                        const decodedString =
-                            await canister.candidDecodeQuery(encodedBytes);
+                    // Verify UTF-8 encoding
+                    const textEncoder = new TextEncoder();
+                    const encodedUtf8 = textEncoder.encode(arbitraryString);
+                    expect(encodedBytes.slice(8)).toEqual(encodedUtf8);
 
-                        // The decoded string should match the original Candid string
-                        expect(decodedString).toBe(candidString);
-                    }
-                ),
+                    // Decode the encoded bytes
+                    const decodedString =
+                        await canister.candidDecodeQuery(encodedBytes);
+
+                    // Verify UTF-8 decoding
+                    const textDecoder = new TextDecoder('utf-8');
+                    const decodedUtf8 = textDecoder.decode(encodedUtf8);
+                    expect(decodedString).toBe(
+                        `("${escapeCandidString(decodedUtf8)}")`
+                    );
+
+                    // The decoded string should match the original Candid string
+                    expect(decodedString).toBe(candidString);
+                }),
                 defaultPropTestParams
             );
         });
 
-        it('should decode Candid bytes and then encode correctly', async () => {
+        it('should decode Candid bytes and then encode correctly, with UTF-8 verification', async () => {
             await fc.assert(
                 fc.asyncProperty(
                     fc.uint8Array({
@@ -56,9 +66,21 @@ export function getTests(canister: ActorSubclass<_SERVICE>): Test {
                         const decodedString =
                             await canister.candidDecodeQuery(candidBytes);
 
+                        // Verify UTF-8 decoding
+                        const textDecoder = new TextDecoder('utf-8');
+                        const decodedUtf8 = textDecoder.decode(arbitraryBytes);
+                        expect(decodedString).toBe(
+                            `("${escapeCandidString(decodedUtf8)}")`
+                        );
+
                         // Encode the decoded string
                         const encodedBytes =
                             await canister.candidEncodeQuery(decodedString);
+
+                        // Verify UTF-8 encoding
+                        const textEncoder = new TextEncoder();
+                        const encodedUtf8 = textEncoder.encode(decodedUtf8);
+                        expect(encodedBytes.slice(8)).toEqual(encodedUtf8);
 
                         // The encoded bytes should match the original Candid bytes
                         expect(encodedBytes).toEqual(candidBytes);
@@ -83,5 +105,5 @@ export function getTests(canister: ActorSubclass<_SERVICE>): Test {
 }
 
 function escapeCandidString(data: string): string {
-    return data.replace(/[\\"]/g, '\\$&');
+    return data.replace(/[\\"']/g, '\\$&');
 }
