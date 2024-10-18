3843edccb3f6649a26eddd754c19854b41685fc4
Benjamin DeMann <benjamin.demann@gmail.com>
2024-10-18 11:00:02 -0600

    add to candid

--- tests/property/ic_api/candid/test/test.ts
+++ tests/property/ic_api/candid/test/test.ts
@@ -1,5 +1,3 @@
-// TODO If we want these tests to be more exact, we can check balances and make sure they are within some margin of error
-
 import { getCanisterId } from 'azle/dfx';
 import { runTests } from 'azle/test';
 
