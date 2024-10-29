1c82f1ec776ce910c06fc5c3f907af9cc98d1ae1
Jordan Last <jordan.michael.last@gmail.com>
2024-10-29 17:52:56 -0600

    add Error prefix

--- src/lib/experimental/error.ts
+++ src/lib/experimental/error.ts
@@ -4,7 +4,7 @@ import { trap } from './ic/trap';
 export function handleUncaughtError(rawError: any): never {
     const error = rawError instanceof Error ? rawError : new Error(rawError);
 
-    const azleError = `Uncaught ${error.name}: ${error.message}${error.stack}`;
+    const azleError = `Uncaught ${error.name}: Error: ${error.message}${error.stack}`;
 
     trap(azleError);
 }
