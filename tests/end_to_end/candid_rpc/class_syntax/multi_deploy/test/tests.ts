14cf69a1f155f22f8632d7d94da4ad8bc817daa3
Benjamin DeMann <benjamin.demann@gmail.com>
2024-10-31 13:16:09 -0600

    pull out file hash checks

--- tests/end_to_end/candid_rpc/class_syntax/multi_deploy/test/tests.ts
+++ tests/end_to_end/candid_rpc/class_syntax/multi_deploy/test/tests.ts
@@ -9,51 +9,30 @@ import { _SERVICE } from './dfx_generated/multi_deploy/multi_deploy.did';
 export function getTests(multiDeployCanister: ActorSubclass<_SERVICE>): Test {
     return () => {
         it('has the same binary between builds', async () => {
-            const originalWasmHash = await getFileHash(
-                '.azle/multi_deploy/multi_deploy.wasm'
-            );
-            const originalMainHash = await getFileHash(
-                '.azle/multi_deploy/main.js'
-            );
-            const originalDidHash = await getFileHash(
-                '.azle/multi_deploy/multi_deploy.did'
-            );
+            const originalHashes = await getFileHashes();
 
             for (let i = 0; i < 10; i++) {
                 execSyncPretty(`dfx build multi_deploy`);
-
-                const updatedWasmHash = await getFileHash(
-                    '.azle/multi_deploy/multi_deploy.wasm'
-                );
-                const updatedMainHash = await getFileHash(
-                    '.azle/multi_deploy/main.js'
-                );
-                const updatedDidHash = await getFileHash(
-                    '.azle/multi_deploy/multi_deploy.did'
-                );
-
-                expect(originalWasmHash).toBe(updatedWasmHash);
-                expect(originalMainHash).toBe(updatedMainHash);
-                expect(originalDidHash).toBe(updatedDidHash);
+                await verifyHashesMatch(originalHashes);
             }
         });
 
-        it("doesn't call post upgrade when deploy is skipped from unchanged binary", async () => {
-            const originalFileHash = await getFileHash(
-                '.azle/multi_deploy/multi_deploy.wasm'
-            );
+        it("doesn't call post upgrade if additional deploy steps are skipped because the binary is unchanged", async () => {
+            const originalHashes = await getFileHashes();
 
             expect(await multiDeployCanister.getInitCalled()).toBe(true);
             expect(await multiDeployCanister.getAzleInitCalled()).toBe(true);
+            expect(await multiDeployCanister.getPostUpgradeCalled()).toBe(
+                false
+            );
+            expect(await multiDeployCanister.getAzlePostUpgradeCalled()).toBe(
+                false
+            );
 
             for (let i = 0; i < 2; i++) {
                 execSyncPretty(`dfx deploy multi_deploy`);
+                await verifyHashesMatch(originalHashes);
 
-                const updatedHash = await getFileHash(
-                    '.azle/multi_deploy/multi_deploy.wasm'
-                );
-
-                expect(originalFileHash).toEqual(updatedHash);
                 expect(await multiDeployCanister.getInitCalled()).toBe(true);
                 expect(await multiDeployCanister.getAzleInitCalled()).toBe(
                     true
@@ -67,7 +46,7 @@ export function getTests(multiDeployCanister: ActorSubclass<_SERVICE>): Test {
             }
         });
 
-        it('does call post upgrade when deploy is forced', async () => {
+        it('does call post upgrade if additional deploy steps are forced', async () => {
             for (let i = 0; i < 2; i++) {
                 execSyncPretty(`dfx deploy multi_deploy --upgrade-unchanged`);
 
@@ -91,4 +70,24 @@ async function getFileHash(path: string): Promise<string> {
     let h = createHash('sha256');
     h.update(fileData);
     return h.digest('hex');
+}
+
+async function getFileHashes(): Promise<{
+    wasmHash: string;
+    mainHash: string;
+    didHash: string;
+}> {
+    return {
+        wasmHash: await getFileHash('.azle/multi_deploy/multi_deploy.wasm'),
+        mainHash: await getFileHash('.azle/multi_deploy/main.js'),
+        didHash: await getFileHash('.azle/multi_deploy/multi_deploy.did')
+    };
+}
+
+async function verifyHashesMatch(
+    originalHashes: Awaited<ReturnType<typeof getFileHashes>>
+): Promise<void> {
+    const updatedHashes = await getFileHashes();
+
+    expect(originalHashes).toEqual(updatedHashes);
 }
