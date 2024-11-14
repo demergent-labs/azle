b04b7f4a0c7f53c87cce20a883211c78195eb973
Benjamin DeMann <benjamin.demann@gmail.com>
2024-11-14 11:54:59 -0700

    add tests for update calls

--- tests/property/ic_api/reject/src/index.ts
+++ tests/property/ic_api/reject/src/index.ts
@@ -1,18 +1,27 @@
 import { IDL, query, reject, reply, update } from 'azle';
 
 export default class {
-    @query([IDL.Text], IDL.Text, { manual: true })
+    @query([IDL.Text], undefined, { manual: true })
     alwaysRejectQuery(message: string): void {
         reject(message);
     }
 
-    @update([IDL.Text], IDL.Text, { manual: true })
+    @update([IDL.Text], undefined, { manual: true })
     alwaysRejectUpdate(message: string): void {
         reject(message);
     }
 
     @query([IDL.Int], IDL.Int, { manual: true })
     evenOrRejectQuery(number: bigint): void {
+        if (number % 2n === 0n) {
+            reply({ data: number, idlType: IDL.Int });
+        } else {
+            reject('Odd numbers are rejected');
+        }
+    }
+
+    @update([IDL.Int], IDL.Int, { manual: true })
+    evenOrRejectUpdate(number: bigint): void {
         if (number % 2n === 0n) {
             reply({ data: number, idlType: IDL.Int });
         } else {
--- tests/property/ic_api/reject/test/tests.ts
+++ tests/property/ic_api/reject/test/tests.ts
@@ -39,6 +39,36 @@ export function getTests(): Test {
                 defaultPropTestParams
             );
         });
+
+        it('should always reject with the provided message in alwaysRejectUpdate', async () => {
+            const canister = await getCanisterActor<Actor>('canister');
+            await fc.assert(
+                fc.asyncProperty(fc.string(), async (message) => {
+                    await expect(
+                        canister.alwaysRejectUpdate(message)
+                    ).rejects.toThrow(escapeCandidString(message));
+                }),
+                defaultPropTestParams
+            );
+        });
+
+        it('should reply with even numbers and reject odd numbers in evenOrRejectUpdate', async () => {
+            const canister = await getCanisterActor<Actor>('canister');
+            await fc.assert(
+                fc.asyncProperty(fc.bigInt(), async (number) => {
+                    if (number % 2n === 0n) {
+                        const result =
+                            await canister.evenOrRejectUpdate(number);
+                        expect(result).toBe(number);
+                    } else {
+                        await expect(
+                            canister.evenOrRejectUpdate(number)
+                        ).rejects.toThrow('Odd numbers are rejected');
+                    }
+                }),
+                defaultPropTestParams
+            );
+        });
     };
 }
 
