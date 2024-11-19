d9b6165f6b6d56cc1ac7aacc5069c6f50582fefc
Jordan Last <jordan.michael.last@gmail.com>
2024-11-19 13:12:06 -0700

    fix comments

--- src/build/stable/commands/compile/wasm_binary/rust/stable_canister_template/src/ic/call_raw.rs
+++ src/build/stable/commands/compile/wasm_binary/rust/stable_canister_template/src/ic/call_raw.rs
@@ -12,6 +12,19 @@ use rquickjs::{
 use crate::{error::quickjs_call_with_error_handling, ic::throw_error};
 
 pub fn get_function(ctx: Ctx) -> QuickJsResult<Function> {
+    // We use a raw pointer here to deal with already borrowed issues
+    // using quickjs_with_ctx after a cross-canister call (await point)
+    // Sometimes the await point won't actually behave like a full await point
+    // because the cross-canister call returns an error where the callback
+    // is never queued and executed by the IC
+    // In this case the outer quickjs_with_ctx for the original call
+    // will still have borrowed something having to do with the ctx
+    // whereas in a full cross-canister call the outer quickjs_with_ctx
+    // will have completed.
+    // Using a raw pointer overcomes the issue, and I believe it is safe
+    // because we only ever have one Runtime, one Context, we never destroy them
+    // and we are in a single-threaded environment. We will of course
+    // engage in intense testing and review to ensure safety
     let ctx_ptr = ctx.as_raw();
 
     Function::new(
