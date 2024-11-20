6ca2e264f1d4468648a08be810069aec75c7ecc2
Benjamin DeMann <benjamin.demann@gmail.com>
2024-11-20 11:21:35 -0700

    remove text decoder

--- tests/end_to_end/candid_rpc/functional_syntax/outgoing_http_requests/test/tests.ts
+++ tests/end_to_end/candid_rpc/functional_syntax/outgoing_http_requests/test/tests.ts
@@ -1,7 +1,6 @@
 import { ActorSubclass } from '@dfinity/agent';
 import { HttpResponse } from 'azle/experimental/canisters/management';
 import { expect, it, Test } from 'azle/test';
-import decodeUtf8 from 'decode-utf8';
 
 // @ts-ignore this path may not exist when these tests are imported into other test projects
 import { _SERVICE } from './dfx_generated/outgoing_http_requests/outgoing_http_requests.did';
@@ -28,7 +27,9 @@ function checkXkcdResult(result: HttpResponse | string): void {
     const resultJson =
         typeof result === 'string'
             ? JSON.parse(result)
-            : JSON.parse(decodeUtf8(Uint8Array.from(result.body)));
+            : JSON.parse(
+                  new TextDecoder().decode(Uint8Array.from(result.body))
+              );
     const expectedJson = JSON.parse(
         `{"month": "9", "num": 642, "link": "", "year": "2009", "news": "", "safe_title": "Creepy", "alt": "And I even got out my adorable new netbook!", "img": "https://imgs.xkcd.com/comics/creepy.png", "title": "Creepy", "day": "28", "transcript":"[[Two people are sitting on chairs.]]\\nMan: Hey, cute netbook.\\nWoman: \\nWhat.\\n\\n\\nMan: Your laptop. I just --\\nWoman: No, why are you talking to me.\\n\\nWoman: Who do you think you are? If I were even slightly interested, I'd have shown it.\\n\\nWoman: Hey everyone, this dude's hitting on me.\\nVoice #1: Haha\\nVoice #2: Creepy\\nVoice #3: Let's get his picture for Facebook to warn others.\\n\\n((This panel fades into a thought bubble of the actual man.))\\n[[The girl is typing on her laptop.]]\\nDear blog,\\nCute boy on train still ignoring me.\\n\\n{{Title text: And I even got out my adorable new netbook!}}"}`
     );
