0f62e3fae7e4851eef451078a38e2233e4d3a5d6
Benjamin DeMann <benjamin.demann@gmail.com>
2024-11-25 13:54:49 -0700

    bring back dev dependencies for front ends

--- tests/end_to_end/candid_rpc/functional_syntax/motoko_examples/superheroes/src/declarations/index.js
+++ tests/end_to_end/candid_rpc/functional_syntax/motoko_examples/superheroes/src/declarations/index.js
@@ -37,3 +37,5 @@ export const createActor = (canisterId, options = {}) => {
         ...options.actorOptions
     });
 };
+
+export const superheroes = canisterId ? createActor(canisterId) : undefined;
