ceb719ab3a0906025a27c385f5dcd04ac39ec2eb
Benjamin DeMann <benjamin.demann@gmail.com>
2024-11-25 10:31:37 -0700

    update arg data raw to return a Uint8Array

--- src/build/stable/commands/compile/wasm_binary/rust/stable_canister_template/src/ic/arg_data_raw.rs
+++ src/build/stable/commands/compile/wasm_binary/rust/stable_canister_template/src/ic/arg_data_raw.rs
@@ -1,6 +1,9 @@
 use ic_cdk::api::call::arg_data_raw;
-use rquickjs::{Ctx, Function, Result};
+use rquickjs::{Ctx, Function, Result, TypedArray};
 
 pub fn get_function(ctx: Ctx) -> Result<Function> {
-    Function::new(ctx, || arg_data_raw())
+    let ctx = ctx.clone();
+    Function::new(ctx.clone(), move || {
+        TypedArray::<u8>::new(ctx.clone(), arg_data_raw())
+    })
 }
