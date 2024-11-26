dcd7723600e4c7d04652268f376a57d588c1c2d2
Benjamin DeMann <benjamin.demann@gmail.com>
2024-11-26 11:00:34 -0700

    get rid of redundant clone

--- src/build/stable/commands/compile/wasm_binary/rust/stable_canister_template/src/ic/arg_data_raw.rs
+++ src/build/stable/commands/compile/wasm_binary/rust/stable_canister_template/src/ic/arg_data_raw.rs
@@ -2,7 +2,6 @@ use ic_cdk::api::call::arg_data_raw;
 use rquickjs::{Ctx, Function, Result, TypedArray};
 
 pub fn get_function(ctx: Ctx) -> Result<Function> {
-    let ctx = ctx.clone();
     Function::new(ctx.clone(), move || {
         TypedArray::<u8>::new(ctx.clone(), arg_data_raw())
     })
