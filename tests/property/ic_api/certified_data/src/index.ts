685fe633b61ffc97ab64eee212f240b2cb87f4ac
Benjamin DeMann <benjamin.demann@gmail.com>
2024-12-04 17:04:47 -0700

    finish making all tests work

--- tests/property/ic_api/certified_data/src/index.ts
+++ tests/property/ic_api/certified_data/src/index.ts
@@ -6,13 +6,19 @@ import {
     preUpgrade,
     query,
     setCertifiedData,
+    StableBTreeMap,
     update
 } from 'azle';
 
 let afterFirstPostUpgrade = false;
+const PRE_UPGRADE_DATA = new Uint8Array([
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
+]);
 
 export default class {
     data: Uint8Array = new Uint8Array();
+    stableStorage = StableBTreeMap<string, boolean>(0);
 
     @init([IDL.Bool, IDL.Vec(IDL.Nat8)])
     init(setData: boolean, data: Uint8Array): void {
@@ -24,20 +30,26 @@ export default class {
 
     @preUpgrade
     preUpgrade(): void {
+        console.log('preUpgrade', afterFirstPostUpgrade);
         // The idea is that the third deploy will always have certified data set from preUpgrade so to test it we need to deploy 3 times
         // We could make it arbitrary but that seems like a lot of work just to test this one case
         if (afterFirstPostUpgrade) {
-            const preUpgradeData = new Uint8Array([
-                0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
-                18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
-            ]);
-            this.data = preUpgradeData;
-            setCertifiedData(preUpgradeData);
+            console.log('setting certified data to preUpgrade data');
+            setCertifiedData(PRE_UPGRADE_DATA);
+            this.stableStorage.insert('certifiedDataSetInPreUpgrade', true);
         }
     }
 
     @postUpgrade([IDL.Bool, IDL.Vec(IDL.Nat8)])
     postUpgrade(setData: boolean, data: Uint8Array): void {
+        const wasSetInPreUpgrade = this.stableStorage.get(
+            'certifiedDataSetInPreUpgrade'
+        );
+        if (wasSetInPreUpgrade) {
+            console.log('certified data was set in preUpgrade');
+            this.data = PRE_UPGRADE_DATA;
+        }
+
         if (setData) {
             this.data = data;
             setCertifiedData(data);
@@ -69,15 +81,10 @@ export default class {
         };
     }
 
-    @update([])
-    getDataCertificateInUpdate(): void {
+    @update([], IDL.Opt(IDL.Vec(IDL.Nat8)))
+    getDataCertificateInUpdate(): [Uint8Array] | [] {
         const certificate = dataCertificate();
-        if (certificate === undefined) {
-            return;
-        }
-        throw new Error(
-            'dataCertificate should have been undefined in update method'
-        );
+        return certificate ? [certificate] : [];
     }
 
     @query([])
