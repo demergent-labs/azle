042aabf9401e2d2890acac5faabb3a97e666190c
Benjamin DeMann <benjamin.demann@gmail.com>
2024-12-06 15:29:37 -0700

    add type checks for rejectMessage

--- tests/property/ic_api/reject_message/src/caller/index.ts
+++ tests/property/ic_api/reject_message/src/caller/index.ts
@@ -5,17 +5,32 @@ export default class {
 
     @update([IDL.Text], IDL.Text)
     async echoThroughReject(message: string): Promise<string> {
-        try {
-            await call(this.rejectorPrincipal, 'echoReject', {
-                paramIdlTypes: [IDL.Text],
-                returnIdlType: IDL.Text,
-                args: [message]
-            });
-        } catch (error) {
-            return rejectMessage();
-        }
-        throw new Error('This should never be thrown');
+        return await getRejectMessage(this.rejectorPrincipal, message);
     }
+
+    @update([IDL.Text], IDL.Bool)
+    async rejectMessageTypesAreCorrect(message: string): Promise<boolean> {
+        return (
+            typeof (await getRejectMessage(this.rejectorPrincipal, message)) ===
+            'string'
+        );
+    }
+}
+
+async function getRejectMessage(
+    rejectorPrincipal: string,
+    message: string
+): Promise<string> {
+    try {
+        await call(rejectorPrincipal, 'echoReject', {
+            paramIdlTypes: [IDL.Text],
+            returnIdlType: IDL.Text,
+            args: [message]
+        });
+    } catch (error) {
+        return rejectMessage();
+    }
+    throw new Error('This should never be thrown');
 }
 
 function getRejectorPrincipal(): string {
