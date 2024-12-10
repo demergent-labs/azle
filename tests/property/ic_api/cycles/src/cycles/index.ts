bef794550b6c97e30d4340bce0633dd404de31f0
Jordan Last <jordan.michael.last@gmail.com>
2024-12-10 10:58:02 -0700

    fix many fuzz tests

--- tests/property/ic_api/cycles/src/cycles/index.ts
+++ tests/property/ic_api/cycles/src/cycles/index.ts
@@ -34,6 +34,10 @@ function acceptCycles(
     receiveAmount?: bigint,
     numChunks?: bigint
 ): CyclesResult {
+    if (numChunks === 0n) {
+        throw new Error('numChunks cannot be 0');
+    }
+
     const startingCanisterBalance = canisterBalance();
     const initialAvailable = msgCyclesAvailable();
     const acceptAmount = receiveAmount ?? initialAvailable;
