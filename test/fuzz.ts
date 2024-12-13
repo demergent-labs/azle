2258cf659c848c851f04f134e35e3b071d9406af
Jordan Last <jordan.michael.last@gmail.com>
2024-12-13 14:05:08 -0700

    update cuzz and fix small issue

--- test/fuzz.ts
+++ test/fuzz.ts
@@ -1,6 +1,6 @@
 import { spawn } from 'child_process';
 import { CuzzConfig } from 'cuzz';
-import { readFile } from 'fs-extra';
+import { readFile } from 'fs/promises';
 import { join } from 'path';
 
 import { DfxJson } from '../src/build/stable/utils/types';
