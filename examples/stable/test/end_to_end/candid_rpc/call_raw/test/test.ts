e4b07de0e7d23b5e0644ec75668f40848a27d7b5
Benjamin DeMann <benjamin.demann@gmail.com>
2024-12-16 11:31:46 -0700

    reorg test dirs

---
+++ examples/stable/test/end_to_end/candid_rpc/call_raw/test/test.ts
@@ -0,0 +1,14 @@
+import { getCanisterId } from 'azle/dfx';
+import { runTests } from 'azle/test';
+import { getTests } from 'call_raw_end_to_end_test_functional_syntax/test/tests';
+
+import { createActor } from './dfx_generated/call_raw';
+
+const canisterName = 'call_raw';
+const callRawCanister = createActor(getCanisterId(canisterName), {
+    agentOptions: {
+        host: 'http://127.0.0.1:8000'
+    }
+});
+
+runTests(getTests(callRawCanister), canisterName);
