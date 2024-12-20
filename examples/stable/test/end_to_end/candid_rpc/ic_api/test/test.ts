c2f4001851f1583b54201e84ea9f77826dd0fb48
Benjamin DeMann <benjamin.demann@gmail.com>
2024-12-20 09:41:31 -0700

    remove query verification

--- examples/stable/test/end_to_end/candid_rpc/ic_api/test/test.ts
+++ examples/stable/test/end_to_end/candid_rpc/ic_api/test/test.ts
@@ -7,7 +7,8 @@ import { createActor } from './dfx_generated/ic_api';
 const canisterName = 'ic_api';
 const icApiCanister = createActor(getCanisterId(canisterName), {
     agentOptions: {
-        host: 'http://127.0.0.1:8000'
+        host: 'http://127.0.0.1:8000',
+        verifyQuerySignatures: false // TODO Major issue: https://forum.dfinity.org/t/agent-js-0-20-0-is-released-replica-signed-query-edition/24743/16?u=lastmjs
     }
 });
 
