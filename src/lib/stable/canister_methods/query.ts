77019f73bc2a186e95834d77cb02295efcd4e3de
Jordan Last <jordan.michael.last@gmail.com>
2025-01-02 14:41:26 -0700

    adding jsdocs to query and update decorators

--- src/lib/stable/canister_methods/query.ts
+++ src/lib/stable/canister_methods/query.ts
@@ -7,11 +7,25 @@ import {
     MethodType
 } from '.';
 
+/**
+ * Decorator to mark a method as a query call entry point.
+ * Query calls are read-only and do not inherit latency from ICP consensus.
+ */
 export function query<This, Args extends any[], Return>(
     originalMethod: MethodType<This, Args, Return>,
     context: ClassMethodDecoratorContext<This, MethodType<This, Args, Return>>
 ): MethodType<ExportedCanisterClass, Args, Return>;
 
+/**
+ * Decorator to mark a method as a query call entry point.
+ * Query calls are read-only and do not inherit latency from ICP consensus.
+ *
+ * @param paramIdlTypes - Optional array of Candid IDL types for the method parameters. The runtime arguments will be decoded using these types.
+ * @param returnIdlType - Optional Candid IDL type for the method return value. The runtime return value will be encoded using this type.
+ * @param options - Optional configuration object
+ * @param options.composite - Optional flag to indicate that the method should be treated as a composite query method capable of some cross-canister query calls.
+ * @param options.manual - Optional flag to indicate manual handling of the method's runtime return value. This is meant to be used with `reply`, skipping automatic Candid encoding of the runtime return value.
+ */
 export function query<This, Args extends any[], Return>(
     paramIdlTypes?: IDL.Type[],
     returnIdlType?: IDL.Type,
--- src/lib/stable/canister_methods/update.ts
+++ src/lib/stable/canister_methods/update.ts
@@ -7,11 +7,24 @@ import {
     MethodType
 } from '.';
 
+/**
+ * Decorator to mark a method as an update call entry point.
+ * Update calls are read-write and inherit latency from ICP consensus.
+ */
 export function update<This, Args extends any[], Return>(
     originalMethod: MethodType<This, Args, Return>,
     context: ClassMethodDecoratorContext<This, MethodType<This, Args, Return>>
 ): MethodType<ExportedCanisterClass, Args, Return>;
 
+/**
+ * Decorator to mark a method as an update call entry point.
+ * Update calls are read-write and inherit latency from ICP consensus.
+ *
+ * @param paramIdlTypes - Optional array of Candid IDL types for the method parameters. The runtime arguments will be decoded using these types.
+ * @param returnIdlType - Optional Candid IDL type for the method return value. The runtime return value will be encoded using this type.
+ * @param options - Optional configuration object
+ * @param options.manual - Optional flag to indicate manual handling of the method's runtime return value. This is meant to be used with `reply`, skipping automatic Candid encoding of the runtime return value.
+ */
 export function update<This, Args extends any[], Return>(
     paramIdlTypes?: IDL.Type[],
     returnIdlType?: IDL.Type,
