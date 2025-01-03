f2749406129adff607da42b09b456e5b5137b7a4
Jordan Last <jordan.michael.last@gmail.com>
2025-01-03 16:48:25 -0700

    add a lot of comments to the strange instantiation section

--- src/build/stable/commands/compile/javascript.ts
+++ src/build/stable/commands/compile/javascript.ts
@@ -41,6 +41,26 @@ export function handleClassApiCanister(): string {
             });
         };
         
+        /**
+         * @internal
+         * 
+         * This function is designed with a very specific purpose.
+         * We need to get the _azle properties off of this class instance to use in generating the candid
+         * and method meta information. But we can't just set the result of instantiating the class to a local variable.
+         * This is because exceptions might be thrown during the class's instantiation, in the constructor or
+         * property initializers. If this happens, we would not be able to proceed to get those _azle properties out of the local variable.
+         * The likelihood of this happening is very high, since to get the candid and method meta information we must
+         * execute the Wasm module outside of the replica in our Node.js Wasm environment. Though the likelihood of this happening
+         * is high, it is not an issue unless our decorators cannot complete the setting of the _azle properties.
+         * We believe there is a very high likelihood of always being able to set the _azle properties, even if errors
+         * are thrown after this process completes.
+         * This environment is not the true canister environment and may not have certain globals or other APIs.
+         * So we use a try/catch. If there is an exception we check if we're in the Node.js Wasm environment.
+         * If we are, we do not throw an error unless the global _azleExportedCanisterClassInstance is undefined.
+         * If it is not undefined, we assume that the decorators have executed during the context.addInitializer callback.
+         * This callback occurs before the class's constructor or properties are initialized.
+         * There may be some rare conditions where this scheme will not work, but we believe the likelihood is extremely low.
+         */
         function getExportedCanisterClassInstance() {     
             try {
                 Canister.default.prototype._azleShouldRegisterCanisterMethods = true;
