6d639764b3789d55d86cc8c57928b151a2eda8a3
Benjamin DeMann <benjamin.demann@gmail.com>
2025-01-06 10:43:25 -0700

    automatically download the correct did file

--- examples/experimental/test/end_to_end/http_server/ic_evm_rpc/test/pretest.ts
+++ examples/experimental/test/end_to_end/http_server/ic_evm_rpc/test/pretest.ts
@@ -1,6 +1,11 @@
 import { execSync } from 'child_process';
 
 function pretest(): void {
+    execSync(
+        `mkdir -p src/candid && curl -L https://github.com/dfinity/evm-rpc-canister/releases/latest/download/evm_rpc.did -o src/candid/evm_rpc.did`,
+        { stdio: 'inherit' }
+    );
+
     execSync(`dfx canister uninstall-code evm_rpc || true`, {
         stdio: 'inherit'
     });
