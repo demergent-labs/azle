d4aa5b53719365ea5f5775a001959ad8606d548b
Jordan Last <jordan.michael.last@gmail.com>
2025-02-05 09:41:04 -0700

    fix experimental examples

--- examples/experimental/test/end_to_end/candid_rpc/notify_raw/src/canister1/index.ts
+++ examples/experimental/test/end_to_end/candid_rpc/notify_raw/src/canister1/index.ts
@@ -1,17 +1,18 @@
-import { candidEncode, notify, trap } from 'azle';
+import { call, candidEncode, trap } from 'azle';
 import { Canister, Principal, update, Void } from 'azle/experimental';
 
 export default Canister({
-    sendNotification: update([], Void, () => {
-        return notify(
+    sendNotification: update([], Void, async () => {
+        return await call(
             Principal.fromText(
                 process.env.CANISTER2_PRINCIPAL ??
                     trap('process.env.CANISTER2_PRINCIPAL is undefined')
             ),
             'receiveNotification',
             {
-                raw: candidEncode('()'),
-                cycles: 0n
+                args: candidEncode('()'),
+                cycles: 0n,
+                oneway: true
             }
         );
     })
