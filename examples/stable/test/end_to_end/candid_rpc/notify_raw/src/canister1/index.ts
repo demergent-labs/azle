d9588c3ecf77709535f7df05191bde56ebd25db1
Jordan Last <jordan.michael.last@gmail.com>
2025-02-06 12:12:29 -0700

    typo

--- examples/stable/test/end_to_end/candid_rpc/notify_raw/src/canister1/index.ts
+++ examples/stable/test/end_to_end/candid_rpc/notify_raw/src/canister1/index.ts
@@ -3,7 +3,7 @@ import { call, Principal, trap, update } from 'azle';
 export default class {
     @update
     async sendNotification(): Promise<void> {
-        return await call<Uint8Array, void>(
+        return await call<undefined, void>(
             Principal.fromText(
                 process.env.CANISTER2_PRINCIPAL ??
                     trap('process.env.CANISTER2_PRINCIPAL is undefined')
