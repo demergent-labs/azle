b1093e05f33bf5360e21ffac5b0a22e5e5e98a76
Jordan Last <jordan.michael.last@gmail.com>
2025-02-07 13:38:29 -0700

    pre-encode the empty params

--- src/build/stable/commands/compile/wasm_binary/rust/stable_canister_template/src/chunk.rs
+++ src/build/stable/commands/compile/wasm_binary/rust/stable_canister_template/src/chunk.rs
@@ -4,6 +4,6 @@ use ic_cdk::{api::call::call_raw128, id};
 pub async fn chunk() {
     let id = id();
     let method = "_azle_chunk";
-    let args_raw = [68, 73, 68, 76, 0, 0]; // '()' pre encoded
+    let args_raw = [68, 73, 68, 76, 0, 0]; // pre-encoded Candid empty params
     let _ = call_raw128(id, method, args_raw, 0).await;
 }
