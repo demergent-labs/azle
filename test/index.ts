1a0859090f6c886b25ba8226111384fe02b69d70
Benjamin DeMann <benjamin.demann@gmail.com>
2025-02-10 16:35:50 -0700

    remove config verification

--- test/index.ts
+++ test/index.ts
@@ -2,12 +2,10 @@ import * as dns from 'node:dns';
 dns.setDefaultResultOrder('ipv4first');
 
 import { describe, expect, test } from '@jest/globals';
-import { join } from 'path';
 
 import { execSyncPretty } from '../src/build/stable/utils/exec_sync_pretty';
 
 export { expect } from '@jest/globals';
-import { readFile } from 'fs/promises';
 
 import { runBenchmarksForCanisters } from './benchmarks';
 import { runFuzzTests } from './fuzz';
@@ -42,7 +40,6 @@ export function runTests(
         describe(`type checks`, () => {
             it('checks types', async () => {
                 const typeCheckCommand = `npm exec --offline tsc -- --noEmit --skipLibCheck`;
-                await verifyTypeCheckCommand(typeCheckCommand);
                 try {
                     execSyncPretty(typeCheckCommand, 'inherit');
                 } catch {
@@ -70,31 +67,6 @@ export function runTests(
             it('runs fuzz tests for all canisters', runFuzzTests);
         });
     }
-}
-
-async function verifyTypeCheckCommand(typeCheckCommand: string): Promise<void> {
-    const configString = execSyncPretty(
-        `${typeCheckCommand} --showConfig`,
-        'pipe'
-    )
-        .toString()
-        .trim();
-    const configJson = JSON.parse(configString);
-    const tsConfigPath = join(process.cwd(), 'tsconfig.json');
-    const tsConfigJson = JSON.parse(await readFile(tsConfigPath, 'utf8'));
-    expect(configJson).toMatchObject({
-        ...tsConfigJson,
-        compilerOptions: {
-            target: tsConfigJson.compilerOptions?.target?.toLowerCase(),
-            module: tsConfigJson.compilerOptions?.module?.toLowerCase(),
-            outDir: expect.stringMatching(
-                tsConfigJson.compilerOptions?.outDir ?? ''
-            ),
-            moduleResolution: expect.stringMatching(
-                tsConfigJson.compilerOptions?.moduleResolution ?? ''
-            )
-        }
-    });
 }
 
 export function wait(name: string, delay: number): void {
