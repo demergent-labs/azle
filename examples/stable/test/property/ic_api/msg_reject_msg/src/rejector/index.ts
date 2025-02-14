23b3f4320fc8dbba8a355201840b1e6107c5d18b
Jordan Last <jordan.michael.last@gmail.com>
2025-02-14 15:58:30 -0700

    fix up some unnecessary package.json dependencies, and add tsconfig.dev.json to npmignore

--- examples/stable/test/property/ic_api/msg_reject_msg/src/rejector/index.ts
+++ examples/stable/test/property/ic_api/msg_reject_msg/src/rejector/index.ts
@@ -3,7 +3,14 @@ import { IDL, msgArgData, msgReject, query } from 'azle';
 export default class {
     @query([IDL.Text], IDL.Text, { manual: true })
     echoReject(): void {
-        const message = IDL.decode([IDL.Text], msgArgData())[0] as string;
+        const argData = msgArgData();
+
+        const message = IDL.decode(
+            [IDL.Text],
+            argData.buffer instanceof ArrayBuffer
+                ? argData.buffer
+                : new Uint8Array(argData).buffer
+        )[0] as string;
 
         msgReject(`reject_message proptest message: ${message}`);
     }
