88d16e314cb97eb6c360a4269ab0a71a6e897c1d
Benjamin DeMann <benjamin.demann@gmail.com>
2025-02-18 16:52:20 -0700

    update escape candid keywords to handle special characters

--- examples/stable/test/end_to_end/candid_rpc/query/src/index.ts
+++ examples/stable/test/end_to_end/candid_rpc/query/src/index.ts
@@ -1,8 +1,41 @@
 import { IDL, query } from 'azle';
 
+const coolThing = IDL.Record({
+    'field.*?+\\': IDL.Text,
+    'field.*?+': IDL.Text
+});
+
+type coolThing = {
+    'field.*?+\\': string;
+    'field.*?+': string;
+};
+
 export default class {
     @query([], IDL.Text)
     simpleQuery(): string {
         return 'This is a query function';
+    }
+
+    @query([], undefined, { hidden: false })
+    'q\\\\u"""oted\\Q"ue\\ry'(): void {
+        console.log('This is a query function');
+    }
+
+    @query([], undefined, { hidden: false })
+    'quoted"Query'(): void {
+        console.log('This is a query function');
+    }
+
+    @query([], undefined, { hidden: false })
+    'crazyQuery.*?+^${}()|[]!~`;,='(): void {
+        console.log('This is a query function');
+    }
+
+    @query([], coolThing)
+    coolQuery(): coolThing {
+        return {
+            'field.*?+\\': 'value',
+            'field.*?+': 'value'
+        };
     }
 }
