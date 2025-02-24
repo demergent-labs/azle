7c2061a9c1a6827388f745604c165d92fdac7fd9
Benjamin DeMann <benjamin.demann@gmail.com>
2025-02-24 14:50:01 -0700

    move DidVisitor to _internal

--- _internal/index.ts
+++ _internal/index.ts
@@ -1,3 +1,4 @@
 export { generateFileOfSize, toBytes, Unit } from '../scripts/file_generator';
 export { createActor } from '../src/build/experimental/commands/upload_assets/file_uploader/uploader_actor';
 export { AZLE_UPLOADER_IDENTITY_NAME } from '../src/build/experimental/commands/upload_assets/uploader_identity';
+export * from '../src/lib/stable/did_file';
