3ae0e68eba47536fc7d731e7fbfc966937757468
Jordan Last <jordan.michael.last@gmail.com>
2025-02-25 17:33:53 -0700

    clean some things up

--- src/build/stable/commands/generate/rust/candid_to_ts_js/pkg/candid_to_ts_js.js
+++ src/build/stable/commands/generate/rust/candid_to_ts_js/pkg/candid_to_ts_js.js
@@ -1,22 +1,17 @@
+
 let imports = {};
 imports['__wbindgen_placeholder__'] = module.exports;
 let wasm;
 const { TextDecoder, TextEncoder } = require(`util`);
 
-let cachedTextDecoder = new TextDecoder('utf-8', {
-    ignoreBOM: true,
-    fatal: true
-});
+let cachedTextDecoder = new TextDecoder('utf-8', { ignoreBOM: true, fatal: true });
 
 cachedTextDecoder.decode();
 
 let cachedUint8ArrayMemory0 = null;
 
 function getUint8ArrayMemory0() {
-    if (
-        cachedUint8ArrayMemory0 === null ||
-        cachedUint8ArrayMemory0.byteLength === 0
-    ) {
+    if (cachedUint8ArrayMemory0 === null || cachedUint8ArrayMemory0.byteLength === 0) {
         cachedUint8ArrayMemory0 = new Uint8Array(wasm.memory.buffer);
     }
     return cachedUint8ArrayMemory0;
@@ -24,39 +19,34 @@ function getUint8ArrayMemory0() {
 
 function getStringFromWasm0(ptr, len) {
     ptr = ptr >>> 0;
-    return cachedTextDecoder.decode(
-        getUint8ArrayMemory0().subarray(ptr, ptr + len)
-    );
+    return cachedTextDecoder.decode(getUint8ArrayMemory0().subarray(ptr, ptr + len));
 }
 
 let WASM_VECTOR_LEN = 0;
 
 let cachedTextEncoder = new TextEncoder('utf-8');
 
-const encodeString =
-    typeof cachedTextEncoder.encodeInto === 'function'
-        ? function (arg, view) {
-              return cachedTextEncoder.encodeInto(arg, view);
-          }
-        : function (arg, view) {
-              const buf = cachedTextEncoder.encode(arg);
-              view.set(buf);
-              return {
-                  read: arg.length,
-                  written: buf.length
-              };
-          };
+const encodeString = (typeof cachedTextEncoder.encodeInto === 'function'
+    ? function (arg, view) {
+    return cachedTextEncoder.encodeInto(arg, view);
+}
+    : function (arg, view) {
+    const buf = cachedTextEncoder.encode(arg);
+    view.set(buf);
+    return {
+        read: arg.length,
+        written: buf.length
+    };
+});
 
 function passStringToWasm0(arg, malloc, realloc) {
-    if (typeof arg !== 'string')
-        throw new Error(`expected a string argument, found ${typeof arg}`);
+
+    if (typeof(arg) !== 'string') throw new Error(`expected a string argument, found ${typeof(arg)}`);
 
     if (realloc === undefined) {
         const buf = cachedTextEncoder.encode(arg);
         const ptr = malloc(buf.length, 1) >>> 0;
-        getUint8ArrayMemory0()
-            .subarray(ptr, ptr + buf.length)
-            .set(buf);
+        getUint8ArrayMemory0().subarray(ptr, ptr + buf.length).set(buf);
         WASM_VECTOR_LEN = buf.length;
         return ptr;
     }
@@ -70,7 +60,7 @@ function passStringToWasm0(arg, malloc, realloc) {
 
     for (; offset < len; offset++) {
         const code = arg.charCodeAt(offset);
-        if (code > 0x7f) break;
+        if (code > 0x7F) break;
         mem[ptr + offset] = code;
     }
 
@@ -78,11 +68,10 @@ function passStringToWasm0(arg, malloc, realloc) {
         if (offset !== 0) {
             arg = arg.slice(offset);
         }
-        ptr = realloc(ptr, len, (len = offset + arg.length * 3), 1) >>> 0;
+        ptr = realloc(ptr, len, len = offset + arg.length * 3, 1) >>> 0;
         const view = getUint8ArrayMemory0().subarray(ptr + offset, ptr + len);
         const ret = encodeString(arg, view);
-        if (ret.read !== arg.length)
-            throw new Error('failed to pass whole string');
+        if (ret.read !== arg.length) throw new Error('failed to pass whole string');
         offset += ret.written;
         ptr = realloc(ptr, len, offset, 1) >>> 0;
     }
@@ -100,12 +89,8 @@ function takeFromExternrefTable0(idx) {
  * @param {string} candid
  * @returns {any}
  */
-module.exports.candid_to_ts_js = function (candid) {
-    const ptr0 = passStringToWasm0(
-        candid,
-        wasm.__wbindgen_malloc,
-        wasm.__wbindgen_realloc
-    );
+module.exports.candid_to_ts_js = function(candid) {
+    const ptr0 = passStringToWasm0(candid, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
     const len0 = WASM_VECTOR_LEN;
     const ret = wasm.candid_to_ts_js(ptr0, len0);
     if (ret[2]) {
@@ -114,7 +99,7 @@ module.exports.candid_to_ts_js = function (candid) {
     return takeFromExternrefTable0(ret[0]);
 };
 
-module.exports.__wbindgen_init_externref_table = function () {
+module.exports.__wbindgen_init_externref_table = function() {
     const table = wasm.__wbindgen_export_0;
     const offset = table.grow(4);
     table.set(0, undefined);
@@ -122,14 +107,15 @@ module.exports.__wbindgen_init_externref_table = function () {
     table.set(offset + 1, null);
     table.set(offset + 2, true);
     table.set(offset + 3, false);
+    ;
 };
 
-module.exports.__wbindgen_string_new = function (arg0, arg1) {
+module.exports.__wbindgen_string_new = function(arg0, arg1) {
     const ret = getStringFromWasm0(arg0, arg1);
     return ret;
 };
 
-module.exports.__wbindgen_throw = function (arg0, arg1) {
+module.exports.__wbindgen_throw = function(arg0, arg1) {
     throw new Error(getStringFromWasm0(arg0, arg1));
 };
 
@@ -142,3 +128,4 @@ wasm = wasmInstance.exports;
 module.exports.__wasm = wasm;
 
 wasm.__wbindgen_start();
+
