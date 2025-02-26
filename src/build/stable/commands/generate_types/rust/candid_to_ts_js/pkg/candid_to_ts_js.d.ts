e69739a53c8e8f0b1a18a6643636e253c1af6fdb
Jordan Last <jordan.michael.last@gmail.com>
2025-02-26 11:37:24 -0700

    add Rust Docs and get rid of unwraps

--- src/build/stable/commands/generate_types/rust/candid_to_ts_js/pkg/candid_to_ts_js.d.ts
+++ src/build/stable/commands/generate_types/rust/candid_to_ts_js/pkg/candid_to_ts_js.d.ts
@@ -1,3 +1,18 @@
 /* tslint:disable */
 /* eslint-disable */
+/**
+ * Converts a Candid service string to its corresponding TypeScript types and JavaScript IDL types.
+ *
+ * These types can be used with `@query`/`@update` and other method decorators and for inter-canister calls using the `call` function.
+ *
+ * # Parameters
+ *
+ * * `candid` - A string containing a valid Candid service definition
+ *
+ * # Returns
+ *
+ * TypeScript types and JavaScript IDL types as a string on success,
+ * or an error message if parsing or compilation fails
+ * ```
+ */
 export function candid_to_ts_js(candid: string): any;
--- src/build/stable/commands/generate_types/rust/candid_to_ts_js/pkg/candid_to_ts_js.js
+++ src/build/stable/commands/generate_types/rust/candid_to_ts_js/pkg/candid_to_ts_js.js
@@ -86,6 +86,19 @@ function takeFromExternrefTable0(idx) {
     return value;
 }
 /**
+ * Converts a Candid service string to its corresponding TypeScript types and JavaScript IDL types.
+ *
+ * These types can be used with `@query`/`@update` and other method decorators and for inter-canister calls using the `call` function.
+ *
+ * # Parameters
+ *
+ * * `candid` - A string containing a valid Candid service definition
+ *
+ * # Returns
+ *
+ * TypeScript types and JavaScript IDL types as a string on success,
+ * or an error message if parsing or compilation fails
+ * ```
  * @param {string} candid
  * @returns {any}
  */
