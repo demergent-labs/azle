a6d06a6dc0a07c1269baecb63deddee7c87d0bf3
Jordan Last <jordan.michael.last@gmail.com>
2025-02-27 15:33:34 -0700

    add a nice test for npx azle generate-types

---
+++ examples/stable/test/end_to_end/candid_rpc/management_canister_generate_types/src/index.ts
@@ -0,0 +1,362 @@
+import { call, IDL, Principal, query, update } from 'azle';
+
+import {
+    canister_info_args,
+    canister_info_result,
+    canister_status_args,
+    canister_status_result,
+    chunk_hash,
+    clear_chunk_store_args,
+    create_canister_args,
+    create_canister_result,
+    delete_canister_args,
+    deposit_cycles_args,
+    install_chunked_code_args,
+    install_code_args,
+    provisional_create_canister_with_cycles_args,
+    provisional_create_canister_with_cycles_result,
+    provisional_top_up_canister_args,
+    raw_rand_result,
+    start_canister_args,
+    stop_canister_args,
+    stored_chunks_args,
+    stored_chunks_result,
+    uninstall_code_args,
+    update_settings_args,
+    upload_chunk_args,
+    upload_chunk_result
+} from './idl/management';
+
+type State = {
+    createdCanisterId: Principal;
+};
+
+export default class {
+    state: State = {
+        createdCanisterId: Principal.fromText('aaaaa-aa')
+    };
+
+    @update([], create_canister_result)
+    async executeCreateCanister(): Promise<create_canister_result> {
+        const createCanisterResult = await createCanister();
+
+        this.state.createdCanisterId = createCanisterResult.canister_id;
+
+        return createCanisterResult;
+    }
+
+    @update([IDL.Principal], IDL.Bool)
+    async executeUpdateSettings(canisterId: Principal): Promise<boolean> {
+        await call<[update_settings_args], void>(
+            'aaaaa-aa',
+            'update_settings',
+            {
+                paramIdlTypes: [update_settings_args],
+                args: [
+                    {
+                        canister_id: canisterId,
+                        settings: {
+                            controllers: [],
+                            compute_allocation: [1n],
+                            log_visibility: [],
+                            memory_allocation: [3_000_000n],
+                            freezing_threshold: [2_000_000n],
+                            reserved_cycles_limit: [],
+                            wasm_memory_limit: []
+                        },
+                        sender_canister_version: []
+                    }
+                ]
+            }
+        );
+
+        return true;
+    }
+
+    @update([IDL.Principal, IDL.Vec(IDL.Nat8)], upload_chunk_result)
+    async executeUploadChunk(
+        canisterId: Principal,
+        chunk: Uint8Array
+    ): Promise<upload_chunk_result> {
+        return await call<[upload_chunk_args], upload_chunk_result>(
+            'aaaaa-aa',
+            'upload_chunk',
+            {
+                paramIdlTypes: [upload_chunk_args],
+                returnIdlType: chunk_hash,
+                args: [
+                    {
+                        canister_id: canisterId,
+                        chunk
+                    }
+                ]
+            }
+        );
+    }
+
+    @update([IDL.Principal], IDL.Bool)
+    async executeClearChunkStore(canisterId: Principal): Promise<boolean> {
+        await call<[clear_chunk_store_args], void>(
+            'aaaaa-aa',
+            'clear_chunk_store',
+            {
+                paramIdlTypes: [clear_chunk_store_args],
+                args: [
+                    {
+                        canister_id: canisterId
+                    }
+                ]
+            }
+        );
+
+        return true;
+    }
+
+    @update([IDL.Principal], stored_chunks_result)
+    async getStoredChunks(
+        canisterId: Principal
+    ): Promise<stored_chunks_result> {
+        return await call<[stored_chunks_args], stored_chunks_result>(
+            'aaaaa-aa',
+            'stored_chunks',
+            {
+                paramIdlTypes: [stored_chunks_args],
+                returnIdlType: stored_chunks_result,
+                args: [
+                    {
+                        canister_id: canisterId
+                    }
+                ]
+            }
+        );
+    }
+
+    @update([IDL.Principal, IDL.Vec(IDL.Nat8)], IDL.Bool)
+    async executeInstallCode(
+        canisterId: Principal,
+        wasmModule: Uint8Array
+    ): Promise<boolean> {
+        await call<[install_code_args], void>('aaaaa-aa', 'install_code', {
+            paramIdlTypes: [install_code_args],
+            args: [
+                {
+                    mode: {
+                        install: null
+                    },
+                    canister_id: canisterId,
+                    wasm_module: wasmModule,
+                    arg: Uint8Array.from([]),
+                    sender_canister_version: []
+                }
+            ],
+            cycles: 100_000_000_000n
+        });
+
+        return true;
+    }
+
+    @update([IDL.Principal, IDL.Vec(chunk_hash), IDL.Vec(IDL.Nat8)], IDL.Bool)
+    async executeInstallChunkedCode(
+        canisterId: Principal,
+        chunkHashes: chunk_hash[],
+        wasmModuleHash: Uint8Array
+    ): Promise<boolean> {
+        await call<[install_chunked_code_args], void>(
+            'aaaaa-aa',
+            'install_chunked_code',
+            {
+                paramIdlTypes: [install_chunked_code_args],
+                args: [
+                    {
+                        mode: {
+                            install: null
+                        },
+                        target_canister: canisterId,
+                        store_canister: [],
+                        chunk_hashes_list: chunkHashes,
+                        wasm_module_hash: wasmModuleHash,
+                        arg: Uint8Array.from([]),
+                        sender_canister_version: []
+                    }
+                ],
+                cycles: 100_000_000_000n
+            }
+        );
+
+        return true;
+    }
+
+    @update([IDL.Principal], IDL.Bool)
+    async executeUninstallCode(canisterId: Principal): Promise<boolean> {
+        await call<[uninstall_code_args], void>('aaaaa-aa', 'uninstall_code', {
+            paramIdlTypes: [uninstall_code_args],
+            args: [
+                {
+                    canister_id: canisterId,
+                    sender_canister_version: []
+                }
+            ]
+        });
+
+        return true;
+    }
+
+    @update([IDL.Principal], IDL.Bool)
+    async executeStartCanister(canisterId: Principal): Promise<boolean> {
+        await call<[start_canister_args], void>('aaaaa-aa', 'start_canister', {
+            paramIdlTypes: [start_canister_args],
+            args: [
+                {
+                    canister_id: canisterId
+                }
+            ]
+        });
+
+        return true;
+    }
+
+    @update([IDL.Principal], IDL.Bool)
+    async executeStopCanister(canisterId: Principal): Promise<boolean> {
+        await call<[stop_canister_args], void>('aaaaa-aa', 'stop_canister', {
+            paramIdlTypes: [stop_canister_args],
+            args: [
+                {
+                    canister_id: canisterId
+                }
+            ]
+        });
+
+        return true;
+    }
+
+    @update([canister_info_args], canister_info_result)
+    async getCanisterInfo(
+        args: canister_info_args
+    ): Promise<canister_info_result> {
+        return await call<[canister_info_args], canister_info_result>(
+            'aaaaa-aa',
+            'canister_info',
+            {
+                paramIdlTypes: [canister_info_args],
+                returnIdlType: canister_info_result,
+                args: [args]
+            }
+        );
+    }
+
+    @update([canister_status_args], canister_status_result)
+    async getCanisterStatus(
+        args: canister_status_args
+    ): Promise<canister_status_result> {
+        return await call<[canister_status_args], canister_status_result>(
+            'aaaaa-aa',
+            'canister_status',
+            {
+                paramIdlTypes: [canister_status_args],
+                returnIdlType: canister_status_result,
+                args: [args]
+            }
+        );
+    }
+
+    @update([IDL.Principal], IDL.Bool)
+    async executeDeleteCanister(canisterId: Principal): Promise<boolean> {
+        await call<[delete_canister_args], void>(
+            'aaaaa-aa',
+            'delete_canister',
+            {
+                paramIdlTypes: [delete_canister_args],
+                args: [
+                    {
+                        canister_id: canisterId
+                    }
+                ]
+            }
+        );
+
+        return true;
+    }
+
+    @update([IDL.Principal], IDL.Bool)
+    async executeDepositCycles(canisterId: Principal): Promise<boolean> {
+        await call<[deposit_cycles_args], void>('aaaaa-aa', 'deposit_cycles', {
+            paramIdlTypes: [deposit_cycles_args],
+            args: [
+                {
+                    canister_id: canisterId
+                }
+            ],
+            cycles: 10_000_000n
+        });
+
+        return true;
+    }
+
+    @update([], raw_rand_result)
+    async getRawRand(): Promise<Uint8Array> {
+        return await call<[], Uint8Array>('aaaaa-aa', 'raw_rand', {
+            returnIdlType: raw_rand_result
+        });
+    }
+
+    // TODO we should test this like we test depositCycles
+    @update([], provisional_create_canister_with_cycles_result)
+    async provisionalCreateCanisterWithCycles(): Promise<provisional_create_canister_with_cycles_result> {
+        return await call<
+            [provisional_create_canister_with_cycles_args],
+            provisional_create_canister_with_cycles_result
+        >('aaaaa-aa', 'provisional_create_canister_with_cycles', {
+            paramIdlTypes: [provisional_create_canister_with_cycles_args],
+            returnIdlType: provisional_create_canister_with_cycles_result,
+            args: [
+                {
+                    amount: [],
+                    settings: [],
+                    sender_canister_version: [],
+                    specified_id: []
+                }
+            ]
+        });
+    }
+
+    // TODO we should test this like we test depositCycles
+    @update([IDL.Principal, IDL.Nat], IDL.Bool)
+    async provisionalTopUpCanister(
+        canisterId: Principal,
+        amount: bigint
+    ): Promise<boolean> {
+        await call<[provisional_top_up_canister_args], void>(
+            'aaaaa-aa',
+            'provisional_top_up_canister',
+            {
+                paramIdlTypes: [provisional_top_up_canister_args],
+                args: [
+                    {
+                        canister_id: canisterId,
+                        amount
+                    }
+                ]
+            }
+        );
+
+        return true;
+    }
+
+    @query([], IDL.Principal)
+    getCreatedCanisterId(): Principal {
+        return this.state.createdCanisterId;
+    }
+}
+
+async function createCanister(): Promise<create_canister_result> {
+    return await call<[create_canister_args], create_canister_result>(
+        'aaaaa-aa',
+        'create_canister',
+        {
+            paramIdlTypes: [create_canister_args],
+            returnIdlType: create_canister_result,
+            args: [{ settings: [], sender_canister_version: [] }],
+            cycles: 50_000_000_000_000n
+        }
+    );
+}
---
+++ examples/stable/test/end_to_end/candid_rpc/management_canister_generate_types/test/pretest.ts
@@ -0,0 +1,88 @@
+import { linkAndInstallPatch } from 'azle/test/jest_link';
+import { execSync } from 'child_process';
+import { lstatSync, readFileSync, writeFileSync } from 'fs';
+import { join } from 'path';
+
+function pretest(): void {
+    linkAndInstallPatch(
+        join(
+            'examples',
+            'experimental',
+            'test',
+            'end_to_end',
+            'candid_rpc',
+            'management_canister'
+        )
+    );
+
+    execSync(`dfx canister uninstall-code management_canister || true`, {
+        stdio: 'inherit'
+    });
+
+    // We generate this file without the workaroundForMultipleIDLPathsProblem so that we can do an exact comparison with the azle/canisters/management/idl/index.ts file
+    execSync(
+        `npm exec --offline azle generate-types ../../../../../../canisters/management/idl/ic.did > src/idl/management_no_workaround.ts`,
+        {
+            stdio: 'inherit'
+        }
+    );
+
+    execSync(
+        'npx prettier --ignore-path null --write src/idl/management_no_workaround.ts',
+        {
+            stdio: 'inherit'
+        }
+    );
+
+    execSync(
+        `npm exec --offline azle generate-types ../../../../../../canisters/management/idl/ic.did > src/idl/management.ts`,
+        {
+            stdio: 'inherit'
+        }
+    );
+
+    workaroundForMultipleIDLPathsProblem();
+
+    execSync('npx prettier --ignore-path null --write src/idl/management.ts', {
+        stdio: 'inherit'
+    });
+
+    execSync(`dfx deploy`, {
+        stdio: 'inherit'
+    });
+
+    execSync(
+        `dfx ledger fabricate-cycles --canister management_canister --cycles 100000000000000`,
+        {
+            stdio: 'inherit'
+        }
+    );
+
+    execSync(`dfx generate`, {
+        stdio: 'inherit'
+    });
+}
+
+pretest();
+
+// TODO remove once this issue is resolved: https://github.com/dfinity/agent-js/issues/977
+// Basically what we do here is to replace import { IDL } from '@dfinity/candid' with import { IDL } from 'azle'
+// but only when azle is a symlink (accomplished usually in our dev environment through npm link azle)
+// The multiple IDL paths problem only occurs at least in the context of this test, in our local symlink
+// testing environment. Users shouldn't run into this issue because they will not have symlinked azle
+// The GitHub Actions tests on a release will run this test against azle as an npm package
+function workaroundForMultipleIDLPathsProblem(): void {
+    if (lstatSync('node_modules/azle').isSymbolicLink() === false) {
+        return;
+    }
+
+    const generatedTypesPath = 'src/idl/management.ts';
+
+    writeFileSync(
+        generatedTypesPath,
+        readFileSync(generatedTypesPath, 'utf-8').replace(
+            /import\s*{\s*IDL\s*}\s*from\s*['"]@dfinity\/candid['"]/g,
+            "import { IDL } from 'azle'"
+        )
+    );
+}
