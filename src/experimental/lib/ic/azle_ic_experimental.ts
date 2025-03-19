def4e7c9249f603a6b5c4e05fa3258e11f35e310
Jordan Last <jordan.michael.last@gmail.com>
2025-03-18 18:11:55 -0600

    callRaw should return void still in experimental

--- src/experimental/lib/ic/azle_ic_experimental.ts
+++ src/experimental/lib/ic/azle_ic_experimental.ts
@@ -13,7 +13,7 @@ export type AzleIcExperimental = {
         method: string,
         argsRaw: ArrayBuffer,
         paymentString: string
-    ) => Promise<ArrayBuffer>;
+    ) => void;
     msgCaller: () => ArrayBuffer;
     candidCompiler: (candidPath: string) => string;
     candidDecode: (candidBytes: ArrayBuffer) => string;
