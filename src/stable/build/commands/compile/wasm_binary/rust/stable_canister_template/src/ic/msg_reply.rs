a3c10d5e1d3919491223233803fe334da8c57cad
Jordan Last <jordan.michael.last@gmail.com>
2025-03-20 15:09:48 -0600

    undo msg_reply changes

--- src/stable/build/commands/compile/wasm_binary/rust/stable_canister_template/src/ic/msg_reply.rs
+++ src/stable/build/commands/compile/wasm_binary/rust/stable_canister_template/src/ic/msg_reply.rs
@@ -3,8 +3,6 @@ use rquickjs::{Ctx, Function, Result, TypedArray};
 
 pub fn get_function(ctx: Ctx) -> Result<Function> {
     Function::new(ctx, |bytes: TypedArray<u8>| -> () {
-        let bytes = bytes.as_ref();
-
-        reply_raw(bytes)
+        reply_raw(bytes.as_ref())
     })
 }
