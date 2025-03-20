77e985d212787674e7da2490e6d95dc6d977ba2e
Jordan Last <jordan.michael.last@gmail.com>
2025-03-20 16:00:29 -0600

    get rid of the raw pointer

--- src/stable/build/commands/compile/wasm_binary/rust/stable_canister_template/src/ic/call_raw.rs
+++ src/stable/build/commands/compile/wasm_binary/rust/stable_canister_template/src/ic/call_raw.rs
@@ -1,34 +1,19 @@
 use candid::Principal;
-use ic_cdk::{api::call::call_raw128, spawn};
-use rquickjs::{Ctx, Exception, Function, Promise, Result, Result as QuickJsResult, TypedArray};
+use ic_cdk::{api::call::call_raw128, spawn, trap};
+use rquickjs::{Ctx, Exception, Function, Promise, Result as RQuickJsResult, TypedArray};
 
 use crate::{
     error::quickjs_call_with_error_handling, ic::throw_error, quickjs_with_ctx::run_event_loop,
 };
 
-pub fn get_function(ctx: Ctx<'static>) -> QuickJsResult<Function<'static>> {
-    // We use a raw pointer here to deal with already borrowed issues
-    // using quickjs_with_ctx after a cross-canister call (await point)
-    // Sometimes the await point won't actually behave like a full await point
-    // because the cross-canister call returns an error where the callback
-    // is never queued and executed by the IC
-    // In this case the outer quickjs_with_ctx for the original call
-    // will still have borrowed something having to do with the ctx
-    // whereas in a full cross-canister call the outer quickjs_with_ctx
-    // will have completed.
-    // Using a raw pointer overcomes the issue, and I believe it is safe
-    // because we only ever have one Runtime, one Context, we never destroy them
-    // and we are in a single-threaded environment. We will of course
-    // engage in intense testing and review to ensure safety
-    let ctx_ptr = ctx.as_raw();
-
+pub fn get_function(ctx: Ctx<'static>) -> RQuickJsResult<Function<'static>> {
     Function::new(
         ctx.clone(),
         move |canister_id_bytes: TypedArray<u8>,
               method: String,
               args_raw: TypedArray<u8>,
               cycles_string: String|
-              -> Result<Promise> {
+              -> RQuickJsResult<Promise> {
             let canister_id = Principal::from_slice(canister_id_bytes.as_ref());
             let args_raw = args_raw
                 .as_bytes()
@@ -41,26 +26,22 @@ pub fn get_function(ctx: Ctx<'static>) -> QuickJsResult<Function<'static>> {
                 .parse()
                 .map_err(|e| throw_error(ctx.clone(), e))?;
 
-            let (promise, resolve, reject) = ctx.promise().unwrap();
+            let (promise, resolve, reject) = ctx.promise()?;
+
+            let ctx_for_spawn = ctx.clone();
 
             spawn(async move {
-                // TODO let's crush this ctx pointer
-                let ctx = unsafe { Ctx::from_raw(ctx_ptr) };
-
                 let call_result = call_raw128(canister_id, &method, args_raw, payment).await;
 
-                match call_result {
-                    Ok(candid_bytes) => {
-                        quickjs_call_with_error_handling(
-                            ctx.clone(),
-                            &resolve,
-                            (TypedArray::<u8>::new(ctx.clone(), candid_bytes),),
-                        )
-                        .unwrap();
-                    }
+                let resolve_or_reject_result = match call_result {
+                    Ok(candid_bytes) => quickjs_call_with_error_handling(
+                        ctx_for_spawn.clone(),
+                        &resolve,
+                        (TypedArray::<u8>::new(ctx_for_spawn.clone(), candid_bytes),),
+                    ),
                     Err(err) => {
                         let err_js_object = Exception::from_message(
-                            ctx.clone(),
+                            ctx_for_spawn.clone(),
                             &format!(
                                 "The inter-canister call failed with reject code {}: {}",
                                 err.0 as i32, &err.1
@@ -71,12 +52,19 @@ pub fn get_function(ctx: Ctx<'static>) -> QuickJsResult<Function<'static>> {
                         err_js_object.set("rejectCode", err.0 as i32).unwrap();
                         err_js_object.set("rejectMessage", &err.1).unwrap();
 
-                        quickjs_call_with_error_handling(ctx.clone(), &reject, (err_js_object,))
-                            .unwrap();
+                        quickjs_call_with_error_handling(
+                            ctx_for_spawn.clone(),
+                            &reject,
+                            (err_js_object,),
+                        )
                     }
                 };
 
-                run_event_loop(ctx.clone());
+                if let Err(e) = resolve_or_reject_result {
+                    trap(&format!("Azle CallRawError: {e}"));
+                }
+
+                run_event_loop(ctx_for_spawn.clone());
             });
 
             Ok(promise)
