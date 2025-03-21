dc81bc9cf04e6d0fa95a11c0eb72c05eb9b459be
Jordan Last <jordan.michael.last@gmail.com>
2025-03-21 11:02:22 -0600

    add comments

--- src/stable/build/commands/compile/wasm_binary/rust/stable_canister_template/src/rquickjs_utils/drain_microtasks.rs
+++ src/stable/build/commands/compile/wasm_binary/rust/stable_canister_template/src/rquickjs_utils/drain_microtasks.rs
@@ -3,6 +3,9 @@ use rquickjs::Ctx;
 // Executes all pending jobs in the QuickJS job queue
 // These jobs are essentially JavaScript microtasks such as
 // Promise then/catch/finally callbacks and the continuation of async functions after await points
+// In Azle on ICP, macrotasks are essentially the following: the first JavaScript module execution in init/postUpgrade
+// JavaScript function executions from update and query calls, JavaScript function executions from timer callbacks,
+// and JavaScript function executions from resolving or rejecting the Promise after an inter-canister call
 pub fn drain_microtasks(ctx: &Ctx) {
     while ctx.execute_pending_job() {}
 }
