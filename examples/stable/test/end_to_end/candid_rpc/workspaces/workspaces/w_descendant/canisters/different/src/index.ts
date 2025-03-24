d6bfb641abf391396b5d9e138017212314f5ea79
Benjamin DeMann <benjamin.demann@gmail.com>
2025-03-24 12:18:58 -0600

    update reported version to be set by azle

--- examples/stable/test/end_to_end/candid_rpc/workspaces/workspaces/w_descendant/canisters/different/src/index.ts
+++ examples/stable/test/end_to_end/candid_rpc/workspaces/workspaces/w_descendant/canisters/different/src/index.ts
@@ -3,6 +3,9 @@ import { IDL, query } from 'azle';
 export default class {
     @query([], IDL.Text)
     getAzleVersionFromWDescendantDifferent(): string {
-        return '0.30.0';
+        if (process.env.AZLE_AZLE_VERSION !== undefined) {
+            return process.env.AZLE_AZLE_VERSION;
+        }
+        throw new Error('AZLE_AZLE_VERSION is not set');
     }
 }
