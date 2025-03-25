c7a7863c3bd418913c8ab107c2004db7f0302111
Benjamin DeMann <benjamin.demann@gmail.com>
2025-03-25 13:19:38 -0600

    get tests working

--- examples/stable/test/end_to_end/candid_rpc/workspaces/test/canisters.ts
+++ examples/stable/test/end_to_end/candid_rpc/workspaces/test/canisters.ts
@@ -1,6 +1,8 @@
 type Canister = {
     name: string;
-    path: string;
+    dfxDir: string;
+    packageJsonDir: string;
+    expectedBin: string;
     method: string;
     result: string;
 };
@@ -8,73 +10,97 @@ type Canister = {
 export const CANISTERS: Canister[] = [
     {
         name: 'ancestor_current',
-        path: 'workspaces/ancestor',
+        dfxDir: 'workspaces/ancestor',
+        packageJsonDir: 'workspaces/ancestor/canisters/current',
+        expectedBin: 'workspaces/ancestor/canisters/current',
         method: 'getAzleVersionFromAncestorCurrent',
         result: '0.30.0'
     },
     {
         name: 'ancestor_old',
-        path: 'workspaces/ancestor',
+        dfxDir: 'workspaces/ancestor',
+        packageJsonDir: 'workspaces/ancestor/canisters/old',
+        expectedBin: 'workspaces/ancestor/canisters/old',
         method: 'getAzleVersionFromAncestorOld',
         result: '0.29.0'
     },
     {
         name: 'descendant_different',
-        path: 'workspaces/descendant_different/dfx',
+        dfxDir: 'workspaces/descendant_different/dfx',
+        packageJsonDir: 'workspaces/descendant_different',
+        expectedBin: 'workspaces/descendant_different',
         method: 'getAzleVersionFromDescendantDifferent',
         result: '0.30.0'
     },
     {
         name: 'descendant_same',
-        path: 'workspaces/descendant_same/src',
+        dfxDir: 'workspaces/descendant_same/src',
+        packageJsonDir: 'workspaces/descendant_same',
+        expectedBin: 'workspaces/descendant_same',
         method: 'getAzleVersionFromDescendantSame',
         result: '0.30.0'
     },
     {
         name: 'sibling',
-        path: 'workspaces/siblings',
+        dfxDir: 'workspaces/siblings',
+        packageJsonDir: 'workspaces/siblings',
+        expectedBin: 'workspaces/siblings',
         method: 'getAzleVersionFromSibling',
         result: '0.30.0'
     },
     {
         name: 'w_ancestor_current',
-        path: 'workspaces/w_ancestor',
+        dfxDir: 'workspaces/w_ancestor',
+        packageJsonDir: 'workspaces/w_ancestor/canisters/current',
+        expectedBin: 'workspaces/w_ancestor',
         method: 'getAzleVersionFromWAncestorCurrent',
         result: '0.30.0'
     },
     {
         name: 'w_ancestor_old',
-        path: 'workspaces/w_ancestor',
+        dfxDir: 'workspaces/w_ancestor',
+        packageJsonDir: 'workspaces/w_ancestor/canisters/old',
+        expectedBin: 'workspaces/w_ancestor/canisters/old',
         method: 'getAzleVersionFromWAncestorOld',
         result: '0.29.0'
     },
     {
         name: 'w_descendant_different',
-        path: 'workspaces/w_descendant/canisters/different/dfx',
+        dfxDir: 'workspaces/w_descendant/canisters/different/dfx',
+        packageJsonDir: 'workspaces/w_descendant/canisters/different',
+        expectedBin: 'workspaces/w_descendant',
         method: 'getAzleVersionFromWDescendantDifferent',
         result: '0.30.0'
     },
     {
         name: 'w_descendant_different_deep',
-        path: 'workspaces/w_descendant/canisters/different_deep/dfx/deeper',
+        dfxDir: 'workspaces/w_descendant/canisters/different_deep/dfx/deeper',
+        packageJsonDir: 'workspaces/w_descendant/canisters/different_deep',
+        expectedBin: 'workspaces/w_descendant',
         method: 'getAzleVersionFromWDescendantDifferentDeep',
         result: '0.30.0'
     },
     {
         name: 'w_descendant_same',
-        path: 'workspaces/w_descendant/canisters/same/src',
+        dfxDir: 'workspaces/w_descendant/canisters/same/src',
+        packageJsonDir: 'workspaces/w_descendant/canisters/same',
+        expectedBin: 'workspaces/w_descendant',
         method: 'getAzleVersionFromWDescendantSame',
         result: '0.30.0'
     },
     {
         name: 'w_descendant_same_deep',
-        path: 'workspaces/w_descendant/canisters/same_deep/src/dfx',
+        dfxDir: 'workspaces/w_descendant/canisters/same_deep/src/dfx',
+        packageJsonDir: 'workspaces/w_descendant/canisters/same_deep',
+        expectedBin: 'workspaces/w_descendant',
         method: 'getAzleVersionFromWDescendantSameDeep',
         result: '0.30.0'
     },
     {
         name: 'w_sibling',
-        path: 'workspaces/w_siblings/w_sibling',
+        dfxDir: 'workspaces/w_siblings/w_sibling',
+        packageJsonDir: 'workspaces/w_siblings/w_sibling',
+        expectedBin: 'workspaces/w_siblings',
         method: 'getAzleVersionFromWSibling',
         result: '0.30.0'
     }
--- examples/stable/test/end_to_end/candid_rpc/workspaces/test/pretest.ts
+++ examples/stable/test/end_to_end/candid_rpc/workspaces/test/pretest.ts
@@ -5,7 +5,7 @@ import { CANISTERS } from './canisters';
 function pretest(): void {
     for (const canister of CANISTERS) {
         execSync(
-            `cd ${canister.path} && dfx canister uninstall-code ${canister.name} || true`,
+            `cd ${canister.dfxDir} && dfx canister uninstall-code ${canister.name} || true`,
             {
                 stdio: 'inherit'
             }
