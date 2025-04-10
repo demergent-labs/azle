bc20d6f3b4e8a6952486d25f0af6be0587268a5f
Benjamin DeMann <benjamin.demann@gmail.com>
2025-04-10 12:59:52 -0600

    try packing

--- src/stable/test/jest_link.ts
+++ src/stable/test/jest_link.ts
@@ -1,16 +1,25 @@
 import { execSync } from 'child_process';
-import { join, resolve } from 'path';
+import { join } from 'path';
+
+import { AZLE_ROOT } from '#build/utils/global_paths';
 
 // TODO remove linkAndInstallPatch after https://github.com/demergent-labs/azle/issues/1807 is resolved
 export function linkAndInstallPatch(pathRelativeToAzle: string): void {
-    const examplesDir =
-        process.env.GITHUB_WORKSPACE ?? resolve(__dirname, '..', '..', '..');
+    const azleRoot = process.env.GITHUB_WORKSPACE ?? AZLE_ROOT;
 
-    execSync(`npm install`, { cwd: join(examplesDir, pathRelativeToAzle) });
+    execSync(`npm install`, { cwd: join(azleRoot, pathRelativeToAzle) });
 
     if (process.env.AZLE_END_TO_END_TEST_LINK_AZLE !== 'false') {
         execSync(`npm link azle`, {
-            cwd: join(examplesDir, pathRelativeToAzle)
+            cwd: join(azleRoot, pathRelativeToAzle)
+        });
+    }
+
+    if (process.env.AZLE_END_TO_END_TEST_PACK_AZLE === 'true') {
+        const distDir = join(azleRoot, 'dist');
+
+        execSync(`npm install ${join(distDir, 'azle.tgz')} --no-save`, {
+            cwd: join(azleRoot, pathRelativeToAzle)
         });
     }
 }
