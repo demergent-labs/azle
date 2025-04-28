8704206ed9e3c59fdeb4a1eba9e77aad8e3f4de8
Benjamin DeMann <benjamin.demann@gmail.com>
2025-04-28 11:50:35 -0600

    remove packing infrastructure

--- src/stable/test/jest_link.ts
+++ src/stable/test/jest_link.ts
@@ -1,26 +1,16 @@
 import { execSync } from 'child_process';
-import { join } from 'path';
-
-import { AZLE_ROOT } from '#build/utils/global_paths';
+import { join, resolve } from 'path';
 
 // TODO remove linkAndInstallPatch after https://github.com/demergent-labs/azle/issues/1807 is resolved
 export function linkAndInstallPatch(pathRelativeToAzle: string): void {
-    const azleRoot = process.env.GITHUB_WORKSPACE ?? AZLE_ROOT;
-    const examplePath = join(azleRoot, pathRelativeToAzle);
-    const isPackMode = process.env.AZLE_END_TO_END_TEST_PACK_AZLE === 'true';
-    const isLinkMode =
-        process.env.AZLE_END_TO_END_TEST_LINK_AZLE !== 'false' && !isPackMode;
+    const examplesDir =
+        process.env.GITHUB_WORKSPACE ?? resolve(__dirname, '..', '..', '..');
 
-    if (isPackMode) {
-        const distDir = join(azleRoot, 'dist');
-        const packPath = join(distDir, 'azle.tgz');
+    execSync(`npm install`, { cwd: join(examplesDir, pathRelativeToAzle) });
 
-        execSync(`npm install ${packPath} --no-save`, { cwd: examplePath });
-    } else {
-        execSync(`npm install`, { cwd: examplePath });
-
-        if (isLinkMode) {
-            execSync(`npm link azle`, { cwd: examplePath });
-        }
+    if (process.env.AZLE_END_TO_END_TEST_LINK_AZLE !== 'false') {
+        execSync(`npm link azle`, {
+            cwd: join(examplesDir, pathRelativeToAzle)
+        });
     }
 }
