f6b4752a78fa29e5da39415626fa1e453a98e050
Benjamin DeMann <benjamin.demann@gmail.com>
2025-05-07 10:36:09 -0600

    move experimental code out of all stable places except globals

--- src/experimental/build/utils/types.ts
+++ src/experimental/build/utils/types.ts
@@ -1,9 +1,14 @@
 import {
+    CanisterConfig as StableCanisterConfig,
     Context as StableContext,
+    CustomConfig as StableCustomConfig,
     WasmData as StableWasmData
 } from '#utils/types';
 
-import { Consumer } from '../commands/build/open_value_sharing/consumer';
+import {
+    Consumer,
+    ConsumerConfig
+} from '../commands/build/open_value_sharing/consumer';
 
 export type Context = {
     canisterId: string;
@@ -17,3 +22,11 @@ export type WasmData = {
     consumer: Consumer;
     managementDid: string;
 } & StableWasmData;
+
+export type CustomConfig = StableCustomConfig & {
+    openValueSharing?: ConsumerConfig;
+};
+
+export interface CanisterConfig extends StableCanisterConfig {
+    custom?: CustomConfig;
+}
