69e05ad2f6539c44ff1bb1722f7819e45e40c8f7
Benjamin DeMann <benjamin.demann@gmail.com>
2025-05-12 12:07:28 -0600

    switch custom js modules to use lib assert experimental

--- src/experimental/build/commands/build/custom_js_modules/async_hooks.ts
+++ src/experimental/build/commands/build/custom_js_modules/async_hooks.ts
@@ -1,4 +1,4 @@
-import '#experimental/build/assert_experimental';
+import '#experimental/lib/assert_experimental';
 
 // TODO this is just a placeholder module so that bundling won't break
 // TODO eventually we would want a real implementation of this in Azle or wasmedge-quickjs or Waden
