5b3a35ac076de964892c1f7e802b5ab38f2f864b
Benjamin DeMann <benjamin.demann@gmail.com>
2025-06-03 15:38:35 -0600

    consolidate list_of_lists

--- examples/stable/test/end_to_end/candid_rpc/list_of_lists/test/test.ts
+++ examples/stable/test/end_to_end/candid_rpc/list_of_lists/test/test.ts
@@ -1,8 +1,8 @@
 import { getCanisterId } from 'azle/_internal/dfx';
 import { runTests } from 'azle/_internal/test';
-import { getTests } from 'list_of_lists_end_to_end_test_functional_syntax/test/tests';
 
 import { createActor } from '../dfx_generated/list_of_lists';
+import { getTests } from './tests';
 
 const canisterName = 'list_of_lists';
 const listOfListsCanister = createActor(getCanisterId(canisterName), {
--- examples/stable/test/end_to_end/candid_rpc/list_of_lists/test/tests.ts
+++ examples/stable/test/end_to_end/candid_rpc/list_of_lists/test/tests.ts
@@ -2,7 +2,6 @@ import { ActorSubclass } from '@dfinity/agent';
 import { Principal } from '@dfinity/principal';
 import { expect, it, Test } from 'azle/_internal/test';
 
-// @ts-ignore this path may not exist when these tests are imported into other test projects
 import { _SERVICE } from '../dfx_generated/list_of_lists/list_of_lists.did';
 
 export function getTests(listOfListsCanister: ActorSubclass<_SERVICE>): Test {
