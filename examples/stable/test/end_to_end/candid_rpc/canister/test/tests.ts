4f88929bb694f06345016b9f4b942ab239ebcf2b
Benjamin DeMann <benjamin.demann@gmail.com>
2025-06-03 15:19:58 -0600

    consolidate canister

--- examples/stable/test/end_to_end/candid_rpc/canister/test/tests.ts
+++ examples/stable/test/end_to_end/candid_rpc/canister/test/tests.ts
@@ -3,7 +3,6 @@ import { Principal } from '@dfinity/principal';
 import { getCanisterId } from 'azle/_internal/dfx';
 import { expect, it, Test } from 'azle/_internal/test';
 
-// @ts-ignore this path may not exist when these tests are imported into other test projects
 import { _SERVICE } from './dfx_generated/canister/canister.did';
 
 export function getTests(canister: ActorSubclass<_SERVICE>): Test {
