b4398c77272930224ec326e229c4b00d5153820a
Benjamin DeMann <benjamin.demann@gmail.com>
2025-06-03 15:57:20 -0600

    consolidate calc

--- examples/experimental/test/end_to_end/candid_rpc/motoko_examples/calc/README.md
+++
@@ -1,3 +0,0 @@
-# Calc
-
-Reimplementation of the [Motoko Calc example](https://github.com/dfinity/examples/tree/master/motoko/calc)
--- examples/stable/test/end_to_end/candid_rpc/motoko_examples/calc/test/tests.ts
+++ examples/stable/test/end_to_end/candid_rpc/motoko_examples/calc/test/tests.ts
@@ -1,7 +1,6 @@
 import { ActorSubclass } from '@dfinity/agent';
 import { expect, it, Test } from 'azle/_internal/test';
 
-// @ts-ignore this path may not exist when these tests are imported into other test projects
 import { _SERVICE } from './dfx_generated/calc/calc.did';
 
 export function getTests(calcCanister: ActorSubclass<_SERVICE>): Test {
