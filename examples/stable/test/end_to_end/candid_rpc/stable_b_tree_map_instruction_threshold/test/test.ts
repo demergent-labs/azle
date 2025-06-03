89193556c96901fb0727467cfde1e857a97a664d
Benjamin DeMann <benjamin.demann@gmail.com>
2025-06-03 15:54:15 -0600

    consolidate stable_b_tree_map_instruction_threshold

--- examples/stable/test/end_to_end/candid_rpc/stable_b_tree_map_instruction_threshold/test/test.ts
+++ examples/stable/test/end_to_end/candid_rpc/stable_b_tree_map_instruction_threshold/test/test.ts
@@ -1,8 +1,8 @@
 import { getCanisterId } from 'azle/_internal/dfx';
 import { runTests } from 'azle/_internal/test';
-import { getTests } from 'stable_b_tree_map_instruction_threshold_end_to_end_test_functional_syntax/test/tests';
 
 import { createActor } from './dfx_generated/stable_b_tree_map_instruction_threshold';
+import { getTests } from './tests';
 
 const canisterName = 'stable_b_tree_map_instruction_threshold';
 const stableBTreeMapInstructionThresholdCanister = createActor(
--- examples/stable/test/end_to_end/candid_rpc/stable_b_tree_map_instruction_threshold/test/tests.ts
+++ examples/stable/test/end_to_end/candid_rpc/stable_b_tree_map_instruction_threshold/test/tests.ts
@@ -1,7 +1,6 @@
 import { ActorSubclass } from '@dfinity/agent';
 import { expect, it, Test } from 'azle/_internal/test';
 
-// @ts-ignore this path may not exist when these tests are imported into other test projects
 import { _SERVICE } from './dfx_generated/stable_b_tree_map_instruction_threshold/stable_b_tree_map_instruction_threshold.did';
 
 export function getTests(
