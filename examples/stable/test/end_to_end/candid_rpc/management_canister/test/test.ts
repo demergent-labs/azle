b6403520ffb4cb3bf31bde36713edbb725e1bb15
Benjamin DeMann <benjamin.demann@gmail.com>
2025-06-03 15:39:02 -0600

    consolidate management canister

--- examples/stable/test/end_to_end/candid_rpc/management_canister/test/test.ts
+++ examples/stable/test/end_to_end/candid_rpc/management_canister/test/test.ts
@@ -1,8 +1,8 @@
 import { getCanisterId } from 'azle/_internal/dfx';
 import { runTests } from 'azle/_internal/test';
-import { getTests } from 'management_canister_end_to_end_test_functional_syntax/test/tests';
 
 import { createActor } from './dfx_generated/management_canister';
+import { getTests } from './tests';
 
 const canisterName = 'management_canister';
 const managementCanister = createActor(getCanisterId(canisterName), {
