cbe9c5d3291aa1f85f8b33286d90f4ba1d465392
Benjamin DeMann <benjamin.demann@gmail.com>
2025-06-03 15:53:38 -0600

    consolidate simple_users_accounts

--- examples/stable/test/end_to_end/candid_rpc/simple_user_accounts/test/test.ts
+++ examples/stable/test/end_to_end/candid_rpc/simple_user_accounts/test/test.ts
@@ -1,8 +1,8 @@
 import { getCanisterId } from 'azle/_internal/dfx';
 import { runTests } from 'azle/_internal/test';
-import { getTests } from 'simple_user_accounts_end_to_end_test_functional_syntax/test/tests';
 
 import { createActor } from './dfx_generated/simple_user_accounts';
+import { getTests } from './tests';
 
 const canisterName = 'simple_user_accounts';
 const simpleUserAccountsCanister = createActor(getCanisterId(canisterName), {
--- examples/stable/test/end_to_end/candid_rpc/simple_user_accounts/test/tests.ts
+++ examples/stable/test/end_to_end/candid_rpc/simple_user_accounts/test/tests.ts
@@ -1,7 +1,6 @@
 import { ActorSubclass } from '@dfinity/agent';
 import { expect, it, Test } from 'azle/_internal/test';
 
-// @ts-ignore this path may not exist when these tests are imported into other test projects
 import { _SERVICE } from './dfx_generated/simple_user_accounts/simple_user_accounts.did';
 
 export function getTests(
