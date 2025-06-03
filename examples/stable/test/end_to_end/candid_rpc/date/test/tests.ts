ead0a64b3817bebfb5e4903e54c469ea6b30e83c
Benjamin DeMann <benjamin.demann@gmail.com>
2025-06-03 15:32:42 -0600

    consolidate date

--- examples/stable/test/end_to_end/candid_rpc/date/test/tests.ts
+++ examples/stable/test/end_to_end/candid_rpc/date/test/tests.ts
@@ -1,7 +1,6 @@
 import { ActorSubclass } from '@dfinity/agent';
 import { expect, it, Test } from 'azle/_internal/test';
 
-// @ts-ignore this path may not exist when these tests are imported into other test projects
 import { _SERVICE } from './dfx_generated/date/date.did.d';
 
 // NOTE: The IC has not concept of a timezone since it's a world computer. It
