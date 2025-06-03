2a019188bbe6d6b3710892e8cc0b29e3e0f8fbe3
Benjamin DeMann <benjamin.demann@gmail.com>
2025-06-03 15:30:01 -0600

    consolidate composite queries

--- examples/experimental/test/end_to_end/candid_rpc/composite_queries/src/canister3/index.ts
+++
@@ -1,7 +0,0 @@
-import { Canister, query, text } from 'azle/experimental';
-
-export default Canister({
-    deepQuery: query([], text, () => {
-        return 'Hello from Canister 3';
-    })
-});
--- examples/stable/test/end_to_end/candid_rpc/composite_queries/test/tests.ts
+++ examples/stable/test/end_to_end/candid_rpc/composite_queries/test/tests.ts
@@ -2,7 +2,6 @@ import { ActorSubclass } from '@dfinity/agent';
 import { getCanisterId } from 'azle/_internal/dfx';
 import { expect, it, Test } from 'azle/_internal/test';
 
-// @ts-ignore this path may not exist when these tests are imported into other test projects
 import { _SERVICE } from './dfx_generated/canister1/canister1.did';
 
 export function getTests(canister1: ActorSubclass<_SERVICE>): Test {
