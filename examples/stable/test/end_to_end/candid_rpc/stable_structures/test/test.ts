bab7c00ef374aaff0841a143694b3ec646417b7f
Benjamin DeMann <benjamin.demann@gmail.com>
2025-06-03 15:54:52 -0600

    consolidate stable_structures

--- examples/stable/test/end_to_end/candid_rpc/stable_structures/test/test.ts
+++ examples/stable/test/end_to_end/candid_rpc/stable_structures/test/test.ts
@@ -1,11 +1,11 @@
 import { getCanisterId } from 'azle/_internal/dfx';
 import { runTests } from 'azle/_internal/test';
-import { getTests } from 'stable_structures_end_to_end_test_functional_syntax/test/tests';
 
 import { createActor as createActorCanister1 } from './dfx_generated/canister1';
 import { createActor as createActorCanister2 } from './dfx_generated/canister2';
 import { createActor as createActorCanister3 } from './dfx_generated/canister3';
 import { _SERVICE } from './dfx_generated/canister3/canister3.did';
+import { getTests } from './tests';
 
 const stableStructuresCanister1Name = 'canister1';
 
