d55fb31b01a21af00f1bfd93f8016e8f61864550
Benjamin DeMann <benjamin.demann@gmail.com>
2025-06-03 15:12:35 -0600

    consolidate call_raw

--- examples/stable/test/end_to_end/candid_rpc/call_raw/test/tests.ts
+++ examples/stable/test/end_to_end/candid_rpc/call_raw/test/tests.ts
@@ -2,7 +2,6 @@ import { ActorSubclass } from '@dfinity/agent';
 import { Principal } from '@dfinity/principal';
 import { expect, it, Test } from 'azle/_internal/test';
 
-// @ts-ignore this path may not exist when these tests are imported into other test projects
 import { _SERVICE } from './dfx_generated/call_raw/call_raw.did';
 
 export function getTests(call_raw_canister: ActorSubclass<_SERVICE>): Test {
