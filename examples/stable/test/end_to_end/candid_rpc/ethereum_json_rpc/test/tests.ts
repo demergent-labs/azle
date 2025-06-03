b23010062e316fc2203d465626f39dbdac1a2bd0
Benjamin DeMann <benjamin.demann@gmail.com>
2025-06-03 15:33:22 -0600

    consolidate ethereum_json_rpc

--- examples/stable/test/end_to_end/candid_rpc/ethereum_json_rpc/test/tests.ts
+++ examples/stable/test/end_to_end/candid_rpc/ethereum_json_rpc/test/tests.ts
@@ -1,7 +1,6 @@
 import { ActorSubclass } from '@dfinity/agent';
 import { expect, it, Test } from 'azle/_internal/test';
 
-// @ts-ignore this path may not exist when these tests are imported into other test projects
 import { _SERVICE } from './dfx_generated/ethereum_json_rpc/ethereum_json_rpc.did';
 
 export function getTests(
