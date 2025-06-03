46ca1ac965bee8ce28a3472c204ed061b10860c7
Benjamin DeMann <benjamin.demann@gmail.com>
2025-06-03 15:52:28 -0600

    consolidate rejections

--- examples/stable/test/end_to_end/candid_rpc/rejections/test/test.ts
+++ examples/stable/test/end_to_end/candid_rpc/rejections/test/test.ts
@@ -1,8 +1,8 @@
 import { getCanisterId } from 'azle/_internal/dfx';
 import { runTests } from 'azle/_internal/test';
-import { getTests } from 'rejections_end_to_end_test_functional_syntax/test/tests';
 
 import { createActor } from './dfx_generated/rejections';
+import { getTests } from './tests';
 
 const canisterName = 'rejections';
 const rejectionsCanister = createActor(getCanisterId(canisterName), {
--- examples/stable/test/end_to_end/candid_rpc/rejections/test/tests.ts
+++ examples/stable/test/end_to_end/candid_rpc/rejections/test/tests.ts
@@ -1,7 +1,6 @@
 import { ActorSubclass } from '@dfinity/agent';
 import { expect, it, Test } from 'azle/_internal/test';
 
-// @ts-ignore this path may not exist when these tests are imported into other test projects
 import { _SERVICE } from './dfx_generated/rejections/rejections.did';
 
 export function getTests(rejectionsCanister: ActorSubclass<_SERVICE>): Test {
