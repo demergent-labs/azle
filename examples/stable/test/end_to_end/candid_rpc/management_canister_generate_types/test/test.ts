ba8c522f58b9ebd328430a29fc4e1cf90e666c66
Benjamin DeMann <benjamin.demann@gmail.com>
2025-06-04 12:03:05 -0600

    clean up example workspace

--- examples/stable/test/end_to_end/candid_rpc/management_canister_generate_types/test/test.ts
+++ examples/stable/test/end_to_end/candid_rpc/management_canister_generate_types/test/test.ts
@@ -1,4 +1,4 @@
-import { getTests } from '@azle/management_canister_end_to_end_test/test/tests';
+import { getTests } from '@azle/management_canister/test/tests';
 import { getCanisterId } from 'azle/_internal/dfx';
 import { expect, it, runTests } from 'azle/_internal/test';
 import { readFileSync } from 'fs';
