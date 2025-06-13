6e0bd2a27b46b67ba648b27ce02e294f5b9e113c
Jordan Last <jordan.michael.last@gmail.com>
2025-06-12 19:16:52 -0600

    create first round of benchmarks

--- examples/stable/test/end_to_end/candid_rpc/benchmarks/test/tests.ts
+++ examples/stable/test/end_to_end/candid_rpc/benchmarks/test/tests.ts
@@ -5,7 +5,50 @@ import { please, Test } from 'azle/_internal/test';
 import { _SERVICE } from './dfx_generated/benchmarks/benchmarks.did';
 
 const iterations = [1, 10, 100, 1_000, 10_000, 100_000, 1_000_000];
-const methodNames = ['blobInitStack', 'blobInitHeap'] as const;
+const methodNames = [
+    'blobInitStack',
+    'blobInitHeap',
+    'boolInitStack',
+    'boolInitHeap',
+    'float32InitStack',
+    'float32InitHeap',
+    'float64InitStack',
+    'float64InitHeap',
+    'intInitStack',
+    'intInitHeap',
+    'int8InitStack',
+    'int8InitHeap',
+    'int16InitStack',
+    'int16InitHeap',
+    'int32InitStack',
+    'int32InitHeap',
+    'int64InitStack',
+    'int64InitHeap',
+    'natInitStack',
+    'natInitHeap',
+    'nat8InitStack',
+    'nat8InitHeap',
+    'nat16InitStack',
+    'nat16InitHeap',
+    'nat32InitStack',
+    'nat32InitHeap',
+    'nat64InitStack',
+    'nat64InitHeap',
+    'nullInitStack',
+    'nullInitHeap',
+    'optInitStack',
+    'optInitHeap',
+    'principalInitStack',
+    'principalInitHeap',
+    'recordInitStack',
+    'recordInitHeap',
+    'textInitStack',
+    'textInitHeap',
+    'variantInitStack',
+    'variantInitHeap',
+    'vecInitStack',
+    'vecInitHeap'
+] as const;
 
 const testCases = methodNames.flatMap((methodName) =>
     iterations.map((iterationCount) => [methodName, iterationCount] as const)
@@ -14,7 +57,12 @@ const testCases = methodNames.flatMap((methodName) =>
 export function getTests(actor: ActorSubclass<_SERVICE>): Test {
     return () => {
         describe.each(testCases)('%s benchmarks', (methodName, iterations) => {
-            please(
+            const pleaseMethod =
+                shouldSkip(methodName, iterations) === true
+                    ? please.skip
+                    : please;
+
+            pleaseMethod(
                 `benchmark with ${iterations} iteration${iterations === 1 ? '' : 's'}`,
                 async () => {
                     await actor[methodName](iterations);
@@ -22,4 +70,19 @@ export function getTests(actor: ActorSubclass<_SERVICE>): Test {
             );
         });
     };
+}
+
+function shouldSkip(methodName: string, iterations: number): boolean {
+    if (
+        methodName === 'principalInitStack' ||
+        methodName === 'principalInitHeap' ||
+        methodName === 'recordInitStack' ||
+        methodName === 'recordInitHeap'
+    ) {
+        if (iterations >= 100_000) {
+            return true;
+        }
+    }
+
+    return false;
 }
