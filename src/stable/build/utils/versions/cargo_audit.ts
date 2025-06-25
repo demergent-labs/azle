9f0a73628dfbe2edaf9131e70be3f81c8e258b98
Benjamin DeMann <benjamin.demann@gmail.com>
2025-06-25 16:46:50 -0600

    don't use a default value

--- src/stable/build/utils/versions/cargo_audit.ts
+++ src/stable/build/utils/versions/cargo_audit.ts
@@ -1,9 +1,5 @@
 import { getCargoVersion } from './get_cargo_version';
 
 export function getLocalCargoAuditVersion(): string {
-    try {
-        return getCargoVersion('cargo-audit');
-    } catch (_error) {
-        return '0.20.0'; // Return default version if cargo-audit is not installed
-    }
+    return getCargoVersion('cargo-audit');
 }
--- src/stable/build/utils/versions/cargo_deny.ts
+++ src/stable/build/utils/versions/cargo_deny.ts
@@ -1,9 +1,5 @@
 import { getCargoVersion } from './get_cargo_version';
 
 export function getLocalCargoDenyVersion(): string {
-    try {
-        return getCargoVersion('cargo-deny');
-    } catch (_error) {
-        return '0.15.0'; // Return default version if cargo-deny is not installed
-    }
+    return getCargoVersion('cargo-deny');
 }
