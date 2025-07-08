d1e8cf4f9e94aff81e55b8309c820b987ea4ee16
Benjamin DeMann <benjamin.demann@gmail.com>
2025-07-08 14:40:44 -0600

    update examples

--- examples/stable/test/property/candid_rpc/canister_methods/http_request_update/test/test.ts
+++ examples/stable/test/property/candid_rpc/canister_methods/http_request_update/test/test.ts
@@ -11,19 +11,16 @@ import { QueryMethod } from 'azle/experimental/_internal/test/property/arbitrari
 import { UpdateMethodArb } from 'azle/experimental/_internal/test/property/arbitraries/canister_methods/update_method_arb';
 import { HttpRequestArb } from 'azle/experimental/_internal/test/property/arbitraries/http/request_arb';
 import { HttpResponseArb } from 'azle/experimental/_internal/test/property/arbitraries/http/response_arb';
-import { Api } from 'azle/experimental/_internal/test/property/arbitraries/types';
 import fc from 'fast-check';
 
 import { generateBody } from './generate_body';
 import { generateTests } from './generate_tests';
 
-const api: Api = 'class';
-const context = { api, constraints: {} };
+const context = { constraints: {} };
 
 const HttpRequestUpdateMethodArb = RecordArb(context).chain((record) => {
     const HttpRequestMethodArb = UpdateMethodArb(
         {
-            api,
             constraints: {
                 name: 'http_request_update'
             }
@@ -32,7 +29,7 @@ const HttpRequestUpdateMethodArb = RecordArb(context).chain((record) => {
             generateBody,
             generateTests
         },
-        fc.tuple(HttpRequestArb(context)),
+        fc.tuple(HttpRequestArb()),
         HttpResponseArb(context, record)
     );
 
