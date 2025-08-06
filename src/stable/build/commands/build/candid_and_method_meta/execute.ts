db4872ce0d93ff39e0f339ae02a3d03b93701ffd
Benjamin DeMann <benjamin.demann@gmail.com>
2025-08-06 13:06:25 -0600

    add optimization

--- src/stable/build/commands/build/candid_and_method_meta/execute.ts
+++ src/stable/build/commands/build/candid_and_method_meta/execute.ts
@@ -3,7 +3,11 @@ import { CandidAndMethodMeta } from '#utils/types';
 export async function execute(
     wasmBinary: Uint8Array
 ): Promise<CandidAndMethodMeta> {
-    const wasmModule = new WebAssembly.Module(new Uint8Array(wasmBinary));
+    const wasmBinaryBuffer =
+        wasmBinary.buffer instanceof ArrayBuffer
+            ? wasmBinary.buffer
+            : new Uint8Array(wasmBinary);
+    const wasmModule = new WebAssembly.Module(wasmBinaryBuffer);
     const wasmInstance = new WebAssembly.Instance(wasmModule, {
         ic0: {
             accept_message: (): void => {},
