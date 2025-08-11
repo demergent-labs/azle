eea2db63e286b4ffd0de7d76b72252e39ca5b978
Benjamin DeMann <benjamin.demann@gmail.com>
2025-08-11 12:01:49 -0600

    add comments for generic func args type

--- src/stable/lib/func.ts
+++ src/stable/lib/func.ts
@@ -1,5 +1,7 @@
 import { IDL } from '@dfinity/candid';
 
+// TODO: Remove type after https://github.com/demergent-labs/azle/issues/3382 is resolved
+// TODO: License info: https://github.com/dfinity/agent-js/blob/main/LICENSE
 export type GenericFuncArgs = [IDL.Type, ...IDL.Type[]] | [];
 
 export function toArgsTuple(types?: IDL.Type[]): GenericFuncArgs {
