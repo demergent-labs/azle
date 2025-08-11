630b6947274cb524e6f634730d2056469500c977
Benjamin DeMann <benjamin.demann@gmail.com>
2025-08-11 11:52:06 -0600

    update dfinity package versions

--- examples/stable/test/end_to_end/candid_rpc/executable_resolution/test/tests.ts
+++ examples/stable/test/end_to_end/candid_rpc/executable_resolution/test/tests.ts
@@ -41,7 +41,7 @@ export function getTests(): Test {
                 async () => {
                     if (process.env.AZLE_EXPERIMENTAL === 'true') {
                         execSync(
-                            `npm install https://github.com/demergent-labs/azle-experimental-deps#d1ccd00dc75e49609949ed7910a4e610c49027b7`,
+                            `npm install https://github.com/demergent-labs/azle-experimental-deps#e43744c9d425d9e1dfc9dbc0def0d332519df6e5`,
                             {
                                 cwd: canister.projectRoot
                             }
