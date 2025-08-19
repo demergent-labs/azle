1bc4b1eae9d4f5eb8657ac74142212c4c356063b
Jordan Last <jordan.michael.last@gmail.com>
2025-08-19 11:41:14 -0600

    add Rust docs

--- src/experimental/build/commands/build/wasm_binary/rust/experimental_canister_template/src/ic/uuid.rs
+++ src/experimental/build/commands/build/wasm_binary/rust/experimental_canister_template/src/ic/uuid.rs
@@ -5,6 +5,10 @@ use crate::INTERNAL_CSPRNG;
 
 pub struct NativeFunction;
 impl JsFn for NativeFunction {
+    /// This function is intended to be used to generate unique identifiers that the end-user developer cannot influence through re-seeding.
+    /// This function removes the risk of an end-user developer repeating a seed and thus potentially causing collisions in unique identifiers.
+    /// Any functionality that relies on Azle's implementation of `crypto.getRandomValues` or `randBytes` is subject to the re-seeding risk.
+    /// Thus by default any internal unique identifiers in the Azle JavaScript runtime should be generated using this function or another method not subject to re-seeding risk.
     fn call(context: &mut Context, _this_val: JsValue, _argv: &[JsValue]) -> JsValue {
         INTERNAL_CSPRNG.with(|csprng_refcell| {
             let mut rng = csprng_refcell.borrow_mut();
--- src/stable/build/commands/build/wasm_binary/rust/stable_canister_template/src/ic/uuid.rs
+++ src/stable/build/commands/build/wasm_binary/rust/stable_canister_template/src/ic/uuid.rs
@@ -3,6 +3,10 @@ use rquickjs::{Ctx, Function, Result};
 
 use crate::INTERNAL_CSPRNG;
 
+/// This function is intended to be used to generate unique identifiers that the end-user developer cannot influence through re-seeding.
+/// This function removes the risk of an end-user developer repeating a seed and thus potentially causing collisions in unique identifiers.
+/// Any functionality that relies on Azle's implementation of `crypto.getRandomValues` or `randBytes` is subject to the re-seeding risk.
+/// Thus by default any internal unique identifiers in the Azle JavaScript runtime should be generated using this function or another method not subject to re-seeding risk.
 pub fn get_function(ctx: Ctx) -> Result<Function> {
     Function::new(ctx.clone(), move || -> Result<String> {
         INTERNAL_CSPRNG.with(|csprng_refcell| {
