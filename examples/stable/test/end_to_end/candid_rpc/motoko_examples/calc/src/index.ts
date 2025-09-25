69b77d9ebcb0554f6bbb8b1e758c3ff258a30996
Jordan Last <jordan.michael.last@gmail.com>
2025-09-25 13:55:03 -0600

    remove some hacks due to Int and Nat not being supported. Bug confirmed fixed

--- examples/stable/test/end_to_end/candid_rpc/motoko_examples/calc/src/index.ts
+++ examples/stable/test/end_to_end/candid_rpc/motoko_examples/calc/src/index.ts
@@ -21,11 +21,6 @@ export default class {
     mul(n: bigint): bigint {
         this.cell *= n;
 
-        // TODO remove this once this bug in @icp-sdk/core/candid is fixed: https://github.com/dfinity/agent-js/issues/953
-        if (Number(this.cell) === Infinity || Number(this.cell) === -Infinity) {
-            this.cell = BigInt(Number.MAX_VALUE);
-        }
-
         return this.cell;
     }
 
