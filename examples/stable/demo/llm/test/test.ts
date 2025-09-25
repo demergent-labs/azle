9bd10a0670daf7f599ee4457959dbbd3aff76dfd
Jordan Last <jordan.michael.last@gmail.com>
2025-09-25 13:08:52 -0600

    add experimental binary and improve a couple comments

--- examples/stable/demo/llm/test/test.ts
+++ examples/stable/demo/llm/test/test.ts
@@ -12,5 +12,5 @@ const actor = createActor(getCanisterId(canisterName), {
     }
 });
 
-// TODO get rid of this cast to any once the dfx generate command uses @icp-sdk/core instead of the old @dfinity packages
+// TODO get rid of this cast to any once the dfx generate command imports from @icp-sdk/core instead of the old @dfinity packages
 runTests(getTests(actor as any));
