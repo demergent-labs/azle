e484a407da099c1ea3ef8426cdc6509906c1d256
Jordan Last <jordan.michael.last@gmail.com>
2025-10-02 12:55:48 -0600

    re-enable recursive

--- src/experimental/test/property/arbitraries/candid/candid_definition_arb/recursive_candid_definition_memo.ts
+++ src/experimental/test/property/arbitraries/candid/candid_definition_arb/recursive_candid_definition_memo.ts
@@ -7,9 +7,8 @@ import { RecursiveDefinitionArb } from '../recursive/definition_arb';
 import { complexCandidDefinitionMemo } from './complex_candid_definition_memo';
 import { CandidDefinitionMemo, RecursiveCandidName } from './types';
 
-// TODO enable the recursive test below by replacing 0 with 8 once https://github.com/demergent-labs/azle/issues/1526 is resolved
 // The number of options below (it's just recursive)
-export const REC_ARB_COUNT = 0;
+export const REC_ARB_COUNT = 8;
 
 export function recursiveCandidDefinitionMemo(
     context: Context,
